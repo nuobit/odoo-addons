1adf09fc9f76221c9f3be977df69da1655b77523
Eric Antones <eantones@nuobit.com>
2016-01-04 16:54:34 +0100

    [IMP] Add color selector

--- event_custom_calendar/models/models.py
+++ event_custom_calendar/models/models.py
@@ -33,12 +33,38 @@ _logger = logging.getLogger(__name__)
 class event_type(models.Model):
     _inherit = 'event.type'
 
-    color = fields.Char(string="Color")
+    color = fields.Selection(selection=[(1,  'Brown'),
+                                        (2,  'Brown-Red'),
+                                        (3,  'Red'),
+                                        (4,  'Light Red'),
+                                        (5,  'Orange'),
+                                        (6,  'Light Orange'),
+                                        (8,  'Green 1'),
+                                        (7,  'Light Green 1'),
+                                        (9,  'Green 2'),
+                                        (10, 'Light Green 2'),
+                                        (11, 'Yellow'),
+                                        (12, 'Yellow-Orange'),
+                                        (13, 'Light Blue-Green'),
+                                        (14, 'Cyan'),
+                                        (15, 'Light Blue'),
+                                        (16, 'Blue'),
+                                        (17, 'Blue-Purple'),
+                                        (18, 'Light Purple'),
+                                        (23, 'Purple'),
+                                        (24, 'Dark Purple'),
+                                        (22, 'Pink'),
+                                        (19, 'Grey'),
+                                        (20, 'Grey-Light Red'),
+                                        (21, 'Grey-Red'),
+                                        ], string="Color")
 
+    @api.multi
     def name_get(self):
         result = []
         for etype in self:
             result.append((etype.id, '%s%s' % (etype.name, ' [%s]' % etype.color if etype.color else '')))
+
         return result
 
 
