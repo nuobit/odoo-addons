3985c49a133688d978d66e3d82b847e8c5625195
Eric Antones <eantones@nuobit.com>
2016-01-16 00:36:07 +0100

    [FIX] Permissions issue

--- import_products_extension/__openerp__.py
+++ import_products_extension/__openerp__.py
@@ -28,7 +28,7 @@
     'website': 'https://www.nuobit.com',
     'depends': ['product'],
     'data': [
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/template_views.xml',
         'views/inherit_views.xml',
