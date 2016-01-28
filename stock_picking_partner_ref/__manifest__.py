044129642de443223d822ff5c21b055916f41aae
Eric Antones <eantones@nuobit.com>
2016-01-28 17:17:57 +0100

    [ADD] Add Stock Picking Partner Reference Module

---
+++ stock_picking_partner_ref/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock picking partner reference",
+    "summary": "Adds a partner reference on picking",
+    "author": "NuoBiT Solutions SL",
+    "maintainers": ["eantones"],
+    "category": "Warehouse",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/NuoBiT/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
---
+++ stock_picking_partner_ref/models/stock_picking.py
@@ -0,0 +1,11 @@
+# Copyright 2021 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+
+from odoo import fields, models
+
+
+class Location(models.Model):
+    _inherit = "stock.picking"
+
+    partner_ref = fields.Char(string="Partner Reference")
