7f1999a72918fc45be3a5ae6ed1ed9ffb4a40bad
Eric Antones <eantones@nuobit.com>
2016-03-04 21:30:37 +0100

    [FIX] ems: Correct security access

--- ems/__openerp__.py
+++ ems/__openerp__.py
@@ -31,6 +31,7 @@
         'views/views.xml',
         'views/template_views.xml',
         'views/inherit_views.xml',
+        'security/ir.model.access.csv',
         ],
     'installable': True,
 }
