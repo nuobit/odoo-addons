e549fd1c65bf5d8edffd5ddd50fe91c410a9c327
Eric Antones <eantones@nuobit.com>
2016-03-22 15:32:58 +0100

    [FIX] product_relation: correct merge

--- product_relation/models/models.py
+++ product_relation/models/models.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-<<<<<<< HEAD
+
 from openerp import models, fields, api, _
 from openerp.exceptions import AccessError, Warning, ValidationError
 
