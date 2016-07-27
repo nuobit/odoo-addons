1091e40d6ac2a801c2bc044fbe0a429d3726addd
Eric Antones <eantones@nuobit.com>
2016-07-27 12:30:43 +0200

    [FIX] website_sale_dt: adapt shop product order to method based custom order from PR

--- website_sale_dt/models/models.py
+++ website_sale_dt/models/models.py
@@ -1,9 +1,9 @@
 from openerp import models, fields, api, _
 
+import openerp.addons.website_sale.controllers.main as main
+
 class dt_product_template(models.Model):
     _inherit = "product.template"
-    _order = 'website_published desc, public_categ_sequence, public_default_code'
-
 
     public_categ_sequence = fields.Integer(compute='_compute_public_categ_sequence', store=True)
     public_default_code = fields.Char(compute='_compute_public_default_code', store=True)
@@ -21,3 +21,10 @@ class dt_product_template(models.Model):
                 record.public_default_code = record.product_variant_ids[0].default_code
 
 
+
+
+class website_sale(main.website_sale):
+    def _get_search_order(self, post):
+        res = super(website_sale, self)._get_search_order(post)
+        # OrderBy will be parsed in orm and so no direct sql injection
+        return 'website_published desc,public_categ_sequence, public_default_code'
