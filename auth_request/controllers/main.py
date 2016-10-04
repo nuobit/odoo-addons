a6dc03b9e9f8620aefcbc74aafa587be5271e7eb
Eric Antones <eantones@nuobit.com>
2016-10-04 18:01:06 +0200

    [ADD] auth_request: add new module

---
+++ auth_request/controllers/main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012-today OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+import logging
+import werkzeug
+
+import openerp
+from openerp.addons.auth_signup.res_users import SignupError
+from openerp.addons.web.controllers.main import ensure_db
+from openerp import http
+from openerp.http import request
+from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+
+class auth_request(openerp.addons.web.controllers.main.Home):
+
+    @http.route('/web/requestaccount', type='http', auth='public', website=True)
+    def request_account(self, *args, **kw):
+
+        subject = _('Request an account')
+        return request.redirect('/page/contactus?name=%s' % subject)
+
+
+
+
