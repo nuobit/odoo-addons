9996009518e88a8437911f193bb01e5f6c73a19e
Eric Antones <eantones@nuobit.com>
2016-12-08 15:43:04 +0100

    [IMP] website_geoip_address: add info about the mandatory proxy-mode option in order this module to work

--- website_geoip_address/README.md
+++ website_geoip_address/README.md
@@ -3,3 +3,11 @@ Website GeoIP Address
 
 * Show different company's addresses based on the location of the user
 
+In order this module to work: If your server is behind a proxy like Nginx, Gunicorn the REMOTE_ADDR 
+variable becomes worthless since every connection looks like it's coming from your proxy server 
+instead of the actual user making the request.
+To avoid that behaviour:
+1. If you're running a standalone Odoo server: start server with **--proxy-mode** command line option.
+2. If you're running Odoo server over a wsgi server: add **conf['proxy_mode'] = True** to odoo-wsgi.py config file.
+
+More info: https://github.com/odoo/odoo/issues/11035
--- website_geoip_address/__manifest__.py
+++ website_geoip_address/__manifest__.py
@@ -21,7 +21,17 @@
 
 {
     'name': "Website GeoIP Address",
-    'description': "Show different company's addresses based on the location of the user",
+    'description': """Show different company's addresses based on the location of the user
+
+In order this module to work: If your server is behind a proxy like Nginx, Gunicorn the REMOTE_ADDR variable becomes worthless since every connection looks like it's coming from your proxy server
+instead of the actual user making the request.
+To avoid that behaviour:
+1. If you're running a standalone Odoo server: start server with **--proxy-mode** command line option.
+2. If you're running Odoo server over a wsgi server: add **conf['proxy_mode'] = True** to odoo-wsgi.py config file.
+
+More info: https://github.com/odoo/odoo/issues/11035
+
+    """,
     'version': '0.0.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
