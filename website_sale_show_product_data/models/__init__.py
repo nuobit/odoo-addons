e215ce53eee513048d3927eb00cdedac1aa260f8
Eric Antones <eantones@nuobit.com>
2016-12-16 11:34:43 +0100

    [IMP] website_sale_show_product_data: add alternate pricelist on product item list

--- website_sale_show_product_data/models/__init__.py
+++ website_sale_show_product_data/models/__init__.py
@@ -19,18 +19,4 @@
 #
 #/#############################################################################
 
-{
-    'name': "Show product data in website sale",
-    'description': "Show product data in website sale: internal reference (default code), providers ",
-    'version': '10.0.0.1.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': ['website_sale'],
-    'data': [
-        'views/website_sale_show_product_data_templates.xml',
-        'views/inherit_views.xml',
-        ],
-    'installable': True,
-}
+import models
--- website_sale_show_product_data/models/models.py
+++ website_sale_show_product_data/models/models.py
@@ -19,18 +19,11 @@
 #
 #/#############################################################################
 
-{
-    'name': "Show product data in website sale",
-    'description': "Show product data in website sale: internal reference (default code), providers ",
-    'version': '10.0.0.1.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': ['website_sale'],
-    'data': [
-        'views/website_sale_show_product_data_templates.xml',
-        'views/inherit_views.xml',
-        ],
-    'installable': True,
-}
+from openerp import api, models, fields, SUPERUSER_ID
+from openerp.api import Environment
+
+
+class ProductPrcelist(models.Model):
+    _inherit = 'product.pricelist'
+
+    show_on_website = fields.Boolean(string="Show on website", default=False)
