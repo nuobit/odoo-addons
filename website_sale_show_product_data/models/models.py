8d1a6900361c87f7d13535af390687d01e59015a
Eric Antones <eantones@nuobit.com>
2016-12-19 12:37:10 +0100

    [FIX] website_sale_show_product_data: seller image not showing on public user

--- website_sale_show_product_data/models/models.py
+++ website_sale_show_product_data/models/models.py
@@ -27,3 +27,22 @@ class ProductPrcelist(models.Model):
     _inherit = 'product.pricelist'
 
     show_on_website = fields.Boolean(string="Show on website", default=False)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    first_seller_image = fields.Char(string="First seller image", compute="_compute_first_seller_image")
+
+    first_seller = fields.Many2one(string="First seller", comodel_name="res.partner", compute="_compute_first_seller")
+
+    @api.depends('seller_ids')
+    def _compute_first_seller_image(self):
+        first_seller = self.sudo().seller_ids.sorted(lambda x: (x.sequence, x.id))[0].name
+
+        self.first_seller_image = first_seller.image
+
+    @api.depends('seller_ids')
+    def _compute_first_seller(self):
+        self.first_seller = self.sudo().seller_ids.sorted(lambda x: (x.sequence, x.id))[0].name
+
