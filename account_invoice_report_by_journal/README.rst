e732c09155aebb95d53d4fb928faf4b1ccd1725e
Eric Antones <eantones@nuobit.com>
2017-04-28 16:58:29 +0200

    [ADD] account_invoice_report_by_journal: new module

---
+++ account_invoice_report_by_journal/README.rst
@@ -0,0 +1,25 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=========================
+Invoice Report by Journal
+=========================
+
+This module was written to extend the functionality of invoice reports to support diferent invoice report by journal and allow you to set the invoice report template in journal form to print this template as default.
+
+Usage
+=====
+
+To use this module, you need to:
+
+ * go to journal configuration
+ * set default invoice report template
+ * print invoice report
+
+Known issues / Roadmap
+======================
+
+* Print button on invoices gets invoice report for the partner, clicking Print on upper dropdown you could get other invoice report
+
+
---
+++ account_invoice_report_by_journal/__openerp__.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Invoice Report by Journal",
+    'version': "8.0.0.1.0",
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com/',
+    'category': 'Accounting & Finance',
+    'license': "AGPL-3",
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/journal_view.xml',
+    ],
+    'installable': True,
+}
