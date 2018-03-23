8b06dde5eeefac5d03b257fc2dbe29f1fe5aa56e
Eric Antones <eantones@nuobit.com>
2018-03-23 19:32:12 +0100

    [FIX] lighting: external field name

--- lighting/models/models.py
+++ lighting/models/models.py
@@ -227,7 +227,7 @@ class LightingProduct(models.Model):
     ext_swidth1 = fields.Float(string='S Width 1', readonly=True)
     ext_slength1 = fields.Float(string='S Length 1', readonly=True)
     ext_svolume = fields.Float(string='S Volume', readonly=True)
-    ext_sweight = fields.Float(string='S Weight', readonly=True)
+    ext_sweight1 = fields.Float(string='S Weight 1', readonly=True)
 
     def _compute_external_data_available(self):
         try:
