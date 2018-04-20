47572bf8299a2e6fd7fcba2d7bde2b1e012ce570
Eric Antones <eantones@nuobit.com>
2018-04-20 14:51:21 +0200

    [ADD] contract_comment: new module

---
+++ contract_comment/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================
+Contract comment
+================
+
+This module adds a comment field in the contract and propagates it to the comment field of the invoice
---
+++ contract_comment/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Contract comment',
+    'summary': 'This module adds a comment field in the contract and propagates it to the comment field of the invoice',
+    'version': '10.0.0.1.0',
+    'category': 'Contract Management',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'contract',
+    ],
+    'data': [
+        'views/account_analytic_contract_view.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
