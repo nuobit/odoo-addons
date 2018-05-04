e3110740a6c24b2c0b461367896572c84e9cc376
Eric Antones <eantones@nuobit.com>
2018-05-04 19:46:47 +0200

    [ADD] l10n_es_account_bank_statement_import_n43_overlap: new module

---
+++ l10n_es_account_bank_statement_import_n43_overlap/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Bank statement import N43 overlap',
+    'summary': 'This module extends functionality of N43 bank statements checking overlapping dates',
+    'version': '10.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_es_account_bank_statement_import_n43',
+    ],
+    'data': [
+        'wizards/account_bank_statement_import_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
