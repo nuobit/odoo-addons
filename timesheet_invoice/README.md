705fe6469a10bf122781c5a776eb9771e1468385
Eric Antones <eantones@nuobit.com>
2018-05-07 16:02:59 +0200

    [REF] timesheet_invoice: license text and clean code

--- timesheet_invoice/README.md
+++
@@ -1,7 +0,0 @@
-Timesheet invoice
-=================
-
-* Allows invoicing timesheets
-
-
-
--- timesheet_invoice/models/models.py
+++ timesheet_invoice/models/models.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
 import time
 
 from odoo import _, api, fields, models
