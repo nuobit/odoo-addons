c78d4c96f60481de3eed529384c6deeeeda7c219
Eric Antones <eantones@nuobit.com>
2018-05-07 17:08:57 +0200

    [ADD] hr_timesheet_pretty_description: new module

---
+++ hr_timesheet_pretty_description/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Timesheet pretty description',
+    'version': '10.0.0.1.0',
+    'category': 'Project',
+    'license': 'AGPL-3',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'depends': ['hr_timesheet'],
+    'data': [
+        'views/project_task_view.xml',
+    ],
+    'installable': True,
+}
