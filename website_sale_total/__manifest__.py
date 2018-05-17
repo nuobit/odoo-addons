6b7df42e496ded4521b9ea195a2249525fd5836b
Eric Antones <eantones@nuobit.com>
2018-05-17 15:50:25 +0200

    [REF] website_sale_total: license and xml reformat

--- website_sale_total/__manifest__.py
+++ website_sale_total/__manifest__.py
@@ -1,28 +1,12 @@
 # -*- coding: utf-8 -*-
-#/#############################################################################
-#
-#   Odoo, Open Source Management Solution
-#   Copyright (C) 2015 NuoBiT Solutions, S.L. (<http://www.nuobit.com>).
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#/#############################################################################
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     'name': "Website total form with cleaner interface",
     'description': "Custom total form with cleaner interface",
-    'version': '10.0.0.1.0',
+    'version': '10.0.0.1.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
