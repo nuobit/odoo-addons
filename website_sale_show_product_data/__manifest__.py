1f0fec4ca7161a718611ed388423e03234f59ec1
Eric Antones <eantones@nuobit.com>
2018-06-21 20:23:45 +0200

    [FIX] website_sale_show_product_data: Performance issues, optimize multiple prices and offers

--- website_sale_show_product_data/__manifest__.py
+++ website_sale_show_product_data/__manifest__.py
@@ -8,7 +8,7 @@
     'description': "Show product data in website sale: "
                    "internal reference (default code), providers, "
                    "helper text, print button, barcode ",
-    'version': '10.0.0.3.0',
+    'version': '10.0.0.3.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
--- website_sale_show_product_data/controllers/main.py
+++ website_sale_show_product_data/controllers/main.py
@@ -4,6 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import odoo.addons.website_sale.controllers.main as main
+from odoo import http
+from odoo.http import request
 
 
 class WebsiteSale(main.WebsiteSale):
@@ -24,3 +26,53 @@ class WebsiteSale(main.WebsiteSale):
             e0 = e
 
         return domain9
+
+    @http.route()
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
+        res = super(WebsiteSale, self).shop(page=page, category=category, search=search, ppg=ppg, **post)
+
+        values = res.qcontext
+
+        ### pricelists
+        product_prices = {}
+
+        pricelist_data = request.env['product.pricelist.item'].search(
+            [('pricelist_id.show_on_website', '=', True), ('product_tmpl_id.id', 'in', values['products'].ids)],
+            order='product_tmpl_id, min_quantity'
+        )
+
+        if pricelist_data:
+            for pricelist_item in pricelist_data:
+                product_id = pricelist_item.product_tmpl_id.id
+                if product_id not in product_prices:
+                    product_prices[product_id]=[]
+                product_prices[product_id].append({'fixed_price': pricelist_item.fixed_price,
+                                       'min_quantity': pricelist_item.min_quantity},
+                                    )
+
+        values['website_prices'] = product_prices
+
+        ### offers
+        product_offers = {}
+
+        priceoffer_pricelist = request.env['product.pricelist.item'].search([('pricelist_id.show_on_website', '=', True),
+                                                                             ('applied_on', '=', '3_global'),
+                                                                             ('compute_price', '=', 'formula'),
+                                                                             ('base', '=', 'pricelist')])
+        if priceoffer_pricelist:
+            offer_pricelist_id = priceoffer_pricelist.base_pricelist_id.id
+            priceoffer_data = request.env['product.pricelist.item'].search(
+                [('pricelist_id', '=', offer_pricelist_id), ('product_tmpl_id.id', 'in', values['products'].ids)],
+                order='product_tmpl_id, min_quantity'
+            )
+            if priceoffer_data:
+                for pricelist_item in priceoffer_data:
+                    product_id = pricelist_item.product_tmpl_id.id
+                    if product_id not in product_offers:
+                        product_offers[product_id] = []
+                        product_offers[product_id].append({'fixed_price': pricelist_item.fixed_price,
+                                                       'min_quantity': pricelist_item.min_quantity},
+                                                      )
+        values['website_offers'] = product_offers
+
+        return request.render("website_sale.products", values)
