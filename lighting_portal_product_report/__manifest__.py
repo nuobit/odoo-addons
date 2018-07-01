e6ea3d3e564d9de5653999e216dac958c6d0bd01
Eric Antones <eantones@nuobit.com>
2018-07-01 18:55:49 +0200

    [FIX] lighting_portal_product_report: allow access fields

--- lighting_portal_product_report/__manifest__.py
+++ lighting_portal_product_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Portal Product report",
     'description': "Lighting Portal Product report datasheet",
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.1.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
