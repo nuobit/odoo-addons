b65541dfebf9fb7980a8c8474e2d256229369cef
Eric Antones <eantones@nuobit.com>
2018-07-03 21:59:21 +0200

    [ADD] lighting_topbar: new module to customize topbar behavior

---
+++ lighting_topbar/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Lighting Top bar",
+    'description': "Remove backoffice root menus and systray",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.nuobit.com',
+    'depends': ['base', 'web'],
+    'data': [
+        'security/topbar_security.xml',
+        'views/web_templates.xml',
+        ],
+    'installable': True,
+}
---
+++ lighting_topbar/models/menu.py
@@ -0,0 +1,31 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+
+class IrUiMenu(models.Model):
+    _inherit = 'ir.ui.menu'
+
+    @api.model
+    @api.returns('self')
+    def get_user_roots(self):
+        """ Return all root menu ids visible for the user.
+
+        :return: the root menu ids
+        :rtype: list(int)
+        """
+        domain = []
+        if self.env['ir.module.module'].sudo().search([('name', '=', 'lighting'),
+                                                ('state', '=', 'installed')]):
+            domain.append(('id', '=', self.env.ref('lighting.main_lighting_menu').id))
+
+        if self.env['ir.module.module'].sudo().search([('name', '=', 'lighting_portal'),
+                                                ('state', '=', 'installed')]):
+            domain.append(('id', '=', self.env.ref('lighting_portal.portal_menu_root').id))
+
+        if domain and self.env.user.has_group('lighting_topbar.topbar_group_hide'):
+            domain = ['|'] * (len(domain) - 1) + domain
+            return self.search([('parent_id', '=', False)] + domain)
+        else:
+            return super(IrUiMenu, self).get_user_roots()
