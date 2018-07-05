b2d9c1a1204d636e49687a41443d24311aa9af95
Eric Antones <eantones@nuobit.com>
2018-07-05 21:00:51 +0200

    [ADD] account_bank_statement_balance_start_protection: new module

---
+++ account_bank_statement_balance_start_protection/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Cash statement balance start protection",
+    'description': "Cash statement balance start protection",
+    'version': '10.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.nuobit.com',
+    'depends': ['account'],
+    'data': [
+        'views/account_views.xml'
+        ],
+    'installable': True,
+}
