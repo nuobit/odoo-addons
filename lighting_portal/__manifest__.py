9344bc4eb2b9c752c1a01ec938c14c8d608291a7
Eric Antones <eantones@nuobit.com>
2018-07-11 18:53:41 +0200

    [REM] lighting_portal: remove atp data

--- lighting_portal/__manifest__.py
+++ lighting_portal/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Portal",
     'description': "Lighting Portal",
-    'version': '11.0.0.3.1',
+    'version': '11.0.0.4.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
--- lighting_portal/models/portal_product.py
+++ lighting_portal/models/portal_product.py
@@ -15,32 +15,9 @@ class LightingPortalProduct(models.Model):
     catalog = fields.Char(string='Catalog')
     qty_available = fields.Integer(string='Quantity available')
 
-    atp_ids = fields.One2many(comodel_name='lighting.portal.product.atp',
-                              inverse_name='portal_product_id', string='ATP quantity')
-    qty_ordered = fields.Integer(string='Quantity ordered', compute="_compute_qty_ordered", store=True)
-    @api.depends('atp_ids.qty_ordered')
-    def _compute_qty_ordered(self):
-        for rec in self:
-            rec.qty_ordered = sum(atp.qty_ordered for atp in rec.atp_ids)
-
     product_id = fields.Many2one(comodel_name='lighting.product', ondelete='set null',
                                  string='Product',
                                  groups='lighting_portal.portal_group_manager')
 
     _sql_constraints = [('reference', 'unique (reference)', 'The reference must be unique!'),
-                        ]
-
-
-class LightingPortalProductATP(models.Model):
-    _name = 'lighting.portal.product.atp'
-    _order = 'ship_date'
-
-    qty_ordered = fields.Integer(string='Quantity ordered', required=True)
-    ship_date = fields.Date(string='Ship date', required=True)
-
-    portal_product_id = fields.Many2one(comodel_name='lighting.portal.product', ondelete='cascade',
-                                        string='Product')
-
-
-    _sql_constraints = [('product_date', 'unique (portal_product_id, ship_date)', _('Only one ship date is allowed for a product')),
                         ]
