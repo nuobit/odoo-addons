2e32eae9f576b2803dc452666bb6467ea03f67e5
Eric Antones <eantones@nuobit.com>
2018-07-17 22:09:50 +0200

    [ADD] account_invoice_tax_analytic_account: new module

---
+++ account_invoice_tax_analytic_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Account tax invoice analytic',
+    'description': 'This module allows to add analytic account manually on invoice taxes.',
+    'version': '10.0.0.1.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'depends': ['account'],
+    'data': [],
+    'installable': True,
+}
