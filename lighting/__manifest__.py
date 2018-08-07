71ef0eac6b8d26a62cb637e35500aaa9783d29b3
Eric Antones <eantones@nuobit.com>
2018-08-07 19:20:28 +0200

    [FIX] lighting: forcecreate False on materials

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.15.5',
+    'version': '11.0.0.15.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
