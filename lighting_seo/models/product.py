12219313f9cd03c6888e10a0324cce93aafa49f7
Eric Antones <eantones@nuobit.com>
2018-08-07 11:33:46 +0200

    [IMP] lighting_seo: add SEO title and rename url

--- lighting_seo/models/product.py
+++ lighting_seo/models/product.py
@@ -7,31 +7,35 @@ from odoo import fields, models
 class LightingProduct(models.Model):
     _inherit = 'lighting.product'
 
+    seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_seo_keyword_rel',
                                        column1='product_id', column2='keyword_id', string='Keywords')
-    url_description = fields.Char(string='URL description')
+    seo_url = fields.Char(string='URL')
 
 class LightingProductFamily(models.Model):
     _inherit = 'lighting.product.family'
 
+    seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_family_seo_keyword_rel',
                                        column1='family_id', column2='keyword_id', string='Keywords')
-    url_description = fields.Char(string='URL description')
+    seo_url = fields.Char(string='URL')
 
 class LightingProductType(models.Model):
     _inherit = 'lighting.product.type'
 
+    seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_type_seo_keyword_rel',
                                        column1='type_id', column2='keyword_id', string='Keywords')
-    url_description = fields.Char(string='URL description')
+    seo_url = fields.Char(string='URL')
 
 class LightingProductApplication(models.Model):
     _inherit = 'lighting.product.application'
 
+    seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_application_seo_keyword_rel',
                                        column1='application_id', column2='keyword_id', string='Keywords')
-    url_description = fields.Char(string='URL description')
+    seo_url = fields.Char(string='URL')
