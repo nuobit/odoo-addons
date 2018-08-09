bdf2f99bb214b255eb03ca12629f0de3109fd0c1
Eric Antones <eantones@nuobit.com>
2018-08-09 18:49:00 +0200

    [IMP] lighting: add link to tp products in family tree view

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.17.5',
+    'version': '11.0.0.17.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
