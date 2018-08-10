d1a5eaa9d4cb37d43a0f5cde58007034b72d1993
Eric Antones <eantones@nuobit.com>
2018-08-10 11:10:11 +0200

    [IMP] lighting_seo: add seo data preview on families

--- lighting_seo/models/product.py
+++ lighting_seo/models/product.py
@@ -2,7 +2,8 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import fields, models, api
+
 
 class LightingProduct(models.Model):
     _inherit = 'lighting.product'
@@ -13,32 +14,49 @@ class LightingProduct(models.Model):
                                        column1='product_id', column2='keyword_id', string='Keywords')
     seo_url = fields.Char(string='URL')
 
+
 class LightingProductFamily(models.Model):
     _inherit = 'lighting.product.family'
 
-    seo_title = fields.Char(string='Title', translate=True)
-    seo_description = fields.Char(string='Description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_family_seo_keyword_rel',
+    seo_title = fields.Char(string='Meta title', translate=True)
+    seo_description = fields.Text(string='Meta description', translate=True)
+    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
+                                       relation='lighting_product_family_seo_keyword_rel',
                                        column1='family_id', column2='keyword_id', string='Keywords')
     seo_url = fields.Char(string='URL')
+
+    meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
+    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths', readonly=True)
+
+    @api.depends('seo_title', 'seo_description')
+    def _compute_lengths(self):
+        for rec in self:
+            if rec.seo_title:
+                rec.meta_title_length = len(rec.seo_title)
+                rec.meta_description_length = len(rec.seo_description)
+
 
 class LightingProductType(models.Model):
     _inherit = 'lighting.product.type'
 
     seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_type_seo_keyword_rel',
+    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
+                                       relation='lighting_product_type_seo_keyword_rel',
                                        column1='type_id', column2='keyword_id', string='Keywords')
     seo_url = fields.Char(string='URL')
+
 
 class LightingProductApplication(models.Model):
     _inherit = 'lighting.product.application'
 
     seo_title = fields.Char(string='Title', translate=True)
     seo_description = fields.Char(string='Description', translate=True)
-    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_application_seo_keyword_rel',
+    seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
+                                       relation='lighting_product_application_seo_keyword_rel',
                                        column1='application_id', column2='keyword_id', string='Keywords')
     seo_url = fields.Char(string='URL')
+
 
 class LightingCatalog(models.Model):
     _inherit = 'lighting.catalog'
