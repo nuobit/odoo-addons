9019a55dc8cc14aa89641d8f811c2142a5a1220d
Eric Antones <eantones@nuobit.com>
2018-08-10 11:41:01 +0200

    [IMP] lighting: family name bigger

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.17.8',
+    'version': '11.0.0.17.9',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
