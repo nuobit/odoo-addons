33b72764d59095a304407d10f666cd78389a63a2
Eric Antones <eantones@nuobit.com>
2018-08-10 16:28:03 +0200

    [IMP] lighting_project: navigate from family to project

--- lighting_project/__manifest__.py
+++ lighting_project/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Project",
     'description': "Add lighting project portfolio",
-    'version': '11.0.0.3.6',
+    'version': '11.0.0.3.7',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
