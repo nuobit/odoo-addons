7cc199b0064ac100f64e930e5a03ce675588f80c
Eric Antones <eantones@nuobit.com>
2018-08-10 11:36:26 +0200

    [REF] lighting_seo: move seo url

--- lighting_seo/models/product.py
+++ lighting_seo/models/product.py
@@ -8,25 +8,26 @@ from odoo import fields, models, api
 class LightingProduct(models.Model):
     _inherit = 'lighting.product'
 
-    seo_title = fields.Char(string='Title', translate=True)
-    seo_description = fields.Char(string='Description', translate=True)
+    seo_title = fields.Char(string='Meta title', translate=True)
+    seo_url = fields.Char(string='URL')
+    seo_description = fields.Char(string='Meta description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_product_seo_keyword_rel',
                                        column1='product_id', column2='keyword_id', string='Keywords')
-    seo_url = fields.Char(string='URL')
 
 
 class LightingProductFamily(models.Model):
     _inherit = 'lighting.product.family'
 
     seo_title = fields.Char(string='Meta title', translate=True)
+    seo_url = fields.Char(string='URL')
     seo_description = fields.Text(string='Meta description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
                                        relation='lighting_product_family_seo_keyword_rel',
                                        column1='family_id', column2='keyword_id', string='Keywords')
-    seo_url = fields.Char(string='URL')
 
     meta_title_length = fields.Integer(string='Meta title length', compute='_compute_lengths', readonly=True)
-    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths', readonly=True)
+    meta_description_length = fields.Integer(string='Meta description length', compute='_compute_lengths',
+                                             readonly=True)
 
     @api.depends('seo_title', 'seo_description')
     def _compute_lengths(self):
@@ -39,30 +40,30 @@ class LightingProductFamily(models.Model):
 class LightingProductType(models.Model):
     _inherit = 'lighting.product.type'
 
-    seo_title = fields.Char(string='Title', translate=True)
-    seo_description = fields.Char(string='Description', translate=True)
+    seo_title = fields.Char(string='Meta title', translate=True)
+    seo_url = fields.Char(string='URL')
+    seo_description = fields.Char(string='Meta description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
                                        relation='lighting_product_type_seo_keyword_rel',
                                        column1='type_id', column2='keyword_id', string='Keywords')
-    seo_url = fields.Char(string='URL')
 
 
 class LightingProductApplication(models.Model):
     _inherit = 'lighting.product.application'
 
-    seo_title = fields.Char(string='Title', translate=True)
-    seo_description = fields.Char(string='Description', translate=True)
+    seo_title = fields.Char(string='Meta title', translate=True)
+    seo_url = fields.Char(string='URL')
+    seo_description = fields.Char(string='Meta description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword',
                                        relation='lighting_product_application_seo_keyword_rel',
                                        column1='application_id', column2='keyword_id', string='Keywords')
-    seo_url = fields.Char(string='URL')
 
 
 class LightingCatalog(models.Model):
     _inherit = 'lighting.catalog'
 
-    seo_title = fields.Char(string='Title', translate=True)
-    seo_description = fields.Char(string='Description', translate=True)
+    seo_title = fields.Char(string='Meta title', translate=True)
+    seo_url = fields.Char(string='URL')
+    seo_description = fields.Char(string='Meta description', translate=True)
     seo_keyword_ids = fields.Many2many(comodel_name='lighting.seo.keyword', relation='lighting_catalog_seo_keyword_rel',
                                        column1='catalog_id', column2='keyword_id', string='Keywords')
-    seo_url = fields.Char(string='URL')
