550d94ef0e970f64bf55705e0fde1857f68dcf6b
Eric Antones <eantones@nuobit.com>
2018-08-10 13:31:41 +0200

    [IMP] lighting_seo: add smart buttons from keyword to family, type, application, catalog, product

--- lighting_seo/__manifest__.py
+++ lighting_seo/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting SEO/SEM",
     'description': "Add SEO/SEM capabilities to Lighting vertical",
-    'version': '11.0.0.3.3',
+    'version': '11.0.0.3.4',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
--- lighting_seo/models/seo_keyword.py
+++ lighting_seo/models/seo_keyword.py
@@ -15,6 +15,24 @@ class LightingSEOKeyword(models.Model):
     _sql_constraints = [('name_uniq', 'unique (name)', 'The keyword must be unique!'),
                         ]
 
+    product_count = fields.Integer(compute='_compute_counts', string='Product(s)')
+    family_count = fields.Integer(compute='_compute_counts', string='Family(s)')
+    type_count = fields.Integer(compute='_compute_counts', string='Type(s)')
+    application_count = fields.Integer(compute='_compute_counts', string='Application(s)')
+    catalog_count = fields.Integer(compute='_compute_counts', string='Catalog(s)')
+
+    def _compute_counts(self):
+        maps = [('product_count', 'lighting.product'),
+                ('family_count', 'lighting.product.family'),
+                ('type_count', 'lighting.product.type'),
+                ('application_count', 'lighting.product.application'),
+                ('catalog_count', 'lighting.catalog'),
+                ]
+        for record in self:
+            for field, model in maps:
+                count = self.env[model].search_count([('seo_keyword_ids', '=', record.id)])
+                setattr(record, field, count)
+
     @api.multi
     def unlink(self):
         models = ['lighting.product', 'lighting.catalog', 'lighting.product.family',
