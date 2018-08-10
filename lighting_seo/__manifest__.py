af6ae99a2dd8c02be36e7a7730a2de3ed7a22d67
Eric Antones <eantones@nuobit.com>
2018-08-10 16:19:54 +0200

    [IMP] lighting_seo: better fa icon for keyword2allproduct smart button

--- lighting_seo/__manifest__.py
+++ lighting_seo/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting SEO/SEM",
     'description': "Add SEO/SEM capabilities to Lighting vertical",
-    'version': '11.0.0.4.1',
+    'version': '11.0.0.4.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
