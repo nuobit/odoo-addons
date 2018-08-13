fb08f8c86614404ac8db0573a297a7383bee291a
Eric Antones <eantones@nuobit.com>
2018-08-13 10:45:15 +0200

    [REF] lighting_advanced_search: rename fields

--- lighting_advanced_search/wizard/product_advanced_search.py
+++ lighting_advanced_search/wizard/product_advanced_search.py
@@ -14,16 +14,19 @@ class LightingProductAdvancedSearch(models.TransientModel):
     _name = "lighting.product.advanced.search"
     _description = "Advanced search for products"
 
-    type_in_ids = fields.Many2many(comodel_name='lighting.attachment.type',
+    # attachment search
+    attachment_type_in_ids = fields.Many2many(comodel_name='lighting.attachment.type',
                                    relation='lighting_product_advanced_search_attachment_type_in_rel',
                                    column1='advanced_search_id',
                                    column2='type_id')
-    type_in_op = fields.Selection(selection=OP_SEL, string='Operator', default='and')
-    type_not_in_ids = fields.Many2many(comodel_name='lighting.attachment.type',
+    attachment_type_in_op = fields.Selection(selection=OP_SEL, string='Operator', default='and')
+    attachment_type_not_in_ids = fields.Many2many(comodel_name='lighting.attachment.type',
                                        relation='lighting_product_advanced_search_attachment_type_not_in_rel',
                                        column1='advanced_search_id',
                                        column2='type_id')
-    type_not_in_op = fields.Selection(selection=OP_SEL, string='Operator', default='and')
+    attachment_type_not_in_op = fields.Selection(selection=OP_SEL, string='Operator', default='and')
+
+
 
     '''@api.multi
     def name_get(self):
@@ -38,18 +41,18 @@ class LightingProductAdvancedSearch(models.TransientModel):
     @api.multi
     def advanced_search(self):
         domain_in = []
-        if self.type_in_ids:
-            for type_id in self.type_in_ids:
+        if self.attachment_type_in_ids:
+            for type_id in self.attachment_type_in_ids:
                 domain_in.append(('attachment_ids.type_id.id', '=', type_id.id))
-            if self.type_in_op == 'or':
+            if self.attachment_type_in_op == 'or':
                 domain_in = ['|']*(len(domain_in)-1) + domain_in
 
         domain_not_in = []
-        if self.type_not_in_ids:
+        if self.attachment_type_not_in_ids:
             domain_not_in_tmp = []
-            for type_id in self.type_not_in_ids:
+            for type_id in self.attachment_type_not_in_ids:
                 domain_not_in_tmp.append(('type_id.id', '=', type_id.id))
-            if self.type_not_in_op == 'and':
+            if self.attachment_type_not_in_op == 'and':
                 domain_not_in_tmp = ['|']*(len(domain_not_in_tmp)-1) + domain_not_in_tmp
 
             attachment_ids = self.env['lighting.attachment'].search(domain_not_in_tmp)
