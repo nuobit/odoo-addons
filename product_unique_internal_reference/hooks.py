096b6facfe66c9cfe3ebbbec6a6516b0bb5d3995
Eric Antones <eantones@nuobit.com>
2018-09-28 12:55:06 +0200

    [FIX] product_unique_internal_reference: make it company aware

--- product_unique_internal_reference/hooks.py
+++ product_unique_internal_reference/hooks.py
@@ -14,17 +14,20 @@ def pre_init_hook_internal_reference_check(cr):
         Database cursor.
     """
     with cr.savepoint():
-        cr.execute("""SELECT distinct(p0.default_code) AS default_code 
-                      FROM product_product p0
-                      WHERE EXISTS (
-                          SELECT 1
-                          FROM product_product p1
-                          WHERE p1.default_code = p0.default_code
-                            AND p1.id != p0.id
-                      )
+        cr.execute("""SELECT distinct t0.company_id, p0.default_code 
+                      FROM product_product p0, product_template t0
+                      WHERE p0.product_tmpl_id = t0.id AND 
+                            EXISTS (
+                               SELECT 1
+                               FROM product_product p1, product_template t1
+                               where p1.product_tmpl_id = t1.id AND 
+                                     t1.company_id = t0.company_id and
+                                     p1.default_code = p0.default_code AND 
+                                     p1.id != p0.id
+                            )
                       """)
 
-        products = sorted([p[0] for p in cr.fetchall()])
+        products = sorted(['[%i] %s' % p for p in cr.fetchall()])
         if products:
             raise ValidationError(
                 _('Conflicting internal references exist: %s' % ', '.join(products))
--- product_unique_internal_reference/models/models.py
+++ product_unique_internal_reference/models/models.py
@@ -3,13 +3,18 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models
+from odoo import models, fields
 from odoo.tools.translate import _
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    company_id = fields.Many2one(
+        'res.company', 'Company',
+        related='product_tmpl_id.company_id', store=True)
+
     _sql_constraints = [
-        ('default_code_uniq', 'unique(default_code)', _("A internal reference can only be assigned to one product !")),
+        ('default_code_uniq', 'unique(company_id, default_code)',
+         _("A internal reference can only be assigned to one product per company!")),
     ]
