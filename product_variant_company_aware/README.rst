f265e691b15961b5b3fe01348d1f05e8a2edc04e
Eric Antones <eantones@nuobit.com>
2018-09-28 13:27:46 +0200

    [ADD] product_variant_company_aware: define company_id field

---
+++ product_variant_company_aware/README.rst
@@ -0,0 +1,10 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=============================
+Product variant company aware
+=============================
+
+This module enable company on product variants.
+
---
+++ product_variant_company_aware/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Product variant company aware',
+    'summary': 'This module enable company on product variants',
+    'version': '10.0.0.1.0',
+    'category': 'Sales',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
