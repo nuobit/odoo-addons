e6325ca3386e78a97fba943b84a1f9eac8922c62
Eric Antones <eantones@nuobit.com>
2018-09-28 13:26:46 +0200

    [ADD] product_unique_barcode: forbid duplicated barcodes for the same company

---
+++ product_unique_barcode/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from . import models
+from .hooks import pre_init_hook_barcode_check
