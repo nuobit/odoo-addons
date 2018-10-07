2fa903ef704c783e02b2c2085e98b608aea87721
Eric Antones <eantones@nuobit.com>
2018-10-07 22:52:53 +0200

    [ADD] lighting_test: new module testing valid fields per product

---
+++ lighting_test/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Lighting TEST module",
+    'description': "Lighting TEST module",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.nuobit.com',
+    'depends': ['lighting'],
+    'data': [
+        'views/product_views.xml',
+        ],
+    'installable': True,
+}
