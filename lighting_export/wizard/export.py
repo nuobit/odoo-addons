f69eb64dbc73728bee6ab867b235034b1df6ce6d
Eric Antones <eantones@nuobit.com>
2018-10-16 18:15:33 +0200

    [IMP] lighting_export: add template selector

--- lighting_export/wizard/export.py
+++ lighting_export/wizard/export.py
@@ -5,23 +5,19 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
+
 class LightingExport(models.TransientModel):
     _name = "lighting.export"
     _description = "Export data"
 
+    template_id = fields.Many2one(comodel_name='lighting.export.template', ondelete='set null',
+                                  string='Template', required=True)
+
     @api.multi
     def export_product_xlsx(self):
         self.ensure_one()
-
-        #active_ids = self._context.get('active_ids')
-        #active_model = self._context.get('active_model')
-        #product_ids = self.env[active_model].browse(active_ids)
-
-        a=1
-
         context = dict(self.env.context,
                        active_ids=self.env.context.get('active_ids'))
-
         return {
             'name': 'Lighting product XLSX report',
             'model': 'lighting.product',
