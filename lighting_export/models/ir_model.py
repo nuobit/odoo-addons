037ad23763b0e613ac98c4b939742b57a207b8d4
Eric Antones <eantones@nuobit.com>
2018-10-16 22:00:09 +0200

    [FIX] lighting_export: add missing file

---
+++ lighting_export/models/ir_model.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+
+
+class IrModelFields(models.Model):
+    _inherit = 'ir.model.fields'
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for field in self:
+            #res.append((field.id, '%s (%s)' % (field.field_description, field.name)))
+            res.append((field.id, '%s' % (field.field_description, )))
+        return res
