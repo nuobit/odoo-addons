fe036b1c5d0eb56f7b794054888878bb42d7ba68
Eric Antones <eantones@nuobit.com>
2018-10-18 22:10:36 +0200

    [IMP] lighting_export: add security

--- lighting_export/__manifest__.py
+++ lighting_export/__manifest__.py
@@ -5,13 +5,15 @@
 {
     'name': "Lighting Export",
     'description': "Lighting export data",
-    'version': '11.0.0.8.0',
+    'version': '11.0.0.9.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
     'website': 'https://www.nuobit.com',
     'depends': ['lighting', 'report_xlsx'],
     'data': [
+        'security/export_security.xml',
+        'security/ir.model.access.csv',
         'views/export_template_views.xml',
         'wizard/export_views.xml',
         'report/export_product_xlsx.xml',
