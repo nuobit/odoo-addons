49d95aecb63869308257605023bc46564d2c490a
Eric Antones <eantones@nuobit.com>
2018-10-23 17:15:23 +0200

    [IMP] lighting_export: reuse image resize from lighting + set all exported attachment product to public

--- lighting_export/__manifest__.py
+++ lighting_export/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Export",
     'description': "Lighting export data",
-    'version': '11.0.0.10.3',
+    'version': '11.0.0.10.4',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
--- lighting_export/models/product_attachment.py
+++ lighting_export/models/product_attachment.py
@@ -2,7 +2,7 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, fields
+from odoo import api, models
 
 
 class LightingAttachment(models.Model):
@@ -10,18 +10,14 @@ class LightingAttachment(models.Model):
 
     @api.multi
     def export_name(self, template_id=None):
-        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         res = []
         for ta in template_id.attachment_ids.sorted(lambda x: x.sequence):
             prod_attachment_ids = self.filtered(lambda x: x.type_id.id == ta.type_id.id)
             if prod_attachment_ids.mapped('attachment_id'):
-                pattern_l = ['%s/web/image/%i']
-                if ta.resolution:
-                    pattern_l.append(ta.resolution)
-                pattern_l.append('%s')
                 for pa in prod_attachment_ids:
+                    pa.public = True
                     type_meta = pa.fields_get(['type_id'], ['string'])['type_id']
                     res.append([(type_meta['string'], pa.type_id.display_name),
-                                ('URL', ('/'.join(pattern_l) % (base_url, pa.attachment_id.id, pa.datas_fname))),
+                                ('URL', pa.url_get(resolution=ta.resolution)),
                                 ])
         return res
