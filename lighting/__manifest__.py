1c7b9c8d53b9e913b52c442416568e563dd73a96
Eric Antones <eantones@nuobit.com>
2018-11-07 15:39:35 +0100

    [REF] lighting: remove non language independant data

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.21.1',
+    'version': '11.0.0.22.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
