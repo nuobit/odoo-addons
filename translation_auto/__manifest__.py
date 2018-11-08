0b37106a1da9482594564709f37ee675f516a396
Eric Antones <eantones@nuobit.com>
2018-11-08 10:47:21 +0100

    [IMP] translation_auto: workaround some lack of capitalization

--- translation_auto/__manifest__.py
+++ translation_auto/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Translation auto",
     'description': "Translates ir.translation literals automagically using external resources",
-    'version': '11.0.0.1.1',
+    'version': '11.0.0.1.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
