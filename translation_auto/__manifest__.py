f49c113045c00ec763a312acb0980bfe5795dc83
Eric Antones <eantones@nuobit.com>
2018-11-08 10:39:09 +0100

    [FIX] translation_auto: crashes on odoo start if py_translator is not installed even if the module is not installed

--- translation_auto/__manifest__.py
+++ translation_auto/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Translation auto",
     'description': "Translates ir.translation literals automagically using external resources",
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.1.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
