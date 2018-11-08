df07e6e17e753224072e2e7c2b45debf72778797
Eric Antones <eantones@nuobit.com>
2018-11-08 12:10:31 +0100

    [REF] translation_auto: spaces

--- translation_auto/__manifest__.py
+++ translation_auto/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Translation auto",
     'description': "Translates ir.translation literals automagically using external resources",
-    'version': '11.0.0.1.2',
+    'version': '11.0.0.1.3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
