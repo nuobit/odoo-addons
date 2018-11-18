31c7b06a97ee2c95053922a1d63a70dff4c07303
Eric Antones <eantones@nuobit.com>
2018-11-18 16:44:39 +0100

    [MIG] web_login_default_logo: Migration to 11.0

---
+++ web_login_default_logo/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Web login default logo',
+    'summary': 'This module forces default Odoo logo at login screen.',
+    'version': '11.0.0.1.0',
+    'category': 'Web',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
