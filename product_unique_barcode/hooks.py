28d897eeb81cce8bc6a9e1886b9a25adcd2cc889
Eric Antones <eantones@nuobit.com>
2018-11-18 17:05:38 +0100

    [MIG] product_unique_barcode: Migration to 11.0

---
+++ product_unique_barcode/hooks.py
@@ -0,0 +1,31 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import _
+from odoo.exceptions import ValidationError
+
+
+def pre_init_hook_barcode_check(cr):
+    """This hook will look to see if any conflicting internal references exist before
+    the module is installed
+    :param odoo.sql_db.Cursor cr:
+        Database cursor.
+    """
+    with cr.savepoint():
+        cr.execute("""SELECT distinct p0.company_id, p0.barcode 
+                      FROM product_product p0
+                      WHERE EXISTS (
+                               SELECT 1
+                               FROM product_product p1
+                               where p1.company_id = p0.company_id and
+                                     p1.barcode = p0.barcode AND 
+                                     p1.id != p0.id
+                            )
+                      """)
+
+        products = sorted(['[%i] %s' % p for p in cr.fetchall()])
+        if products:
+            raise ValidationError(
+                _('Conflicting barcodes exist: %s' % ', '.join(products))
+            )
