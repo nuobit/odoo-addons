a1638278a4bbccc5218c4c0ad7ac01a516e268f2
Eric Antones <eantones@nuobit.com>
2018-11-18 18:45:49 +0100

    [MIG] account_bank_statement_balance_start_protection: Migration to 11.0

---
+++ account_bank_statement_balance_start_protection/models/account_bank_statement.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+
+
+class AccountBankStatement(models.Model):
+    _inherit = "account.bank.statement"
+
+    disable_balance_start = fields.Boolean() #compute="_disable_balance_start")
+
+    @api.model
+    def create(self, vals):
+        vals.update({'disable_balance_start': True})
+        result = super().create(vals)
+        return result
+
+    @api.multi
+    def write(self, vals):
+        vals.update({'disable_balance_start': True})
+        result = super().write(vals)
+        return result
