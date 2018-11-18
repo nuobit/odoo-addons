7186707a75b002b76a0eb3c595ffb16611955122
Eric Antones <eantones@nuobit.com>
2018-11-18 16:22:37 +0100

    [MIG] partner_name_vat: Migration to 11.0

---
+++ partner_name_vat/models/models.py
@@ -0,0 +1,26 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def name_get(self):
+        result = []
+        orig_name = dict(super().name_get())
+        for partner in self:
+            name = orig_name[partner.id]
+            if partner.vat:
+                name = "%s (%s)" % (name, partner.vat)
+
+            result.append((partner.id, name))
+
+        return result
+
+    @api.depends('vat')
+    def _compute_display_name(self):
+        super()._compute_display_name()
