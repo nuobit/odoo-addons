b254f2dc31be6f3c63be4262858766c0b79897bb
Eric Antones <eantones@nuobit.com>
2018-11-18 15:31:51 +0100

    [MIG] account_invoice_hide_payment: Migration to 11.0

---
+++ account_invoice_hide_payment_button/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Invoice hide payment button',
+    'summary': 'This module hides payment button on invoices',
+    'version': '11.0.0.1.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_view.xml'
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
