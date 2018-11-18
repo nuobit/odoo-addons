4ad2580378c700b84b83d0aa19ebfcb6a0eacce2
Eric Antones <eantones@nuobit.com>
2018-11-18 17:55:57 +0100

    [MIG] account_invoice_mass_email: Migration to 11.0

---
+++ account_invoice_mass_email/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Mass Email Invoices',
+    'description': 'Sending multiple invoices by email.',
+    'category': 'Accounting',
+    'version': '11.0.0.1.0',
+    'website': 'https://www.nuobit.com/',
+    'license': 'AGPL-3',
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    'depends': [
+        'account',
+    ],
+    'data': [
+        "views/mass_email_view.xml"
+    ],
+    'installable': True,
+}
