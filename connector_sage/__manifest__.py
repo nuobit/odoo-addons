88e1288987746ef1e20c29b55306e2e3d38c6bd2
Eric Antones <eantones@nuobit.com>
2018-12-13 23:56:20 +0100

    [IMP] connector_sage: add more fields on backend tree view

--- connector_sage/__manifest__.py
+++ connector_sage/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sage-Odoo connector",
-    'version': '11.0.0.1.5',
+    'version': '11.0.0.1.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
