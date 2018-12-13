4c463bd14c992b8283b204dbe7c12a95560b3cec
Eric Antones <eantones@nuobit.com>
2018-12-13 23:36:05 +0100

    [FIX] connector_sage: pymssql import error when on not installed module

--- connector_sage/models/payroll_sage_labour_agreement/adapter.py
+++ connector_sage/models/payroll_sage_labour_agreement/adapter.py
@@ -7,8 +7,6 @@ from odoo import models, fields
 from odoo.addons.component.core import Component
 from odoo.addons.queue_job.job import job
 
-import pymssql
-
 
 class PayrollSageLabourAgreementAdapter(Component):
     _name = 'sage.payroll.sage.labour.agreement.adapter'
