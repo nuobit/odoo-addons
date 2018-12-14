205502ceae57db6874b85578a849d02cdd9f10bf
Eric Antones <eantones@nuobit.com>
2018-12-14 09:28:14 +0100

    [ADD] connector_ambugest: new module

---
+++ connector_ambugest/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Ambugest-Odoo connector",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Connector',
+    'website': 'https://www.nuobit.com',
+    'depends': [
+        'connector',
+        'sale',
+    ],
+    'external_dependencies': {
+        'python': [
+            'pymssql',
+        ],
+    },
+    'data': [
+        # 'data/cron.xml',
+        'views/ambugest_backend_view.xml',
+        'views/res_partner_view.xml',
+        'views/product_product_view.xml',
+        'views/sale_order_view.xml',
+        'views/connector_ambugest_menu.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ connector_ambugest/models/res_partner/importer.py
@@ -0,0 +1,27 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import re
+
+from odoo import _
+
+from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import (
+    mapping, external_to_m2o, only_create)
+
+
+class ResPartnerBatchImporter(Component):
+    """ Import the Ambugest Partners.
+
+    For every partner in the list, a delayed job is created.
+    """
+    _name = 'ambugest.res.partner.batch.importer'
+    _inherit = 'ambugest.delayed.batch.importer'
+    _apply_on = 'ambugest.res.partner'
+
+
+class ResPartnerImporter(Component):
+    _name = 'ambugest.res.partner.importer'
+    _inherit = 'ambugest.importer'
+    _apply_on = 'ambugest.res.partner'
