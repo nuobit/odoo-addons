4066de45f77253e77fd78316329f51f908d98dbd
Eric Antones <eantones@nuobit.com>
2018-12-14 09:28:14 +0100

    [ADD] connector_ambugest: new module

---
+++ connector_ambugest/models/__init__.py
@@ -0,0 +1,12 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+# keep this at the top!
+from . import ambugest_binding
+
+from . import ambugest_backend
+
+from . import res_partner
+from . import product_product
+from . import sale_order
