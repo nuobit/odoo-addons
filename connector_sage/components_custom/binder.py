10407ded8ed17434e07859575f9453e3a5b6bdaa
Eric Antones <eantones@nuobit.com>
2018-12-17 12:33:41 +0100

    Revert "[FIX] connector_sage: protect against non installed module"
    This reverts commit 869c1d90f3d56bb42a82545c0a2330df8f0b4e69.

--- connector_sage/components_custom/binder.py
+++ connector_sage/components_custom/binder.py
@@ -17,7 +17,7 @@ from odoo import fields, models, tools
 try:
     from odoo.addons.component.core import AbstractComponent
 except ImportError:
-    AbstractComponent = None
+    pass
 
 
 class BinderComposite(AbstractComponent):
