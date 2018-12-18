4e168655b692ea4df15391aace203d5f681f3f98
Eric Antones <eantones@nuobit.com>
2018-12-18 18:06:15 +0100

    [ADD] account_cash_statement_restrict: new module

---
+++ account_cash_statement_restrict/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Cash statement restrict",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.nuobit.com',
+    'depends': [
+        'base',
+        'account'
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        #'views/account_views.xml',
+        'views/res_users_views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+}
---
+++ account_cash_statement_restrict/models/account_bank_statement.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+
+
+class AccountBankStatement(models.Model):
+    _inherit = "account.bank.statement"
+
+    state = fields.Selection(selection=[('open', 'New'), ('close', 'Closed'), ('confirm', 'Validated')])
+
+    @api.multi
+    def close_bank(self, vals):
+        for rec in self:
+            rec.state = 'close'
+
