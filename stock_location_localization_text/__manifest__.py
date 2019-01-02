0bc4aceeab861e9fa6884b0de00e4c8a641ab277
Eric Antones <eantones@nuobit.com>
2019-01-02 19:10:39 +0100

    [ADD] stock_location_localization_text: new module
        * Allows to use non numeric values for localization coordinates

---
+++ stock_location_localization_text/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Stock location localization text',
+    'summary': "This module allows to use non numeric values for localization "
+               "coordinates in stock locations",
+    'version': '11.0.0.1.0',
+    'category': 'Warehouse',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
