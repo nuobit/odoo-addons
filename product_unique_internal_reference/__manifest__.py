28af39f8a2a7a74fba1063437e2bb567c78bc0a5
Eric Antones <eantones@nuobit.com>
2019-01-02 18:24:38 +0100

    [REF] product_unique_internal_reference: integrity by soft constraint

--- product_unique_internal_reference/__manifest__.py
+++ product_unique_internal_reference/__manifest__.py
@@ -5,13 +5,13 @@
 {
     'name': 'Product unique internal reference',
     'summary': 'This module ensures that you enter a Unique Internal Reference (default_code) for your Products',
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.2.0',
     'category': 'Sales',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'website': 'https://www.nuobit.com',
     'license': 'AGPL-3',
     'depends': [
-        'product_variant_company_aware',
+        'product',
     ],
     'pre_init_hook': 'pre_init_hook_internal_reference_check',
     'installable': True,
