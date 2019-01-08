2ea7cd469cff4679ae58150c0335b587a569f601
Eric Antones <eantones@nuobit.com>
2019-01-08 13:08:22 +0100

    [IMP] lighting_project: replace prescriptor by agent on project tree view

--- lighting_project/__manifest__.py
+++ lighting_project/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Project",
     'description': "Add lighting project portfolio",
-    'version': '11.0.0.5.0',
+    'version': '11.0.0.5.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
