6f4cee811c9586f94f0097a22cf986f4b9cfd9b4
Eric Antones <eantones@nuobit.com>
2019-01-22 17:53:38 +0100

    [FIX] connector_sage: incorrect binder inheritance

--- connector_sage/__manifest__.py
+++ connector_sage/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sage-Odoo connector",
-    'version': '11.0.0.2.11',
+    'version': '11.0.0.2.12',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
