793c8b441da381ec57c928a8d65c23377bacb2c9
Eric Antones <eantones@nuobit.com>
2019-02-13 17:17:22 +0100

    [IMP] connector_sage: split transfers and payrolls

---
+++ connector_sage/models/payroll_sage_payslip_line_payroll/binding.py
@@ -0,0 +1,34 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+from odoo.addons.queue_job.job import job
+
+
+class PayslipLinePayrollBinding(models.Model):
+    _name = 'sage.payroll.sage.payslip.line.payroll'
+    _inherit = 'sage.payroll.sage.payslip.line'
+
+    _sql_constraints = [
+        ('uniq',
+         'unique(odoo_id, sage_codigo_empresa, sage_codigo_convenio, sage_fecha_registro_cv, sage_ano, sage_mesd, sage_codigo_empleado, sage_codigo_concepto_nom)',
+         'Payroll Payslip with same ID on Sage already exists.'),
+    ]
+
+    @job(default_channel='root.sage')
+    def import_payslip_lines(self, payslip_id, backend_record):
+        """ Prepare the import of payslip from Sage """
+        filters = {
+            'CodigoEmpresa': backend_record.sage_company_id,
+            'CodigoConvenio': payslip_id.labour_agreement_id.code,
+            'FechaRegistroCV': fields.Date.from_string(payslip_id.labour_agreement_id.registration_date_cv),
+
+            'Año': fields.Date.from_string(payslip_id.date).year,
+            'MesD': fields.Date.from_string(payslip_id.date).month,
+        }
+
+        self.env['sage.payroll.sage.payslip.line.payroll'].import_batch(
+            backend=backend_record, filters=filters)
+
+        return True
