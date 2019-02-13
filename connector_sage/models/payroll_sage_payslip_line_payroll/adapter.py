0e6795ca97d3a45e7e63a9898ecfdf69e05b1ca0
Eric Antones <eantones@nuobit.com>
2019-02-13 19:30:03 +0100

    [FIX] connector_sage: use differnet query for transfer and payroll

--- connector_sage/models/payroll_sage_payslip_line_payroll/adapter.py
+++ connector_sage/models/payroll_sage_payslip_line_payroll/adapter.py
@@ -13,5 +13,21 @@ class PayslipLinePayrollAdapter(Component):
 
     _apply_on = 'sage.payroll.sage.payslip.line.payroll'
 
+    _sql = """select n.CodigoEmpresa, n.Año, n.MesD, 
+                     n.CodigoEmpleado, n.CodigoConceptoNom,    
+                     c.CodigoConvenio, c.FechaRegistroCV,
+                     min(n.ConceptoLargo) as ConceptoLargo,
+                     sum(n.ImporteNom) as ImporteNom
+              from Historico n, ConvenioConcepto c
+              where n.CodigoEmpresa in (1, 2, 4, 5) AND
+                    n.Año >= 2018 AND
+                    n.CodigoConceptoNom = c.CodigoConceptoNom AND
+                    n.CodigoEmpresa = c.CodigoEmpresa
+              group by n.CodigoEmpresa, n.Año, n.MesD, 
+                       n.CodigoEmpleado, n.CodigoConceptoNom,    
+                       c.CodigoConvenio, c.FechaRegistroCV
+              having sum(n.importenom) != 0
+    """
+
     _id = ('CodigoEmpresa', 'Año', 'MesD', 'CodigoEmpleado', 'CodigoConceptoNom',
            'CodigoConvenio', 'FechaRegistroCV')
