ec51c906bff143444371562c55d758ad0db50f31
Eric Antones <eantones@nuobit.com>
2019-02-13 19:30:03 +0100

    [FIX] connector_sage: use differnet query for transfer and payroll

--- connector_sage/models/payroll_sage_payslip_line/adapter.py
+++ connector_sage/models/payroll_sage_payslip_line/adapter.py
@@ -10,24 +10,3 @@ from odoo.addons.component.core import AbstractComponent
 class PayslipLineAdapter(AbstractComponent):
     _name = 'sage.payroll.sage.payslip.line.adapter'
     _inherit = 'sage.adapter'
-
-    _sql = """select n.CodigoEmpresa, n.CodigoEmpleado, n.IdEmpleado,
-                        n.Año, n.MesD, n.CodigoConceptoNom, 
-                        n.TipoProceso,n.ClaveEspecial,
-                        n.ConceptoCorto, n.ConceptoLargo,
-                        n.FechaCobro,
-                     c.CodigoConvenio, c.FechaRegistroCV,
-                     sum(n.ImporteNom) as ImporteNom
-              from Historico n, ConvenioConcepto c
-              where n.CodigoEmpresa in (1, 2, 4, 5) AND
-                    n.Año >= 2018 AND
-                    n.CodigoConceptoNom = c.CodigoConceptoNom AND
-                    n.CodigoEmpresa = c.CodigoEmpresa
-              group by n.CodigoEmpresa, n.CodigoEmpleado, n.IdEmpleado,
-                       n.Año, n.MesD, n.CodigoConceptoNom, 
-                       n.TipoProceso,n.ClaveEspecial,
-                       n.ConceptoCorto, n.ConceptoLargo,
-                       n.FechaCobro,
-                       c.CodigoConvenio, c.FechaRegistroCV
-              having sum(n.importenom) != 0
-    """
