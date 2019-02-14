e84fdc8db520c5623a5ee006855c636ccf07b57c
Eric Antones <eantones@nuobit.com>
2019-02-14 10:41:36 +0100

    [IMP+REF] payroll_sage: add fields on tree view, add code on wage type

--- payroll_sage/models/wage_tag.py
+++ payroll_sage/models/wage_tag.py
@@ -10,6 +10,8 @@ class WageTag(models.Model):
     _name = 'payroll.sage.wage.tag'
     _description = 'Wage Tag'
 
+    code = fields.Integer('Code', required=True, default=lambda self: self.default_code())
+
     type = fields.Selection([('transfer', _('Transfer')), ('payroll', _('Payroll'))], string='Type', required=True)
 
     account_id = fields.Many2one('account.account', string='Account', required=True,
@@ -27,9 +29,23 @@ class WageTag(models.Model):
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True,
                                  copy=False, default=lambda self: self.env['res.company']._company_default_get())
 
+    note = fields.Text('Note')
+
     _sql_constraints = [
         ('wage_tag_uniq', 'unique(type,account_id,credit_debit,aggregate,negative_withholding,description,company_id)',
-         'Already exists a Wage Tag with the same parameters')]
+         'Already exists a Wage Tag with the same parameters'),
+        ('wage_code_tag_uniq', 'unique(code,company_id)',
+         'Already exists a Wage Tag with the same code'),
+    ]
+
+    @api.model
+    def default_code(self):
+        n_l = self.env[self._name].search([])
+        if not n_l:
+            return 1
+        else:
+            n = max(n_l.mapped('code'))
+            return n + 1
 
     @api.multi
     def name_get(self):
@@ -40,7 +56,8 @@ class WageTag(models.Model):
         res = []
         for rec in self:
             fd = []
-            fd1 = ["%s %s (%s)" % (
+            fd1 = ["%i - %s %s (%s)" % (
+                rec.code,
                 type_option_d.get(rec.type), rec.account_id.code,
                 credit_debit_option_d.get(rec.credit_debit))]
             fd2 = []
