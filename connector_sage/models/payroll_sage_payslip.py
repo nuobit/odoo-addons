386ba5b40f28021d1276ecc430e0971d6caa8ff8
Eric Antones <eantones@nuobit.com>
2019-02-15 12:16:18 +0100

    [FIX] connector_sage: error when import data from payslip

--- connector_sage/models/payroll_sage_payslip.py
+++ connector_sage/models/payroll_sage_payslip.py
@@ -17,8 +17,10 @@ class Payslip(models.Model):
                 raise UserError(_("Expected 1 backend for the current company, found %i" % len(backend)))
 
             # import lines and checks
+            backend.import_payslip_line_id = rec
             if rec.type == 'transfer':
                 self.env['sage.payroll.sage.payslip.line.transfer'].with_delay().import_payslip_lines(rec, backend)
+                backend.import_payslip_check_id = rec
                 self.env['sage.payroll.sage.payslip.check'].with_delay().import_payslip_checks(rec, backend)
             elif rec.type == 'payroll':
                 self.env['sage.payroll.sage.payslip.line.payroll'].with_delay().import_payslip_lines(rec, backend)
