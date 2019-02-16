efd2e4e6c467bc9d2051552be54adc85423f386f
Eric Antones <eantones@nuobit.com>
2019-02-16 12:28:28 +0100

    [IMP] payroll_sage: add balancing account to help checking errors

--- payroll_sage/__manifest__.py
+++ payroll_sage/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sage Payroll",
-    'version': '11.0.0.11.7',
+    'version': '11.0.0.11.8',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Human Resources',
