4b78330d6d4f9cedbc18662853d74841647d88c4
Eric Antones <eantones@nuobit.com>
2019-02-18 14:02:34 +0100

    [IMP] payroll_sage: create fake move to force balance

--- payroll_sage/__manifest__.py
+++ payroll_sage/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sage Payroll",
-    'version': '11.0.0.11.10',
+    'version': '11.0.0.12.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Human Resources',
