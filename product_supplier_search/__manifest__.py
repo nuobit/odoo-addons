f66943841d490382e3eddc341fc0ab09f56db114
Eric Antones <eantones@nuobit.com>
2019-03-01 13:52:30 +0100

    [ADD] product_supplier_search: new module

---
+++ product_supplier_search/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Product supplier search',
+    'summary': 'Search products by supplier reference on selection fields and tree product view',
+    'version': '11.0.0.1.0',
+    'category': 'Purchases',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/product_views.xml',
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
