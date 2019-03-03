14827bae59a79838519479c41e10916922f08424
Eric Antones <eantones@nuobit.com>
2019-03-03 16:46:26 +0100

    [FIX] lighting_export_xlsx: hide report from print menu

--- lighting_export_xlsx/__manifest__.py
+++ lighting_export_xlsx/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting XLSX Export",
     'description': "Lighting export data XLSX",
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.1.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
