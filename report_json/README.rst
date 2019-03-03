1d0f04315289b3b8b509a3e4813fa41baf9a6103
Eric Antones <eantones@nuobit.com>
2019-03-03 16:15:45 +0100

    [ADD] report_json: new module

---
+++ report_json/README.rst
@@ -0,0 +1,26 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+================
+Base report json
+================
+
+This module provides a basic report class to generate json report.
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/nuobit/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eric Antones <eantones@nuobit.com>
---
+++ report_json/report/report_json.py
@@ -0,0 +1,39 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models
+
+
+class ReportJsonAbstract(models.AbstractModel):
+    _name = 'report.report_json.abstract'
+
+    def _get_objs_for_report(self, docids, data):
+        """
+        Returns objects for json report.  From WebUI these
+        are either as docids taken from context.active_ids or
+        in the case of wizard are in data.  Manual calls may rely
+        on regular context, setting docids, or setting data.
+
+        :param docids: list of integers, typically provided by
+            qwebactionmanager for regular Models.
+        :param data: dictionary of data, if present typically provided
+            by qwebactionmanager for TransientModels.
+        :param ids: list of integers, provided by overrides.
+        :return: recordset of active model for ids.
+        """
+        if docids:
+            ids = docids
+        elif data and 'context' in data:
+            ids = data["context"].get('active_ids', [])
+        else:
+            ids = self.env.context.get('active_ids', [])
+        return self.env[self.env.context.get('active_model')].browse(ids)
+
+    def create_json_report(self, docids, data):
+        objs = self._get_objs_for_report(docids, data)
+        json_data = self.generate_json_report(data, objs)
+        return json_data, 'json'
+
+    def generate_json_report(self, data, objs):
+        raise NotImplementedError()
