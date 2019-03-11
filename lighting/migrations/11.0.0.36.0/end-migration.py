2bd4e6872b7e7ff79e9b681590a9bc3a5cf74d3e
Eric Antones <eantones@nuobit.com>
2019-03-11 19:11:25 +0100

    [IMP] lighting: convert Color temperature field type from integer to many2one bis

---
+++ lighting/migrations/11.0.0.36.0/end-migration.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    if not version:
+        return
+
+    openupgrade.drop_columns(
+        env.cr,
+        [('lighting_product_source_line', 'color_temperature'),
+         ]
+    )
---
+++ lighting/models/product_color_temperature.py
@@ -0,0 +1,24 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class LightingProductColorTemperature(models.Model):
+    _name = 'lighting.product.color.temperature'
+    _rec_name = 'value'
+    _order = 'value'
+
+    value = fields.Integer(string='Value', required=True)
+
+    product_count = fields.Integer(compute='_compute_product_count', string='Product(s)')
+
+    def _compute_product_count(self):
+        for record in self:
+            record.product_count = self.env['lighting.product'].search_count(
+                [('source_ids.line_ids.color_temperature_id', '=', record.id)])
+
+    _sql_constraints = [('name_uniq', 'unique (value)', 'The color temperature must be unique!'),
+                        ]
