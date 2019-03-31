2098e072c82d2ee0675275ecc5ab090be14ff00b
Eric Antones <eantones@nuobit.com>
2019-03-31 14:50:25 +0200

    [ADD] account_cryptocurrency: new module

---
+++ account_cryptocurrency/models/res_currency.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
+import json
+
+
+class Currency(models.Model):
+    _inherit = "res.currency"
+
+    rate = fields.Float(digits=(25, 18))
+    rounding = fields.Float(digits=(25, 18))
+
+
+class CurrencyRate(models.Model):
+    _inherit = "res.currency.rate"
+
+    rate = fields.Float(digits=(25, 18))
