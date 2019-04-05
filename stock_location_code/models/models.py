21efef62ed9810bec73e40a96abc2a5b54f07516
Eric Antones <eantones@nuobit.com>
2019-04-05 09:31:32 +0200

    [ADD] stock_location_code: new module

---
+++ stock_location_code/models/models.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+
+
+class Location(models.Model):
+    _inherit = "stock.location"
+
+    code = fields.Char(string='Code')
+
+    _sql_constraints = [
+        ('location_code_uniq', 'unique(code, company_id)', "A code can only be assigned to one location per company!"),
+    ]
