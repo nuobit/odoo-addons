40ac1101d42d69cd45bd9fa510a3ce573d3bf35e
Eric Antones <eantones@nuobit.com>
2019-04-09 17:08:48 +0200

    [ADD] stock_picking_partner_ref: new module

---
+++ stock_picking_partner_ref/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Stock picking partner reference",
+    'summary': "Adds a partner reference on pickingg",
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'category': 'Warehouse',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+}
