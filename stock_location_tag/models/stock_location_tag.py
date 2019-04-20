0e98760758a261cade45a2abb225c3d589827c46
Eric Antones <eantones@nuobit.com>
2019-04-20 11:27:02 +0200

    [ADD] stock_location_tag: new module

---
+++ stock_location_tag/models/stock_location_tag.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+
+
+class StockLocationTag(models.Model):
+    _name = "stock.location.tag"
+    _description = "Location Tags"
+
+    name = fields.Char(required=True, translate=True)
+    color = fields.Integer(string='Color Index')
+    company_id = fields.Many2one('res.company', string='Company',
+                                 default=lambda self: self.env.user.company_id,
+                                 readonly=True, required=True)
+
+    _sql_constraints = [
+        ('name_company_uniq', 'unique (name,company_id)',
+         "Tag name already exists. It must be unique per company!"),
+    ]
