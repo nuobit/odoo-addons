2c96e7e55836f56c5e2cfa41a971ec3641843814
Eric Antones <eantones@nuobit.com>
2019-05-05 20:31:29 +0200

    [REF] lighting: field relocation

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.56.1',
+    'version': '11.0.0.56.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
