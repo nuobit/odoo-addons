51fff8fe0f7650099bb9bb5cc869aa04027a4ea7
Eric Antones <eantones@nuobit.com>
2019-05-22 18:43:04 +0200

    [REF] connector_ambugest: using original reference value from ambugest and adapt to a type change

--- connector_ambugest/__manifest__.py
+++ connector_ambugest/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Ambugest-Odoo connector",
-    'version': '11.0.0.2.1',
+    'version': '11.0.0.2.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
