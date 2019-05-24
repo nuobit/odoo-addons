dd2d48e2e66bee2b6a861cf5ab8a23b11e0083f1
Eric Antones <eantones@nuobit.com>
2019-05-24 19:59:25 +0200

    [FIX] connector_ambugest: force company of the current backend

--- connector_ambugest/__manifest__.py
+++ connector_ambugest/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Ambugest-Odoo connector",
-    'version': '11.0.0.4.2',
+    'version': '11.0.0.5.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
