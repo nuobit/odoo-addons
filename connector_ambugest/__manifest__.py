8bcfd22ca7fbea5ecb26eeae3c9e14eabbdcabb0
Eric Antones <eantones@nuobit.com>
2019-06-06 16:51:28 +0200

    [IMP] connector_ambugest: not sync order line price and total

--- connector_ambugest/__manifest__.py
+++ connector_ambugest/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Ambugest-Odoo connector",
-    'version': '11.0.0.8.5',
+    'version': '11.0.0.8.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
