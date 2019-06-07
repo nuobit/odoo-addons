1c467de6d724e49a1d1942cf0afa2cce4e06a45a
Eric Antones <eantones@nuobit.com>
2019-06-07 12:00:21 +0200

    [FIX] connector_ambugest: payment terms, payment method and product prices not populated

--- connector_ambugest/__manifest__.py
+++ connector_ambugest/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Ambugest-Odoo connector",
-    'version': '11.0.0.8.12',
+    'version': '11.0.0.8.13',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
