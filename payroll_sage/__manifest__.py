13376d07d139818988b5f634dc721ff9167798e2
Eric Antones <eantones@nuobit.com>
2019-06-14 19:18:54 +0200

    [IMP] payroll_sage: add process type

--- payroll_sage/__manifest__.py
+++ payroll_sage/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sage Payroll",
-    'version': '11.0.0.12.6',
+    'version': '11.0.0.13.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Human Resources',
