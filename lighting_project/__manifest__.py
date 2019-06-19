1f95070b8ed77b81a1fcc8316f3bffd56887ecbe
Eric Antones <eantones@nuobit.com>
2019-06-19 11:10:16 +0200

    [FIX] lighting_project: error accessing product common data when user has not any permission on project module

--- lighting_project/__manifest__.py
+++ lighting_project/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Project",
     'description': "Add lighting project portfolio",
-    'version': '11.0.0.5.1',
+    'version': '11.0.0.5.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
