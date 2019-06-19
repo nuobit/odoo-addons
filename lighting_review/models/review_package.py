530e3d79d2e0fdf6ad5d650de00dd3e1da804786
Eric Antones <eantones@nuobit.com>
2019-06-19 21:22:52 +0200

    [IMP] lighting_review: added statistics

--- lighting_review/models/review_package.py
+++ lighting_review/models/review_package.py
@@ -5,6 +5,8 @@
 from odoo import api, fields, models, _, tools
 from odoo.exceptions import UserError
 
+import datetime
+
 
 class LightingProductReview(models.Model):
     _name = 'lighting.review.package'
@@ -12,8 +14,9 @@ class LightingProductReview(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
-    start_date = fields.Date(required=True)
-    end_date = fields.Date()
+    start_date = fields.Date(string='Start date', required=True)
+    due_date = fields.Date(string='Due date', required=True, help='Maximum completion date')
+    end_date = fields.Date(string='End date', help='Actual completion date')
 
     responsible_ids = fields.Many2many(comodel_name='res.users',
                                        relation='lighting_review_package_user_rel',
@@ -26,21 +29,93 @@ class LightingProductReview(models.Model):
     _sql_constraints = [('name_uniq', 'unique (name)', 'The review package name must be unique!'),
                         ]
 
-    product_reviewed_count = fields.Integer(string='Reviewed product(s)', compute='_compute_product_count')
-    product_pending_count = fields.Integer(string='Pending product(s)', compute='_compute_product_count')
+    reviewed_count = fields.Integer(string='Reviewed product(s)', compute='_compute_reviewed_count')
+    pending_count = fields.Integer(string='Pending product(s)', compute='_compute_reviewed_count')
 
-    def _compute_product_count(self):
+    def _compute_reviewed_count(self):
         for rec in self:
-            rec.product_reviewed_count = len(rec.review_ids.filtered(lambda x: x.reviewed))
-            rec.product_pending_count = len(rec.review_ids.filtered(lambda x: not x.reviewed))
+            rec.reviewed_count = len(rec.review_ids.filtered(lambda x: x.reviewed))
+            rec.pending_count = len(rec.review_ids.filtered(lambda x: not x.reviewed))
 
-    product_reviewed_percent = fields.Float(compute='_compute_product_reviewed_percent', string='% Reviewed')
+    completed_percent = fields.Float(compute='_compute_completed_percent', string='% Completed')
 
-    def _compute_product_reviewed_percent(self):
+    def _compute_completed_percent(self):
         for rec in self:
-            product_count = len(rec.review_ids.mapped('product_id'))
+            product_count = len(rec.review_ids)
             if product_count != 0:
-                rec.product_reviewed_percent = rec.product_reviewed_count / product_count * 100
+                rec.completed_percent = rec.reviewed_count / product_count * 100
+
+    days = fields.Integer(string='Days', help='Days from the begining',
+                          readonly=True, compute='_compute_revision_stats')
+
+    velocity = fields.Float(string='Velocity (rev/day)', help='Revision velocity (revisions/day)',
+                            readonly=True,
+                            compute='_compute_revision_stats')
+
+    estimated_total_days = fields.Integer(string='Total days (est.)',
+                                          help='Total estimated days of completion at current velocity',
+                                          readonly=True, compute='_compute_revision_stats')
+
+    estimated_date = fields.Date(string='Date (est.)',
+                                 help='Estimated date of completion at current velocity (excluding weekends)',
+                                 readonly=True, compute='_compute_revision_stats')
+
+    estimated_remaining_days = fields.Integer(string='Remaining days (est.)',
+                                              help='Remaining days at current velocity (excluding weekends)',
+                                              readonly=True, compute='_compute_revision_stats')
+
+    estimated_days_late = fields.Integer(string='Days late (est.)',
+                                         help='Days late at current velocity (excluding weekends)',
+                                         readonly=True, compute='_compute_revision_stats')
+
+    def _compute_revision_stats(self):
+        today = fields.date.today()
+        from_string = fields.Date.from_string
+        for rec in self:
+            total_count = len(rec.review_ids)
+
+            rec.days = (today - from_string(rec.start_date)).days
+
+            days_from = rec.days
+            if days_from != 0:
+                rec.velocity = rec.reviewed_count / days_from
+
+            if rec.velocity:
+                rec.estimated_total_days = total_count / rec.velocity
+
+                work_days = 0
+                est_date = from_string(rec.start_date)
+                while work_days < rec.estimated_total_days:
+                    if est_date.isoweekday() not in (6, 7):
+                        work_days += 1
+                    est_date += datetime.timedelta(days=1)
+
+                rec.estimated_date = est_date - datetime.timedelta(days=1)
+
+                remaining_days = 0
+                cur_date = today
+                while cur_date <= from_string(rec.estimated_date):
+                    if cur_date.isoweekday() not in (6, 7):
+                        remaining_days += 1
+                    cur_date += datetime.timedelta(days=1)
+
+                rec.estimated_remaining_days = remaining_days
+
+                if rec.due_date:
+                    days_late = 0
+                    date0 = from_string(rec.due_date)
+                    date1 = from_string(rec.estimated_date)
+                    abs = 1
+                    if date0 > date1:
+                        date0, date1 = date1, date0
+                        abs = -1
+
+                    while date0 <= date1:
+                        if date0.isoweekday() not in (6, 7):
+                            days_late += 1
+                        date0 += datetime.timedelta(days=1)
+
+                    rec.estimated_days_late = days_late * abs
 
     def action_product_reviewed(self):
         return {
