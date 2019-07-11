8d20f99320e678196a63afb12e6222c13d7e493e
Eric Antones <eantones@nuobit.com>
2019-07-11 19:17:11 +0200

    [IMP] lighting: replace group attributes and fields by types

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.77.1',
+    'version': '11.0.0.77.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
