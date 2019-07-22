a796cc0aecbaa328b8cf08d3041ffb993f8d3249
Eric Antones <eantones@nuobit.com>
2019-07-22 13:14:46 +0200

    [IMP] lighting: added kanban view on catalog

--- lighting/models/product_catalog.py
+++ lighting/models/product_catalog.py
@@ -2,7 +2,7 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -24,8 +24,37 @@ class LightingCatalog(models.Model):
 
     color = fields.Integer(string='Color Index')
 
+    # image: all image fields are base64 encoded and PIL-supported
+    image = fields.Binary(
+        "Image", attachment=True,
+        help="This field holds the image used as image for the product, limited to 1024x1024px.")
+    image_medium = fields.Binary(
+        "Medium-sized image", attachment=True,
+        help="Medium-sized image of the product. It is automatically "
+             "resized as a 128x128px image, with aspect ratio preserved, "
+             "only when the image exceeds one of those sizes. Use this field in form views or some kanban views.")
+    image_small = fields.Binary(
+        "Small-sized image", attachment=True,
+        help="Small-sized image of the product. It is automatically "
+             "resized as a 64x64px image, with aspect ratio preserved. "
+             "Use this field anywhere a small image is required.")
+
     _sql_constraints = [('name_uniq', 'unique (name)', 'The name of catalog must be unique!'),
                         ]
+
+    @api.model
+    def create(self, values):
+        tools.image_resize_images(values)
+        res = super().create(values)
+
+        return res
+
+    @api.multi
+    def write(self, values):
+        tools.image_resize_images(values)
+        res = super().write(values)
+
+        return res
 
     @api.multi
     def unlink(self):
