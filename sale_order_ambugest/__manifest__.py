98485dfb27540df1eaa41eedc65f98d24cf759ad
Eric Antones <eantones@nuobit.com>
2019-07-23 19:12:24 +0200

    [ADD] sale_order_ambugest: new module

---
+++ sale_order_ambugest/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Sale Order Ambugest',
+    'summary': 'This module adds Ambugest specific data to sale orders',
+    'version': '11.0.0.1.0',
+    'category': 'Sales',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://www.nuobit.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ sale_order_ambugest/models/sale_order.py
@@ -0,0 +1,27 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # patient data
+    contract_number = fields.Char(string="Contract number")
+    insured_name = fields.Char(string="Insured person's name")
+    insured_ident_cardnum = fields.Char(string="Insured identity card number")
+    policy_number = fields.Char(string="Policy number")
+    auth_number = fields.Char(string="Authorization Number")
+
+    # service data
+    plate_number = fields.Char(string="Plate")
+    service_number = fields.Char(string="Service number")
+    service_date = fields.Date(string="Service date")
+    origin = fields.Char(string="Origin")
+    destination = fields.Char(string="Destination")
+    service_direction = fields.Selection(selection=[('going', _('Going')), ('return', _('Return'))],
+                                         string="Service direction")
