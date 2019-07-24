ab596bdde926a65e9aa740988203aeaea835cd80
Eric Antones <eantones@nuobit.com>
2019-07-24 22:47:38 +0200

    [IMP] connector_ambugest: update order lines

--- connector_ambugest/models/sale_order/mapper.py
+++ connector_ambugest/models/sale_order/mapper.py
@@ -11,6 +11,67 @@ from odoo.addons.connector.components.mapper import (
     mapping, external_to_m2o, only_create)
 
 
+class SaleOrderImportMapChild(Component):
+    _name = 'ambugest.sale.order.map.child.import'
+    _inherit = 'ambugest.map.child.import'
+    _apply_on = 'ambugest.sale.order.line'
+
+    def get_item_values(self, map_record, to_attr, options):
+        """ Get the raw values from the child Mappers for the items.
+
+        It can be overridden for instance to:
+
+        * Change options
+        * Use a :py:class:`~connector.connector.Binder` to know if an
+          item already exists to modify an existing item, rather than to
+          add it
+
+        :param map_record: record that we are converting
+        :type map_record: :py:class:`MapRecord`
+        :param to_attr: destination field (can be used for introspecting
+                        the relation)
+        :type to_attr: str
+        :param options: dict of options, herited from the main mapper
+
+        """
+        key = ['EMPRESA', 'Fecha_Servicio', 'Codigo_Servicio', 'Servicio_Dia',
+               'Servicio_Ano', 'Articulo']
+        external_id = tuple([map_record.source[x] for x in key])
+
+        binder = self.binder_for('ambugest.sale.order.line')
+        ambugest_order_line = binder.to_internal(external_id, unwrap=False)
+
+        if ambugest_order_line:
+            map_record.update(id=ambugest_order_line.id)
+
+        return map_record.values(**options)
+
+    def format_items(self, items_values):
+        """ Format the values of the items mapped from the child Mappers.
+
+        It can be overridden for instance to add the OpenERP
+        relationships commands ``(6, 0, [IDs])``, ...
+
+        As instance, it can be modified to handle update of existing
+        items: check if an 'id' has been defined by
+        :py:meth:`get_item_values` then use the ``(1, ID, {values}``)
+        command
+
+        :param items_values: list of values for the items to create
+        :type items_values: list
+
+        """
+        ops = []
+        for values in items_values:
+            id = values.pop('id', None)
+            if id:
+                ops.append((1, id, values))
+            else:
+                ops.append((0, False, values))
+
+        return ops
+
+
 def nullif(field):
     def modifier(self, record, to_attr):
         value = record[field]
@@ -60,6 +121,8 @@ class SaleOrderImportMapper(Component):
     children = [(_get_order_lines, 'ambugest_order_line_ids', 'ambugest.sale.order.line')]
 
     def _map_child(self, map_record, from_attr, to_attr, model_name):
+        assert self._map_child_usage is not None, "_map_child_usage required"
+
         source = map_record.source
         # TODO patch ImportMapper in connector to support callable
         if callable(from_attr):
@@ -67,18 +130,19 @@ class SaleOrderImportMapper(Component):
         else:
             child_records = source[from_attr]
 
+        adapter = self.component(
+            usage='backend.adapter', model_name=model_name
+        )
+
         children = []
         for child_record in child_records:
-            adapter = self.component(
-                usage='backend.adapter', model_name=model_name
-            )
             detail_record = adapter.read(child_record)
-
             mapper = self._get_map_child_component(model_name)
             items = mapper.get_items(
                 [detail_record], map_record, to_attr, options=self.options
             )
             children.extend(items)
+
         return children
 
     @only_create
