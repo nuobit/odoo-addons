1242b31c24de6da686ba24c819007c5162bb50de
Eric Antones <eantones@nuobit.com>
2019-07-30 22:33:51 +0200

    [FIX] custom_gj: wrong field

--- custom_gj/__manifest__.py
+++ custom_gj/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "GJ Customizations",
     'description': "GJ Customizations",
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.1.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
