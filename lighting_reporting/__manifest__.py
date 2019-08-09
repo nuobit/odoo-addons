9c497596656a068cdddb0114fcf56f137cde81eb
Eric Antones <eantones@nuobit.com>
2019-08-09 18:58:45 +0200

    [IMP] lighting_reporting: change booleans to selections to allow null values

--- lighting_reporting/__manifest__.py
+++ lighting_reporting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Reporting",
     'description': """* Product data sheet""",
-    'version': '11.0.0.9.1',
+    'version': '11.0.0.9.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
