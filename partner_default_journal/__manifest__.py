61813d9f800e8c581cc24e7806e82a74cb8eafa3
Eric Antones <eantones@nuobit.com>
2019-09-24 21:42:33 +0200

    [ADD] partner_default_journal: new module

---
+++ partner_default_journal/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Partner default journal",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'website': 'https://www.nuobit.com',
+    'depends': [
+        'account',
+        'purchase',
+        'sale',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+}
---
+++ partner_default_journal/models/account_invoice.py
@@ -0,0 +1,38 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.model
+    def create(self, vals):
+        if 'journal_id' not in vals and 'partner_id' in vals and 'type' in vals:
+            partner_id = self.env['res.partner'].browse(vals['partner_id'])
+            if partner_id.sale_journal_id and vals['type'] in ('out_invoice', 'out_refund'):
+                vals['journal_id'] = self.partner_id.sale_journal_id
+            elif partner_id.purchase_journal_id and vals['type'] in ('in_invoice', 'in_refund'):
+                vals['journal_id'] = self.partner_id.purchase_journal_id
+
+        invoice = super(AccountInvoice, self).create(vals)
+
+        return invoice
+
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        res = super(AccountInvoice, self)._onchange_partner_id()
+
+        if self.partner_id.sale_journal_id and self.type in ('out_invoice', 'out_refund'):
+            self.journal_id = self.partner_id.sale_journal_id
+        elif self.partner_id.purchase_journal_id and self.type in ('in_invoice', 'in_refund'):
+            self.journal_id = self.partner_id.purchase_journal_id
+
+        if not self.partner_id.sale_journal_id and not self.partner_id.purchase_journal_id:
+            default_journal = self.with_context(type=self.type)._default_journal()
+            if default_journal != self.journal_id:
+                self.journal_id = default_journal
+
+        return res
