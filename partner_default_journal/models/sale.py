7202777a91974b68a5357e76345cf777fc54f99d
Eric Antones <eantones@nuobit.com>
2019-09-24 21:42:33 +0200

    [ADD] partner_default_journal: new module

---
+++ partner_default_journal/models/sale.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+
+        if self.partner_id.sale_journal_id:
+            invoice_vals['journal_id'] = self.partner_id.sale_journal_id.id
+
+        return invoice_vals
