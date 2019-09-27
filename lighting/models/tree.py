14a86335facc88c5000fd5f73eedbfe2fe83df9b
Eric Antones <eantones@nuobit.com>
2019-09-27 20:37:00 +0200

    [FIX] lighting: search category by complete name

--- lighting/models/tree.py
+++ lighting/models/tree.py
@@ -8,7 +8,9 @@ from odoo import api, fields, models, _
 class LightingTreeMixin(models.AbstractModel):
     _name = 'lighting.tree.mixin'
 
-    complete_name = fields.Char('Complete Name', compute='_compute_complete_name')
+    complete_name = fields.Char('Complete Name',
+                                compute='_compute_complete_name',
+                                search='_search_complete_name')
 
     @api.depends('name', 'parent_id.complete_name')
     def _compute_complete_name(self):
@@ -17,6 +19,9 @@ class LightingTreeMixin(models.AbstractModel):
                 rec.complete_name = '%s / %s' % (rec.parent_id.complete_name, rec.name)
             else:
                 rec.complete_name = rec.name
+
+    def _search_complete_name(self, operator, value):
+        return [('name', operator, value)]
 
     child_count = fields.Integer(compute='_compute_child_count', string='Childs')
 
