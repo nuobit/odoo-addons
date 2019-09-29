c022ac59a75264c8e56030dadec85d05c5c4edbe
Eric Antones <eantones@nuobit.com>
2019-09-29 17:16:56 +0200

    [ADD] sale_order_invoiced_unlink: new module

---
+++ sale_order_invoiced_unlink/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Sale order invoiced unlink",
+    'summary': "If a sale order has any of its lines linked to an invoice "
+               "this module does not allow to delete the order.",
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'category': 'Sales',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
