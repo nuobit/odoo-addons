2d66bcef0ac741b8f1f32d0b412cd8902e2c5ea2
Eric Antones <eantones@nuobit.com>
2019-10-28 21:53:53 +0100

    [ADD] l10n_es_aeat_sii_mock: Backport of 14.0 l10n_es_aeat_sii_oca_mock module

---
+++ l10n_es_aeat_sii_mock/models/account_invoice.py
@@ -0,0 +1,63 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    def _connect_sii(self, mapping_key):
+        class SiiMock:
+            _res = {
+                "EstadoEnvio": "Correcto",
+                "CSV": "CSVTEST",
+                "RespuestaLinea": [{"CodigoErrorRegistro": False}],
+            }
+
+            def __init__(self, *args, **kwargs):
+                self.invoice = kwargs.get("invoice")
+
+            def SuministroLRFacturasEmitidas(self, header, inv_dict):
+                _logger.info(
+                    "SuministroLRFacturasEmitidas:\nHeader: %s\nInvoice: %s",
+                    header,
+                    inv_dict,
+                )
+                return self._res
+
+            def SuministroLRFacturasRecibidas(self, header, inv_dict):
+                _logger.info(
+                    "SuministroLRFacturasRecibidas:\nHeader: %s\nInvoice: %s",
+                    header,
+                    inv_dict,
+                )
+                return self._res
+
+            def AnulacionLRFacturasEmitidas(self, header, inv_dict):
+                _logger.info(
+                    "AnulacionLRFacturasEmitidas:\nHeader: %s\nInvoice: %s",
+                    header,
+                    inv_dict,
+                )
+                if self.invoice:
+                    self.invoice.sii_header_sent = False
+                    self.invoice.sii_content_sent = False
+                return self._res
+
+            def AnulacionLRFacturasRecibidas(self, header, inv_dict):
+                _logger.info(
+                    "AnulacionLRFacturasRecibidas:\nHeader: %s\nInvoice: %s",
+                    header,
+                    inv_dict,
+                )
+                if self.invoice:
+                    self.invoice.sii_header_sent = False
+                    self.invoice.sii_content_sent = False
+                return self._res
+
+        return SiiMock(invoice=self)
