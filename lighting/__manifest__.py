acceefae2c1bdcdbd9da54171b5148ae5b15a9ac
Eric Antones <eantones@nuobit.com>
2019-11-08 19:18:22 +0100

    [TRL] lighting: translations

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.86.19',
+    'version': '11.0.0.86.20',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
