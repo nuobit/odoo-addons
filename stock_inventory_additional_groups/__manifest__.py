5688db3995ecc94e60b9f28eb8e31d757cc685df
Eric Antones <eantones@nuobit.com>
2019-11-12 18:36:40 +0100

    [ADD] stock_inventory_additional_groups: new module

---
+++ stock_inventory_additional_groups/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Stock inventory additional groups",
+    'summary': "This module adds adittional permission groups on inventory module",
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'category': 'Warehouse',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'security/stock_security.xml',
+        'views/stock_inventory_views.xml'
+    ],
+    'installable': True,
+}
