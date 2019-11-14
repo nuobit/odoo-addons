18168caf6e73396b5e36ff2e724f6e343ccc0f47
Eric Antones <eantones@nuobit.com>
2019-11-14 11:54:35 +0100

    [IMP] lighting_topbar: separate permissions for top bar icons and menus

--- lighting_topbar/__manifest__.py
+++ lighting_topbar/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting Top bar",
     'description': "Remove backoffice root menus and systray",
-    'version': '11.0.0.1.2',
+    'version': '11.0.0.2.0',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
