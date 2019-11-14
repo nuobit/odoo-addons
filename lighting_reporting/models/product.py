cb0ec161e85dad19a0832076493be2e6bdefd7e8
Eric Antones <eantones@nuobit.com>
2019-11-14 15:33:40 +0100

    [IMP] lighting_reporting: resize images on the fly

--- lighting_reporting/models/product.py
+++ lighting_reporting/models/product.py
@@ -5,7 +5,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageEnhance
 import io
 import base64
 
@@ -40,6 +40,30 @@ def expand2square(im, bgcolor):
         result = Image.new(im.mode, (height, height), bgcolor)
         result.paste(im, ((height - width) // 2, 0))
         return result
+
+
+def resize(im, asked_size, by_side_long=False, allow_scale=True):
+    if asked_size and asked_size != (None, None):
+        im_size = im.size
+        if by_side_long and im.size[0] < im.size[1]:
+            im_size = (im_size[1], im_size[0])
+
+        hw_ratio = im_size[1] / im_size[0]
+        asked_width, asked_height = asked_size
+        if asked_height is None:
+            asked_height = int(float(asked_width) * hw_ratio)
+        if asked_width is None:
+            asked_width = int(float(asked_height) * 1 / hw_ratio)
+
+        asked_size = asked_width, asked_height
+        if im_size != asked_size:
+            if by_side_long and im.size[0] < im.size[1]:
+                asked_size = (asked_size[1], asked_size[0])
+            if not allow_scale and (asked_size[0] > im.size[0] or asked_size[1] > im.size[1]):
+                return im
+            return im.resize(asked_size, Image.ANTIALIAS)
+
+    return im
 
 
 def chunks(li, n):
@@ -177,18 +201,23 @@ class LightingProduct(models.Model):
 class LightingAttachment(models.Model):
     _inherit = 'lighting.attachment'
 
-    def get_optimized_image(self, enabled=False):
+    def get_optimized_image(self, enabled=True):
         if not enabled:
             return self.datas
 
         datas = base64.decodebytes(self.datas)
         im = Image.open(io.BytesIO(datas))
 
-        im9 = autocrop(im, (255, 255, 255))
-        if not im9:
-            return self.datas
+        im99 = resize(im, (500, None), by_side_long=True, allow_scale=False)
 
-        im99 = expand2square(im9, (255, 255, 255))
+        #sharpener = ImageEnhance.Sharpness(im7)
+        #im99 = sharpener.enhance(2.0)
+
+        # im9 = autocrop(im7, (255, 255, 255))
+        # if not im9:
+        #     return self.datas
+        #
+        # im99 = expand2square(im9, (255, 255, 255))
 
         in_mem_file = io.BytesIO()
         im99.save(in_mem_file, format=im.format)
