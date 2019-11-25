270b895bb61f5ad9cba37c69a11e325ebd240d0e
Eric Antones <eantones@nuobit.com>
2019-11-25 19:25:48 +0100

    [ADD] partner_review: new module to keep track of partners to review

---
+++ partner_review/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Partner review",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'website': 'https://www.nuobit.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
