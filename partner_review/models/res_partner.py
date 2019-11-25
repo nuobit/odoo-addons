1acd9cd364e45d0b358af2e57966b9ec105b6b74
Eric Antones <eantones@nuobit.com>
2019-11-25 19:25:48 +0100

    [ADD] partner_review: new module to keep track of partners to review

---
+++ partner_review/models/res_partner.py
@@ -0,0 +1,11 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    to_review = fields.Boolean("To review")
