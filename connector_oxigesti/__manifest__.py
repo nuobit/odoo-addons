c9894d8689693d15da253c2ae57c9631f122bf48
Eric Antones <eantones@nuobit.com>
2019-11-25 19:24:07 +0100

    [UPD] connector_oxigesti: add partner_review dependency

--- connector_oxigesti/__manifest__.py
+++ connector_oxigesti/__manifest__.py
@@ -4,12 +4,13 @@
 
 {
     'name': "Oxigesti-Odoo connector",
-    'version': '11.0.0.1.3',
+    'version': '11.0.0.1.4',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
     'website': 'https://www.nuobit.com',
     'depends': [
+        'partner_review',
         'connector',
     ],
     'external_dependencies': {
