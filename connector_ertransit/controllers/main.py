945243edfd011695153fefc82f5d39a9215b729a
Eric Antones <eantones@nuobit.com>
2019-12-02 15:43:35 +0100

    [IMP] connector_ertransit: log remote ip address

--- connector_ertransit/controllers/main.py
+++ connector_ertransit/controllers/main.py
@@ -12,6 +12,7 @@ class ERTransitController(http.Controller):
     @http.route(['/tracking/ertransit/<string:tracking_number>',
                  ], type='http', auth="public")
     def tracking_data(self, tracking_number=None):
+        remote_ip = http.request.httprequest.environ['REMOTE_ADDR']
         ertransit_backend = http.request.env['ertransit.backend'].search([
             ('active', '=', True),
             ('state', '=', 'checked'),
@@ -23,13 +24,13 @@ class ERTransitController(http.Controller):
         er = ERTransit(username=ertransit_backend.username,
                        password=ertransit_backend.password)
 
-        _logger.info("Getting %s shipment..." % tracking_number)
+        _logger.info("Asking %s ERTransit shipment... from %s" % (tracking_number, remote_ip))
         if not er.login():
             raise werkzeug.exceptions.Unauthorized()
         data = er.filter_by_reffra(tracking_number)
         if not er.logout():
             raise werkzeug.exceptions.InternalServerError("Logout not successful")
-        _logger.info("Shipment %s successfully retrieved." % tracking_number)
+        _logger.info("ERTransit shipment %s successfully retrieved from %s." % (tracking_number, remote_ip))
 
         headers = ['Ref. Fra.', 'Fecha Carga / BL', 'Origen', 'Remitente', 'Fecha Llegada', 'Fecha descarga',
                    'Destino', 'Destinatario', 'Bultos', 'Peso', 'Volumen', 'Fecha Entrega', 'Estado Entrega']
