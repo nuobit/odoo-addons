d80426fff5fc733ad4dea07b221e663cd69f46a2
Eric Antones <eantones@nuobit.com>
2019-12-02 15:37:51 +0100

    [ADD] connector_ertransit: new module

---
+++ connector_ertransit/README.rst
@@ -0,0 +1,29 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=============================
+Connector ERTransit / Erhardt
+=============================
+
+* Coonector to get tracking info from ERTransit / Erhardt
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/nuobit/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eric Antones <eantones@nuobit.com>
+
+
+
+
---
+++ connector_ertransit/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Connector ERTransit / Erhardt",
+    'description': "ERTransit / Erhardtconnector",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Connector',
+    'website': 'https://github.com/nuobit',
+    'external_dependencies': {
+        'python': [
+            'requests',
+            'lxml',
+        ],
+    },
+    'depends': [
+        'connector',
+    ],
+    'data': [
+        'views/backend_views.xml',
+        'views/menus.xml',
+        'templates/ertransit_template.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
