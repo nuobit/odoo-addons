39ce76fcc6c493ed3f00ae3951f8e629c15f8505
Eric Antones <eantones@nuobit.com>
2019-12-04 19:18:03 +0100

    [IMP] lighting_etim: added search capabilities to group

--- lighting_etim/__manifest__.py
+++ lighting_etim/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting ETIM",
     'description': "Lighting ETIM",
-    'version': '11.0.0.1.9',
+    'version': '11.0.0.1.10',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
