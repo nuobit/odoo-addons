fe4ce1771446ebc2e66cee183dfde8c4c7aa2791
Eric Antones <eantones@nuobit.com>
2019-12-04 19:21:34 +0100

    [IMP] lighting_etim: added search capabilities to unit

--- lighting_etim/models/etim_unit.py
+++ lighting_etim/models/etim_unit.py
@@ -25,3 +25,10 @@ class LightingETIMUnit(models.Model):
             vals.append((record.id, name))
 
         return vals
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        if args is None:
+            args = []
+        recs = self.search(['|', ('code', operator, name), ('name', operator, name)] + args, limit=limit)
+        return recs.name_get()
