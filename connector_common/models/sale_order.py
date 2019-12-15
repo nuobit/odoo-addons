4f8422d9376fe317facddcb550511eb7d4098b70
Eric Antones <eantones@nuobit.com>
2019-12-15 19:33:29 +0100

    [ADD] connector_common: new module to group common features between connectors

---
+++ connector_common/models/sale_order.py
@@ -0,0 +1,25 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        for rec in self:
+            super(SaleOrder, rec).action_confirm()
+            self._event('on_confirm_order').notify(rec)
+
+        return True
+
+    @api.multi
+    def action_cancel(self):
+        for rec in self:
+            super(SaleOrder, rec).action_cancel()
+            self._event('on_cancel_order').notify(rec)
+
+        return True
