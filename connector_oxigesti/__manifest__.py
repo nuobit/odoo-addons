f93d464b869cc934faf9d872a534fd0efac26843
Eric Antones <eantones@nuobit.com>
2019-12-15 19:31:15 +0100

    [REF] connector_oxigesti: using connector_common module

--- connector_oxigesti/__manifest__.py
+++ connector_oxigesti/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Oxigesti-Odoo connector",
-    'version': '11.0.0.3.0',
+    'version': '11.0.0.3.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
@@ -12,7 +12,7 @@
     'depends': [
         'partner_review',
         'sale_line_partner_description',
-        'connector',
+        'connector_common',
     ],
     'external_dependencies': {
         'python': [
