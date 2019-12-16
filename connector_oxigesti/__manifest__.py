647ef02d6f1c0fcdffaa975099ddace6c0f53fa1
Eric Antones <eantones@nuobit.com>
2019-12-16 18:25:17 +0100

    [IMP] connector_oxigesti: better error messages

--- connector_oxigesti/__manifest__.py
+++ connector_oxigesti/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Oxigesti-Odoo connector",
-    'version': '11.0.0.3.5',
+    'version': '11.0.0.3.6',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
