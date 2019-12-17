8a886973dc8c5a0e168ea813014f0fa5770ecb56
Eric Antones <eantones@nuobit.com>
2019-12-17 21:09:56 +0100

    [FIX] connector_oxigesti: wrong references in error message

--- connector_oxigesti/__manifest__.py
+++ connector_oxigesti/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Oxigesti-Odoo connector",
-    'version': '11.0.0.5.0',
+    'version': '11.0.0.5.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
