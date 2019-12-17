2e35baef1c3cecd3b84720071f90a53014445592
Eric Antones <eantones@nuobit.com>
2019-12-17 17:04:15 +0100

    [IMP] connector_oxigesti: Several improvements
        * Change filters on backend queries similar to Odoo native domains.
        * Added xml data for a default cron tasks.
        * Use modification dates and sync date to only imoprt/export changed records.
        * Improve error messages.
        * Fix minor bugs.

--- connector_oxigesti/models/oxigesti_backend/common.py
+++ connector_oxigesti/models/oxigesti_backend/common.py
@@ -75,16 +75,17 @@ class OxigestiBackend(models.Model):
         self._check_connection()
         self.write({'state': 'checked'})
 
-    import_services_since_date = fields.Datetime('Import Services since')
     import_customers_since_date = fields.Datetime('Import Customers since')
     export_products_since_date = fields.Datetime('Export Products since')
     export_products_by_customer_since_date = fields.Datetime('Export Products by customer since')
     export_product_prices_by_customer_since_date = fields.Datetime('Export Product prices by customer since')
+    import_services_since_date = fields.Datetime('Import Services since')
 
+    # Backend data methods
     @api.multi
     def import_customers_since(self):
         for rec in self:
-            since_date = rec.import_customers_since_date
+            since_date = fields.Datetime.from_string(rec.import_customers_since_date)
             self.env['oxigesti.res.partner'].with_delay(
             ).import_customers_since(
                 backend_record=rec, since_date=since_date)
@@ -94,7 +95,7 @@ class OxigestiBackend(models.Model):
     @api.multi
     def export_products_since(self):
         for rec in self:
-            since_date = rec.export_products_by_customer_since_date
+            since_date = rec.export_products_since_date
             self.env['oxigesti.product.product'].with_delay(
             ).export_products_since(
                 backend_record=rec, since_date=since_date)
@@ -124,13 +125,14 @@ class OxigestiBackend(models.Model):
     @api.multi
     def import_services_since(self):
         for rec in self:
-            since_date = rec.import_services_since_date
+            since_date = fields.Datetime.from_string(rec.import_services_since_date)
             self.env['oxigesti.sale.order'].with_delay(
             ).import_services_since(
                 backend_record=rec, since_date=since_date)
 
         return True
 
+    # Scheduler methods
     @api.model
     def get_current_user_company(self):
         if self.env.user.id == self.env.ref('base.user_root').id:
@@ -141,29 +143,39 @@ class OxigestiBackend(models.Model):
     @api.model
     def _scheduler_import_customers(self):
         company_id = self.get_current_user_company()
-
         domain = [
             ('company_id', '=', company_id.id)
         ]
-
         self.search(domain).import_customers_since()
 
     @api.model
     def _scheduler_export_products(self):
         company_id = self.get_current_user_company()
-
         domain = [
             ('company_id', '=', company_id.id)
         ]
+        self.search(domain).export_products_since()
 
-        self.search(domain).import_export_since()
+    @api.model
+    def _scheduler_export_products_by_customer(self):
+        company_id = self.get_current_user_company()
+        domain = [
+            ('company_id', '=', company_id.id)
+        ]
+        self.search(domain).export_products_by_customer_since()
 
-    # @api.model
-    # def _scheduler_import_services(self):
-    #     company_id = self.get_current_user_company()
-    #
-    #     domain = [
-    #         ('company_id', '=', company_id.id)
-    #     ]
-    #
-    #     self.search(domain).import_services_since()
+    @api.model
+    def _scheduler_export_product_prices_by_customer(self):
+        company_id = self.get_current_user_company()
+        domain = [
+            ('company_id', '=', company_id.id)
+        ]
+        self.search(domain).export_product_prices_by_customer_since()
+
+    @api.model
+    def _scheduler_import_services(self):
+        company_id = self.get_current_user_company()
+        domain = [
+            ('company_id', '=', company_id.id)
+        ]
+        self.search(domain).import_services_since()
