4d5bee213c736d3b8f1e5193f1bd35979e53e117
Eric Antones <eantones@nuobit.com>
2019-12-18 00:51:25 +0100

    [IMP+REF] connector_oxigesti: removed sale lines importer, added import_dependencies from order header and refactoring

--- connector_oxigesti/models/sale_order_line/__init__.py
+++ connector_oxigesti/models/sale_order_line/__init__.py
@@ -2,7 +2,6 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from . import importer
 from . import adapter
 from . import import_mapper
 from . import binder
