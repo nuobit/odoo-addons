0e28d06871483c91396e88ea790ea8fb477c44dd
Eric Antones <eantones@nuobit.com>
2019-12-19 00:29:26 +0100

    [ADD] connector_cbl: new module

---
+++ connector_cbl/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "CBL Connector",
+    'description': "Connector to get tracking info from CBL",
+    'version': '11.0.0.1.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Connector',
+    'website': 'https://github.com/nuobit',
+    'external_dependencies': {
+        'python': [
+            'requests',
+            'lxml',
+            'json',
+        ],
+    },
+    'depends': [
+        'connector',
+    ],
+    'data': [
+        'views/backend_views.xml',
+        'views/menus.xml',
+        'templates/cbl_template.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
---
+++ connector_cbl/models/backend.py
@@ -0,0 +1,66 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields, api, exceptions, _
+
+from . import cbl
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class CBLBackend(models.Model):
+    _name = 'cbl.backend'
+    _inherit = 'connector.backend'
+
+    _description = 'CBL Backend Configuration'
+
+    @api.model
+    def _select_state(self):
+        return [('draft', 'Draft'),
+                ('checked', 'Checked'),
+                ('production', 'In Production')]
+
+    name = fields.Char('Name', required=True)
+
+    sequence = fields.Integer('Sequence', required=True, default=1)
+
+    username = fields.Char('Username', required=True)
+    password = fields.Char('Password', required=True)
+
+    output = fields.Text('Output', readonly=True)
+
+    active = fields.Boolean(
+        string='Active',
+        default=True
+    )
+    state = fields.Selection(
+        selection='_select_state',
+        string='State',
+        default='draft'
+    )
+
+    @api.multi
+    def button_reset_to_draft(self):
+        self.ensure_one()
+        self.write({'state': 'draft', 'output': None})
+
+    @api.multi
+    def _check_connection(self):
+        self.ensure_one()
+        er = cbl.CBL(username=self.username, password=self.password)
+
+        if not er.login():
+            raise exceptions.ValidationError("Error on logging in")
+
+        # if not er.logout():
+        #     raise exceptions.ValidationError("Error on logging out")
+
+        self.output = "OK"
+
+    @api.multi
+    def button_check_connection(self):
+        self._check_connection()
+        self.write({'state': 'checked'})
