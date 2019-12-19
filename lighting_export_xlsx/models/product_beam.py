f8793c4843f031e8512bfb14590661ef406919a3
Eric Antones <eantones@nuobit.com>
2019-12-19 18:48:02 +0100

    [FIX] lighting_export_xlsx: beam dimension error

--- lighting_export_xlsx/models/product_beam.py
+++ lighting_export_xlsx/models/product_beam.py
@@ -9,28 +9,47 @@ from collections import OrderedDict
 class LightingProductBeam(models.Model):
     _inherit = 'lighting.product.beam'
 
+    @property
+    def xlsx_dimensions(self):
+        field = 'dimension_ids'
+        meta = self.env[self._name].fields_get([field], ['string'])[field]
+        datum = self.dimension_ids.get_display()
+        return {meta['string']: datum or None}
+
     @api.multi
     def export_xlsx(self, template_id=None):
-        valid_field = ['num', 'photometric_distribution_ids', 'dimension_ids']
+        valid_field = ['num', 'photometric_distribution_ids', 'xlsx_dimensions']
+
+        field_meta_base = self.fields_get(valid_field, ['string', 'type', 'selection'])
         res = []
-        for rec in self:
+        for rec in self.sorted(lambda x: x.sequence):
             line = OrderedDict()
             for field in valid_field:
-                field_meta = self.fields_get([field], ['string', 'type'])[field]
                 datum = getattr(rec, field)
-                if field_meta['type'] == 'many2one':
+                if field in field_meta_base:
+                    field_meta = field_meta_base[field]
+                else:
+                    field_meta = {
+                        'type': None,
+                        'string': list(datum.keys())[0],
+                    }
+                    datum = list(datum.values())[0]
+                if field_meta['type'] == 'selection':
+                    datum = dict(field_meta['selection']).get(datum)
+                elif field_meta['type'] == 'many2one':
                     datum = datum.display_name
                 elif field_meta['type'] == 'many2many':
                     datum = ','.join([x.display_name for x in datum])
-                elif field_meta['type'] == 'one2many':
-                    datum = datum.export_xlsx()
                 elif field_meta['type'] == 'date':
                     datum = fields.Date.from_string(datum)
 
                 if field_meta['type'] != 'boolean' and not datum:
                     datum = None
 
-                line[field_meta['string']] = datum
+                if not isinstance(datum, OrderedDict):
+                    datum = OrderedDict([(field_meta['string'], datum)])
+
+                line.update(datum)
 
             res.append(line)
 
