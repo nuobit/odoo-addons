1f6bba02a3c67dc95e6aa7c9ae927c5fc24ba016
Eric Antones <eantones@nuobit.com>
2019-12-19 14:49:10 +0100

    [IMP] lighting_export_xlsx: split source desciption

--- lighting_export_xlsx/models/product_source.py
+++ lighting_export_xlsx/models/product_source.py
@@ -2,22 +2,70 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
 from collections import OrderedDict
 
 
 class LightingProductSource(models.Model):
     _inherit = 'lighting.product.source'
 
+    @property
+    def xlsx_types(self):
+        SourceLine = self.env[self._name].line_ids
+        field = 'type_id'
+        meta = SourceLine.fields_get([field], ['string'])[field]
+        datum = self.line_ids.get_source_type()
+        return {meta['string']: datum and '/'.join(datum) or None}
+
+    @property
+    def xlsx_wattage(self):
+        SourceLine = self.env[self._name].line_ids
+        field = 'wattage'
+        meta = SourceLine.fields_get([field], ['string'])[field]
+        datum = self.line_ids.get_wattage()
+        return {meta['string']: datum or None}
+
+    @property
+    def xlsx_color_temperature(self):
+        SourceLine = self.env[self._name].line_ids
+        field = 'color_temperature_id'
+        meta = SourceLine.fields_get([field], ['string'])[field]
+        datum = self.line_ids.get_color_temperature()
+        return {meta['string']: datum and '/'.join(datum) or None}
+
+    @property
+    def xlsx_cri(self):
+        SourceLine = self.env[self._name].line_ids
+        field = 'cri_min'
+        meta = SourceLine.fields_get([field], ['string'])[field]
+        datum = [str(x) for x in self.line_ids.get_cri() if x]
+        return {meta['string']: datum and '/'.join(datum) or None}
+
+    @property
+    def xlsx_luminous_flux(self):
+        datum = self.line_ids.get_luminous_flux()
+        return {'Flux': datum and '/'.join(datum) or None}
+
     @api.multi
     def export_xlsx(self, template_id=None):
-        valid_field = ['relevance', 'num', 'lampholder_id', 'line_display']
+        valid_field = ['relevance', 'num', 'lampholder_id',
+                       'xlsx_types', 'xlsx_wattage', 'xlsx_color_temperature',
+                       'xlsx_luminous_flux', 'xlsx_cri']
+
+        field_meta_base = self.fields_get(valid_field, ['string', 'type', 'selection'])
         res = []
         for rec in self.sorted(lambda x: x.sequence):
             line = OrderedDict()
             for field in valid_field:
-                field_meta = self.fields_get([field], ['string', 'type', 'selection'])[field]
                 datum = getattr(rec, field)
+                if field in field_meta_base:
+                    field_meta = field_meta_base[field]
+                else:
+                    field_meta = {
+                        'type': None,
+                        'string': list(datum.keys())[0],
+                    }
+                    datum = list(datum.values())[0]
                 if field_meta['type'] == 'selection':
                     datum = dict(field_meta['selection']).get(datum)
                 elif field_meta['type'] == 'many2one':
@@ -38,4 +86,3 @@ class LightingProductSource(models.Model):
             res.append(line)
 
         return res
-
