c47cb9810c8102115a727aa1396445f588590b86
Eric Antones <eantones@nuobit.com>
2019-12-27 11:16:48 +0100

    [ADD] sale_order_invoicing_exclude: new module to exclude sales order from being invoiced

---
+++ sale_order_invoicing_exclude/models/sale_order.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    sale_invoicing_exclude_from_invoicing = fields.Boolean(
+        string="Exclude from invoicing",
+    )
+
+    def action_invoice_create(self, grouped=False, final=False):
+        return super(SaleOrder, self.filtered(
+            lambda x: not x.sale_invoicing_exclude_from_invoicing and
+                      not x.partner_id.sale_invoicing_exclude_from_invoicing)
+                     ).action_invoice_create(
+            grouped=grouped, final=final)
