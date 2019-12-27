a77098bc9e2b8686d530cc5290559f41869124bb
Eric Antones <eantones@nuobit.com>
2019-12-27 17:33:30 +0100

    [ADD] account_asset_non_deductible: new module to add non-deductible tax amount to asset value

---
+++ account_asset_non_deductible/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Non-deductible asset value',
+    'summary': 'Adds a non-deductible tax amount to the asset value',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_es_account_asset',
+        'l10n_es_aeat_vat_special_prorrate',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
