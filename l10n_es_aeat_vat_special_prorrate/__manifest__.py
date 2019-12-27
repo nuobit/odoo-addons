8a54bc594b594ac8d2d8ac44ebb7be4d6f5f1570
Eric Antones <eantones@nuobit.com>
2019-12-27 17:31:52 +0100

    [IMP] l10n_es_aeat_vat_special_prorrate: added method to get the non-deductible amount of a tax including its children

--- l10n_es_aeat_vat_special_prorrate/__manifest__.py
+++ l10n_es_aeat_vat_special_prorrate/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'AEAT - Prorrata especial de IVA',
     'summary': 'Módulo para gestionar la prorrata especial del IVA '
                'en las facturas de la AEAT',
-    'version': '11.0.0.1.2',
+    'version': '11.0.0.2.0',
     'category': 'Accounting',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'website': 'https://www.nuobit.com',
