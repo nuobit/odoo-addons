591bcd7996e021eb9b7c0ccb6ab736d5abd2afb6
Eric Antones <eantones@nuobit.com>
2019-12-29 17:50:27 +0100

    [IMP] account_invoice_report_service: added delivery report

--- account_invoice_report_service/models/account_invoice.py
+++ account_invoice_report_service/models/account_invoice.py
@@ -5,16 +5,19 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
+import re
+
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
     def print_report_invoice_service(self):
-        return self.env.ref('account_invoice_report_service.action_report_invoice_service').report_action(self)
+        if not self.company_id.report_service_id:
+            raise UserError(_("There's no report defined on invoice company"))
+        return self.company_id.report_service_id.report_action(self)
 
-    def get_lines_by_order(self):
-        # grouping by order
+    def _group_by_order(self):
         ilines_trace = self.env['account.invoice.line']
         order_d, no_order = {}, self.env['account.invoice.line']
         for iline in self.invoice_line_ids:
@@ -30,6 +33,12 @@ class AccountInvoice(models.Model):
                     order_d[order] |= iline
             else:
                 no_order += iline
+
+        return order_d, no_order
+
+    def get_service_lines_by_order(self):
+        # group by order
+        order_d, no_order = self._group_by_order()
 
         # grouping by round trip code
         order_rt_date_l = []
@@ -65,3 +74,55 @@ class AccountInvoice(models.Model):
             order_sorted_l.append((None, no_order.sorted(lambda x: (x.sequence, x.id))))
 
         return order_sorted_l
+
+    def get_delivery_lines_by_order(self):
+        # group by order
+        order_d, no_order = self._group_by_order()
+
+        # sort and join with lines w/o orders
+        order_sorted_l = []
+        for order, invoice_lines in sorted(order_d.items(),
+                                           key=lambda x: (x[0].confirmation_date,
+                                                          x[0].date_order,
+                                                          x[0].client_order_ref)):
+            order_sorted_l.append((order, order_d[order].sorted(lambda x: (x.sequence, x.id))))
+
+        if no_order:
+            order_sorted_l.append((None, no_order.sorted(lambda x: (x.sequence, x.id))))
+
+        return order_sorted_l
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    def get_line_lots(self):
+        return self.move_line_ids.mapped('move_line_ids.lot_id') \
+            .sorted(lambda x: x.name).mapped('name')
+
+    def _extract_product_code(self, name):
+        m = re.match(r'^ *\[([^]]+)\] *(.*)$', name, flags=re.DOTALL)
+        if not m:
+            return None, name
+
+        return m.groups()
+
+    def get_splited_line_description(self):
+        if not self.product_id:
+            return None, self.name
+
+        # find product data
+        part = self.invoice_id.partner_id
+        if part.lang:
+            product = self.product_id.with_context(lang=part.lang)
+        else:
+            product = self.product_id
+
+        product_partner_ref, product_name = self._extract_product_code(product.partner_ref)
+
+        # find invoice line data
+        line_partner_ref, line_name = self._extract_product_code(self.name)
+        if line_partner_ref and line_partner_ref == product_partner_ref:
+            return line_partner_ref, line_name
+
+        return product_partner_ref, self.name
---
+++ account_invoice_report_service/models/res_company.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    report_service_id = fields.Many2one(
+        string="Service Invoice report",
+        comodel_name='ir.actions.report',
+        domain=[
+            ('model', '=', 'account.invoice'),
+            ('report_name', '=like', 'account_invoice_report_service.%'),
+        ]
+    )
