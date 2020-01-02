714bf71f6bf4d205ebd94b196e7f4bbfd174b797
Eric Antones <eantones@nuobit.com>
2020-01-02 22:47:35 +0100

    [ADD] account_invoice_batches: new module

---
+++ account_invoice_batches/models/__init__.py
@@ -0,0 +1,11 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from . import res_company
+from . import res_config_settings
+from . import common
+from . import res_partner
+from . import sale_order
+from . import account_invoice
+from . import account_invoice_batch
---
+++ account_invoice_batches/models/res_company.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    default_invoice_batch_sending_email_template_id = fields.Many2one(
+        string="Default Invoice batches e-mail template",
+        comodel_name='mail.template',
+        domain=[('model_id', '=', 'account.invoice')]
+    )
