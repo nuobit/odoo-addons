310da3e1c656358b31d530ed7b78ed98ff4049df
Eric Antones <eantones@nuobit.com>
2020-01-02 22:47:35 +0100

    [ADD] account_invoice_batches: new module

---
+++ account_invoice_batches/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Invoice batches",
+    'version': '11.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'sale',
+        'l10n_es_facturae',
+        'account_invoice_report_service',
+    ],
+    'data': [
+        'security/account_invoice_batch.xml',
+        'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml',
+        'wizard/account_invoice_batch_process.xml',
+        'views/res_partner.xml',
+        'views/account_invoice.xml',
+        'views/account_invoice_batch.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+}
---
+++ account_invoice_batches/models/res_partner.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+from .common import BATCH_SENDING_METHODS
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    invoice_batch_sending_method = fields.Selection(selection=BATCH_SENDING_METHODS,
+                                                    string='Method',
+                                                    track_visibility='onchange')
+    invoice_batch_email_partner_id = fields.Many2one(comodel_name='res.partner',
+                                                     domain="[('id', 'child_of', active_id), ('email', '!=', False)]",
+                                                     ondelete='restrict',
+                                                     string='Contact',
+                                                     track_visibility='onchange')
