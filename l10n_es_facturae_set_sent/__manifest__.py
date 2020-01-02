116181fd01340f537ce066a1821e84a1db82c7ad
Eric Antones <eantones@nuobit.com>
2020-01-02 21:53:11 +0100

    [ADD] l10n_es_facturae_set_sent: new module

---
+++ l10n_es_facturae_set_sent/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'FacturaE set sent flag',
+    'summary': 'Set invoice "sent" flag after successfully facturae creation',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'category': 'Accounting',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'l10n_es_facturae',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
