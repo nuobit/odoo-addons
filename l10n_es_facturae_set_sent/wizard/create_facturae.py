81a7d89bf24fbe18b230a67764e7c3acea962eae
Eric Antones <eantones@nuobit.com>
2020-01-02 21:53:11 +0100

    [ADD] l10n_es_facturae_set_sent: new module

---
+++ l10n_es_facturae_set_sent/wizard/create_facturae.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import base64
+
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning as UserError
+
+
+class CreateFacturae(models.TransientModel):
+    _inherit = "create.facturae"
+
+    @api.multi
+    def create_facturae_file(self):
+        res = super(CreateFacturae, self).create_facturae_file()
+
+        invoice_ids = self.env.context.get('active_ids', [])
+        invoice = self.env['account.invoice'].browse(invoice_ids[0])
+        invoice.sent = True
+
+        return res
