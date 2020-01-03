7999b346a48efa77c7011ed2b824860b1d4d020a
Eric Antones <eantones@nuobit.com>
2020-01-03 19:23:36 +0100

    [FIX] account_invoice_batches: trying to sent emails to an empty invoice object

--- account_invoice_batches/__manifest__.py
+++ account_invoice_batches/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Invoice batches",
-    'version': '11.0.1.0.2',
+    'version': '11.0.1.0.3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Accounting',
