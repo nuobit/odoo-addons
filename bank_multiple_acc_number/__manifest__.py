4093c08ef453b2508ff08b6163b7d205751bd998
Eric Antones <eantones@nuobit.com>
2020-02-04 12:48:21 +0100

    [ADD] bank_multiple_acc_number: new module

---
+++ bank_multiple_acc_number/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Bank multiple account number',
+    'summary': 'This module allows to have more than one bank account number per different partners',
+    'version': '11.0.1.0.0',
+    'category': 'Banking',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
