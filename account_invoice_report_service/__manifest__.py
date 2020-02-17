45629995428547dee2c5c925995508881b13cef8
Eric Antones <eantones@nuobit.com>
2020-02-17 22:37:24 +0100

    [IMP] account_invoice_report_service: added double header for fields date, service no., document no., customer ref.

--- account_invoice_report_service/__manifest__.py
+++ account_invoice_report_service/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Service invoice report",
     'description': """Service invoice report""",
-    'version': '11.0.0.2.8',
+    'version': '11.0.0.2.9',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Accounting',
