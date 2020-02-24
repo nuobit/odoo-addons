9d43f3bd0d3f42f5d149af3abaa4ade11a1b3db7
Eric Antones <eantones@nuobit.com>
2020-02-24 17:09:57 +0100

    [IMP] connector_sapb1: added ibox data import

--- connector_sapb1/__manifest__.py
+++ connector_sapb1/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Connector SAP B1",
     'description': "SAP Business One connector",
-    'version': '11.0.0.2.0',
+    'version': '11.0.0.2.1',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
