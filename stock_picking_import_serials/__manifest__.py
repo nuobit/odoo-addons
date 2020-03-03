c98bf15124d36354506936eccd20285942f620cd
Eric Antones <eantones@nuobit.com>
2020-03-03 23:52:06 +0100

    [FIX] stock_picking_import_serials: error string susbtitution with translation function

--- stock_picking_import_serials/__manifest__.py
+++ stock_picking_import_serials/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Stock picking import serials',
     'summary': "Import serial numbers to a stock picking from spreadsheet file",
-    'version': '11.0.1.0.2',
+    'version': '11.0.1.0.3',
     'category': 'Warehouse Management',
     'license': 'AGPL-3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
