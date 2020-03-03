57f9e856fd90b805f14223335848fc82826c1a07
Eric Antones <eantones@nuobit.com>
2020-03-03 22:15:01 +0100

    [IMP] account_invoice_report_service: added comment

--- account_invoice_report_service/__manifest__.py
+++ account_invoice_report_service/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Service invoice report",
     'description': """Service invoice report""",
-    'version': '11.0.0.2.10',
+    'version': '11.0.0.2.11',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Accounting',
