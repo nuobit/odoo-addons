e419631f3d4e84db61ac97350040abeb76871759
Eric Antones <eantones@nuobit.com>
2020-03-05 16:06:36 +0100

    [FIX] connector_sapb1: reference structure not expected

--- connector_sapb1/__manifest__.py
+++ connector_sapb1/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Connector SAP B1",
     'description': "SAP Business One connector",
-    'version': '11.0.0.2.8',
+    'version': '11.0.0.2.9',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Connector',
