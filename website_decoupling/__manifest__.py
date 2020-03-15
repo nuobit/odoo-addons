eaa95cf5633a3181cf3524cccf9eb866d1a5a323
Eric Antones <eantones@nuobit.com>
2020-03-15 21:41:11 +0100

    [ADD] website_decoupling: new module

---
+++ website_decoupling/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Website decoupling',
+    'summary': 'This module decouples the website from the ERP appearance',
+    'version': '11.0.1.0.0',
+    'category': 'Website',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/website_templates.xml',
+        'views/website_decoupling_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ website_decoupling/controllers/main.py
@@ -0,0 +1,37 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import logging
+
+from odoo import http
+
+from odoo.addons.website.controllers.main import Website
+
+import re
+
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteDecoupled(Website):
+
+    def _is_erp_host(self):
+        IrConfig = http.request.env['ir.config_parameter'].sudo()
+        erp_hosts_regexp = IrConfig.get_param('website.erp.hosts')
+        if erp_hosts_regexp:
+            current_host = http.request.httprequest.host.split(':')[0]
+            return bool(re.match(r'%s' % erp_hosts_regexp, current_host))
+        return False
+
+    @http.route('/', type='http', auth="public", website=True)
+    def index(self, **kw):
+        if self._is_erp_host():
+            return http.local_redirect('/web', query=http.request.params, keep_hash=True)
+        else:
+            return super(WebsiteDecoupled, self).index(**kw)
+
+    @http.route(website=True, auth="public")
+    def web_login(self, redirect=None, *args, **kw):
+        response = super(WebsiteDecoupled, self).web_login(redirect=redirect, *args, **kw)
+        response.qcontext['hide_website_layout'] = self._is_erp_host()
+        return response
