11b04436bd38321df6abfc5be1c9d9b917d7957f
Eric Antones <eantones@nuobit.com>
2020-03-19 18:32:49 +0100

    [ADD] product_bike_info: new module

---
+++ product_bike_info/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Product Bike info",
+    'description': "This module adds field for bike data",
+    'version': '12.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'website': 'https://github.com/nuobit',
+    'depends': ['product'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+}
