a88f646f381c76f64cfdb3333172d764e3efb86b
Eric Antones <eantones@nuobit.com>
2020-03-31 12:36:59 +0200

    [TRL] lighting: price and cost currency label translation

--- lighting/__manifest__.py
+++ lighting/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Lighting vertical",
     'description': "Lighting vertical",
-    'version': '11.0.0.92.2',
+    'version': '11.0.0.92.3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Custom',
