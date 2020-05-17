10fe03d11fdec5392f4d96199a317c29016dfab1
Eric Antones <eantones@nuobit.com>
2020-05-18 00:45:27 +0200

    [FIX] stock_putaway_no_internal: error validating pickings and treat destination location usage view

--- stock_putaway_no_internal/models/stock_location.py
+++ stock_putaway_no_internal/models/stock_location.py
@@ -3,13 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models, _
+from odoo.exceptions import UserError
 
 
 class Location(models.Model):
     _inherit = "stock.location"
 
     def get_putaway_strategy(self, product):
-        super().get_putaway_strategy(product)
+        putaway_location = super().get_putaway_strategy(product)
+        if self.usage == 'view':
+            return putaway_location
         current_location = self
         putaway_location = self.env['stock.location']
         while current_location and not putaway_location:
@@ -17,9 +20,15 @@ class Location(models.Model):
             if putaway_strategy:
                 putaway_location = putaway_strategy.putaway_apply(product)
                 if putaway_strategy.exclude_internal_operations:
-                    stock_picking_type = self.env['stock.picking.type'].browse(
-                        self.env.context.get('active_id'))
-                    if stock_picking_type.code == 'internal':
+                    active_model, active_id = [
+                        self.env.context.get(x) for x in ['active_model', 'active_id']]
+                    if active_model in ['stock.picking.type', 'stock.picking']:
+                        picking_type = self.env[active_model].browse(active_id)
+                        if active_model == 'stock.picking':
+                            picking_type = picking_type.picking_type_id
+                    else:
+                        raise UserError(_("Active model '%s' not expected") % active_model)
+                    if picking_type.code == 'internal':
                         putaway_location = self.env['stock.location']
             current_location = current_location.location_id
         return putaway_location
