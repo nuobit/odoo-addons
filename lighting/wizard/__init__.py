2275f55d490a3ce28c9e1e8015331e22077aeaca
Eric Antones <eantones@nuobit.com>
2020-05-20 19:33:17 +0200

    [REM] lighting-vertical: moved all modules to a new standalone repo
    https://github.com/nuobit/lighting-vertical

--- lighting/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
-from . import lighting_product_addattachment
-from . import product_define_substitute
