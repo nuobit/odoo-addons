44a7dd0f973d0e26c485792ba2c2308565fbc19b
Eric Antones <eantones@nuobit.com>
2020-06-01 20:20:26 +0200

    [ADD] project_timeline_calendar: new module

---
+++ project_timeline_calendar/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Project timeline calendar',
+    'version': '12.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Project',
+    'website': 'https://github.com/nuobit',
+    'summary': "This module adds a new calendar view on project tasks and places "
+               "the tasks from the start and end date point of view, allowing to "
+               "move them and change its duration.",
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_task_views.xml',
+    ],
+    'installable': True,
+}
