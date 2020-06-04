80e3bebfcbfad775c9a02f8ccc8eeaed35560f73
Eric Antones <eantones@nuobit.com>
2020-06-04 11:32:00 +0200

    [ADD] base_automation_last_run: new module

---
+++ base_automation_last_run/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Base automation last run",
+    'summary': "This module shows and makes writable the field 'last_run' "
+               "on automated actions.",
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'category': 'Automation',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'base_automation',
+    ],
+    'data': [
+        'views/base_automation_views.xml',
+    ],
+    'installable': True,
+}
---
+++ base_automation_last_run/models/base_automation.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class BaseAutomation(models.Model):
+    _inherit = 'base.automation'
+
+    last_run = fields.Datetime(readonly=False, copy=False)
