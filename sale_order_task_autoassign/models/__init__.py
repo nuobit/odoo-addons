3a20da931ebf95147a8f58b05afc6c8b849e49d1
Eric Antones <eantones@nuobit.com>
2020-06-09 00:56:11 +0200

    [ADD] sale_order_task_autoassign: new module

---
+++ sale_order_task_autoassign/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from . import product
+from . import product_template
+
+from . import tzinterval
+from . import sale_order
+from . import resource
