ac4a64b74aff69e85da0b7a6a231429d46cbfff4
Eric Antones <eantones@nuobit.com>
2020-06-09 00:56:11 +0200

    [ADD] sale_order_task_autoassign: new module

---
+++ sale_order_task_autoassign/models/resource.py
@@ -0,0 +1,72 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models
+
+import datetime
+
+from .tzinterval import TzInterval
+
+
+class ResourceResource(models.Model):
+    _inherit = "resource.resource"
+
+    def get_next_attendance(self, cint):
+        """ return next attendance period from datetime dt"""
+        self.ensure_one()
+        for a in self.calendar_id.attendance_ids \
+                .filtered(lambda x: int(x.dayofweek) == cint.weekday(self.tz)) \
+                .sorted(lambda x: x.hour_from):
+            vint = TzInterval(a.date_from, a.date_to, tz=self.calendar_id.tz,
+                              base_tz=self.calendar_id.tz, to_tz=self.tz)
+            if cint.is_included(vint):
+                dint_date_start_tz = cint.date_start_tz(self.tz).replace(
+                    hour=int(a.hour_from),
+                    minute=round((a.hour_from - int(a.hour_from)) * 60))
+                dint = TzInterval(
+                    dint_date_start_tz,
+                    datetime.timedelta(
+                        minutes=int(round((a.hour_to - a.hour_from) * 60))),
+                    tz=self.tz)
+                if cint.date_start < dint.date_end:
+                    return dint
+        return None
+
+    def find_next_available(self, cint, project):
+        """ get the next free periode after (cint) of the resource (r) """
+        self.ensure_one()
+        att = self.get_next_attendance(cint)
+        if not att:
+            cint1_date_start_tz = (cint.date_start_tz(self.tz) + datetime.timedelta(days=1)) \
+                .replace(minute=0, hour=0)
+            cint1 = TzInterval(cint1_date_start_tz, cint.duration, tz=self.tz)
+            return self.find_next_available(cint1, project)
+        else:
+            if cint.is_included(att):
+                # global and user leaves
+                for gl in self.calendar_id.leave_ids.sorted(lambda x: x.date_from):
+                    if gl.date_from < gl.date_to:
+                        if not gl.resource_id or gl.resource_id == self:
+                            gint = TzInterval(gl.date_from, gl.date_to,
+                                              base_tz=self.calendar_id.tz, to_tz=self.tz)
+                            if cint.is_overlaped(gint):
+                                return self.find_next_available(cint.copy(gint.date_end), project)
+                else:
+                    overlaped_tasks = self.env['project.task'].search([
+                        ('project_id', '=', project.id),
+                        ('user_id', '=', self.user_id.id),
+                        ('date_start', '!=', False),
+                        ('date_end', '!=', False),
+                        ('date_end', '>', cint.date_start),
+                        ('date_start', '<', cint.date_end),
+                    ], order='date_start', limit=1)
+                    if overlaped_tasks:
+                        return self.find_next_available(cint.copy(overlaped_tasks[0].date_end), project)
+                    else:
+                        return cint
+            else:
+                if cint.starts_before(att):
+                    return self.find_next_available(cint.copy(att.date_start), project)
+                else:
+                    return self.find_next_available(cint.copy(att.date_end), project)
