4240cbde8efe67b33f14409a4896cd2f2c30fbe8
Eric Antones <eantones@nuobit.com>
2020-06-26 18:57:37 +0200

    [ADD] base_report_gs1_barcode: new module

---
+++ base_report_gs1_barcode/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Report GS1 barcodes',
+    'summary': 'This module adds a GS1-128 and GS1-Datamatrix barcode format support',
+    'version': '11.0.1.0.0',
+    'category': 'Reporting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'external_dependencies': {
+        'python': [
+            'pystrich',
+        ],
+    },
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+    'auto_install': False,
+}
