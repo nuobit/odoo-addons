048ba38aa97a86cba967b9d3f5067f3028f38619
Eric Antones <eantones@nuobit.com>
2020-06-26 18:56:43 +0200

    [ADD] base_report_pdf_css_reset: new module

---
+++ base_report_pdf_css_reset/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Report PDF CSS reset',
+    'summary': 'This module enables you with the capability to reset the default '
+               'paddings on the pdf minimal_layout template',
+    'version': '11.0.1.0.0',
+    'category': 'Reporting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+    'auto_install': False,
+}
