b99f0eeeabfc6f3a9e69f07578b3bdff8ee98d26
Eric Antones <eantones@nuobit.com>
2020-06-26 18:56:43 +0200

    [ADD] base_report_pdf_css_reset: new module

---
+++ base_report_pdf_css_reset/models/ir_actions_report.py
@@ -0,0 +1,37 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, api
+
+import lxml.html
+
+
+class IrActionsReport(models.Model):
+    _inherit = 'ir.actions.report'
+
+    @api.multi
+    def _prepare_html(self, html):
+        bodies, html_ids, header, footer, specific_paperformat_args = super()._prepare_html(html)
+
+        if self.env.context.get('no_paddings', False):
+            bodies_tmp = []
+            for body in bodies:
+                root = lxml.html.fromstring(body)
+                body_class = root.xpath("body[@class]")
+                if body_class:
+                    elem = body_class[0]
+                    classes_tmp = []
+                    for klass in elem.attrib['class'].split(' '):
+                        if klass != 'container':
+                            classes_tmp.append(klass)
+                    if not classes_tmp:
+                        del elem.attrib['class']
+                    else:
+                        elem.attrib['class'] = ' '.join(classes_tmp)
+
+                    bodies_tmp.append(lxml.html.tostring(root))
+
+            bodies = bodies_tmp
+
+        return bodies, html_ids, header, footer, specific_paperformat_args
