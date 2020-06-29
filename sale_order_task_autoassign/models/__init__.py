aad82534337c8e98591768c09e8524b0642fd467
Eric Antones <eantones@nuobit.com>
2020-06-29 14:05:06 +0200

    [REF] sale_order_task_autoassign: extract the service time logic to module 'product_service_time'

--- sale_order_task_autoassign/models/__init__.py
+++ sale_order_task_autoassign/models/__init__.py
@@ -2,9 +2,6 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from . import product
-from . import product_template
-
 from . import tzinterval
 from . import sale_order
 from . import resource
