302624495f52a81fab67c52456666e1de32927a1
Eric Antones <eantones@nuobit.com>
2020-06-29 14:05:58 +0200

    [ADD] product_service_time: new module as a result of splitting sale_order_task_autoassign module

---
+++ product_service_time/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Product service time',
+    'version': '12.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Product',
+    'website': 'https://github.com/nuobit',
+    'summary': "This module adds a field to products and product variants "
+               "of type service which allows to define the time required to "
+               "complete the service.",
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
