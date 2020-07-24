4e9905202dacbf6f282ebbc6f1e6bb6c5c89b425
Eric Antones <eantones@nuobit.com>
2020-07-24 15:31:49 +0200

    [ADD] pos_restaurant_note_print: new module

---
+++ pos_restaurant_note_print/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'POS restaurant line note receipt',
+    'version': '12.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Point of Sale',
+    'website': 'https://github.com/nuobit',
+    'summary': "This module shows the line note on the receipt on screen and printer.",
+    'depends': [
+        'pos_restaurant',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'installable': True,
+}
---
+++ pos_restaurant_note_print/static/src/js/pos.js
@@ -0,0 +1,18 @@
+/* Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+   Eric Antones <eantones@nuobit.com>
+   License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl) */
+
+odoo.define('pos_restaurant_note_print.pos_restaurant_note_print', function (require) {
+    "use strict";
+
+    var module = require('point_of_sale.models');
+
+    var _super_orderline = module.Orderline.prototype;
+    module.Orderline = module.Orderline.extend({
+        export_for_printing: function () {
+            var json = _super_orderline.export_for_printing.apply(this, arguments);
+            json.note = this.get_note();
+            return json;
+        },
+    });
+});
