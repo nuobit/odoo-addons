fa5c1ae1e3244c723642803ef2a777a41cde0139
Eric Antones <eantones@nuobit.com>
2020-08-12 13:08:28 +0200

    [FIX] stock_barcodes_gs1_extended: error scanning on inventory

--- stock_barcodes_gs1_extended/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1_extended/wizard/stock_barcodes_read.py
@@ -79,7 +79,7 @@ class WizStockBarcodesRead(models.AbstractModel):
                 ('product_id', '=', self.product_id.id),
             ])
             if not lot:
-                if self.picking_type_id.use_create_lots:
+                if self.inventory_id or self.picking_type_id.use_create_lots:
                     lot = self.env['stock.production.lot'].create({
                         'name': tracking_code,
                         'product_id': self.product_id.id,
