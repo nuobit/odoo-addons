dba718a0e99c945e1c1de3b198350237c9a59971
Eric Antones <eantones@nuobit.com>
2020-09-17 17:45:27 +0200

    [ADD] stock_move_line_sale_order: new module

---
+++ stock_move_line_sale_order/models/stock_move_line.py
@@ -0,0 +1,11 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    sale_id = fields.Many2one(related="picking_id.sale_id", readonly=True)
