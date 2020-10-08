e47f3c0980307a60d8cf21e7948c8407330222c4
Eric Antones <eantones@nuobit.com>
2020-10-08 18:31:09 +0200

    [FIX] sale_order_task_autoassign: wrong task start date

--- sale_order_task_autoassign/__manifest__.py
+++ sale_order_task_autoassign/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Sale order task autoassign',
-    'version': '12.0.1.0.2',
+    'version': '12.0.1.0.3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Project',
