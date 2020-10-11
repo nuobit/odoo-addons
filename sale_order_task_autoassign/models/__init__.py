dc37c8a5264ef0b4e346f3e57d57019f33f704ce
Eric Antones <eantones@nuobit.com>
2020-10-11 19:07:32 +0200

    [IMP] sale_order_task_autoassign: all tasks same order to same user

--- sale_order_task_autoassign/models/__init__.py
+++ sale_order_task_autoassign/models/__init__.py
@@ -2,6 +2,5 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from . import tzinterval
 from . import sale_order
 from . import resource
--- sale_order_task_autoassign/models/sale_order.py
+++ sale_order_task_autoassign/models/sale_order.py
@@ -2,15 +2,18 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
-
+import logging
 import datetime
-
 import math
 import random
 
-from .tzinterval import TzInterval
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+from ..lib.task import TaskList, Task, UserTasks
+from ..lib.tzinterval import TzInterval
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
@@ -28,84 +31,113 @@ class SaleOrder(models.Model):
     @api.multi
     def _action_confirm(self):
         result = super(SaleOrder, self)._action_confirm()
-        tasks = self.tasks_ids.filtered(lambda x: x.date_end)
-        if tasks:
-            self.tasks_ids.write({'date_deadline': max(tasks.mapped('date_end'))})
-        return result
-
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    def _free_time_selection(self, free_time_by_user, project):
-        now = fields.datetime.now().replace(tzinfo=None)
-        tasks_per_user = self.env['project.task'].read_group([
-            ('project_id', '=', project.id),
-            ('user_id', 'in', tuple(free_time_by_user.keys())),
-            ('date_start', '!=', False),
-            ('date_end', '!=', False),
-            ('date_end', '>', now),
-        ], ['id'], ['user_id'], orderby='user_id')
-        tasks_count_per_user = {x['user_id'][0]: x['user_id_count'] for x in tasks_per_user}
-        return sorted(free_time_by_user.items(), key=lambda x: (
-            x[1].date_start,
-            tasks_count_per_user.get(x[0], 0),
-            x[1].duration,
-            random.randrange(100),
-        ))[0]
-
-    def _timesheet_create_task_prepare_values(self, project):
-        res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
-
-        if not self.product_id.service_time:
-            return res
 
         # initial date_start
         now = fields.datetime.now().replace(second=0, microsecond=0)
         quarters = now.minute / 15
         base_date_start = now + datetime.timedelta(minutes=round((math.ceil(quarters) - quarters) * 15))
-        base_duration = int(round(self.product_id.service_time * 60))
 
-        # check resources availability
+        # tasks to allocate
+        tasks_to_allocate = TaskList([
+            Task(
+                task,
+                base_date_start,
+                duration=int(round(task.sale_line_id.product_id.service_time * 60))
+            ) for task in self.tasks_ids.filtered(
+                lambda x: x.sale_line_id.product_id.service_time > 0)
+        ])
+
+        # get resource candidates for that tasks
         domain = [
             ('user_id.active', '!=', False),
             ('resource_type', '=', 'user'),
+            ('calendar_id.attendance_ids', '!=', False),
         ]
-        if project.resource_calendar_id:
-            domain.append(('calendar_id', '=', project.resource_calendar_id.id))
+        calendar_ids = self.tasks_ids.mapped('project_id.resource_calendar_id.id')
+        if calendar_ids:
+            domain.append(('calendar_id', 'in', calendar_ids))
         resources = self.env['resource.resource'].search(domain)
         if not resources:
             raise UserError(_("There's no active resources"))
 
-        free_time_by_user = {}
-        for r in resources:
-            duration_task = int(round(base_duration / (r.time_efficiency / 100)))
-            duration_td = datetime.timedelta(minutes=duration_task)
-            cint = TzInterval(base_date_start, base_date_start + duration_td)
-
-            # check durations: if it's ever possible to fit the cint duration
-            # on at least one day, if not, stop searching going forward and throw an error
-            for a in r.calendar_id.attendance_ids:
-                if a.hour_from < a.hour_to:
-                    duration_slot = int(round((a.hour_to - a.hour_from) * 60))
-                    if duration_slot >= duration_task:
-                        break
-            else:
-                continue
-
-            # find available date slot
-            rint = r.find_next_available(cint, project)
-            if rint:
-                free_time_by_user[r.user_id.id] = rint
-
-        if not free_time_by_user:
+        # calculate the tasks by user
+        max_it = int(self.env['ir.config_parameter'].get_param('sale_order_task_autoassign.max_it', 200))
+        _logger.info("Maximum iterations established to %i" % max_it)
+        candidate_usertasks = self._allocate_tasks(tasks_to_allocate, resources, max_it=max_it)
+        if not candidate_usertasks:
             raise UserError(_("No candidates found, cannot validate order"))
-        available_user_time = self._free_time_selection(free_time_by_user, project)
 
-        res.update({
-            'user_id': available_user_time[0],
-            'date_start': available_user_time[1].date_start,
-            'date_end': available_user_time[1].date_end,
-        })
+        # choose only one of the candidates
+        t = random.randrange(0, len(candidate_usertasks))
+        resource_id, ctasklist = list(candidate_usertasks.tasks.items())[t]
+        user = candidate_usertasks.resources[resource_id].user_id
+        for e in ctasklist.tasks:
+            task = e.obj
+            task.write({
+                'user_id': user.id,
+                'date_start': e.start,
+                'date_end': e.end,
+            })
 
-        return res
+        # assign the deadline of each task
+        tasks = self.tasks_ids.filtered(lambda x: x.date_end)
+        if tasks:
+            self.tasks_ids.write({'date_deadline': max(tasks.mapped('date_end'))})
+
+        return result
+
+    def _allocate_tasks(self, src_tasks, resources, max_it=None):
+        N = len(src_tasks)
+        it = 0
+
+        now = fields.datetime.now().replace(tzinfo=None)
+        current_tasks = self.env['project.task'].search([
+            ('user_id', 'in', resources.mapped('user_id.id')),
+            ('date_start', '!=', False),
+            ('date_end', '!=', False),
+            ('date_end', '>', now)
+        ])
+        tasks_by_user = {}
+        for ct in current_tasks:
+            tasks_by_user.setdefault(ct.user_id.id, []).append(TzInterval(ct.date_start, ct.date_end))
+
+        def _allocate_tasks0(src_tasks, current_usertasks=UserTasks(), candidate_usertasks=None, level=0):
+            nonlocal it
+            it += 1
+            if candidate_usertasks and max_it and it >= max_it:
+                return candidate_usertasks
+
+            if not src_tasks:
+                candidate_usertasks1 = current_usertasks.get_quickest(N)
+                if candidate_usertasks:
+                    if candidate_usertasks < candidate_usertasks1:
+                        candidate_usertasks1 = candidate_usertasks.copy()
+                    elif candidate_usertasks == candidate_usertasks1:
+                        candidate_usertasks1 = candidate_usertasks.merge(candidate_usertasks1)
+
+                return candidate_usertasks1
+            else:
+                for i in range(len(src_tasks)):
+                    task_base_root, task_base_children = src_tasks.rotate_split(i)
+                    current_usertasks2 = current_usertasks.copy()
+                    for r in current_usertasks.get_resources() or resources:
+                        tsk = task_base_root.copy()
+                        tsk.update_duration(r.time_efficiency / 100, is_rate=True)
+                        if r.is_allocatable(tsk.interval):
+                            cint = r.find_next_available(tsk.interval, current_usertasks2.get(r), tasks_by_user)
+                            if not cint:
+                                current_usertasks2.del_user(r)
+                            else:
+                                tsk_avail = Task(tsk.obj, cint.date_start, cint.date_end)
+                                current_usertasks2.add(r, tsk_avail)
+                                if (candidate_usertasks and current_usertasks2.get(r).end >= candidate_usertasks.end):
+                                    current_usertasks2.del_user(r)
+                        else:
+                            current_usertasks2.del_user(r)
+
+                    if current_usertasks2:
+                        candidate_usertasks = _allocate_tasks0(task_base_children, current_usertasks2,
+                                                               candidate_usertasks, level + 1)
+                return candidate_usertasks
+
+        return _allocate_tasks0(src_tasks)
