0e03d74601a85136b7aeec37c7ca0331ddd1bace
Eric Antones <eantones@nuobit.com>
2020-10-12 15:10:53 +0200

    [ADD] contract_line_tax: new module

---
+++ contract_line_tax/models/account_analytic_contract_line.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+
+
+class AccountAnalyticContractLine(models.Model):
+    _inherit = 'account.analytic.contract.line'
+
+    tax_ids = fields.Many2many(comodel_name='account.tax',
+                               string='Taxes',
+                               domain=[('type_tax_use', '!=', 'none'), '|', ('active', '=', False),
+                                       ('active', '=', True)])
