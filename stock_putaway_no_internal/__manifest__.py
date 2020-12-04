ee64369396f0fc466129030ce46ec8d2337759c5
Eric Antones <eantones@nuobit.com>
2020-12-04 16:09:15 +0100

    [FIX] stock_putaway_no_internal: error on validate picking navigating from the purchase order

--- stock_putaway_no_internal/__manifest__.py
+++ stock_putaway_no_internal/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Putaway strategy no internal',
     'summary': "Allow to exclude putaway strategies if they are applied on an internal operations",
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.2',
     'category': 'Warehouse Management',
     'license': 'AGPL-3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
---
+++ stock_putaway_no_internal/models/stock_move.py
@@ -0,0 +1,13 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+        return super(StockMove, self.with_context(stock_picking_type=self.picking_type_id)) \
+            ._prepare_move_line_vals(quantity=quantity, reserved_quant=reserved_quant)
