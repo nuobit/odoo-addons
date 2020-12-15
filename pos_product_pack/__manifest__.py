bc04fcece920d9336487fe355ba1ee9a82de8650
Eric Antones <eantones@nuobit.com>
2020-12-15 18:26:36 +0100

    [ADD] pos_product_pack: new module

---
+++ pos_product_pack/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'POS product pack',
+    'version': '12.0.1.0.0',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'license': 'AGPL-3',
+    'category': 'Point of Sale',
+    'website': 'https://github.com/nuobit',
+    'summary': "Make the product pack feature available on POS.",
+    'depends': [
+        'point_of_sale',
+        'sale_product_pack',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
