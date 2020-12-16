5892674c62ca6536f0d3e78bd09aa43c16ad3c22
Eric Antones <eantones@nuobit.com>
2020-12-16 18:44:19 +0100

    [FIX] pos_product_pack: only delete childs on parent deletion not all the order products

--- pos_product_pack/__manifest__.py
+++ pos_product_pack/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'POS product pack',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'license': 'AGPL-3',
     'category': 'Point of Sale',
