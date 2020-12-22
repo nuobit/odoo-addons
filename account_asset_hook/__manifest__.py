4a6e3d983810a47c6e796853b59899c1fc0c5b14
Eric Antones <eantones@nuobit.com>
2020-12-22 15:14:38 +0100

    [ADD] account_asset_hook: new module to make the extension easier

---
+++ account_asset_hook/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Asset Hooks',
+    'summary': 'Adds a hooks on standard Odoo asset module',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_asset',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
