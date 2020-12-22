76f4a6ca2ee1b3c000b07d5aa71609c92c13c358
Eric Antones <eantones@nuobit.com>
2020-12-22 15:48:59 +0100

    [IMP] l10n_es_aeat_vat_special_prorrate: added final percentage and helper methods

--- l10n_es_aeat_vat_special_prorrate/models/aeat_map_special_prorrate_year.py
+++ l10n_es_aeat_vat_special_prorrate/models/aeat_map_special_prorrate_year.py
@@ -13,6 +13,19 @@ class MapSpecialProrrateYear(models.Model):
     year = fields.Integer(string="Year", required=True)
     tax_percentage = fields.Float(string="Tax %", required=True)
 
+    tax_final_percentage = fields.Float(string="Final tax %", readonly=True,
+                                        compute='_compute_tax_final_percentage')
+
+    @api.depends('year', 'tax_percentage')
+    def _compute_tax_final_percentage(self):
+        for rec in self:
+            next_map = self.search([
+                ('company_id', '=', rec.company_id.id),
+                ('year', '=', rec.year + 1)
+            ])
+            if next_map:
+                rec.tax_final_percentage = next_map.tax_percentage
+
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
@@ -24,3 +37,21 @@ class MapSpecialProrrateYear(models.Model):
         ('unique_year', 'unique(year, company_id)',
          'AEAT year must be unique'),
     ]
+
+    @api.model
+    def get_by_ukey(self, company_id, year):
+        return self.search([
+            ('company_id', '=', company_id),
+            ('year', '=', year),
+        ])
+
+    @api.depends('year', 'tax_percentage')
+    def name_get(self):
+        to_percent_str = lambda x: (x.is_integer() and '%i%%' or '%.2f%%') % x
+        result = []
+        for rec in self:
+            name = [to_percent_str(rec.tax_percentage)]
+            if rec.tax_final_percentage:
+                name.append(to_percent_str(rec.tax_final_percentage))
+            result.append((rec.id, '%i: %s' % (rec.year, ' -> '.join(name))))
+        return result
