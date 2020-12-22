eecdb934c053bf813d03bacad6aed28ad6d3f7fb
Eric Antones <eantones@nuobit.com>
2020-12-22 15:22:35 +0100

    [ADD] account_asset_product_item: new module to create as many assets as product units

---
+++ account_asset_product_item/README.rst
@@ -0,0 +1,31 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+======================
+Assets by product item
+======================
+
+* This module allows to create as many assets as the quantity on
+the invoice line.
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/nuobit/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eric Antones <eantones@nuobit.com>
+
+
+
+
---
+++ account_asset_product_item/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Assets by product item',
+    'summary': 'This module allows to create as many '
+               'assets as the quantity on the invoice line.',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_asset_hook',
+    ],
+    'data': [
+        'views/account_asset_category_views.xml'
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
