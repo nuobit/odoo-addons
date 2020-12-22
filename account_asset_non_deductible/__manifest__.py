d9b89fcffb8ef3f1e8c64acd0e00f5fe6c06122a
Eric Antones <eantones@nuobit.com>
2020-12-22 15:12:48 +0100

    [IMP] account_asset_non_deductible: make it compatible with account_asset_product_item

--- account_asset_non_deductible/__manifest__.py
+++ account_asset_non_deductible/__manifest__.py
@@ -5,13 +5,13 @@
 {
     'name': 'Non-deductible asset value',
     'summary': 'Adds a non-deductible tax amount to the asset value',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.2.0',
     'category': 'Accounting',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'website': 'https://github.com/nuobit',
     'license': 'AGPL-3',
     'depends': [
-        'l10n_es_account_asset',
+        'account_asset_product_item',
         'l10n_es_aeat_vat_special_prorrate',
     ],
     'installable': True,
