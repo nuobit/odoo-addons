54f5a0c9595f8554c9c2a321a00afd06488d17e8
Eric Antones <eantones@nuobit.com>
2020-12-23 19:27:01 +0100

    [ADD] account_financial_report_number: new module to add move number to journal ledger report

---
+++ account_financial_report_number/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Account Financial Reports Number',
+    'summary': 'This module adds a filter to Journal Ledger '
+               'report to filter by journal entry number',
+    'version': '11.0.1.0.0',
+    'category': 'Reporting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_financial_report',
+    ],
+    'data': [
+        'wizard/journal_ledger_wizard_view.xml',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
