e004d8753f17e3cc3789819c1689f5f700f80606
Eric Antones <eantones@nuobit.com>
2020-12-28 14:58:10 +0100

    [ADD] account_asset_active: new module

---
+++ account_asset_active/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Asset active button',
+    'summary': 'This module adds a "Active" button',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_asset',
+    ],
+    'data': [
+        'views/account_asset_view.xml',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
