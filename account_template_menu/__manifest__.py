e02c79bba09c0617d6558b04aa218f1aa0afcad4
Eric Antones <eantones@nuobit.com>
2020-12-29 16:47:35 +0100

    [ADD] account_template_menu: enable menus to access to all account templates

---
+++ account_template_menu/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Account Template Menu',
+    'summary': "This module adds menu entries *Account Chart Templates*, *Account Templates*, "
+               "*Account Tax Templates* and *Account Fiscal Pôsition Templates* under "
+               "*Accounting > Configuration > Accounting*, because these menu entries "
+               "doesn't exists in the official *account* module of Odoo 11.",
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_template_menuitem.xml',
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
