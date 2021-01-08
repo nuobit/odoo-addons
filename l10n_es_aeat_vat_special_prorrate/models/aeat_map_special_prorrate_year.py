04adbed39ed7acd1864c63d99ba46e43d7c25d6d
Eric Antones <eantones@nuobit.com>
2021-01-08 14:18:40 +0100

    [IMP] l10n_es_aeat_vat_special_prorrate: added prorrate regularization

--- l10n_es_aeat_vat_special_prorrate/models/aeat_map_special_prorrate_year.py
+++ l10n_es_aeat_vat_special_prorrate/models/aeat_map_special_prorrate_year.py
@@ -18,8 +18,17 @@ class MapSpecialProrrateYear(models.Model):
         required=True,
         default=lambda self: self.env.company,
     )
-    year = fields.Integer(string="Year", required=True)
-    tax_percentage = fields.Float(string="Tax %", required=True)
+
+    def _default_year(self):
+        last = self.search(
+            [("company_id", "=", self.env.company.id)],
+            order="company_id,year desc",
+            limit=1,
+        )
+        return last and last.year + 1 or fields.Date.context_today(self).year
+
+    year = fields.Integer(string="Year", default=_default_year, required=True)
+    tax_percentage = fields.Float(string="Temporary %", required=True)
 
     map_prorrate_next_year_id = fields.Many2one(
         comodel_name="aeat.map.special.prorrate.year",
@@ -75,13 +84,13 @@ class MapSpecialProrrateYear(models.Model):
 
     @api.depends("year", "tax_percentage")
     def name_get(self):
-        to_percent_str = lambda x: (x.is_integer() and "%i%%" or "%.2f%%") % x  # noqa
         result = []
         for rec in self:
-            name = [to_percent_str(rec.tax_percentage)]
+            percents = [rec.tax_percentage]
             if rec.tax_final_percentage:
-                name.append(to_percent_str(rec.tax_final_percentage))
-            result.append((rec.id, "%i: %s" % (rec.year, " -> ".join(name))))
+                percents.append(rec.tax_final_percentage)
+            name_l = ["%g" % round(x, 2) for x in percents]
+            result.append((rec.id, "%i: %s" % (rec.year, " -> ".join(name_l))))
         return result
 
     @api.constrains("map_prorrate_next_year_id", "year")
@@ -121,112 +130,44 @@ class MapSpecialProrrateYear(models.Model):
                 map_prorrate_previous_year_id.map_prorrate_next_year_id = False
             super(MapSpecialProrrateYear, rec).unlink()
 
-    # business logic
-    # compute final prorrate
-    # extracted from "l10n.es.aeat.report.tax.mapping"
-    def _get_partner_domain(self):
-        return []
-
-    # extracted from "l10n.es.aeat.report"
-    def get_taxes_from_templates(self, tax_templates):
-        company = self.company_id or self.env.user.company_id
-        return company.get_taxes_from_templates(tax_templates)
-
-    # extracted from "l10n.es.aeat.report"
-    def get_account_from_template(self, account_template):
-        company = self.company_id or self.env.user.company_id
-        return company.get_account_from_template(account_template)
-
-    # extracted from "l10n.es.aeat.report.tax.mapping"
-    def _get_move_line_domain(self, date_start, date_end, map_line):
-        self.ensure_one()
-        if map_line != map_line._origin:
-            taxes = self.env["account.tax.template"].browse(
-                [x._origin.id for x in map_line.tax_ids]
-            )
-        else:
-            taxes = map_line.tax_ids
-        taxes = self.get_taxes_from_templates(taxes)
-        move_line_domain = [
-            ("company_id", "child_of", self.company_id.id),
-            ("date", ">=", date_start),
-            ("date", "<=", date_end),
-            ("parent_state", "=", "posted"),
-        ]
-        if map_line.move_type == "regular":
-            move_line_domain.append(
-                ("move_id.financial_type", "in", ("receivable", "payable", "liquidity"))
-            )
-        elif map_line.move_type == "refund":
-            move_line_domain.append(
-                (
-                    "move_id.financial_type",
-                    "in",
-                    ("receivable_refund", "payable_refund"),
-                )
-            )
-        if map_line.field_type == "base":
-            move_line_domain.append(("tax_ids", "in", taxes.ids))
-        elif map_line.field_type == "amount":
-            move_line_domain.append(("tax_line_id", "in", taxes.ids))
-        else:  # map_line.field_type == 'both'
-            move_line_domain += [
-                "|",
-                ("tax_line_id", "in", taxes.ids),
-                ("tax_ids", "in", taxes.ids),
-            ]
-        if map_line.account_id:
-            account = self.get_account_from_template(map_line.account_id)
-            move_line_domain.append(("account_id", "in", account.ids))
-        if map_line.sum_type == "debit":
-            move_line_domain.append(("debit", ">", 0))
-        elif map_line.sum_type == "credit":
-            move_line_domain.append(("credit", ">", 0))
-        if map_line.exigible_type == "yes":
-            move_line_domain.append(("tax_exigible", "=", True))
-        elif map_line.exigible_type == "no":
-            move_line_domain.append(("tax_exigible", "=", False))
-        move_line_domain += self._get_partner_domain()
-        return move_line_domain
-
-    # extracted from "l10n.es.aeat.report.tax.mapping"
-    def _get_tax_lines(self, date_start, date_end, map_line):
-        """Get the move lines for the codes and periods associated
-        :param date_start: Start date of the period
-        :param date_end: Stop date of the period
-        :param map_line: Mapping line record
-        :return: Move lines recordset that matches the criteria.
-        """
-        domain = self._get_move_line_domain(date_start, date_end, map_line)
-        return self.env["account.move.line"].search(domain)
-
     def _compute_prorrate_percent(self):
         self.ensure_one()
-
         date_from = "%s-01-01" % self.year
         date_to = "%s-12-31" % self.year
 
+        mod303 = self.env["l10n.es.aeat.mod303.report"].new({})
+
         # Get base amount for taxed operations
+        affected_taxes = [
+            "l10n_es.account_tax_template_s_iva4b",
+            "l10n_es.account_tax_template_s_iva4s",
+            "l10n_es.account_tax_template_s_iva10b",
+            "l10n_es.account_tax_template_s_iva10s",
+            "l10n_es.account_tax_template_s_iva21b",
+            "l10n_es.account_tax_template_s_iva21s",
+            "l10n_es.account_tax_template_s_iva21isp",
+        ]
         MapLine = self.env["l10n.es.aeat.map.tax.line"]
-        map_line = MapLine.new(
-            {
-                "move_type": "all",
-                "field_type": "base",
-                "sum_type": "both",
-                "exigible_type": "yes",
-            }
-        )
-        move_lines = self._get_tax_lines(
-            date_from,
-            date_to,
-            map_line,
-        )
+        mapline_vals = {
+            "move_type": "all",
+            "field_type": "base",
+            "sum_type": "both",
+            "exigible_type": "yes",
+            "tax_ids": [(4, self.env.ref(x).id) for x in affected_taxes],
+        }
+        map_line = MapLine.new(mapline_vals)
+        move_lines = mod303._get_tax_lines(date_from, date_to, map_line)
         taxed = sum(move_lines.mapped("credit")) - sum(move_lines.mapped("debit"))
-
-        # compute final prorrate percentage performing ceiling operation
-        prorrate_percent = math.ceil(taxed / taxed * 100)
-
-        return prorrate_percent
+        # Get base amount of exempt operations
+        mapline_vals["tax_ids"] = [
+            (4, self.env.ref("l10n_es.account_tax_template_s_iva0").id)
+        ]
+        map_line = MapLine.new(mapline_vals)
+        move_lines = mod303._get_tax_lines(date_from, date_to, map_line)
+        exempt = sum(move_lines.mapped("credit")) - sum(move_lines.mapped("debit"))
+        # compute prorrate percentage performing ceiling operation
+        vat_prorrate_percent = math.ceil(taxed / (taxed + exempt) * 100)
+        return vat_prorrate_percent
 
     def compute_prorrate(self):
         self.ensure_one()
@@ -283,3 +224,265 @@ class MapSpecialProrrateYear(models.Model):
     def set_temporary(self):
         self.tax_final_percentage_aux = False
         self.state = "temporary"
+
+    # prorrate regularitzation
+    prorrate_reg_move_date = fields.Date(
+        string="Date",
+        compute="_compute_reg_move_date",
+        store=True,
+        readonly=False,
+    )
+
+    def _compute_reg_move_date(self):
+        for rec in self:
+            if rec.year != 0:
+                rec.prorrate_reg_move_date = "%i-12-31" % rec.year
+            else:
+                rec.prorrate_reg_move_date = False
+
+    prorrate_reg_positive_adjust_account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Positive adjustment account",
+        domain=[("deprecated", "=", False)],
+    )
+    prorrate_reg_negative_adjust_account_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Negative adjustment account",
+        domain=[("deprecated", "=", False)],
+    )
+
+    @api.model
+    def _default_journal(self):
+        return self.env["account.journal"].search([("code", "=", "MISC")], limit=1)
+
+    prorrate_reg_journal_id = fields.Many2one(
+        comodel_name="account.journal",
+        string="Journal",
+        default=_default_journal,
+        domain=[("type", "=", "general")],
+    )
+    prorrate_reg_ref = fields.Char(
+        string="Reference", default="Regularización prorrata definitiva"
+    )
+    prorrate_reg_move_id = fields.Many2one(
+        comodel_name="account.move",
+        string="Journal Entry",
+        readonly=True,
+        index=True,
+        ondelete="set null",
+        copy=False,
+        help="Link to the first regularization final prorrate journal entry",
+    )
+
+    def compute_prorrate_regularization(self):
+        self.ensure_one()
+
+        if self.prorrate_reg_move_id:
+            return
+
+        date_from = "%s-01-01" % self.year
+        date_to = "%s-12-31" % self.year
+
+        mod303 = self.env["l10n.es.aeat.mod303.report"].new({})
+
+        # TODO: put this as configurable from the UI
+        # target_tax_name = "P_PRD44_IVA"
+        # target_tax_id = self.env["account.tax"].search(
+        #     [
+        #         ("description", "=", target_tax_name),
+        #         ("company_id", "child_of", self.company_id.id),
+        #     ]
+        # )
+        # if not target_tax_id:
+        #     raise ValidationError(
+        #         _("Tax '%s' not found on current company") % target_tax_name
+        #     )
+
+        # TODO: put these as configurable from the UItipus gr
+        prorrate_taxes = self.env["account.tax"].search(
+            [
+                ("company_id", "=", self.company_id.id),
+                ("amount_type", "!=", "group"),
+                ("prorrate_type", "=", "deductible"),
+            ]
+        )
+        MapLine = self.env["l10n.es.aeat.map.tax.line"]
+        map_line = MapLine.new(
+            {
+                "move_type": "all",
+                "field_type": "base",
+                "sum_type": "both",
+                "exigible_type": "yes",
+                "tax_ids": [(4, self.env.ref(x).id) for x in affected_taxes],
+            }
+        )
+        move_lines = mod303._get_tax_lines(
+            date_from,
+            date_to,
+            map_line,
+        )
+
+        line_values = []
+        for line in move_lines:
+            # compute prorrate difference
+            tax_id = line.tax_line_id
+            if tax_id.amount_type != "percent":
+                raise ValidationError(
+                    _("Tax of type %s not supported") % tax_id.amount_type
+                )
+
+            round_curr = line.move_id.currency_id.round
+            prorrated_tax_amount = line.balance  # round_curr(line.debit - line.credit)
+            tax_amount = prorrated_tax_amount / (self.tax_percentage / 100)
+            round_curr(tax_amount / (tax_id.amount / 100))
+            new_prorrated_tax_amount = round_curr(
+                tax_amount * self.tax_final_percentage / 100
+            )
+
+            diff = round_curr(new_prorrated_tax_amount - prorrated_tax_amount)
+            if diff == 0:
+                continue
+
+            # build journal item
+            # common
+            values_common = {
+                "company_id": line.company_id.id,
+                "partner_id": line.partner_id.id,
+                "invoice_id": line.invoice_id.id,
+                "tax_exigible": line.tax_exigible,
+            }
+
+            # tax
+            values_tax = dict(values_common)
+            values_tax.update(
+                {
+                    "account_id": line.account_id.id,
+                    "name": line.name,
+                    "tax_line_id": target_tax_id.id,
+                    "debit": diff > 0 and diff or 0,
+                    "credit": diff < 0 and abs(diff) or 0,
+                }
+            )
+            line_values.append(values_tax)
+
+            # adjust
+            values_adj = dict(values_common)
+            values_adj.update(
+                {
+                    "account_id": diff > 0
+                    and self.prorrate_reg_positive_adjust_account_id.id
+                    or self.prorrate_reg_negative_adjust_account_id.id,
+                    "name": "%s - %s" % (self.prorrate_reg_ref, line.name),
+                    "debit": diff < 0 and abs(diff) or 0,
+                    "credit": diff > 0 and diff or 0,
+                }
+            )
+            # values_adj.update({
+            #     'tax_ids': [(6, 0, [parent_tax.id] + parent_tax.children_tax_ids.ids)],
+            # })
+
+            # TODO comteps i etiqe analitycs en el compte 63xx
+            # values.update({
+            #     'analytic_account_id': line.analytic_account_id.id,
+            #     'analytic_tag_ids': [(6, 0, line.analytic_tag_ids.mapped('id'))],
+            # })
+
+            line_values.append(values_adj)
+
+            # invoice_line_id = self.find_invoice_line(line, base, parent_tax)
+            # asset_id = self.find_asset(invoice_line_id)
+
+        # create journal entry
+        values = {
+            "date": self.prorrate_reg_move_date,
+            "ref": self.prorrate_reg_ref,
+            "company_id": self.company_id.id,
+            "journal_id": self.prorrate_reg_journal_id.id,
+            "move_type": "other",
+            "line_ids": [(0, False, lv) for lv in line_values],
+        }
+
+        # create jornal item
+        move = self.env["account.move"].create(values)
+        move.action_post()
+        self.write(
+            {
+                "prorrate_reg_move_id": move.id,
+            }
+        )
+
+    #########################################3
+
+    def get_account(self, child, move_type):
+        account = False
+        repartition_line = {
+            "in_invoice": child.invoice_repartition_line_ids,
+            "in_refund": child.refund_repartition_line_ids,
+        }
+        rt = ["base", "tax"]
+        for ir in repartition_line[move_type]:
+            if ir.repartition_type not in rt:
+                raise Exception(
+                    "Tipus de repartició de la taxa child unexpected %s"
+                    % ir.repartition_type
+                )
+            rt.remove(ir.repartition_type)
+            if ir.repartition_type == "tax":
+                if ir.account_id:
+                    if not account:
+                        account = ir.account_id
+                    else:
+                        raise Exception(
+                            "Unexpected, only one line with axxount expeted"
+                        )
+        return account
+
+    def fix_taxes(self):
+        """
+        heuristic non deterministi method. Get original databse non migrated
+        and get the taaxes from the move_lines, the id's of the move_lines @api.onchange('FIELD_NAME')
+        def _onchange_FIELD_NAME(self):
+            passold and new databse are teh same, so, only mapping tax_line_id
+        Fix taxes tax:line_id from move_lines
+        not do: - fix descriptions on move_lines
+                - fix tax_ids of the move (former invoice)
+        """
+        move_lines = self.env["account.move.line"].search(
+            [
+                ("move_id.company_id", "=", self.company_id.id),
+                ("move_id.date", ">=", "2021-01-01"),
+                ("move_id.date", "<=", "2021-12-31"),
+                ("move_id.state", "=", "posted"),
+                ("move_id.move_type", "in", ("in_invoice", "in_refund")),
+                ("tax_line_id", "!=", False),
+                ("tax_line_id.amount_type", "=", "group"),
+                # ('move_id', '=', 642260)
+            ]
+        )
+        N = len(move_lines)
+        for i, l in enumerate(move_lines, 1):
+            print(
+                " %i/%i -------" % (i, N),
+                l.tax_line_id.name,
+                l.move_id.name,
+                l.tax_base_amount,
+            )
+            children = {}
+            for child in l.tax_line_id.children_tax_ids:
+                account = self.get_account(child, l.move_id.move_type)
+                if account in children:
+                    raise Exception(
+                        "Unexpected, only one account per tax and move expected"
+                    )
+                children[account] = child
+            if False not in children:
+                raise Exception("Unexpected, one children must have null account")
+
+            tax = children.get(l.account_id, children[False])
+            self.env.cr.execute(
+                """
+                update account_move_line set tax_line_id = %s where id = %s
+                """
+                % (tax.id, l.id)
+            )
+            # l.tax_line_id = tax
