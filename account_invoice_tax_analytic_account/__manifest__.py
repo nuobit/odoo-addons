2cde1cad28bf5bd6de29e6190efaf39da60efab0
Eric Antones <eantones@nuobit.com>
2021-01-17 20:58:40 +0100

    [FIX] account_invoice_tax_analytic_account: tax base not computed when invoice line has analytic account defined

--- account_invoice_tax_analytic_account/__manifest__.py
+++ account_invoice_tax_analytic_account/__manifest__.py
@@ -5,11 +5,14 @@
 {
     'name': 'Account tax invoice analytic',
     'description': 'This module allows to add analytic account manually on invoice taxes.',
-    'version': '11.0.0.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'website': 'https://www.nuobit.com',
     'depends': ['account'],
+    'data': [
+        'views/account_invoice_view.xml',
+    ],
     'installable': True,
 }
--- account_invoice_tax_analytic_account/models/account_invoice.py
+++ account_invoice_tax_analytic_account/models/account_invoice.py
@@ -1,31 +1,45 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
-from odoo import models
 import logging
 
+from odoo import models, _, api, fields
+
 _logger = logging.getLogger(__name__)
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.multi
+    def get_taxes_values(self):
+        res = super(AccountInvoice, self).get_taxes_values()
+
+        bmap = {'False': False, 'True': True}
+        for key, tax in res.items():
+            _, _, res[key]['origin_account_analytic_id'] = \
+                [x not in bmap and int(x) or bmap[x] for x in key.split('-')]
+
+        return res
 
 
 class AccountInvoiceTax(models.Model):
     _inherit = "account.invoice.tax"
 
+    origin_account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic account Origin')
+
     def _compute_base_amount(self):
+        super(AccountInvoiceTax, self)._compute_base_amount()
         tax_grouped = {}
         for invoice in self.mapped('invoice_id'):
             tax_grouped[invoice.id] = invoice.get_taxes_values()
         for tax in self:
             tax.base = 0.0
             if tax.tax_id:
-                if tax.tax_id.analytic:
-                    account_analytic_id = tax.account_analytic_id.id
-                else:
-                    account_analytic_id = False
                 key = tax.tax_id.get_grouping_key({
                     'tax_id': tax.tax_id.id,
                     'account_id': tax.account_id.id,
-                    'account_analytic_id': account_analytic_id,
+                    'account_analytic_id': tax.origin_account_analytic_id.id,
                 })
                 if tax.invoice_id and key in tax_grouped[tax.invoice_id.id]:
                     tax.base = tax_grouped[tax.invoice_id.id][key]['base']
