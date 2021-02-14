26b0495530c69a6d4cc319bcb872b989e2372627
Eric Antones <eantones@nuobit.com>
2021-02-14 18:34:14 +0100

    [ADD] stock_putaway_no_internal_mrp: added support for mrp pickings

---
+++ stock_putaway_no_internal_mrp/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Putaway strategy no internal for MRP',
+    'summary': "Allow to exclude putaway strategies if they are applied on an manufacturing operations",
+    'version': '11.0.1.0.0',
+    'category': 'Warehouse Management',
+    'license': 'AGPL-3',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'mrp',
+        'stock_putaway_no_internal',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
---
+++ stock_putaway_no_internal_mrp/models/mrp_production.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models
+from ast import literal_eval
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.multi
+    def open_produce_product(self):
+        action = super(MrpProduction, self).open_produce_product()
+        action['context'] = {
+            **literal_eval(action['context']),
+            **dict(stock_picking_type_code=self.picking_type_id.code)}
+        return action
