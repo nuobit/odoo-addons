a81396c1957ecaf3aeea6d74fdc81df1f9d6765d
Eric Antones <eantones@nuobit.com>
2021-02-14 18:34:14 +0100

    [ADD] stock_putaway_no_internal_mrp: added support for mrp pickings

---
+++ stock_putaway_no_internal_mrp/models/stock_location.py
@@ -0,0 +1,13 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class Location(models.Model):
+    _inherit = "stock.location"
+
+    def _excluded_picking_types(self):
+        return super(Location, self)._excluded_picking_types() + ['mrp_operation']
