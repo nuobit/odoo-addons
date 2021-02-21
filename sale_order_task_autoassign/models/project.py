5d6303518e57ed8744cf81c36d893204f2290e95
Eric Antones <eantones@nuobit.com>
2021-02-21 18:31:42 +0100

    [IMP] sale_order_task_autoassign: assign departments to projects

---
+++ sale_order_task_autoassign/models/project.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import logging
+
+from odoo import fields, models
+
+
+_logger = logging.getLogger(__name__)
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    department_ids = fields.Many2many(
+        comodel_name='hr.department',
+        string='Departments'
+    )
