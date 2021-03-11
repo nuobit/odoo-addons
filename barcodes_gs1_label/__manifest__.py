b4e7568ae47a5d21723ec1440162545303066d46
Eric Antones <eantones@nuobit.com>
2021-03-11 12:53:33 +0100

    [FIX] barcodes_gs1_label: cannot print barcodes from other model than stock.picking

--- barcodes_gs1_label/__manifest__.py
+++ barcodes_gs1_label/__manifest__.py
@@ -6,7 +6,7 @@
     'name': "Barcodes label",
     'description': "Generate barcode labels enabling barcode printing "
                    "on products, lot/serial and picking",
-    'version': '12.0.2.4.0',
+    'version': '12.0.2.4.1',
     'license': 'AGPL-3',
     'category': 'Tools',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
