e43d8b6bc80603a25b88a329e2f9244dd146eca4
Eric Antones <eantones@nuobit.com>
2021-03-11 22:06:04 +0100

    [FIX] sale_order_task_autoassign_pack: wrong total task duration

--- sale_order_task_autoassign_pack/models/sale_order.py
+++ sale_order_task_autoassign_pack/models/sale_order.py
@@ -12,11 +12,6 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _prepare_valid_tasks(self):
-        tasks_ids = super(SaleOrder, self)._prepare_valid_tasks()
-        pack_product_tasks = tasks_ids.filtered(lambda x: x.sale_line_id.pack_parent_line_id)
-        pack_product_tasks.write({
-            'sale_line_id': False
-        })
-        pack_product_tasks.unlink()
-        return self.tasks_ids
+    def _prepare_main_tasks(self):
+        tasks_ids = super(SaleOrder, self)._prepare_main_tasks()
+        return tasks_ids.filtered(lambda x: not x.sale_line_id.pack_parent_line_id)
