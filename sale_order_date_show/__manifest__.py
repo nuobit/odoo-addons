2ae784f658a6fdc205a5508e345d64b39c069393
Eric Antones <eantones@nuobit.com>
2021-03-29 18:28:45 +0200

    [REF] sale_order_date_show: pre-commit

--- sale_order_date_show/__manifest__.py
+++ sale_order_date_show/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Sale order date show',
-    'version': '12.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Sale',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module shows the order date on sale orders and allows "
-               "to be modified only if the related invoice is not yet validated.",
-    'depends': [
-        'sale',
+    "name": "Sale order date show",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module shows the order date on sale orders and allows "
+    "to be modified only if the related invoice is not yet validated.",
+    "depends": [
+        "sale",
     ],
-    'data': [
-        'views/sale_order_views.xml',
+    "data": [
+        "views/sale_order_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
