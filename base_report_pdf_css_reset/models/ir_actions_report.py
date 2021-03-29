e0d2246a7492c0f7a261fcb283604f51a0f6f34f
Eric Antones <eantones@nuobit.com>
2021-03-29 18:26:50 +0200

    [REF] base_report_pdf_css_reset: pre-commit

--- base_report_pdf_css_reset/models/ir_actions_report.py
+++ base_report_pdf_css_reset/models/ir_actions_report.py
@@ -2,19 +2,25 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
-
 import lxml.html
+
+from odoo import api, models
 
 
 class IrActionsReport(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.multi
     def _prepare_html(self, html):
-        bodies, res_ids, header, footer, specific_paperformat_args = super()._prepare_html(html)
+        (
+            bodies,
+            res_ids,
+            header,
+            footer,
+            specific_paperformat_args,
+        ) = super()._prepare_html(html)
 
-        if self.env.context.get('no_paddings', False):
+        if self.env.context.get("no_paddings", False):
             bodies_tmp = []
             for body in bodies:
                 root = lxml.html.fromstring(body)
@@ -22,13 +28,13 @@ class IrActionsReport(models.Model):
                 if body_class:
                     elem = body_class[0]
                     classes_tmp = []
-                    for klass in elem.attrib['class'].split(' '):
-                        if klass != 'container':
+                    for klass in elem.attrib["class"].split(" "):
+                        if klass != "container":
                             classes_tmp.append(klass)
                     if not classes_tmp:
-                        del elem.attrib['class']
+                        del elem.attrib["class"]
                     else:
-                        elem.attrib['class'] = ' '.join(classes_tmp)
+                        elem.attrib["class"] = " ".join(classes_tmp)
 
                     bodies_tmp.append(lxml.html.tostring(root))
 
