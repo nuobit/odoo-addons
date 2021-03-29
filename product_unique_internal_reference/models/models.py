74cb9a87f01981ba06efa2449f9797f977963a83
Eric Antones <eantones@nuobit.com>
2021-03-29 23:41:08 +0200

    [IMP] product_unique_internal_reference: black, isort, prettier

--- product_unique_internal_reference/models/models.py
+++ product_unique_internal_reference/models/models.py
@@ -2,26 +2,35 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
-from odoo.tools.translate import _
+from odoo import api, models
 from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
-    @api.constrains('default_code')
+    @api.constrains("default_code")
     def _check_default_code(self):
         for record in self:
             if record.default_code:
-                default_code = self.with_context(active_test=False).search([
-                    ('product_tmpl_id.company_id', '=', record.product_tmpl_id.company_id.id),
-                    ('default_code', '=', record.default_code),
-                    ('id', '!=', record.id),
-                ], limit=1)
+                default_code = self.with_context(active_test=False).search(
+                    [
+                        (
+                            "product_tmpl_id.company_id",
+                            "=",
+                            record.product_tmpl_id.company_id.id,
+                        ),
+                        ("default_code", "=", record.default_code),
+                        ("id", "!=", record.id),
+                    ],
+                    limit=1,
+                )
 
                 if default_code:
                     raise ValidationError(
-                        _("Error! The Default Code %s already exists. Check also the archived ones." %
-                          record.default_code)
+                        _(
+                            "Error! The Default Code %s already exists. Check also the archived ones."
+                            % record.default_code
+                        )
                     )
