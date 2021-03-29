e525a31b5ce6ce0a2a11f124c53b2cd32552abf6
Eric Antones <eantones@nuobit.com>
2021-03-29 18:26:50 +0200

    [REF] base_report_pdf_css_reset: pre-commit

--- base_report_pdf_css_reset/__manifest__.py
+++ base_report_pdf_css_reset/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Report PDF CSS reset',
-    'summary': 'This module enables you with the capability to reset the default '
-               'paddings on the pdf minimal_layout template',
-    'version': '12.0.1.0.0',
-    'category': 'Reporting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
+    "name": "Report PDF CSS reset",
+    "summary": "This module enables you with the capability to reset the default "
+    "paddings on the pdf minimal_layout template",
+    "version": "12.0.1.0.1",
+    "category": "Reporting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
-    'auto_install': False,
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
+    "auto_install": False,
 }
