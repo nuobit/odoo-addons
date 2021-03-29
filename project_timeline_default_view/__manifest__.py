da949dd2e722a9f3ce8cf7a90f4346831a80fdbd
Eric Antones <eantones@nuobit.com>
2021-03-29 18:18:28 +0200

    [REF] project_timeline_default_view: pre-commit

--- project_timeline_default_view/__manifest__.py
+++ project_timeline_default_view/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Project timeline default view',
-    'version': '12.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Project',
-    'website': 'https://github.com/nuobit',
-    'description': "This module allows to define what projects should open the"
-                   " timeline view from the default kanban view.",
-    'depends': [
-        'project_timeline',
+    "name": "Project timeline default view",
+    "version": "12.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module allows to define what projects should open the"
+    " timeline view from the default kanban view.",
+    "depends": [
+        "project_timeline",
     ],
-    'data': [
-        'views/project_views.xml',
+    "data": [
+        "views/project_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
