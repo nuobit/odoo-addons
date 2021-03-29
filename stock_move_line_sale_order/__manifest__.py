b1d05465ea23cde067e5b71f1983c7a5bdf291a8
Eric Antones <eantones@nuobit.com>
2021-03-29 18:30:55 +0200

    [REF] stock_move_line_sale_order: pre-commit

--- stock_move_line_sale_order/__manifest__.py
+++ stock_move_line_sale_order/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock move line sale order",
-    'description': "This module adds field sale order to stock move line views",
-    'version': '12.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'sale_stock',
+    "name": "Stock move line sale order",
+    "summary": "This module adds field sale order to stock move line views",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "sale_stock",
     ],
-    'data': [
-        'views/stock_move_line_views.xml',
+    "data": [
+        "views/stock_move_line_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
