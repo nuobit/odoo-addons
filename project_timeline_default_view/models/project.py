165545505be62badfa6a330861221e07c2e9dc30
Eric Antones <eantones@nuobit.com>
2021-03-29 18:18:28 +0200

    [REF] project_timeline_default_view: pre-commit

--- project_timeline_default_view/models/project.py
+++ project_timeline_default_view/models/project.py
@@ -2,21 +2,29 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class Project(models.Model):
     _inherit = "project.project"
 
-    default_timeline_view = fields.Boolean(
-        string="Default timeline view"
-    )
+    default_timeline_view = fields.Boolean(string="Default timeline view")
 
     @api.multi
     def action_project_task(self):
         action = super(Project, self).open_tasks()
         if self.default_timeline_view:
-            action['view_mode'] = 'timeline,kanban,tree,form,calendar,pivot,graph,activity'
-            action['views'] = [(False, 'timeline'), (False, 'kanban'), (False, 'tree'), (False, 'form'),
-                               (False, 'calendar'), (False, 'pivot'), (False, 'graph'), (False, 'activity')]
+            action[
+                "view_mode"
+            ] = "timeline,kanban,tree,form,calendar,pivot,graph,activity"
+            action["views"] = [
+                (False, "timeline"),
+                (False, "kanban"),
+                (False, "tree"),
+                (False, "form"),
+                (False, "calendar"),
+                (False, "pivot"),
+                (False, "graph"),
+                (False, "activity"),
+            ]
         return action
