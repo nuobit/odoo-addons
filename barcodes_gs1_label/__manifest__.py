75cf53e9307be539aef73f5f452d4c595da72ea4
Eric Antones <eantones@nuobit.com>
2021-03-29 18:20:01 +0200

    [REF] barcodes_gs1_label: pre-commit

--- barcodes_gs1_label/__manifest__.py
+++ barcodes_gs1_label/__manifest__.py
@@ -3,28 +3,28 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Barcodes label",
-    'description': "Generate barcode labels enabling barcode printing "
-                   "on products, lot/serial and picking",
-    'version': '12.0.2.4.1',
-    'license': 'AGPL-3',
-    'category': 'Tools',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
-        'base_report_gs1_barcode',
-        'base_report_pdf_css_reset',
+    "name": "Barcodes label",
+    "summary": "Generate barcode labels enabling barcode printing "
+    "on products, lot/serial and picking",
+    "version": "12.0.2.4.2",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "stock",
+        "base_report_gs1_barcode",
+        "base_report_pdf_css_reset",
     ],
-    'data': [
-        'wizard/options_wizard_views.xml',
-        'report/report.xml',
-        'views/report_barcode.xml',
-        'views/product_views.xml',
-        'views/stock_production_lot_views.xml',
-        'views/stock_picking_views.xml',
+    "data": [
+        "wizard/options_wizard_views.xml",
+        "report/report.xml",
+        "views/report_barcode.xml",
+        "views/product_views.xml",
+        "views/stock_production_lot_views.xml",
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
 }
