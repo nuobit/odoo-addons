0e7497a1d2fc177780f460fa9ab8b90412eade89
Eric Antones <eantones@nuobit.com>
2021-03-29 23:41:08 +0200

    [IMP] product_unique_internal_reference: black, isort, prettier

--- product_unique_internal_reference/__manifest__.py
+++ product_unique_internal_reference/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Product unique internal reference',
-    'summary': 'This module ensures that you enter a Unique Internal Reference (default_code) for your Products',
-    'version': '11.0.0.2.2',
-    'category': 'Sales',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
+    "name": "Product unique internal reference",
+    "summary": "This module ensures that you enter a Unique Internal Reference (default_code) for your Products",
+    "version": "11.0.0.2.2",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
     ],
-    'pre_init_hook': 'pre_init_hook_internal_reference_check',
-    'installable': True,
-    'auto_install': False,
+    "pre_init_hook": "pre_init_hook_internal_reference_check",
+    "installable": True,
+    "auto_install": False,
 }
