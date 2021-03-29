2c34977a0cbe22655f32b7c440dcca9b5a9eeedd
Eric Antones <eantones@nuobit.com>
2021-03-29 18:19:01 +0200

    [REF] pos_restaurant_note_print: pre-commit

--- pos_restaurant_note_print/__manifest__.py
+++ pos_restaurant_note_print/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'POS restaurant line note receipt',
-    'version': '12.0.1.0.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Point of Sale',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module shows the line note on the receipt on screen and printer.",
-    'depends': [
-        'pos_restaurant',
+    "name": "POS restaurant line note receipt",
+    "version": "12.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module shows the line note on the receipt on screen and printer.",
+    "depends": [
+        "pos_restaurant",
     ],
-    'data': [
-        'views/templates.xml',
+    "data": [
+        "views/templates.xml",
     ],
-    'qweb': [
-        'static/src/xml/pos.xml',
+    "qweb": [
+        "static/src/xml/pos.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
