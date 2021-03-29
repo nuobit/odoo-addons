931ba8dda25ed45c4e9f6463097b596e956feadb
Eric Antones <eantones@nuobit.com>
2021-03-29 18:13:22 +0200

    [REF] stock_move_line_lot: pre-commit

--- stock_move_line_lot/__manifest__.py
+++ stock_move_line_lot/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock move line lot",
-    'description': "This module adds field lot to stock move line tree view",
-    'version': '12.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock move line lot",
+    "summary": "This module adds field lot to stock move line tree view",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_move_line_views.xml',
+    "data": [
+        "views/stock_move_line_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
