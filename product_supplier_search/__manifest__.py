9888674ae2e1aab738135c14100c01cd389ae147
Eric Antones <eantones@nuobit.com>
2021-03-29 22:39:55 +0200

    [IMP] product_supplier_search: black, isort, prettier

--- product_supplier_search/__manifest__.py
+++ product_supplier_search/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Product supplier search',
-    'summary': 'Search products by supplier reference on selection fields and tree product view',
-    'version': '11.0.0.1.0',
-    'category': 'Purchases',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "name": "Product supplier search",
+    "summary": "Search products by supplier reference on selection fields and tree product view",
+    "version": "11.0.0.1.0",
+    "category": "Purchases",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/product_views.xml',
-        'views/stock_picking_views.xml',
+    "data": [
+        "views/product_views.xml",
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
