fa12439c39a9190183567b167d233f3ae919c01f
Eric Antones <eantones@nuobit.com>
2021-03-29 18:28:08 +0200

    [REF] product_bike_info: pre-commit

--- product_bike_info/__manifest__.py
+++ product_bike_info/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Product Bike info",
-    'description': "This module adds field for bike data",
-    'version': '12.0.1.0.3',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'product',
+    "name": "Product Bike info",
+    "summary": "This module adds field for bike data",
+    "version": "12.0.1.0.4",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_template_views.xml',
+    "data": [
+        "views/product_template_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
