55e624b6f643c650d6d5d904613e63bfa811e57a
Eric Antones <eantones@nuobit.com>
2021-03-29 18:16:59 +0200

    [REF] sale_order_task_cancel: pre-commit

--- sale_order_task_cancel/__manifest__.py
+++ sale_order_task_cancel/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Sale order task cancel',
-    'version': '12.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Project',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module deletes all the tasks linked to a sale order"
-               " when it's been canceled.",
-    'depends': [
-        'sale_timesheet',
+    "name": "Sale order task cancel",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module deletes all the tasks linked to a sale order"
+    " when it's been canceled.",
+    "depends": [
+        "sale_timesheet",
     ],
-    'installable': True,
+    "installable": True,
 }
