2cab410e172afea7961c1950d769b35b08bcf55b
Eric Antones <eantones@nuobit.com>
2021-03-29 18:26:14 +0200

    [REF] base_report_gs1_barcode: pre-commit

--- base_report_gs1_barcode/__manifest__.py
+++ base_report_gs1_barcode/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Report GS1 barcodes',
-    'summary': 'This module adds a GS1-128 and GS1-Datamatrix barcode format support',
-    'version': '12.0.1.0.1',
-    'category': 'Reporting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'external_dependencies': {
-        'python': [
-            'pystrich',
+    "name": "Report GS1 barcodes",
+    "summary": "This module adds a GS1-128 and GS1-Datamatrix barcode format support",
+    "version": "12.0.1.0.2",
+    "category": "Reporting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [
+            "pystrich",
         ],
     },
-    'depends': [
-        'base',
+    "depends": [
+        "base",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
-    'auto_install': False,
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
+    "auto_install": False,
 }
