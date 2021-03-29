fd114eba6ceb5a9c56268604924e7a330007ada2
Eric Antones <eantones@nuobit.com>
2021-03-29 18:29:22 +0200

    [REF] stock_barcodes_sku: pre-commit

--- stock_barcodes_sku/__manifest__.py
+++ stock_barcodes_sku/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Stock Barcodes SKU',
-    'summary': 'This module enables you to scan barcodes that contain'
-               ' a SKU (default_code) additionally to the ones that '
-               'contain regular barcodes.',
-    'version': '12.0.1.0.0',
-    'category': 'Extra tools',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock_barcodes',
+    "name": "Stock Barcodes SKU",
+    "summary": "This module enables you to scan barcodes that contain"
+    " a SKU (default_code) additionally to the ones that "
+    "contain regular barcodes.",
+    "version": "12.0.1.0.1",
+    "category": "Extra tools",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_barcodes",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
-    'auto_install': False,
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
+    "auto_install": False,
 }
