0ff6554a52d02b265c11497dc82a1394aa3e6603
Eric Antones <eantones@nuobit.com>
2021-03-29 18:26:14 +0200

    [REF] base_report_gs1_barcode: pre-commit

--- base_report_gs1_barcode/models/ir_actions_report.py
+++ base_report_gs1_barcode/models/ir_actions_report.py
@@ -2,18 +2,16 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
+from odoo import api, models
 
 try:
-    import pystrich.datamatrix as dmx
     import pystrich.code128 as c128
-
+    import pystrich.datamatrix as dmx
 
     class DataMatrixRendererMod(dmx.DataMatrixRenderer):
         def add_border(self, colour=1):
             self.quiet_zone = 0
             return super().add_border(colour=colour)
-
 
     dmx.DataMatrixRenderer = DataMatrixRendererMod
 
@@ -22,17 +20,25 @@ except ImportError:
 
 
 class IrActionsReport(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.model
     def barcode(self, barcode_type, value, width=600, height=100, humanreadable=0):
-        if barcode_type == 'gs1-128':
-            data = value.replace(r'\F', '\xf1')
-            encoder = c128.Code128Encoder(data, options=dict(show_label=False, height=int(height)))
+        if barcode_type == "gs1-128":
+            data = value.replace(r"\F", "\xf1")
+            encoder = c128.Code128Encoder(
+                data, options=dict(show_label=False, height=int(height))
+            )
             return encoder.get_imagedata()
-        elif barcode_type == 'gs1-datamatrix':
-            data = value.replace(r'\F', '\xe7')
+        elif barcode_type == "gs1-datamatrix":
+            data = value.replace(r"\F", "\xe7")
             encoder = dmx.DataMatrixEncoder(data)
             return encoder.get_imagedata()
         else:
-            return super().barcode(barcode_type, value, width=width, height=height, humanreadable=humanreadable)
+            return super().barcode(
+                barcode_type,
+                value,
+                width=width,
+                height=height,
+                humanreadable=humanreadable,
+            )
