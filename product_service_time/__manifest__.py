632a447ec6f15bd8575731d69c4ce60304b4ea5f
Eric Antones <eantones@nuobit.com>
2021-03-29 18:16:17 +0200

    [REF] product_service_time: pre-commit

--- product_service_time/__manifest__.py
+++ product_service_time/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Product service time',
-    'version': '12.0.1.0.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Product',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module adds a field to products and product variants "
-               "of type service which allows to define the time required to "
-               "complete the service.",
-    'depends': [
-        'product',
+    "name": "Product service time",
+    "version": "12.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Product",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module adds a field to products and product variants "
+    "of type service which allows to define the time required to "
+    "complete the service.",
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_views.xml',
+    "data": [
+        "views/product_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- product_service_time/models/product_template.py
+++ product_service_time/models/product_template.py
@@ -2,31 +2,35 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 import odoo.addons.decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     service_time = fields.Float(
-        string='Service Time',
-        digits=dp.get_precision('Product UoM'),
-        help='Time to complete this service.',
-        compute='_compute_service_time',
-        inverse='_set_service_time', store=True
+        string="Service Time",
+        digits=dp.get_precision("Product UoM"),
+        help="Time to complete this service.",
+        compute="_compute_service_time",
+        inverse="_inverse_set_service_time",
+        store=True,
     )
 
-    @api.depends('product_variant_ids', 'product_variant_ids.service_time')
+    @api.depends("product_variant_ids", "product_variant_ids.service_time")
     def _compute_service_time(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        unique_variants = self.filtered(
+            lambda template: len(template.product_variant_ids) == 1
+        )
         for template in unique_variants:
             template.service_time = template.product_variant_ids.service_time
-        for template in (self - unique_variants):
+        for template in self - unique_variants:
             template.service_time = False
 
-    @api.one
-    def _set_service_time(self):
-        if len(self.product_variant_ids) == 1:
-            self.product_variant_ids.service_time = self.service_time
+    @api.multi
+    def _inverse_set_service_time(self):
+        for rec in self:
+            if len(rec.product_variant_ids) == 1:
+                rec.product_variant_ids.service_time = rec.service_time
