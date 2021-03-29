d006903d1a95503963240baf00177fcb92d79f6a
Eric Antones <eantones@nuobit.com>
2021-03-29 18:21:24 +0200

    [REF] pos_product_pack: pre-commit

--- pos_product_pack/__manifest__.py
+++ pos_product_pack/__manifest__.py
@@ -3,21 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'POS product pack',
-    'version': '12.0.1.0.2',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Point of Sale',
-    'website': 'https://github.com/nuobit',
-    'summary': "Make the product pack feature available on POS.",
-    'depends': [
-        'point_of_sale',
-        'sale_product_pack',
+    "name": "POS product pack",
+    "version": "12.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pms",
+    "summary": "Make the product pack feature available on POS.",
+    "depends": [
+        "point_of_sale",
+        "sale_product_pack",
     ],
-    'data': [
-        'views/templates.xml',
+    "data": [
+        "views/templates.xml",
     ],
-    'qweb': [
-    ],
-    'installable': True,
+    "qweb": [],
+    "installable": True,
 }
