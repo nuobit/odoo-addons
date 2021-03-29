90e4f3ed238ce319bbd9b0cfca6ffbae2b821885
Eric Antones <eantones@nuobit.com>
2021-03-29 18:15:39 +0200

    [REF] sale_order_task_autoassign_pack: pre-commit

--- sale_order_task_autoassign_pack/__manifest__.py
+++ sale_order_task_autoassign_pack/__manifest__.py
@@ -3,17 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Sale order task autoassign pack',
-    'version': '12.0.1.0.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Project',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module allows dealing with packs on "
-               "task autoassign module.",
-    'depends': [
-        'sale_order_task_autoassign',
-        'product_pack',
+    "name": "Sale order task autoassign pack",
+    "version": "12.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module allows dealing with packs on " "task autoassign module.",
+    "depends": [
+        "sale_order_task_autoassign",
+        "product_pack",
     ],
-    'installable': True,
+    "installable": True,
 }
