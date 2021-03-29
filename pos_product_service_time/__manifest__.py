74fc3959dbd76f2848601b05adf45fb8fe07d5fc
Eric Antones <eantones@nuobit.com>
2021-03-29 18:27:32 +0200

    [REF] pos_product_service_time: pre-commit

--- pos_product_service_time/__manifest__.py
+++ pos_product_service_time/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "POS product service time",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.3",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Point of Sale",
