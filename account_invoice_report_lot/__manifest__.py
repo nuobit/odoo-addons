55ba8fdc0e4d7046e2fc87509fcd4538d80f8af3
Eric Antones <eantones@nuobit.com>
2021-03-29 18:20:35 +0200

    [REF] account_invoice_report_lot: pre-commit

--- account_invoice_report_lot/__manifest__.py
+++ account_invoice_report_lot/__manifest__.py
@@ -3,18 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Invoice report lot',
-    'version': '12.0.1.0.1',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Invoicing Management',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module adds a lot/serial number on lines of invoice reports",
-    'depends': [
-        'stock_picking_invoice_link',
+    "name": "Invoice report lot",
+    "version": "12.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Invoicing Management",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module adds a lot/serial number on lines of invoice reports",
+    "depends": [
+        "stock_picking_invoice_link",
     ],
-    'data': [
-        'views/report_invoice.xml'
-    ],
-    'installable': True,
+    "data": ["views/report_invoice.xml"],
+    "installable": True,
 }
