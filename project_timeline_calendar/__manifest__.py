7749dd1505da0c962c70b1d32f8990b44d872be9
Eric Antones <eantones@nuobit.com>
2021-03-29 18:18:05 +0200

    [REF] project_timeline_calendar: pre-commit

--- project_timeline_calendar/__manifest__.py
+++ project_timeline_calendar/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Project timeline calendar',
-    'version': '12.0.2.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Project',
-    'website': 'https://github.com/nuobit',
-    'summary': "This module adds a new calendar view on project tasks and places "
-               "the tasks from the start and end date point of view, allowing to "
-               "move them and change its duration.",
-    'depends': [
-        'project',
+    "name": "Project timeline calendar",
+    "version": "12.0.2.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Project",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module adds a new calendar view on project tasks and places "
+    "the tasks from the start and end date point of view, allowing to "
+    "move them and change its duration.",
+    "depends": [
+        "project",
     ],
-    'data': [
-        'views/project_task_views.xml',
+    "data": [
+        "views/project_task_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- project_timeline_calendar/models/project.py
+++ project_timeline_calendar/models/project.py
@@ -2,24 +2,30 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import _, api, models
 
 
 class Task(models.Model):
     _inherit = "project.task"
 
     @api.multi
-    @api.constrains('date_start', 'date_start', 'user_id', 'project_id')
+    @api.constrains("date_start", "date_start", "user_id", "project_id")
     def check_ovelap(self):
         for task in self:
-            overlaped_tasks = self.search([
-                ('id', '!=', task.id),
-                ('project_id', '=', task.project_id.id),
-                ('user_id', '=', task.user_id.id),
-                ('date_start', '!=', False),
-                ('date_end', '!=', False),
-                ('date_end', '>', task.date_start),
-                ('date_start', '<', task.date_end),
-            ], limit=1)
+            overlaped_tasks = self.search(
+                [
+                    ("id", "!=", task.id),
+                    ("project_id", "=", task.project_id.id),
+                    ("user_id", "=", task.user_id.id),
+                    ("date_start", "!=", False),
+                    ("date_end", "!=", False),
+                    ("date_end", ">", task.date_start),
+                    ("date_start", "<", task.date_end),
+                ],
+                limit=1,
+            )
             if overlaped_tasks:
-                raise ValueError(_("The task would be overlaped with the task %s") % overlaped_tasks[0].name)
+                raise ValueError(
+                    _("The task would be overlaped with the task %s")
+                    % overlaped_tasks[0].name
+                )
