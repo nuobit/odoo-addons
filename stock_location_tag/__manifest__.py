0b700ab9f0b7692c667de5b96d79c2a4ab7cf31b
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-08 14:57:44 +0530

    [IMP] stock_location_tag: black, isort, prettier

--- stock_location_tag/__manifest__.py
+++ stock_location_tag/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock location tag",
-    'summary': "This module adds tags on location",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Warehouse',
-    'version': '11.0.0.1.1',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock location tag",
+    "summary": "This module adds tags on location",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Warehouse",
+    "version": "11.0.0.1.1",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'security/stock_location_tag_security.xml',
-        'security/ir.model.access.csv',
-        'views/stock_location_views.xml',
-        'views/stock_location_tag_views.xml',
+    "data": [
+        "security/stock_location_tag_security.xml",
+        "security/ir.model.access.csv",
+        "views/stock_location_views.xml",
+        "views/stock_location_tag_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
