c41fc5d16406729294869e5649321d2e9f7b6ae3
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-08 15:08:06 +0530

    [MIG] stock_location_tag: Migration to 12.0

--- stock_location_tag/__manifest__.py
+++ stock_location_tag/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": "This module adds tags on location",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "category": "Warehouse",
-    "version": "11.0.0.1.1",
+    "version": "12.0.0.1.1",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/pms",
     "depends": [
