2d906261ca83d17611627e7ff9f11ecfc0cdda92
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-15 14:50:58 +0530

    [IMP] partner_name_vat: black, isort, prettier

--- partner_name_vat/hooks.py
+++ partner_name_vat/hooks.py
@@ -2,13 +2,12 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api
-from odoo import SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook_vat_update(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env['res.partner'].search([])._compute_display_name()
+    env["res.partner"].search([])._compute_display_name()
 
 
 def uninstall_hook_vat_remove(cr, registry):
