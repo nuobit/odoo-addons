294a0b2100f67c0544a15beae85a70e8db7edc25
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-15 14:50:58 +0530

    [IMP] partner_name_vat: black, isort, prettier

--- partner_name_vat/__manifest__.py
+++ partner_name_vat/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Partner name with vat',
-    'summary': 'This module concatenates vat number to partner name on display_name',
-    'version': '11.0.0.1.0',
-    'category': 'Web',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
+    "name": "Partner name with vat",
+    "summary": "This module concatenates vat number to partner name on display_name",
+    "version": "11.0.0.1.0",
+    "category": "Web",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
     ],
-    'post_init_hook': 'post_init_hook_vat_update',
-    'uninstall_hook': 'uninstall_hook_vat_remove',
-    'installable': True,
-    'auto_install': False,
+    "post_init_hook": "post_init_hook_vat_update",
+    "uninstall_hook": "uninstall_hook_vat_remove",
+    "installable": True,
+    "auto_install": False,
 }
