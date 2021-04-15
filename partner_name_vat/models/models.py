ba7b6038a40cf047b11ae87d1ab6160ed487e3e0
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-15 14:55:38 +0530

    [MIG] partner_name_vat: Migration to 12.0

--- partner_name_vat/models/models.py
+++ partner_name_vat/models/models.py
@@ -8,7 +8,6 @@ from odoo import api, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    @api.multi
     def name_get(self):
         result = []
         orig_name = dict(super().name_get())
