129792750d77463932360f4d48c4a3c9d2beb015
Eric Antones <eantones@nuobit.com>
2021-04-18 18:25:18 +0200

    [ADD] sale_order_line_page_size: new module

---
+++ sale_order_line_page_size/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale order line page size",
+    "version": "12.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module sets the page size of the " "order/quotation lines to 200",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
