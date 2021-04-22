663a6cf492d65bf7f0b7292d08f4d92f8b1f35ef
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-22 22:34:15 +0530

    [IMP] partner_default_journal: black, isort, prettier

--- partner_default_journal/models/res_partner.py
+++ partner_default_journal/models/res_partner.py
@@ -2,13 +2,15 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    sale_journal_id = fields.Many2one('account.journal', 'Default journal',
-                                      domain=[('type', '=', 'sale')])
-    purchase_journal_id = fields.Many2one('account.journal', 'Default journal',
-                                          domain=[('type', '=', 'purchase')])
+    sale_journal_id = fields.Many2one(
+        "account.journal", "Default journal", domain=[("type", "=", "sale")]
+    )
+    purchase_journal_id = fields.Many2one(
+        "account.journal", "Default journal", domain=[("type", "=", "purchase")]
+    )
