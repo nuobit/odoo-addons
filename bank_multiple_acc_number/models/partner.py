b3b423111e4b5640eb70fb2801d9d624ea14669a
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-22 22:30:14 +0530

    [IMP] bank_multiple_acc_number: black, isort, prettier

--- bank_multiple_acc_number/models/partner.py
+++ bank_multiple_acc_number/models/partner.py
@@ -2,23 +2,33 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
-from odoo.tools.translate import _
+from odoo import api, models
 from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    @api.constrains('vat')
+    @api.constrains("vat")
     def check_acc_number_vat(self):
         for rec in self:
-            partner_bank = self.env['res.partner.bank'].search_count([
-                ('company_id', 'in', rec.bank_ids.mapped('company_id.id')),
-                ('sanitized_acc_number', 'in', rec.bank_ids.mapped('sanitized_acc_number')),
-                ('id', 'not in', rec.bank_ids.mapped('id')),
-                ('partner_id.vat', '!=', rec.vat),
-            ])
+            partner_bank = self.env["res.partner.bank"].search_count(
+                [
+                    ("company_id", "in", rec.bank_ids.mapped("company_id.id")),
+                    (
+                        "sanitized_acc_number",
+                        "in",
+                        rec.bank_ids.mapped("sanitized_acc_number"),
+                    ),
+                    ("id", "not in", rec.bank_ids.mapped("id")),
+                    ("partner_id.vat", "!=", rec.vat),
+                ]
+            )
             if partner_bank:
-                raise ValidationError(_("If you change the VAT number, you can no longer use the bank "
-                                        "account number of another partner with the old VAT"))
+                raise ValidationError(
+                    _(
+                        "If you change the VAT number, you can no longer use the bank "
+                        "account number of another partner with the old VAT"
+                    )
+                )
--- bank_multiple_acc_number/models/partner_bank.py
+++ bank_multiple_acc_number/models/partner_bank.py
@@ -2,27 +2,36 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
-from odoo.tools.translate import _
+from odoo import api, models
 from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class ResPartnerBank(models.Model):
-    _inherit = 'res.partner.bank'
+    _inherit = "res.partner.bank"
 
-    @api.constrains('acc_number', 'partner_id', 'partner_id')
+    @api.constrains("acc_number", "partner_id", "partner_id")
     def check_acc_number_vat(self):
         for rec in self:
-            partner_bank = self.env['res.partner.bank'].search_count([
-                ('company_id', '=', rec.company_id.id),
-                ('sanitized_acc_number', '=', rec.sanitized_acc_number),
-                ('id', '!=', rec.id),
-                ('partner_id.vat', '!=', rec.partner_id.vat),
-            ])
+            partner_bank = self.env["res.partner.bank"].search_count(
+                [
+                    ("company_id", "=", rec.company_id.id),
+                    ("sanitized_acc_number", "=", rec.sanitized_acc_number),
+                    ("id", "!=", rec.id),
+                    ("partner_id.vat", "!=", rec.partner_id.vat),
+                ]
+            )
             if partner_bank:
-                raise ValidationError(_("Account number must be unique for partners with a different VAT number"))
+                raise ValidationError(
+                    _(
+                        "Account number must be unique for partners with a different VAT number"
+                    )
+                )
 
     _sql_constraints = [
-        ('unique_number', 'unique(sanitized_acc_number, partner_id, company_id)',
-         'Account Number must be unique per partner'),
+        (
+            "unique_number",
+            "unique(sanitized_acc_number, partner_id, company_id)",
+            "Account Number must be unique per partner",
+        ),
     ]
