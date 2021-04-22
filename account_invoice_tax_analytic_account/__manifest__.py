1242b9d05041c83af34c1d1699dd95922f542b2c
Vraja Technologies <vrajatechnologies@gmail.com>
2021-04-22 22:22:43 +0530

    [IMP] account_invoice_tax_analytic_account: black, isort, prettier

--- account_invoice_tax_analytic_account/__manifest__.py
+++ account_invoice_tax_analytic_account/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Account tax invoice analytic',
-    'description': 'This module allows to add analytic account manually on invoice taxes.',
-    'version': '11.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'depends': ['account'],
-    'data': [
-        'views/account_invoice_view.xml',
+    "name": "Account tax invoice analytic",
+    "description": "This module allows to add analytic account manually on invoice taxes.",
+    "version": "11.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
