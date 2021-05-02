e2c7672885c15cd00e32d300cede107d143c0c4c
Eric Antones <eantones@nuobit.com>
2021-05-02 18:15:09 +0200

    [ADD] l10n_es_facturae_service: new module

---
+++ l10n_es_facturae_service/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'FacturaE service info',
+    'summary': 'This module propagates the service info of the '
+               'sale order to the invoice line facturae data.',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    'website': 'https://github.com/nuobit',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_es_facturae',
+        'sale_order_service',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'development_status': 'Beta',
+    'maintainers': ['eantones'],
+}
---
+++ l10n_es_facturae_service/models/sale_order.py
@@ -0,0 +1,35 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError, UserError
+from odoo.tools.translate import _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_invoice_create(self, grouped=False, final=False):
+        invoice_ids = super(SaleOrder, self).action_invoice_create(grouped=grouped, final=final)
+        for invoice in self.env['account.invoice'].browse(invoice_ids):
+            if invoice.facturae:
+                for line in invoice.invoice_line_ids:
+                    order = line.sale_line_ids.mapped('order_id')
+                    if order:
+                        if len(order) > 1:
+                            raise UserError(
+                                _("The invoice line with id '%i' references more than one order") % invoice.id)
+                        values = {}
+                        if order.insured_ident_cardnum:
+                            values['facturae_file_reference'] = order.insured_ident_cardnum
+                        if order.policy_number:
+                            values['facturae_receiver_transaction_reference'] = order.policy_number
+                        if order.auth_number:
+                            values['facturae_receiver_contract_reference'] = order.auth_number
+                        if order.service_date:
+                            values['facturae_receiver_transaction_date'] = order.service_date
+                        if values:
+                            line.write(values)
+        return invoice_ids
