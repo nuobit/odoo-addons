7f596cb0e39875901a695cae8c89b70801c63144
Eric Antones <eantones@nuobit.com>
2021-05-04 17:07:42 +0200

    [ADD] account_project_task_state: new module

---
+++ account_project_task_state/models/account_invoice.py
@@ -0,0 +1,72 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+import datetime
+import logging
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.multi
+    def write(self, vals):
+        meta_type = {
+            ("draft", "open"): "done",
+            ("open", "paid"): "parking",
+        }.get((self.state, vals.get("state")))
+        if meta_type:
+            for rec in self:
+                for order in rec.mapped("invoice_line_ids.sale_line_ids.order_id"):
+                    for task in order.tasks_ids:
+                        task_type = self.env["project.task.type"].search(
+                            [
+                                ("project_ids", "in", task.project_id.ids),
+                                ("meta_type", "=", meta_type),
+                            ]
+                        )
+                        if task_type:
+                            if len(task_type) > 1:
+                                raise UserError(
+                                    _(
+                                        "The project '%s' has defined "
+                                        "more than one stage %s of type '%s'"
+                                    )
+                                    % (
+                                        task.project_id.name,
+                                        task_type.mapped("name"),
+                                        meta_type,
+                                    )
+                                )
+                            task.stage_id = task_type.id
+        return super(AccountInvoice, self).write(vals)
+
+    @api.model
+    def archive_paid_invoices(self, days=10):
+        invoices = self.env["account.invoice"].search(
+            [
+                ("state", "=", "paid"),
+            ]
+        )
+        _logger.info(
+            "Archiving tasks of %i invoices paid %i days ago..." % (len(invoices), days)
+        )
+        archived = 0
+        for invoice in invoices:
+            last_payment_date = datetime.date(1900, 1, 1)
+            payment_ids = invoice.payment_ids.sorted("payment_date")
+            if payment_ids:
+                last_payment_date = payment_ids[-1].payment_date
+            for order in invoice.mapped("invoice_line_ids.sale_line_ids.order_id"):
+                for task in order.tasks_ids.filtered("active"):
+                    if (datetime.date.today() - last_payment_date).days >= days:
+                        task.toggle_active()
+                        archived += 1
+        _logger.info(
+            "Archived %i tasks of %i invoices paid %i days ago..."
+            % (archived, len(invoices), days)
+        )
---
+++ account_project_task_state/models/project_task_type.py
@@ -0,0 +1,32 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class ProjectTaskType(models.Model):
+    _inherit = "project.task.type"
+
+    meta_type = fields.Selection(selection=[("done", "Done"), ("parking", "Parking")])
+
+    @api.constrains("project_ids", "meta_type")
+    def _check_meta_type_duplicated(self):
+        for rec in self:
+            if rec.meta_type:
+                other = self.env["project.task.type"].search(
+                    [
+                        ("id", "!=", rec.id),
+                        ("project_ids", "in", rec.project_ids.ids),
+                        ("meta_type", "=", rec.meta_type),
+                    ]
+                )
+                if other:
+                    raise ValidationError(
+                        _(
+                            "There's other stage(s) %s with the same "
+                            "type '%s' on the same project(s)"
+                        )
+                        % (other.mapped("name"), rec.meta_type)
+                    )
