79e40a46310e71c57c9a3c52eda7a7b0e4e5f2c1
Eric Antones <eantones@nuobit.com>
2021-05-04 17:07:42 +0200

    [ADD] account_project_task_state: new module

---
+++ account_project_task_state/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Project Task State",
+    "version": "12.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module links the invoice and payment state "
+    "to related task state.",
+    "depends": [
+        "project",
+        "sale",
+        "account",
+    ],
+    "data": [
+        "data/account_project_task_state_cron.xml",
+        "views/project_task_type_views.xml",
+    ],
+    "installable": True,
+}
