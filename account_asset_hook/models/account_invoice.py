0e77ea92a9bbcb48b939de9de3b08c029cb9fcc0
Eric Antones <eantones@nuobit.com>
2021-05-06 22:00:02 +0200

    [IMP] account_asset_hook: Added _get_asset_date hook

--- account_asset_hook/models/account_invoice.py
+++ account_asset_hook/models/account_invoice.py
@@ -1,8 +1,11 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
-from odoo import api, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from odoo import api, models, _
+from odoo.exceptions import UserError
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 
 
 class AccountInvoiceLine(models.Model):
@@ -33,3 +36,27 @@ class AccountInvoiceLine(models.Model):
             if self.asset_category_id.open_asset:
                 asset.validate()
         return True
+
+    def _check_null_depreciations(self, cat):
+        if cat.method_number == 0 or cat.method_period == 0:
+            raise UserError(
+                _('The number of depreciations or the period length of your asset category cannot be null.'))
+        return True
+
+    @api.one
+    @api.depends('asset_category_id', 'invoice_id.date_invoice')
+    def _get_asset_date(self):
+        self.asset_mrr = 0
+        self.asset_start_date = False
+        self.asset_end_date = False
+        cat = self.asset_category_id
+        if cat:
+            self._check_null_depreciations(cat)
+            months = cat.method_number * cat.method_period
+            if self.invoice_id.type in ['out_invoice', 'out_refund']:
+                self.asset_mrr = self.price_subtotal_signed / months
+            if self.invoice_id.date_invoice:
+                start_date = datetime.strptime(self.invoice_id.date_invoice, DF).replace(day=1)
+                end_date = (start_date + relativedelta(months=months, days=-1))
+                self.asset_start_date = start_date.strftime(DF)
+                self.asset_end_date = end_date.strftime(DF)
