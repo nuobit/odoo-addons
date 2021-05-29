269a33b1f07bd8251f6a90116443dfb90cc57880
Eric Antones <eantones@nuobit.com>
2021-05-29 13:48:17 +0200

    [ADD] sale_report_product_type: new module

---
+++ sale_report_product_type/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Report product Type",
+    "summary": "This module adds a product_type field to "
+    "'Sales -> Reporting -> Sale' in order to "
+    "be able to filter and group for this field.",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/OCA/pms",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_report_views.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
+}
