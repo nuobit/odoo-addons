46106a06d6714189b277a5d71bdcde10a125af4f
Eric Antones <eantones@nuobit.com>
2021-07-03 15:44:29 +0200

    [FIX] account_asset_product_item: asset base value without non deductible part on invoices with accounting date

--- account_asset_product_item/__manifest__.py
+++ account_asset_product_item/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Assets by product item',
     'summary': 'This module allows to create as many '
                'assets as the quantity on the invoice line.',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'Accounting',
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'website': 'https://github.com/nuobit',
