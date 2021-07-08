02d054388d6e3d7011451643d16a73b96ffb6fba
Eric Antones <eantones@nuobit.com>
2021-07-08 14:12:07 +0200

    [MIG] partner_default_journal: Migration to 12.0

--- partner_default_journal/models/sale.py
+++ partner_default_journal/models/sale.py
@@ -2,7 +2,7 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
