526178768af03906e26ddab2f7a61137d07abddc
Eric Antones <eantones@nuobit.com>
2021-07-26 23:05:59 +0200

    [ADD] sale_order_bike_location: module to add bike location field on sale order

---
+++ sale_order_bike_location/models/sale_order.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    bike_location = fields.Selection(
+        selection=[
+            ("bring_in", "Bring In"),
+            ("in_shop", "In Shop"),
+            ("na", "Not Applicable"),
+        ],
+        required=True,
+        default="na",
+    )
