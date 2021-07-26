9290a215a6d40fb7596c1043e78cc0bb3c1a517e
Eric Antones <eantones@nuobit.com>
2021-07-26 23:05:59 +0200

    [ADD] sale_order_bike_location: module to add bike location field on sale order

---
+++ sale_order_bike_location/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale order bike location",
+    "version": "12.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/OCA/pms",
+    "summary": "This module adds a Bike Location on sale orders",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
