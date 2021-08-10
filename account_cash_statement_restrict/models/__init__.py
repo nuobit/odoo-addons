6f5bb580b57d84d9c7c7741d883932b9a5a8213f
mreficent <miquel.raich@eficent.com>
2021-08-10 13:42:59 +0200

    [MIG] account_cash_statement_restrict: Migration to 14.0

--- account_cash_statement_restrict/models/__init__.py
+++ account_cash_statement_restrict/models/__init__.py
@@ -1,6 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-# from . import account_bank_statement
+from . import account_bank_statement
 from . import res_users
