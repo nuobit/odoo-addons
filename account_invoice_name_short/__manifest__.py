8bf9245c8e88c0472c31aee371df68c2963b4218
mreficent <miquel.raich@eficent.com>
2021-08-10 16:36:16 +0200

    [IMP] account_invoice_name_short: black, isort, prettier

--- account_invoice_name_short/__manifest__.py
+++ account_invoice_name_short/__manifest__.py
@@ -3,18 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Invoices with name and origin short",
-    'summary': "If name or origin of an invoice are too long, "
-               "this module tries to short them",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Sales',
-    'version': '11.0.0.1.1',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'account',
-    ],
-    'data': [
-    ],
-    'installable': True,
+    "name": "Invoices with name and origin short",
+    "summary": "If name or origin of an invoice are too long, "
+    "this module tries to short them",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Sales",
+    "version": "11.0.0.1.1",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
 }
