4fe301196eb174ce804822a36b7637b239ff1efa
mreficent <miquel.raich@eficent.com>
2021-08-10 13:42:59 +0200

    [MIG] account_cash_statement_restrict: Migration to 14.0

--- account_cash_statement_restrict/models/account_bank_statement.py
+++ account_cash_statement_restrict/models/account_bank_statement.py
@@ -2,17 +2,22 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
     state = fields.Selection(
-        selection=[("open", "New"), ("close", "Closed"), ("confirm", "Validated")]
+        selection_add=[
+            ("close", "Closed"),
+        ],
+        ondelete={"close": lambda recs: recs.write({"state": "open"})},
     )
 
-    @api.multi
-    def close_bank(self, vals):
-        for rec in self:
-            rec.state = "close"
+    def close_bank(self):
+        self.write(
+            {
+                "state": "close",
+            }
+        )
