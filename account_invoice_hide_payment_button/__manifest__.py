48246e11dfadbac9d6299327bfec67fd450161e6
mreficent <miquel.raich@eficent.com>
2021-08-10 14:39:54 +0200

    [IMP] account_invoice_hide_payment_button: black, isort, prettier

--- account_invoice_hide_payment_button/__manifest__.py
+++ account_invoice_hide_payment_button/__manifest__.py
@@ -3,21 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Invoice hide payment button',
-    'summary': 'This module hides payment button on invoices',
-    'version': '11.0.0.1.0',
-    'category': 'Accounting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'views/account_invoice_view.xml'
-    ],
-    'qweb': [
-    ],
-    'installable': True,
-    'auto_install': False,
+    "name": "Invoice hide payment button",
+    "summary": "This module hides payment button on invoices",
+    "version": "11.0.0.1.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/account_invoice_view.xml"],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
 }
