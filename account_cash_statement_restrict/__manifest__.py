f1953aa90b6d0ad5dcaf92c0d602e35b5f972d5b
mreficent <miquel.raich@eficent.com>
2021-08-10 12:46:43 +0200

    [IMP] account_cash_statement_restrict: black, isort, prettier

--- account_cash_statement_restrict/__manifest__.py
+++ account_cash_statement_restrict/__manifest__.py
@@ -3,23 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Cash statement restrict",
-    'version': '11.0.0.1.2',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': [
-        'base',
-        'account'
+    "name": "Cash statement restrict",
+    "version": "11.0.0.1.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["base", "account"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/account_journal_dashboard_views.xml",
+        # 'views/account_views.xml',
+        "views/res_users_views.xml",
+        "views/menu.xml",
     ],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/account_journal_dashboard_views.xml',
-        #'views/account_views.xml',
-        'views/res_users_views.xml',
-        'views/menu.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
