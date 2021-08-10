1d7b162632f507bf23d3b94b3cf1e70f5c497766
mreficent <miquel.raich@eficent.com>
2021-08-10 11:47:44 +0200

    [IMP] account_bank_statement_balance_start_protection: black, isort, prettier

--- account_bank_statement_balance_start_protection/__manifest__.py
+++ account_bank_statement_balance_start_protection/__manifest__.py
@@ -3,16 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Cash statement balance start protection",
-    'description': "Cash statement balance start protection",
-    'version': '11.0.0.1.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Custom',
-    'website': 'https://www.nuobit.com',
-    'depends': ['account'],
-    'data': [
-        'views/account_views.xml'
-    ],
-    'installable': True,
+    "name": "Cash statement balance start protection",
+    "description": "Cash statement balance start protection",
+    "version": "11.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account"],
+    "data": ["views/account_views.xml"],
+    "installable": True,
 }
--- account_bank_statement_balance_start_protection/models/account_bank_statement.py
+++ account_bank_statement_balance_start_protection/models/account_bank_statement.py
@@ -2,22 +2,22 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
-    disable_balance_start = fields.Boolean() #compute="_disable_balance_start")
+    disable_balance_start = fields.Boolean()  # compute="_disable_balance_start")
 
     @api.model
     def create(self, vals):
-        vals.update({'disable_balance_start': True})
+        vals.update({"disable_balance_start": True})
         result = super().create(vals)
         return result
 
     @api.multi
     def write(self, vals):
-        vals.update({'disable_balance_start': True})
+        vals.update({"disable_balance_start": True})
         result = super().write(vals)
         return result
