b89af35020948cf9e3240e438809925f5fb7a862
mreficent <miquel.raich@eficent.com>
2021-08-10 15:19:13 +0200

    [IMP] account_invoice_mass_email: black, isort, prettier

--- account_invoice_mass_email/__manifest__.py
+++ account_invoice_mass_email/__manifest__.py
@@ -3,18 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Mass Email Invoices',
-    'description': 'Sending multiple invoices by email.',
-    'category': 'Accounting',
-    'version': '11.0.0.1.0',
-    'website': 'https://www.nuobit.com/',
-    'license': 'AGPL-3',
+    "name": "Mass Email Invoices",
+    "description": "Sending multiple invoices by email.",
+    "category": "Accounting",
+    "version": "11.0.0.1.0",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    'depends': [
-        'account',
-    ],
-    'data': [
-        "views/mass_email_view.xml"
-    ],
-    'installable': True,
+    "depends": ["account"],
+    "data": ["views/mass_email_view.xml"],
+    "installable": True,
 }
