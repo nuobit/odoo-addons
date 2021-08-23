0d3fabd151551857dc7da905d0ffc1aae0ab2e11
mreficent <miquel.raich@eficent.com>
2021-08-23 14:48:00 +0200

    [IMP] sale_order_service: black, isort, prettier

--- sale_order_service/__manifest__.py
+++ sale_order_service/__manifest__.py
@@ -3,19 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Sale order Service',
-    'summary': 'This module adds Service specific data to sale orders',
-    'version': '11.0.0.3.1',
-    'category': 'Sales',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale',
+    "name": "Sale order Service",
+    "summary": "This module adds Service specific data to sale orders",
+    "version": "11.0.0.3.1",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
     ],
-    'data': [
-        'views/sale_order_views.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
