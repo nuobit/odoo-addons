90ef2422d73110e5540c2026c15dc6e3dc839c06
mreficent <miquel.raich@eficent.com>
2021-08-23 15:09:58 +0200

    [IMP] account_invoice_sent_flag: black, isort, prettier

--- account_invoice_sent_flag/__manifest__.py
+++ account_invoice_sent_flag/__manifest__.py
@@ -3,18 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Account invoice sent flag',
-    'description': 'This module shows the "sent" flag on invoices and adds a filter',
-    'category': 'Accounting',
-    'version': '11.0.1.0.0',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    "author": 'NuoBiT Solutions, S.L., Eric Antones',
-    'depends': [
-        'account',
+    "name": "Account invoice sent flag",
+    "description": 'This module shows the "sent" flag on invoices and adds a filter',
+    "category": "Accounting",
+    "version": "11.0.1.0.0",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_views.xml",
     ],
-    'data': [
-        'views/account_invoice_views.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
