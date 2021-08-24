53f0d70134c90b2b18e4148e26ac5cea590e35c5
mreficent <miquel.raich@eficent.com>
2021-08-24 15:44:53 +0200

    [MIG] partner_default_journal: Migration to 14.0

--- partner_default_journal/models/account_invoice.py
+++ partner_default_journal/models/account_invoice.py
@@ -5,51 +5,46 @@
 from odoo import api, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     @api.model
     def create(self, vals):
-        if "journal_id" not in vals and "partner_id" in vals and "type" in vals:
+        if "journal_id" not in vals and "partner_id" in vals and "move_type" in vals:
             partner_id = self.env["res.partner"].browse(vals["partner_id"])
-            if partner_id.sale_journal_id and vals["type"] in (
+            if partner_id.sale_journal_id and vals["move_type"] in (
                 "out_invoice",
                 "out_refund",
+                "out_receipt",
             ):
                 vals["journal_id"] = partner_id.sale_journal_id.id
-            elif partner_id.purchase_journal_id and vals["type"] in (
+            elif partner_id.purchase_journal_id and vals["move_type"] in (
                 "in_invoice",
                 "in_refund",
+                "in_receipt",
             ):
                 vals["journal_id"] = partner_id.purchase_journal_id.id
-
-        invoice = super(AccountInvoice, self).create(vals)
-
+        invoice = super().create(vals)
         return invoice
 
     @api.onchange("partner_id", "company_id")
     def _onchange_partner_id(self):
-        res = super(AccountInvoice, self)._onchange_partner_id()
-
-        if self.partner_id.sale_journal_id and self.type in (
-            "out_invoice",
-            "out_refund",
+        res = super()._onchange_partner_id()
+        if self.partner_id.sale_journal_id and self.is_sale_document(
+            include_receipts=True
         ):
             self.journal_id = self.partner_id.sale_journal_id
-        elif self.partner_id.purchase_journal_id and self.type in (
-            "in_invoice",
-            "in_refund",
+        elif self.partner_id.purchase_journal_id and self.is_purchase_document(
+            include_receipts=True
         ):
             self.journal_id = self.partner_id.purchase_journal_id
-
         if (
             not self.partner_id.sale_journal_id
             and not self.partner_id.purchase_journal_id
         ):
             default_journal = self.with_context(
-                type=self.type, company_id=self.company_id.id
-            )._default_journal()
+                default_move_type=self.move_type, default_company_id=self.company_id.id
+            )._get_default_journal()
             if default_journal != self.journal_id:
                 self.journal_id = default_journal
-
         return res
