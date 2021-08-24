a5ae2cab5a1e7d5cbe98139b5b48a3fe27cf38dd
mreficent <miquel.raich@eficent.com>
2021-08-24 15:15:32 +0200

    [IMP] mrp_repair_employee: black, isort, prettier

--- mrp_repair_employee/__manifest__.py
+++ mrp_repair_employee/__manifest__.py
@@ -3,19 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'MRP Repair Employee',
-    'summary': 'This module adds a comment field in the contract and propagates it to the comment field of the invoice',
-    'version': '11.0.1.0.1',
-    'category': 'Contract Management',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'mrp_repair',
+    "name": "MRP Repair Employee",
+    "summary": "This module adds a comment field in the contract and "
+    "propagates it to the comment field of the invoice",
+    "version": "11.0.1.0.1",
+    "category": "Contract Management",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_repair",
     ],
-    'data': [
-        'views/mrp_repair_views.xml',
+    "data": [
+        "views/mrp_repair_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
--- mrp_repair_employee/models/mrp_repair.py
+++ mrp_repair_employee/models/mrp_repair.py
@@ -2,17 +2,15 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, fields
-from odoo.exceptions import ValidationError
-from odoo.tools.translate import _
+from odoo import fields, models
 
 
 class Repair(models.Model):
-    _inherit = 'mrp.repair'
+    _inherit = "mrp.repair"
 
     employee_id = fields.Many2one(
-        comodel_name='hr.employee',
+        comodel_name="hr.employee",
         required=True,
-        ondelete='restrict',
+        ondelete="restrict",
         domain="['|',('company_id', '=', False), ('company_id', '=', company_id)]",
     )
