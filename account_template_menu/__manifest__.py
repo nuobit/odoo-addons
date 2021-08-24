243b814a0cf4bcd20e8842a26a035b73f42c2d0b
mreficent <miquel.raich@eficent.com>
2021-08-24 09:51:42 +0200

    [IMP] account_template_menu: black, isort, prettier

--- account_template_menu/__manifest__.py
+++ account_template_menu/__manifest__.py
@@ -3,22 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Account Template Menu',
-    'summary': "This module adds menu entries *Account Chart Templates*, *Account Templates*, "
-               "*Account Tax Templates* and *Account Fiscal Pôsition Templates* under "
-               "*Accounting > Configuration > Accounting*",
-    'version': '11.0.1.0.1',
-    'category': 'Accounting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Account Template Menu",
+    "summary": "This module adds menu entries *Account Chart Templates*, *Account Templates*, "
+    "*Account Tax Templates* and *Account Fiscal Pôsition Templates* under "
+    "*Accounting > Configuration > Accounting*",
+    "version": "11.0.1.0.1",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_template_menuitem.xml",
     ],
-    'data': [
-        'views/account_template_menuitem.xml',
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
 }
