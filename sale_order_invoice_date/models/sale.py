16371a53de268afad926faa976e8acf92113540a
mreficent <miquel.raich@eficent.com>
2021-08-30 14:15:39 +0200

    [IMP] sale_order_invoice_date: black, isort, prettier

--- sale_order_invoice_date/models/sale.py
+++ sale_order_invoice_date/models/sale.py
@@ -2,7 +2,7 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
@@ -10,11 +10,13 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_invoice_create(self, grouped=False, final=False):
-        invoice_ids = super(SaleOrder, self).action_invoice_create(grouped=grouped, final=final)
+        invoice_ids = super(SaleOrder, self).action_invoice_create(
+            grouped=grouped, final=final
+        )
         if invoice_ids:
-            date_invoice = self.env.context.get('date_invoice')
+            date_invoice = self.env.context.get("date_invoice")
             if date_invoice:
-                invoices = self.env['account.invoice'].browse(invoice_ids)
-                invoices.write({'date_invoice': date_invoice})
+                invoices = self.env["account.invoice"].browse(invoice_ids)
+                invoices.write({"date_invoice": date_invoice})
 
         return invoice_ids
