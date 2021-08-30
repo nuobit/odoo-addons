7a8278bc4e57bc1837fcdf64af6b32cb4de5ff26
mreficent <miquel.raich@eficent.com>
2021-08-30 11:28:24 +0200

    [MIG] product_supplier_search: Migration to 14.0

--- product_supplier_search/__manifest__.py
+++ product_supplier_search/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Product supplier search",
     "summary": "Search products by supplier reference on "
     "selection fields and tree product view",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Purchases",
     "author": "NuoBiT Solutions,S.L.,Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
