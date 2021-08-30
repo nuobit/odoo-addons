cc8500586043a0205896963aef6c0479993f3d98
mreficent <miquel.raich@eficent.com>
2021-08-30 15:41:03 +0200

    [IMP] stock_location_localization_text: black, isort, prettier

--- stock_location_localization_text/__manifest__.py
+++ stock_location_localization_text/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Stock location localization text',
-    'summary': "This module allows to use non numeric values for localization "
-               "coordinates in stock locations",
-    'version': '11.0.0.1.0',
-    'category': 'Warehouse',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock',
+    "name": "Stock location localization text",
+    "summary": "This module allows to use non numeric values for localization "
+    "coordinates in stock locations",
+    "version": "11.0.0.1.0",
+    "category": "Warehouse",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
