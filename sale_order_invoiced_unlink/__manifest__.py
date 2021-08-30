72df93324665ef584db5b7b54d649a30ff63a67a
mreficent <miquel.raich@eficent.com>
2021-08-30 14:51:31 +0200

    [IMP] sale_order_invoiced_unlink: black, isort, prettier

--- sale_order_invoiced_unlink/__manifest__.py
+++ sale_order_invoiced_unlink/__manifest__.py
@@ -3,18 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Sale order invoiced unlink",
-    'summary': "If a sale order has any of its lines linked to an invoice "
-               "this module does not allow to delete the order.",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Sales',
-    'version': '11.0.0.1.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'sale',
+    "name": "Sale order invoiced unlink",
+    "summary": "If a sale order has any of its lines linked to an invoice "
+    "this module does not allow to delete the order.",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Sales",
+    "version": "11.0.0.1.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "sale",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
