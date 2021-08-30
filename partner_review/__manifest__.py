2260a53f905cc4024b65359fc4795e23b1eda97c
mreficent <miquel.raich@eficent.com>
2021-08-30 10:58:51 +0200

    [IMP] partner_review: black, isort, prettier

--- partner_review/__manifest__.py
+++ partner_review/__manifest__.py
@@ -3,18 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Partner review",
-    'version': '11.0.0.1.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Extra Tools',
-    'website': 'https://www.nuobit.com',
-    'depends': [
-        'base',
+    "name": "Partner review",
+    "version": "11.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_view.xml",
     ],
-    'data': [
-        'views/res_partner_view.xml',
-    ],
-    'installable': True,
-    'application': True,
+    "installable": True,
+    "application": True,
 }
