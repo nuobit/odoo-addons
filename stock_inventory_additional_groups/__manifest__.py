caf11b3e05ef382c4b4fd5f64043a738888cc6c8
mreficent <miquel.raich@eficent.com>
2021-08-30 15:32:29 +0200

    [MIG] stock_inventory_additional_groups: Migration to 14.0

--- stock_inventory_additional_groups/__manifest__.py
+++ stock_inventory_additional_groups/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": "This module adds adittional permission groups on inventory module",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "category": "Warehouse",
-    "version": "11.0.0.1.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
