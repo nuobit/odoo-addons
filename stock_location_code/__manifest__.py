bd16054bfa4c643289a49660f4c1dec05426fc0b
mreficent <miquel.raich@eficent.com>
2021-08-30 15:36:00 +0200

    [IMP] stock_location_code: black, isort, prettier

--- stock_location_code/__manifest__.py
+++ stock_location_code/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock location code",
-    'summary': "This module adds unique code per company on location",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Warehouse',
-    'version': '11.0.0.2.1',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock location code",
+    "summary": "This module adds unique code per company on location",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Warehouse",
+    "version": "11.0.0.2.1",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_location_views.xml',
+    "data": [
+        "views/stock_location_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
