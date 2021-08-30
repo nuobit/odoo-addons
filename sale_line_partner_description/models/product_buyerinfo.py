3b5829490e06c642699c19231171fc287dd17627
mreficent <miquel.raich@eficent.com>
2021-08-30 13:48:00 +0200

    [IMP] sale_line_partner_description: black, isort, prettier

--- sale_line_partner_description/models/product_buyerinfo.py
+++ sale_line_partner_description/models/product_buyerinfo.py
@@ -2,46 +2,61 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
 
 class ProductBuyerInfo(models.Model):
-    _name = 'product.buyerinfo'
+    _name = "product.buyerinfo"
 
-    partner_id = fields.Many2one(comodel_name='res.partner',
-                                 domain=[('customer', '=', True)], ondelete='cascade',
-                                 string='Customer', required=True)
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        domain=[("customer", "=", True)],
+        ondelete="cascade",
+        string="Customer",
+        required=True,
+    )
 
-    code = fields.Char(string='Product code')
+    code = fields.Char(string="Product code")
 
-    name = fields.Text(string='Product name', translate=True)
+    name = fields.Text(string="Product name", translate=True)
 
-    product_id = fields.Many2one(comodel_name='product.product', required=True, ondelete='cascade')
+    product_id = fields.Many2one(
+        comodel_name="product.product", required=True, ondelete="cascade"
+    )
 
     _sql_constraints = [
-        ('buyerinfo_uniq', 'unique(product_id, partner_id)', "Already exists this same line!"),
+        (
+            "buyerinfo_uniq",
+            "unique(product_id, partner_id)",
+            "Already exists this same line!",
+        ),
     ]
 
     @api.multi
     def name_get(self):
         vals = []
         for record in self:
-            key_name = '%s - %s' % (
+            key_name = "%s - %s" % (
                 record.partner_id.display_name,
                 record.product_id.display_name,
             )
-            data_name_l = ['[%s]' % record.code or '']
+            data_name_l = ["[%s]" % record.code or ""]
             if record.name:
                 data_name_l.append(record.name)
 
-            vals.append((record.id, '%s: %s' % (key_name, ' '.join(data_name_l))))
+            vals.append((record.id, "%s: %s" % (key_name, " ".join(data_name_l))))
 
         return vals
 
-    @api.constrains('code', 'name')
+    @api.constrains("code", "name")
     def _check_code_name(self):
         for rec in self:
             if not rec.code and not rec.name:
-                raise ValidationError(_("If you're not gonna define code and name, you better remove the entire line"))
+                raise ValidationError(
+                    _(
+                        "If you're not gonna define code and name, "
+                        "you better remove the entire line"
+                    )
+                )
