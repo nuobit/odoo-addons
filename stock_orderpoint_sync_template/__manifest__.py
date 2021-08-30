078448d03952869bd75697bcede881c1024941c9
mreficent <miquel.raich@eficent.com>
2021-08-30 17:06:21 +0200

    [IMP] stock_orderpoint_sync_template: black, isort, prettier

--- stock_orderpoint_sync_template/__manifest__.py
+++ stock_orderpoint_sync_template/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock orderpoint sync template",
-    'summary': "Allows create reordering rules for multiple products and different "
-               "parameters per product in one go and keep them synchronized with "
-               "the base template",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Warehouse',
-    'version': '11.0.0.1.1',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock orderpoint sync template",
+    "summary": "Allows create reordering rules for multiple products and different "
+    "parameters per product in one go and keep them synchronized with "
+    "the base template",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Warehouse",
+    "version": "11.0.0.1.1",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/orderpoint_views.xml',
-        'views/orderpoint_sync_template_views.xml',
+    "data": [
+        "views/orderpoint_views.xml",
+        "views/orderpoint_sync_template_views.xml",
         "security/orderpoint_sync_template_security.xml",
         "security/ir.model.access.csv",
     ],
-    'installable': True,
+    "installable": True,
 }
