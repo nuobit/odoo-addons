6b47c22fe66e202a7960eb1c916d707b41d0c8c2
mreficent <miquel.raich@eficent.com>
2021-08-30 14:15:39 +0200

    [IMP] sale_order_invoice_date: black, isort, prettier

--- sale_order_invoice_date/__manifest__.py
+++ sale_order_invoice_date/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Sale order invoice date",
-    'version': '11.0.1.0.0',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Accounting',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'sale',
+    "name": "Sale order invoice date",
+    "version": "11.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "sale",
     ],
-    'data': [
-        'wizard/sale_make_invoice_advance_views.xml',
+    "data": [
+        "wizard/sale_make_invoice_advance_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
