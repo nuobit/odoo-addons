7ebfd38b8c69d205e90cb4001ad7b0442d557c89
mreficent <miquel.raich@eficent.com>
2021-08-30 13:08:58 +0200

    [MIG] quality_partner: Migration to 14.0

--- quality_partner/models/quality_partner_document_type.py
+++ quality_partner/models/quality_partner_document_type.py
@@ -2,21 +2,20 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 from odoo.exceptions import UserError
 
 
 class QualityPartnerDocumentType(models.Model):
     _name = "quality.partner.document.type"
+    _description = "Quality Partner Document Type"
     _order = "sequence"
 
     name = fields.Char(string="Name", translate=True, required=True)
     code = fields.Char(string="Code")
-
     description = fields.Text(string="Description")
     sequence = fields.Integer(string="Sequence", required=True, default=1)
 
-    @api.multi
     def unlink(self):
         records = self.env["quality.partner.classification"].search(
             [("document_type_ids", "in", self.ids)]
@@ -32,5 +31,4 @@ class QualityPartnerDocumentType(models.Model):
             raise UserError(
                 _("You are trying to delete a record that is still referenced!")
             )
-
         return super(QualityPartnerDocumentType, self).unlink()
