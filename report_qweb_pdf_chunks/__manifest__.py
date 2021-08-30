d70e51dda41f7c900181ec296150651b189f9253
mreficent <miquel.raich@eficent.com>
2021-08-30 13:12:51 +0200

    [IMP] report_qweb_pdf_chunks: black, isort, prettier

--- report_qweb_pdf_chunks/__manifest__.py
+++ report_qweb_pdf_chunks/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Report qweb PDF chunks',
-    'summary': 'This module temporarily splits all selected documents to print into chunks '
-               'and invokes the external Wkhtmltopdf (0.12.5) program for each chunk to '
-               'avoid the well known memory problems when printing many documents.',
-    'version': '11.0.1.0.1',
-    'category': 'Reporting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
+    "name": "Report qweb PDF chunks",
+    "summary": "This module temporarily splits all selected documents to print into chunks "
+    "and invokes the external Wkhtmltopdf (0.12.5) program for each chunk to "
+    "avoid the well known memory problems when printing many documents.",
+    "version": "11.0.1.0.1",
+    "category": "Reporting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'data/ir_config_parameter.xml',
+    "data": [
+        "data/ir_config_parameter.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
--- report_qweb_pdf_chunks/models/ir_action_report.py
+++ report_qweb_pdf_chunks/models/ir_action_report.py
@@ -2,47 +2,58 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
-
-from math import ceil
-import logging
 import io
+import logging
+from math import ceil
+
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
 try:
-    from PyPDF2 import PdfFileReader, PdfFileMerger  # pylint: disable=W0404
+    from PyPDF2 import PdfFileMerger, PdfFileReader  # pylint: disable=W0404
 except ImportError:
-    _logger.debug('Can not import PyPDF2')
+    _logger.debug("Can not import PyPDF2")
 
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i : i + n]
 
 
 class Report(models.Model):
-    _inherit = 'ir.actions.report'
+    _inherit = "ir.actions.report"
 
     @api.multi
     def render_qweb_pdf(self, res_ids=None, data=None):
-        ir_config = self.env['ir.config_parameter'].sudo()
-        chunk_threshold = int(ir_config.get_param('qweb.report.pdf.chunk.threshold', 50))
-        chunk_size = int(ir_config.get_param('qweb.report.pdf.chunk.size', 50))
+        ir_config = self.env["ir.config_parameter"].sudo()
+        chunk_threshold = int(
+            ir_config.get_param("qweb.report.pdf.chunk.threshold", 50)
+        )
+        chunk_size = int(ir_config.get_param("qweb.report.pdf.chunk.size", 50))
 
-        if not res_ids or chunk_threshold < 0 or \
-                len(res_ids) <= chunk_threshold or len(res_ids) <= chunk_size:
-            pdf_content, _ = super(Report, self).render_qweb_pdf(res_ids=res_ids, data=data)
+        if (
+            not res_ids
+            or chunk_threshold < 0
+            or len(res_ids) <= chunk_threshold
+            or len(res_ids) <= chunk_size
+        ):
+            pdf_content, _ = super(Report, self).render_qweb_pdf(
+                res_ids=res_ids, data=data
+            )
         else:
             chunk_count = ceil(len(res_ids) / chunk_size)
-            _logger.info("Many documents to print, splitting in %i chunks of %i (max) "
-                         "documents each" % (chunk_count, chunk_size))
+            _logger.info(
+                "Many documents to print, splitting in %i chunks of %i (max) "
+                "documents each" % (chunk_count, chunk_size)
+            )
             pdf_merger = PdfFileMerger()
             for i, res_ids_chunk in enumerate(chunks(res_ids, chunk_size), 1):
                 _logger.info("Processing chunk %i of %i..." % (i, chunk_count))
-                pdf_content_chunk, _ = super(Report, self) \
-                    .render_qweb_pdf(res_ids=res_ids_chunk, data=data)
+                pdf_content_chunk, _ = super(Report, self).render_qweb_pdf(
+                    res_ids=res_ids_chunk, data=data
+                )
 
                 pdf_mem_file_chunk = io.BytesIO()
                 pdf_mem_file_chunk.write(pdf_content_chunk)
@@ -56,4 +67,4 @@ class Report(models.Model):
                 pdf_content = pdf_mem_file.getvalue()
                 pdf_mem_file.close()
 
-        return pdf_content, 'pdf'
+        return pdf_content, "pdf"
