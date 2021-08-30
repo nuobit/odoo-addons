8ed6dbb08b7eadd5bce594be818e96437e1bb53d
mreficent <miquel.raich@eficent.com>
2021-08-30 12:44:55 +0200

    [IMP] quality_partner: black, isort, prettier

--- quality_partner/models/quality_partner_document_type.py
+++ quality_partner/models/quality_partner_document_type.py
@@ -2,27 +2,35 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class QualityPartnerDocumentType(models.Model):
-    _name = 'quality.partner.document.type'
-    _order = 'sequence'
+    _name = "quality.partner.document.type"
+    _order = "sequence"
 
-    name = fields.Char(string='Name', translate=True, required=True)
-    code = fields.Char(string='Code')
+    name = fields.Char(string="Name", translate=True, required=True)
+    code = fields.Char(string="Code")
 
     description = fields.Text(string="Description")
     sequence = fields.Integer(string="Sequence", required=True, default=1)
 
     @api.multi
     def unlink(self):
-        records = self.env['quality.partner.classification'].search([('document_type_ids', 'in', self.ids)])
+        records = self.env["quality.partner.classification"].search(
+            [("document_type_ids", "in", self.ids)]
+        )
         if records:
-            raise UserError(_("You are trying to delete a record that is still referenced!"))
-        records = self.env['quality.partner.document'].search([('document_type_id', 'in', self.ids)])
+            raise UserError(
+                _("You are trying to delete a record that is still referenced!")
+            )
+        records = self.env["quality.partner.document"].search(
+            [("document_type_id", "in", self.ids)]
+        )
         if records:
-            raise UserError(_("You are trying to delete a record that is still referenced!"))
+            raise UserError(
+                _("You are trying to delete a record that is still referenced!")
+            )
 
         return super(QualityPartnerDocumentType, self).unlink()
