754fb16b5ef9f84b3c54796c8e2e5fa4c82ee1b3
mreficent <miquel.raich@eficent.com>
2021-08-30 15:36:00 +0200

    [IMP] stock_location_code: black, isort, prettier

--- stock_location_code/models/models.py
+++ stock_location_code/models/models.py
@@ -2,14 +2,18 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Location(models.Model):
     _inherit = "stock.location"
 
-    code = fields.Char(string='Code')
+    code = fields.Char(string="Code")
 
     _sql_constraints = [
-        ('location_code_uniq', 'unique(code, company_id)', "A code can only be assigned to one location per company!"),
+        (
+            "location_code_uniq",
+            "unique(code, company_id)",
+            "A code can only be assigned to one location per company!",
+        ),
     ]
