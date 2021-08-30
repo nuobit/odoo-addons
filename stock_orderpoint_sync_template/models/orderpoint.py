05b27ffcf956cd60e75c4e0ed13fc9bd57b358ce
mreficent <miquel.raich@eficent.com>
2021-08-30 17:06:21 +0200

    [IMP] stock_orderpoint_sync_template: black, isort, prettier

--- stock_orderpoint_sync_template/models/orderpoint.py
+++ stock_orderpoint_sync_template/models/orderpoint.py
@@ -1,11 +1,18 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Orderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    sync_template_line_id = fields.Many2one(comodel_name='stock.warehouse.orderpoint.sync.template.line',
-                                            string='Template line',
-                                            readonly=True, ondelete='restrict')
-    sync_template_id = fields.Many2one(related='sync_template_line_id.sync_template_id',
-                                       string='Template', readonly=True, store=True)
+    sync_template_line_id = fields.Many2one(
+        comodel_name="stock.warehouse.orderpoint.sync.template.line",
+        string="Template line",
+        readonly=True,
+        ondelete="restrict",
+    )
+    sync_template_id = fields.Many2one(
+        related="sync_template_line_id.sync_template_id",
+        string="Template",
+        readonly=True,
+        store=True,
+    )
