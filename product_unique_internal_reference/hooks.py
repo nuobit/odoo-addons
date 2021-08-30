e9ca60afc6f046a3cc84c8891520e9ad1aa94493
mreficent <miquel.raich@eficent.com>
2021-08-30 12:12:03 +0200

    [IMP] product_unique_internal_reference: black, isort, prettier

--- product_unique_internal_reference/hooks.py
+++ product_unique_internal_reference/hooks.py
@@ -18,16 +18,14 @@ def internal_reference_duplicate_check(cr):
             SELECT p0.id, p0.default_code, t0.company_id
             FROM product_product p0, product_template t0
             WHERE p0.product_tmpl_id = t0.id AND
-                    EXISTS (
-                        SELECT 1
-                        FROM product_product p1, product_template t1
-                        WHERE p1.product_tmpl_id = t1.id AND
-                              (t1.company_id is null OR t0.company_id is null OR
-                               t1.company_id = t0.company_id ) AND
-                              p1.default_code = p0.default_code AND
-                              p1.id != p0.id
-                    )
-            """
+                EXISTS (
+                    SELECT 1
+                    FROM product_product p1, product_template t1
+                    WHERE p1.product_tmpl_id = t1.id AND
+                        (t1.company_id is null OR t0.company_id is null OR
+                             t1.company_id = t0.company_id) AND
+                        p1.default_code = p0.default_code AND p1.id != p0.id
+                )"""
         )
         products = sorted(["[%i] %s (%i)" % p for p in cr.fetchall()])
         if products:
