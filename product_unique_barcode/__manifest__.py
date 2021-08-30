6c2b841cd28e3430ac6d201be268507fcffa2d01
mreficent <miquel.raich@eficent.com>
2021-08-30 11:57:54 +0200

    [IMP] product_unique_barcode: black, isort, prettier

--- product_unique_barcode/__manifest__.py
+++ product_unique_barcode/__manifest__.py
@@ -3,17 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Product unique barcode',
-    'summary': 'This module ensures that you enter a Unique Barcode for your Products',
-    'version': '11.0.0.2.1',
-    'category': 'Sales',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
+    "name": "Product unique barcode",
+    "summary": "This module ensures that you enter a Unique Barcode for your Products",
+    "version": "11.0.0.2.1",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
     ],
-    'pre_init_hook': 'pre_init_hook_barcode_check',
-    'installable': True,
-    'auto_install': False,
+    "pre_init_hook": "pre_init_hook_barcode_check",
+    "installable": True,
+    "auto_install": False,
 }
--- product_unique_barcode/hooks.py
+++ product_unique_barcode/hooks.py
@@ -7,27 +7,28 @@ from odoo.exceptions import ValidationError
 
 
 def pre_init_hook_barcode_check(cr):
-    """This hook will look to see if any conflicting internal references exist before
-    the module is installed
+    """This hook will look to see if any conflicting internal references exist
+    before the module is installed
     :param odoo.sql_db.Cursor cr:
         Database cursor.
     """
     with cr.savepoint():
-        cr.execute("""SELECT distinct t0.company_id, p0.barcode 
-                      FROM product_product p0, product_template t0
-                      WHERE p0.product_tmpl_id = t0.id AND
-                            EXISTS (
-                               SELECT 1
-                               FROM product_product p1, product_template t1
-                               where p1.product_tmpl_id = t1.id AND 
-                                     t1.company_id = t0.company_id and
-                                     p1.barcode = p0.barcode AND 
-                                     p1.id != p0.id
-                            )
-                      """)
+        cr.execute(
+            """
+            SELECT distinct t0.company_id, p0.barcode
+            FROM product_product p0, product_template t0
+            WHERE p0.product_tmpl_id = t0.id AND
+                EXISTS (
+                    SELECT 1
+                    FROM product_product p1, product_template t1
+                    WHERE p1.product_tmpl_id = t1.id AND
+                        t1.company_id = t0.company_id AND
+                        p1.barcode = p0.barcode AND p1.id != p0.id
+                )"""
+        )
 
-        products = sorted(['[%i] %s' % p for p in cr.fetchall()])
+        products = sorted(["[%i] %s" % p for p in cr.fetchall()])
         if products:
             raise ValidationError(
-                _('Conflicting barcodes exist: %s' % ', '.join(products))
+                _("Conflicting barcodes exist: %s" % ", ".join(products))
             )
