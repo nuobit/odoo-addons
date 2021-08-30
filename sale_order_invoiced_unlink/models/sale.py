548e3787b438da54fd458ac0246e7d1bd33a1cc2
mreficent <miquel.raich@eficent.com>
2021-08-30 14:53:24 +0200

    [MIG] sale_order_invoiced_unlink: Migration to 14.0

--- sale_order_invoiced_unlink/models/sale.py
+++ sale_order_invoiced_unlink/models/sale.py
@@ -2,14 +2,13 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.multi
     def unlink(self):
         if self.mapped("order_line.invoice_lines"):
             raise UserError(
