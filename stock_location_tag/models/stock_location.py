60f56883c1deed45346cf22faff4bd38ed067a3a
mreficent <miquel.raich@eficent.com>
2021-08-30 15:46:33 +0200

    [IMP] stock_location_tag: black, isort, prettier

--- stock_location_tag/models/stock_location.py
+++ stock_location_tag/models/stock_location.py
@@ -2,12 +2,16 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class StockLocation(models.Model):
     _inherit = "stock.location"
 
-    tag_ids = fields.Many2many(comodel_name='stock.location.tag', string='Tags',
-                               relation='stock_location_location_tag_rel',
-                               column1='location_id', column2='tag_id')
+    tag_ids = fields.Many2many(
+        comodel_name="stock.location.tag",
+        string="Tags",
+        relation="stock_location_location_tag_rel",
+        column1="location_id",
+        column2="tag_id",
+    )
