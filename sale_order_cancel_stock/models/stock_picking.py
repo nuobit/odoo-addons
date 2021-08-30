55a9818645aa3e94eefa475619cc22731e06924a
mreficent <miquel.raich@eficent.com>
2021-08-30 13:59:34 +0200

    [IMP] sale_order_cancel_stock: black, isort, prettier

--- sale_order_cancel_stock/models/stock_picking.py
+++ sale_order_cancel_stock/models/stock_picking.py
@@ -2,34 +2,34 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, _
-from odoo.exceptions import UserError
+from odoo import api, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     @api.multi
     def action_cancel(self):
-        if not self.env.context.get('from_order'):
+        if not self.env.context.get("from_order"):
             return super(StockPicking, self).action_cancel()
 
         qty_balances = {}
-        moves_lines_done = self.mapped('move_lines') \
-            .filtered(lambda x: x.state == 'done').mapped('move_line_ids')
+        moves_lines_done = (
+            self.mapped("move_lines")
+            .filtered(lambda x: x.state == "done")
+            .mapped("move_line_ids")
+        )
         for line in moves_lines_done:
             if line.qty_done:
                 lot_id = not line.lot_id and None or line.lot_id.id
                 key = (line.product_id.id, lot_id)
 
-                qty_balances.setdefault(line.location_id.id, {}) \
-                    .setdefault(key, 0)
+                qty_balances.setdefault(line.location_id.id, {}).setdefault(key, 0)
                 qty_balances[line.location_id.id][key] -= line.qty_done
                 if not qty_balances[line.location_id.id][key]:
                     del qty_balances[line.location_id.id][key]
 
-                qty_balances.setdefault(line.location_dest_id.id, {}) \
-                    .setdefault(key, 0)
+                qty_balances.setdefault(line.location_dest_id.id, {}).setdefault(key, 0)
                 qty_balances[line.location_dest_id.id][key] += line.qty_done
                 if not qty_balances[line.location_dest_id.id][key]:
                     del qty_balances[line.location_dest_id.id][key]
@@ -42,9 +42,8 @@ class StockPicking(models.Model):
         if qty_balances:
             return super(StockPicking, self).action_cancel()
 
-        self.mapped('move_lines') \
-            .filtered(lambda x: x.state != 'done')._action_cancel()
+        self.mapped("move_lines").filtered(lambda x: x.state != "done")._action_cancel()
 
-        self.write({'is_locked': True})
+        self.write({"is_locked": True})
 
         return True
