d2ee345347d38c4697a7b9d618fac6d51ef435b9
mreficent <miquel.raich@eficent.com>
2021-08-30 11:57:54 +0200

    [IMP] product_unique_barcode: black, isort, prettier

--- product_unique_barcode/models/models.py
+++ product_unique_barcode/models/models.py
@@ -2,31 +2,41 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api
-from odoo.tools.translate import _
+from odoo import api, models
 from odoo.exceptions import ValidationError
+from odoo.tools.translate import _
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
-    @api.constrains('barcode')
+    @api.constrains("barcode")
     def _check_barcode(self):
         for record in self:
             if record.barcode:
-                barcode = self.search([
-                    ('product_tmpl_id.company_id', '=', record.product_tmpl_id.company_id.id),
-                    ('barcode', '=', record.barcode),
-                    ('id', '!=', record.id),
-                ], limit=1)
+                barcode = self.search(
+                    [
+                        (
+                            "product_tmpl_id.company_id",
+                            "=",
+                            record.product_tmpl_id.company_id.id,
+                        ),
+                        ("barcode", "=", record.barcode),
+                        ("id", "!=", record.id),
+                    ],
+                    limit=1,
+                )
 
                 if barcode:
                     raise ValidationError(
-                        _("Error! The Barcode %s already exists" %
-                          record.barcode)
+                        _("Error! The Barcode %s already exists" % record.barcode)
                     )
 
     # replace current sql_constraint with a dummy one
     _sql_constraints = [
-        ('barcode_uniq', 'unique(barcode, id)', "A barcode can only be assigned to one product !"),
+        (
+            "barcode_uniq",
+            "unique(barcode, id)",
+            "A barcode can only be assigned to one product !",
+        ),
     ]
