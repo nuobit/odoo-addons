5dbadc89971354a8880ddbe079ce3f100b676134
mreficent <miquel.raich@eficent.com>
2021-08-30 13:59:34 +0200

    [IMP] sale_order_cancel_stock: black, isort, prettier

--- sale_order_cancel_stock/__manifest__.py
+++ sale_order_cancel_stock/__manifest__.py
@@ -3,18 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Sale order cancel stock",
-    'summary': "Allows to cancel a sale order with done stock moves "
-               "only if they cancel each other.",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Sales',
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'sale_stock',
+    "name": "Sale order cancel stock",
+    "summary": "Allows to cancel a sale order with done stock moves "
+    "only if they cancel each other.",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Sales",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "sale_stock",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
--- sale_order_cancel_stock/models/sale_order.py
+++ sale_order_cancel_stock/models/sale_order.py
@@ -3,12 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
-from odoo import api, models, _
-from odoo.exceptions import UserError
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def action_cancel(self):
