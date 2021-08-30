3ccc872e5c5f5fb2e5ce65b4f719b5e6ed3ccf4c
mreficent <miquel.raich@eficent.com>
2021-08-30 13:08:58 +0200

    [MIG] quality_partner: Migration to 14.0

--- quality_partner/models/quality_partner_classification.py
+++ quality_partner/models/quality_partner_classification.py
@@ -12,13 +12,14 @@ MAP_LEVELS = {
 }
 MAP_LEVELS_ORD = {v[0]: (k, v[1]) for k, v in MAP_LEVELS.items()}
 LEVELS = [
-    (k, trl) for k, (ord, trl) in sorted(MAP_LEVELS.items(), key=lambda x: x[1][0])
+    (k, trl) for k, (_ord, trl) in sorted(MAP_LEVELS.items(), key=lambda x: x[1][0])
 ]
 
 
 class QualityPartnerClassification(models.Model):
     _name = "quality.partner.classification"
     _inherit = "quality.tree.mixin"
+    _description = "Quality Partner Classification"
     _rec_name = "complete_name"
     _order = "sequence"
 
@@ -32,7 +33,7 @@ class QualityPartnerClassification(models.Model):
     parent_id = fields.Many2one(
         comodel_name="quality.partner.classification",
         string="Parent",
-        on_delete="restrict",
+        ondelete="restrict",
     )
 
     document_type_ids = fields.Many2many(
