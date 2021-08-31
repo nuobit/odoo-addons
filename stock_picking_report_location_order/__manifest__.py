3fe9f365af71f6d64994b435ce0442e6aa3843b0
mreficent <miquel.raich@eficent.com>
2021-08-31 14:11:15 +0200

    [IMP] stock_picking_report_location_order: black, isort, prettier

--- stock_picking_report_location_order/__manifest__.py
+++ stock_picking_report_location_order/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock picking report location order",
-    'description': "Sort report picking lines by location",
-    'version': '11.0.1.0.2',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Warehouse',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock picking report location order",
+    "description": "Sort report picking lines by location",
+    "version": "11.0.1.0.2",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Warehouse",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'report/report_stockpicking_operations.xml',
+    "data": [
+        "report/report_stockpicking_operations.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
