448bd5cd2fbb105f6116ef9c4502b57a355e360c
mreficent <miquel.raich@eficent.com>
2021-08-31 12:31:18 +0200

    [IMP] stock_picking_line_description: black, isort, prettier

--- stock_picking_line_description/__manifest__.py
+++ stock_picking_line_description/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock picking line description",
-    'summary': "Adds description on picking lines.",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Warehouse',
-    'version': '11.0.0.2.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock picking line description",
+    "summary": "Adds description on picking lines.",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Warehouse",
+    "version": "11.0.0.2.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_picking_views.xml',
+    "data": [
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
