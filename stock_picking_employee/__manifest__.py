4d11a9a6ad4867b1c5d0a7a5b1e2228b0049dadb
mreficent <miquel.raich@eficent.com>
2021-08-31 12:05:01 +0200

    [IMP] stock_picking_employee: black, isort, prettier

--- stock_picking_employee/__manifest__.py
+++ stock_picking_employee/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock picking employee",
-    'summary': "Adds employees on picking",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Warehouse',
-    'version': '11.0.0.1.1',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock picking employee",
+    "summary": "Adds employees on picking",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Warehouse",
+    "version": "11.0.0.1.1",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_picking_views.xml',
+    "data": [
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- stock_picking_employee/models/models.py
+++ stock_picking_employee/models/models.py
@@ -2,12 +2,16 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Location(models.Model):
     _inherit = "stock.picking"
 
-    employee_ids = fields.Many2many(comodel_name='hr.employee', relation='stock_picking_hr_employee_rel',
-                                    column1="picking_id", column2='employee_id',
-                                    string='Employees')
+    employee_ids = fields.Many2many(
+        comodel_name="hr.employee",
+        relation="stock_picking_hr_employee_rel",
+        column1="picking_id",
+        column2="employee_id",
+        string="Employees",
+    )
