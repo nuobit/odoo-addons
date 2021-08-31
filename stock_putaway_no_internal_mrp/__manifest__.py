8ae122562438742d997ee73c40b3d8a141fbfd41
mreficent <miquel.raich@eficent.com>
2021-08-31 15:34:42 +0200

    [IMP] stock_putaway_no_internal_mrp: black, isort, prettier

--- stock_putaway_no_internal_mrp/__manifest__.py
+++ stock_putaway_no_internal_mrp/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Putaway strategy no internal for MRP',
-    'summary': "Allow to exclude putaway strategies if they are applied on an manufacturing operations",
-    'version': '11.0.1.0.0',
-    'category': 'Warehouse Management',
-    'license': 'AGPL-3',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'mrp',
-        'stock_putaway_no_internal',
+    "name": "Putaway strategy no internal for MRP",
+    "summary": "Allow to exclude putaway strategies "
+    "if they are applied on an manufacturing operations",
+    "version": "11.0.1.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "mrp",
+        "stock_putaway_no_internal",
     ],
-    'data': [
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
+    "data": [],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
 }
