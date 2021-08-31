837bc6ca1ce3e8d46d589eb59c8995e1c063d548
mreficent <miquel.raich@eficent.com>
2021-08-31 13:45:18 +0200

    [IMP] stock_picking_report_delivery_note: black, isort, prettier

--- stock_picking_report_delivery_note/__manifest__.py
+++ stock_picking_report_delivery_note/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock picking delivery note",
-    'description': """Stock picking delivery note""",
-    'version': '11.0.1.0.6',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'license': 'AGPL-3',
-    'category': 'Warehouse',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
-        'sale_line_partner_description',
+    "name": "Stock picking delivery note",
+    "description": """Stock picking delivery note""",
+    "version": "11.0.1.0.6",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Warehouse",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
+        "sale_line_partner_description",
     ],
-    'data': [
-        'report/report.xml',
-        'views/report_stock_picking_report_delivery_note.xml',
+    "data": [
+        "report/report.xml",
+        "views/report_stock_picking_report_delivery_note.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- stock_picking_report_delivery_note/models/stock_move.py
+++ stock_picking_report_delivery_note/models/stock_move.py
@@ -2,11 +2,12 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
-from odoo.addons import decimal_precision as dp
+import re
+
+from odoo import _, fields, models
 from odoo.exceptions import UserError
 
-import re
+from odoo.addons import decimal_precision as dp
 
 
 class StockMove(models.Model):
@@ -15,7 +16,7 @@ class StockMove(models.Model):
     def _get_uom_rate_conversion(self, uom_from, uom_to):
         def _get_uom_rate(uom):
             uom_rate = uom.factor_inv
-            if uom.uom_type == 'smaller':
+            if uom.uom_type == "smaller":
                 uom_rate = 1 / uom_rate
 
             return uom_rate
@@ -24,44 +25,64 @@ class StockMove(models.Model):
             return 1
 
         if uom_from.category_id != uom_to.category_id:
-            raise UserError(_("Cannot convert the units of the product %s") % (
-                    self.product_id.default_code or self.product_id.name))
+            raise UserError(
+                _("Cannot convert the units of the product %s")
+                % (self.product_id.default_code or self.product_id.name)
+            )
 
         return _get_uom_rate(uom_from) / _get_uom_rate(uom_to)
 
-    sale_price_unit = fields.Float("Sale unit price", compute="_compute_sale_price_unit",
-                                   digits=dp.get_precision('Product Price'))
+    sale_price_unit = fields.Float(
+        "Sale unit price",
+        compute="_compute_sale_price_unit",
+        digits=dp.get_precision("Product Price"),
+    )
 
     def _compute_sale_price_unit(self):
         for rec in self:
             factor_rate = rec._get_uom_rate_conversion(
-                rec.sale_line_id.product_uom, rec.product_uom)
-            rec.sale_price_unit = rec.sale_line_id.price_unit / factor_rate * \
-                                  (1 - (rec.sale_line_id.discount or 0.0) / 100.0)
+                rec.sale_line_id.product_uom, rec.product_uom
+            )
+            rec.sale_price_unit = (
+                rec.sale_line_id.price_unit
+                / factor_rate
+                * (1 - (rec.sale_line_id.discount or 0.0) / 100.0)
+            )
 
     def _get_amounts(self):
         self.ensure_one()
         taxes = self.sale_line_id.tax_id.compute_all(
-            self.sale_price_unit, self.sale_line_id.order_id.currency_id,
-            self.quantity_done, product=self.product_id, partner=self.picking_id.partner_id)
+            self.sale_price_unit,
+            self.sale_line_id.order_id.currency_id,
+            self.quantity_done,
+            product=self.product_id,
+            partner=self.picking_id.partner_id,
+        )
         return {
-            'tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-            'total': taxes['total_included'],
-            'subtotal': taxes['total_excluded'],
+            "tax": sum(t.get("amount", 0.0) for t in taxes.get("taxes", [])),
+            "total": taxes["total_included"],
+            "subtotal": taxes["total_excluded"],
         }
 
     def get_line_lots(self):
-        if self.product_id.tracking in ('lot', 'serial'):
+        if self.product_id.tracking in ("lot", "serial"):
             lots = []
-            for l in self.move_line_ids:
-                qty = {'assigned': l.product_qty, 'done': l.qty_done}
-                if l.state in qty:
-                    lots.append("%s (%s %s)" % (l.lot_id.name, str(qty[l.state]), l.product_uom_id.name))
+            for line in self.move_line_ids:
+                qty = {"assigned": line.product_qty, "done": line.qty_done}
+                if line.state in qty:
+                    lots.append(
+                        "%s (%s %s)"
+                        % (
+                            line.lot_id.name,
+                            str(qty[line.state]),
+                            line.product_uom_id.name,
+                        )
+                    )
             return lots
         return None
 
     def _extract_product_code(self, name):
-        m = re.match(r'^ *\[([^]]+)\] *(.*)$', name, flags=re.DOTALL)
+        m = re.match(r"^ *\[([^]]+)\] *(.*)$", name, flags=re.DOTALL)
         if not m:
             return None, name
 
@@ -70,7 +91,9 @@ class StockMove(models.Model):
     def get_product_codes(self):
         default_code = self.product_id.default_code or None
         customer_ref = None
-        buyer = self.product_id.buyer_ids.filtered(lambda x: x.partner_id == self.picking_id.partner_id)
+        buyer = self.product_id.buyer_ids.filtered(
+            lambda x: x.partner_id == self.picking_id.partner_id
+        )
         if buyer and buyer.code:
             customer_ref = buyer.code
 
