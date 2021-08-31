a0255a06375fa7731b750ab1517e1a3a535afbd8
mreficent <miquel.raich@eficent.com>
2021-08-31 13:45:18 +0200

    [IMP] stock_picking_report_delivery_note: black, isort, prettier

--- stock_picking_report_delivery_note/models/stock_picking.py
+++ stock_picking_report_delivery_note/models/stock_picking.py
@@ -3,20 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
-import uuid
-
-from itertools import groupby
-from datetime import datetime, timedelta
-from werkzeug.urls import url_encode
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, AccessError
-from odoo.osv import expression
-from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
-
-from odoo.tools.misc import formatLang
-
-from odoo.addons import decimal_precision as dp
+from odoo import api, models
 
 
 class Picking(models.Model):
@@ -27,12 +14,12 @@ class Picking(models.Model):
         amount_untaxed = amount_tax = 0.0
         for line in self.move_lines:
             amounts = line._get_amounts()
-            amount_untaxed += amounts['subtotal']
-            amount_tax += amounts['tax']
+            amount_untaxed += amounts["subtotal"]
+            amount_tax += amounts["tax"]
         return {
-            'untaxed': amount_untaxed,
-            'tax': amount_tax,
-            'total': amount_untaxed + amount_tax,
+            "untaxed": amount_untaxed,
+            "tax": amount_tax,
+            "total": amount_untaxed + amount_tax,
         }
 
     @api.multi
@@ -41,16 +28,19 @@ class Picking(models.Model):
         res = {}
         for line in self.move_lines:
             taxes = line.sale_line_id.tax_id.compute_all(
-                line.sale_price_unit, quantity=line.quantity_done,
-                product=line.product_id, partner=self.partner_id)['taxes']
+                line.sale_price_unit,
+                quantity=line.quantity_done,
+                product=line.product_id,
+                partner=self.partner_id,
+            )["taxes"]
             for tax in line.sale_line_id.tax_id:
                 group = tax.tax_group_id
-                res.setdefault(group, {'amount': 0.0, 'base': 0.0})
+                res.setdefault(group, {"amount": 0.0, "base": 0.0})
                 for t in taxes:
-                    if t['id'] == tax.id or t['id'] in tax.children_tax_ids.ids:
-                        res[group]['amount'] += t['amount']
-                        res[group]['base'] += t['base']
+                    if t["id"] == tax.id or t["id"] in tax.children_tax_ids.ids:
+                        res[group]["amount"] += t["amount"]
+                        res[group]["base"] += t["base"]
         res = sorted(res.items(), key=lambda l: l[0].sequence)
-        res = [(l[0].name, l[1]['amount'], l[1]['base'], len(res)) for l in res]
+        res = [(val[0].name, val[1]["amount"], val[1]["base"], len(res)) for val in res]
 
         return res
