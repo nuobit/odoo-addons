ee9a13489f148d7da811b12b122b1e36bbb34523
mreficent <miquel.raich@eficent.com>
2021-08-31 15:34:42 +0200

    [IMP] stock_putaway_no_internal_mrp: black, isort, prettier

--- stock_putaway_no_internal_mrp/models/mrp_production.py
+++ stock_putaway_no_internal_mrp/models/mrp_production.py
@@ -2,17 +2,19 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
 from ast import literal_eval
+
+from odoo import api, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     @api.multi
     def open_produce_product(self):
         action = super(MrpProduction, self).open_produce_product()
-        action['context'] = {
-            **literal_eval(action['context']),
-            **dict(stock_picking_type_code=self.picking_type_id.code)}
+        action["context"] = {
+            **literal_eval(action["context"]),
+            **dict(stock_picking_type_code=self.picking_type_id.code),
+        }
         return action
--- stock_putaway_no_internal_mrp/models/stock_location.py
+++ stock_putaway_no_internal_mrp/models/stock_location.py
@@ -2,12 +2,11 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, _
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class Location(models.Model):
     _inherit = "stock.location"
 
     def _excluded_picking_types(self):
-        return super(Location, self)._excluded_picking_types() + ['mrp_operation']
+        return super(Location, self)._excluded_picking_types() + ["mrp_operation"]
