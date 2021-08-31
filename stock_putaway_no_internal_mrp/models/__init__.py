8446d207c7ccc550517c830c9ca9d028077ecb6a
mreficent <miquel.raich@eficent.com>
2021-08-31 16:28:53 +0200

    [MIG] stock_putaway_no_internal_mrp: Migration to 14.0

--- stock_putaway_no_internal_mrp/models/__init__.py
+++ stock_putaway_no_internal_mrp/models/__init__.py
@@ -1,6 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import stock_location
-from . import mrp_production
+from . import mrp_workorder
