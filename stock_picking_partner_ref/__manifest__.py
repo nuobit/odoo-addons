56f23975541ded739ab72bcdd672bb17ba4e3412
mreficent <miquel.raich@eficent.com>
2021-08-31 13:09:47 +0200

    [MIG] stock_picking_partner_ref: Migration to 14.0

--- stock_picking_partner_ref/__manifest__.py
+++ stock_picking_partner_ref/__manifest__.py
@@ -4,10 +4,10 @@
 
 {
     "name": "Stock picking partner reference",
-    "summary": "Adds a partner reference on pickingg",
+    "summary": "Adds a partner reference on picking",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "category": "Warehouse",
-    "version": "11.0.0.1.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
