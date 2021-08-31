12837cb353e10c48bac24a212e4f067dffc227dc
mreficent <miquel.raich@eficent.com>
2021-08-31 12:17:36 +0200

    [IMP] stock_picking_import_serials: black, isort, prettier

--- stock_picking_import_serials/__manifest__.py
+++ stock_picking_import_serials/__manifest__.py
@@ -3,26 +3,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Stock picking import serials',
-    'summary': "Import serial numbers to a stock picking from spreadsheet file",
-    'version': '11.0.1.0.4',
-    'category': 'Warehouse Management',
-    'license': 'AGPL-3',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock',
+    "name": "Stock picking import serials",
+    "summary": "Import serial numbers to a stock picking from spreadsheet file",
+    "version": "11.0.1.0.4",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
     ],
-    'external_dependencies': {
-        'python': [
-            'xlrd',
+    "external_dependencies": {
+        "python": [
+            "xlrd",
         ],
     },
-    'data': [
-        'wizard/stock_picking_import_serials.xml',
-        'views/stock_picking_views.xml',
+    "data": [
+        "wizard/stock_picking_import_serials.xml",
+        "views/stock_picking_views.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
 }
