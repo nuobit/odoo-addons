4525f0b1ff1111476223ab4d88319c2c1a8c823a
mreficent <miquel.raich@eficent.com>
2021-09-07 11:44:26 +0200

    [IMP] l10n_es_aeat_vat_special_prorrate: black, isort, prettier

--- l10n_es_aeat_vat_special_prorrate/__manifest__.py
+++ l10n_es_aeat_vat_special_prorrate/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'AEAT - Prorrata especial de IVA',
-    'summary': 'Módulo para gestionar la prorrata especial del IVA '
-               'en las facturas de la AEAT',
-    'version': '11.0.0.2.5',
-    'category': 'Accounting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es_aeat',
+    "name": "AEAT - Prorrata especial de IVA",
+    "summary": "Módulo para gestionar la prorrata especial del IVA "
+    "en las facturas de la AEAT",
+    "version": "11.0.0.2.5",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/aeat_map_special_prorrate_year.xml',
-        'views/aeat_map_special_prorrate_year_views.xml',
-        'views/account_tax_views.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "security/aeat_map_special_prorrate_year.xml",
+        "views/aeat_map_special_prorrate_year_views.xml",
+        "views/account_tax_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
