acefa50e4ae1b1c486104291d57bb2c5d3c88bf2
Eric Antones <eantones@nuobit.com>
2021-09-13 16:31:14 +0200

    [IMP] connector_oximovil: added endpoint with product and lot info

--- connector_oximovil/__manifest__.py
+++ connector_oximovil/__manifest__.py
@@ -6,7 +6,7 @@
     'name': "Odoo-Oximovil connector",
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'category': 'Connector',
-    'version': '11.0.0.1.0',
+    'version': '11.0.0.2.0',
     'license': 'AGPL-3',
     'website': 'https://github.com/nuobit',
     'depends': [
--- connector_oximovil/controllers/main.py
+++ connector_oximovil/controllers/main.py
@@ -1,9 +1,9 @@
-from odoo import http
+import json
+import logging
 
 import werkzeug.exceptions
-import json
 
-import logging
+from odoo import http
 
 _logger = logging.getLogger(__name__)
 
@@ -58,6 +58,64 @@ class OxiMovilController(http.Controller):
                 'product_id': l.product_id.id,
                 'product_code': l.product_id.default_code,
             })
+
+        ## send data
+        headers = {
+            ('Content-Type', content_type),
+        }
+        return http.request.make_response(json.dumps(data), headers=headers)
+
+    @http.route([
+        '/api/v1/products',
+    ], type='http', methods=['GET'], auth="user")
+    def get_product_and_lots(self, code=None):
+        ## validate client call
+        content_type = 'application/json'
+        if not http.request.httprequest.accept_mimetypes.accept_json:
+            return werkzeug.exceptions.NotAcceptable("Server only generates %s and the client only accepts %s" % (
+                content_type,
+                http.request.httprequest.accept_mimetypes,
+            ))
+
+        ## get current user
+        user = http.request.env['res.users'].search([
+            ('id', '=', http.request.uid),
+        ])
+        if not user:
+            return werkzeug.exceptions.InternalServerError("No user found with current id")
+        elif len(user) > 1:
+            return werkzeug.exceptions.InternalServerError("Detected more than one user with the same id")
+
+        ## get current company
+        company_id = user.company_id.id
+        if not company_id:
+            return werkzeug.exceptions.InternalServerError("Cannot get the company from the user")
+        domain = [('company_id', '=', company_id)]
+
+        ## validate not implemented functonalities
+        if not code:
+            return werkzeug.exceptions.NotImplemented("The full product list is not implemented")
+
+        ## get product
+        product = http.request.env['product.product'].search(
+            domain + [('default_code', '=', code)]
+        )
+        if not product:
+            return werkzeug.exceptions.NotFound("The product '%s' does not exist" % code)
+        if len(product) > 1:
+            return werkzeug.exceptions.InternalServerError("There's more than one product with code '%s'" % code)
+        data = {
+            'id': product.id,
+            'code': product.default_code,
+            'description': product.with_context(lang='es_ES').name,
+            'category': product.with_context(lang='es_ES').categ_id.name,
+        }
+
+        ## get lots
+        lots = http.request.env['stock.production.lot'].search(
+            domain + [('product_id', '=', product.id)]
+        )
+        data['lots'] = [[l.name, l.product_qty] for l in lots]
 
         ## send data
         headers = {
