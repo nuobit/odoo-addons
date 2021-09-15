5caa265b08dcc29973a645eae1ed1e27234cfd44
Eric Antones <eantones@nuobit.com>
2021-09-15 19:14:57 +0200

    [IMP] connector_oxigesti: export product categories

---
+++ connector_oxigesti/models/product_category/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2021 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from . import exporter
+from . import adapter
+from . import export_mapper
+from . import binder
+from . import binding
+
+from . import product_category
