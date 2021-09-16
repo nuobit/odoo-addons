25ad9e25b1f5b657fe92cbab526f04ffa6bfa03a
mreficent <miquel.raich@eficent.com>
2021-09-16 12:52:51 +0200

    [IMP] account_asset_comment: black, isort, prettier

--- account_asset_comment/__manifest__.py
+++ account_asset_comment/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Account Asset comment',
-    'summary': 'This module adds a comment field in the assets',
-    'version': '11.0.0.1.0',
-    'category': 'Accounting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_asset',
+    "name": "Account Asset comment",
+    "summary": "This module adds a comment field in the assets",
+    "version": "11.0.0.1.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset",
     ],
-    'data': [
-        'views/account_asset_view.xml',
+    "data": [
+        "views/account_asset_view.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
