b396f058977fcb4c9a9f188fa797da29e097ec39
Eric Antones <eantones@nuobit.com>
2021-09-16 13:03:19 +0200

    [IMP] connector_oxigesti: delete backend categories when deleted from Odoo

---
+++ connector_oxigesti/models/product_category/listener.py
@@ -0,0 +1,12 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo.addons.component.core import Component
+
+
+class ProductCategoryListener(Component):
+    _name = 'oxigesti.product.category.listener'
+    _inherit = 'oxigesti.event.listener'
+
+    _apply_on = 'product.category'
