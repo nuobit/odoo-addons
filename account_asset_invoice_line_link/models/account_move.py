048d8692be8c9ba5157b05d8c6830d1f64d1655a
mreficent <miquel.raich@eficent.com>
2021-09-17 14:47:39 +0200

    [MIG] account_asset_invoice_line_link: Migration to 14.0

---
+++ account_asset_invoice_line_link/models/account_move.py
@@ -0,0 +1,25 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _prepare_asset_vals(self, aml):
+        vals = super()._prepare_asset_vals(aml)
+        vals["move_id"] = aml.move_id.id
+        vals["move_line_id"] = aml.id
+        return vals
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.move.line"
+
+    asset_ids = fields.One2many(
+        comodel_name="account.asset",
+        inverse_name="move_line_id",
+        string="Assets",
+    )
