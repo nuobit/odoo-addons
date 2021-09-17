1ec3929ece5d44e10072923252bf90855d3d4cd4
mreficent <miquel.raich@eficent.com>
2021-09-17 14:12:04 +0200

    [MIG] account_asset_invoice_account_date: Migration to 14.0

--- account_asset_invoice_account_date/__manifest__.py
+++ account_asset_invoice_account_date/__manifest__.py
@@ -6,13 +6,13 @@
     "name": "Account Asset Invoice Account Date",
     "summary": "This module creates the assets from the invoice using "
     "the accounting date instead of the invoice date.",
-    "version": "11.0.1.0.1",
+    "version": "14.0.1.0.1",
     "category": "Accounting",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
-        "account_asset_hook",
+        "account_asset_management",
     ],
     "installable": True,
     "development_status": "Beta",
--- account_asset_invoice_account_date/models/account_invoice.py
+++ account_asset_invoice_account_date/models/account_invoice.py
@@ -1,42 +1,18 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from datetime import datetime
-
-from dateutil.relativedelta import relativedelta
-
-from odoo import api, models
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
+from odoo import models
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
-    def _prepare_asset_values(self):
-        if (
-            self.invoice_id.date
-            and self.invoice_id.date != self.invoice_id.date_invoice
-        ):
-            self = self.with_context(
-                update_asset_values={
-                    **self.env.context.get("update_asset_values", {}),
-                    "date": self.invoice_id.date,
+    def _prepare_asset_vals(self, aml):
+        vals = super()._prepare_asset_vals(aml)
+        if self.date and self.date != self.invoice_date:
+            vals.update(
+                {
+                    "date": self.date,
                 }
             )
-        return super(AccountInvoiceLine, self)._prepare_asset_values()
-
-    @api.one
-    @api.depends("asset_category_id", "invoice_id.date_invoice", "invoice_id.date")
-    def _get_asset_date(self):
-        super(AccountInvoiceLine, self)._get_asset_date()
-        cat = self.asset_category_id
-        if cat:
-            months = cat.method_number * cat.method_period
-            if (
-                self.invoice_id.date
-                and self.invoice_id.date != self.invoice_id.date_invoice
-            ):
-                start_date = datetime.strptime(self.invoice_id.date, DF).replace(day=1)
-                end_date = start_date + relativedelta(months=months, days=-1)
-                self.asset_start_date = start_date.strftime(DF)
-                self.asset_end_date = end_date.strftime(DF)
+        return vals
