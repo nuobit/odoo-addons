6c6c50c188530c43b834522f989cb0a786bd6bab
Eric Antones <eantones@nuobit.com>
2021-10-19 18:48:33 +0200

    [FIX] account_invoice_tax_analytic_account: wrong website key on manifest

--- account_invoice_tax_analytic_account/__manifest__.py
+++ account_invoice_tax_analytic_account/__manifest__.py
@@ -10,7 +10,7 @@
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/OCA/pms",
+    "website": "https://github.com/nuobit",
     "depends": ["account"],
     "data": [
         "views/account_invoice_view.xml",
