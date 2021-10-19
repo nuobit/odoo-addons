bbd6fea59a4d4327ec8e68a0d93649c89e210489
Eric Antones <eantones@nuobit.com>
2021-10-19 18:48:33 +0200

    [FIX] account_invoice_report_lot: wrong website key on manifest

--- account_invoice_report_lot/__manifest__.py
+++ account_invoice_report_lot/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Invoicing Management",
-    "website": "https://github.com/OCA/pms",
+    "website": "https://github.com/nuobit",
     "summary": "This module adds a lot/serial number on lines of invoice reports",
     "depends": [
         "stock_picking_invoice_link",
