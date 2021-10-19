b7abbcb57927592864f0ced0c85ef243c503a6a0
Eric Antones <eantones@nuobit.com>
2021-10-19 18:48:34 +0200

    [FIX] pos_restaurant_note_print: wrong website key on manifest

--- pos_restaurant_note_print/__manifest__.py
+++ pos_restaurant_note_print/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Point of Sale",
-    "website": "https://github.com/OCA/pms",
+    "website": "https://github.com/nuobit",
     "summary": "This module shows the line note on the receipt on screen and printer.",
     "depends": [
         "pos_restaurant",
