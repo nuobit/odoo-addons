664396114aae184a11fbc1450620056aa29b3074
Eric Antones <eantones@nuobit.com>
2021-10-19 18:48:34 +0200

    [FIX] sale_order_bike_location: wrong website key on manifest

--- sale_order_bike_location/__manifest__.py
+++ sale_order_bike_location/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Sale",
-    "website": "https://github.com/OCA/pms",
+    "website": "https://github.com/nuobit",
     "summary": "This module adds a Bike Location on sale orders",
     "depends": [
         "sale",
