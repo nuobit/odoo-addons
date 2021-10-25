362428da309255145273019fb31cb0289b6ca567
KNVx <kniubo@nuobit.com>
2021-10-25 13:37:15 +0200

    [IMP]-project_task_metatype: meta_type cancel added in field selection

--- project_task_metatype/__manifest__.py
+++ project_task_metatype/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Eric Antones<eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Project Task Metatype",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Project",
