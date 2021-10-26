f0ea1ea8d92283f01eed7a8efbe11980dc8ad871
KNVx <kniubo@nuobit.com>
2021-10-26 10:10:52 +0200

    [ADD] stock_order_forecast: shows sales and purchase orders with pending products

---
+++ stock_order_forecast/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Order Forecast",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Kilian Niubo",
+    "license": "AGPL-3",
+    "category": "Product",
+    "website": "https://github.com/nuobit",
+    "summary": "This module shows sales and purchase orders with pending products",
+    "depends": [
+        "sale",
+        "purchase",
+        "stock",
+    ],
+    "installable": True,
+}
