6b205041aae7e23916e38e7d45d6919019da6080
KNVx <kniubo@nuobit.com>
2021-10-27 12:37:44 +0200

    [ADD] stock_picking_partner_ref_search: adds partner ref in stock picking to search by this field

---
+++ stock_picking_partner_ref_search/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock picking partner ref search",
+    "summary": "This module adds partner ref in the stock "
+    "picking to search by this field",
+    "version": "12.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L., Kilian Niubo",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "website": "https://github.com/nuobit",
+    "depends": [
+        "stock",
+        "sale",
+        "purchase",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
