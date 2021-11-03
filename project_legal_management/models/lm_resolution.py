9ed2fc034e25b1155cad540f1cdbd2840f60ba07
KNVx <92428614+KNVx@users.noreply.github.com>
2021-11-03 14:30:00 +0100

    [ADD] project_legal_management: new module to extend projects as a legal management

---
+++ project_legal_management/models/lm_resolution.py
@@ -0,0 +1,19 @@
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, api, fields
+
+
+class LMResolution(models.Model):
+    _name = 'lm.resolution'
+    _parent_name = 'parent_id'
+    _inherit = 'lm.tree.mixin'
+
+    name = fields.Char(required=True)
+    parent_id = fields.Many2one(comodel_name='lm.resolution')
+
+    complete_chain_ids = fields.Many2many(
+        string="Complete Chain",
+        comodel_name='lm.resolution',
+        compute='_compute_complete_chain_ids',
+    )
---
+++ project_legal_management/models/project.py
@@ -0,0 +1,17 @@
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    lm_date = fields.Date(string="Date")
+    lm_employee_ids = fields.Many2many(string="Employees", comodel_name="hr.employee")
+    lm_expedient_number = fields.Char(string="Expedient Number")
+    lm_issue_ids = fields.Many2many(string="Issues", comodel_name="lm.issue")
+    lm_resolution_id = fields.Many2one(string="Resolution", comodel_name="lm.resolution")
+    lm_amount = fields.Float(string="Amount")
+    lm_probability_id = fields.Many2one(string="Probability", comodel_name="lm.probability")
