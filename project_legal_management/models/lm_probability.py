a5099dcfd1dd54ab415d3993f99f614c49403eaa
KNVx <92428614+KNVx@users.noreply.github.com>
2021-11-03 14:30:00 +0100

    [ADD] project_legal_management: new module to extend projects as a legal management

---
+++ project_legal_management/models/lm_probability.py
@@ -0,0 +1,20 @@
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
+
+
+class LMProbability(models.Model):
+    _name = 'lm.probability'
+    _parent_name = 'parent_id'
+    _inherit = 'lm.tree.mixin'
+
+    name = fields.Char(required=True)
+    parent_id = fields.Many2one(comodel_name='lm.probability')
+
+    complete_chain_ids = fields.Many2many(
+        string="Complete Chain",
+        comodel_name='lm.probability',
+        compute='_compute_complete_chain_ids',
+    )
