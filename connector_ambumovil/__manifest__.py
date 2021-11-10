ce1ad9c13c3758d05a344d04081924a3961341bf
KNVx <kniubo@nuobit.com>
2021-11-10 14:00:37 +0100

    [FIX] connector_ambumovil: added account_asset dependency on manifest

--- connector_ambumovil/__manifest__.py
+++ connector_ambumovil/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     'name': "Odoo-Ambumovil connector",
     'author': 'NuoBiT Solutions, S.L., Eric Antones',
     'category': 'Connector',
-    'version': '11.0.0.2.1',
+    'version': '11.0.0.2.2',
     'license': 'AGPL-3',
     'website': 'https://github.com/nuobit',
     'depends': [
@@ -16,6 +16,7 @@
         'stock_picking_partner',
         'stock_location_code',
         'connector_sage',
+        'account_asset',
     ],
     'data': [
     ],
