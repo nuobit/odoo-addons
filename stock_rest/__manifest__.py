e9356102dc8344863ac06691d43089b86b70dd89
KNVx <kniubo@nuobit.com>
2021-12-01 13:00:11 +0100

    [ADD] stock_rest: new module stock rest

---
+++ stock_rest/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': "Stock REST",
+    'author': 'NuoBiT Solutions, S.L.',
+    'category': 'Stock',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/nuobit',
+    'depends': [
+        'stock',
+        'connector_sage',
+        'base_rest'
+    ],
+    'data': [],
+    'installable': True,
+}
