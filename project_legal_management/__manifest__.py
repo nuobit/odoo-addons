9db08cf836278e0bd37942099bbacd52bd0c2849
KNVx <kniubo@nuobit.com>
2021-12-02 09:29:50 +0100

    [IMP] project_legal_management: black, isort, prettier

--- project_legal_management/__manifest__.py
+++ project_legal_management/__manifest__.py
@@ -2,26 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Project legal management',
-    'summary': 'This module adds the necessary fields to work with '
-               'the legal management in the project module',
-    'version': '11.0.1.0.0',
-    'category': 'Project',
-    'author': 'NuoBiT Solutions, S.L., Kilian Niubo',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'project',
-        'hr',
+    "name": "Project legal management",
+    "summary": "This module adds the necessary fields to work with "
+    "the legal management in the project module",
+    "version": "11.0.1.0.0",
+    "category": "Project",
+    "author": "NuoBiT Solutions, S.L., Kilian Niubo",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "hr",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/project_views.xml',
-        'views/lm_issue_views.xml',
-        'views/lm_probability_views.xml',
-        'views/lm_resolution_views.xml',
-        'views/project_tree.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_views.xml",
+        "views/lm_issue_views.xml",
+        "views/lm_probability_views.xml",
+        "views/lm_resolution_views.xml",
+        "views/project_tree.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
