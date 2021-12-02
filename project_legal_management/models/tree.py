d35e41fdc838a25a293486c60a4d2b6cd1fa1048
KNVx <kniubo@nuobit.com>
2021-12-02 09:29:50 +0100

    [IMP] project_legal_management: black, isort, prettier

--- project_legal_management/models/tree.py
+++ project_legal_management/models/tree.py
@@ -2,17 +2,19 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class LMTreeMixin(models.AbstractModel):
-    _name = 'lm.tree.mixin'
+    _name = "lm.tree.mixin"
 
     # complete_name
-    complete_name = fields.Char(string='Complete Name',
-                                compute='_compute_complete_name',
-                                search='_search_complete_name')
+    complete_name = fields.Char(
+        string="Complete Name",
+        compute="_compute_complete_name",
+        search="_search_complete_name",
+    )
 
     @api.multi
     def name_get(self):
@@ -23,25 +25,26 @@ class LMTreeMixin(models.AbstractModel):
 
     def _get_node_ancestors_chain(self):
         self.ensure_one()
-        return (self.parent_id and self.parent_id._get_node_ancestors_chain() or \
-                self.env[self._name]) | self
+        return (
+            self.parent_id
+            and self.parent_id._get_node_ancestors_chain()
+            or self.env[self._name]
+        ) | self
 
-    @api.depends('parent_id')
+    @api.depends("parent_id")
     def _compute_complete_chain_ids(self):
         for rec in self:
             rec.complete_chain_ids = rec._get_node_ancestors_chain()
 
     def get_complete_name(self):
         self.ensure_one()
-        return ' / '.join(
-            self.complete_chain_ids.mapped('name')
-        )
+        return " / ".join(self.complete_chain_ids.mapped("name"))
 
-    @api.depends('name', 'parent_id.complete_name')
+    @api.depends("name", "parent_id.complete_name")
     def _compute_complete_name(self):
         for rec in self:
             if rec.parent_id:
-                rec.complete_name = '%s / %s' % (rec.parent_id.complete_name, rec.name)
+                rec.complete_name = "%s / %s" % (rec.parent_id.complete_name, rec.name)
             else:
                 rec.complete_name = rec.name
 
@@ -49,31 +52,31 @@ class LMTreeMixin(models.AbstractModel):
         node_ids = []
         for node in self.env[self._name].search([]):
             complete_name = node.get_complete_name()
-            if operator == '=':
+            if operator == "=":
                 if value == complete_name:
                     node_ids.append(node.id)
-            elif operator == '!=':
+            elif operator == "!=":
                 if value != complete_name:
                     node_ids.append(node.id)
-            elif operator == 'like':
+            elif operator == "like":
                 if value in complete_name:
                     node_ids.append(node.id)
-            elif operator == 'not like':
+            elif operator == "not like":
                 if value not in complete_name:
                     node_ids.append(node.id)
-            elif operator == 'ilike':
+            elif operator == "ilike":
                 if value.lower() in complete_name.lower():
                     node_ids.append(node.id)
-            elif operator == 'not ilike':
+            elif operator == "not ilike":
                 if value.lower() not in complete_name.lower():
                     node_ids.append(node.id)
-            elif operator == '=like':
+            elif operator == "=like":
                 if value == complete_name:
                     node_ids.append(node.id)
-            elif operator == '=ilike':
+            elif operator == "=ilike":
                 if value.lower() == complete_name.lower():
                     node_ids.append(node.id)
             else:
                 raise UserError(_("Operator %s not implemented") % operator)
 
-        return [('id', 'in', node_ids)]
+        return [("id", "in", node_ids)]
