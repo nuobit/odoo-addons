0a9cc906aedc8c03ff28a177fa9645601f78e6e1
KNVx <kniubo@nuobit.com>
2021-12-02 09:29:50 +0100

    [IMP] project_legal_management: black, isort, prettier

--- project_legal_management/models/lm_probability.py
+++ project_legal_management/models/lm_probability.py
@@ -1,20 +1,19 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, api, fields, _
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class LMProbability(models.Model):
-    _name = 'lm.probability'
-    _parent_name = 'parent_id'
-    _inherit = 'lm.tree.mixin'
+    _name = "lm.probability"
+    _parent_name = "parent_id"
+    _inherit = "lm.tree.mixin"
 
     name = fields.Char(required=True)
-    parent_id = fields.Many2one(comodel_name='lm.probability')
+    parent_id = fields.Many2one(comodel_name="lm.probability")
 
     complete_chain_ids = fields.Many2many(
         string="Complete Chain",
-        comodel_name='lm.probability',
-        compute='_compute_complete_chain_ids',
+        comodel_name="lm.probability",
+        compute="_compute_complete_chain_ids",
     )
