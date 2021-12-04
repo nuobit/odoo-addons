67aa84bce640d5c9a8aea695e13e2965c75eba0e
Eric Antones <eantones@nuobit.com>
2021-12-04 09:51:52 +0100

    [REF] stock_rest: remove unused logger

--- stock_rest/services/stock_lot_service.py
+++ stock_rest/services/stock_lot_service.py
@@ -2,11 +2,8 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-import logging
 from odoo.addons.component.core import Component
 from odoo.exceptions import MissingError
-
-_logger = logging.getLogger(__name__)
 
 
 class LotService(Component):
