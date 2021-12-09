2d4e4aee9ce8333fbcac03fe5fa1f1cf88345745
KNVx <kniubo@nuobit.com>
2021-12-09 15:44:24 +0100

    [MIG] payroll_sage: Migration to 14.0

--- payroll_sage/models/payslip.py
+++ payroll_sage/models/payslip.py
@@ -1,8 +1,8 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 from odoo.exceptions import UserError, ValidationError
 
 PROCESS_TYPES = [
@@ -25,6 +25,7 @@ PROCESS_TYPES = [
 class Payslip(models.Model):
     _name = "payroll.sage.payslip"
     _description = "Payslip"
+    _order = "entry_date desc, type, process"
 
     name = fields.Char(string="Name", required=True)
     entry_date = fields.Date(string="Entry date", required=True)
@@ -36,7 +37,6 @@ class Payslip(models.Model):
     labour_agreement_id = fields.Many2one(
         "payroll.sage.labour.agreement",
         string="Labour agreement",
-        # domain=[('registration_date_cv', '>=')]
         required=True,
     )
 
@@ -102,16 +102,14 @@ class Payslip(models.Model):
         copy=False,
     )
 
-    @api.multi
     def action_paysplip_set_to_draft(self):
         for rec in self:
             rec.payslip_wage_type_ids.unlink()
             rec.write({"state": "draft"})
 
-    @api.multi
     def action_paysplip_validate(self):
         for rec in self:
-            ### agrupem i acumulem imports per wage type
+            # group and aggregate amounts per wage type
             items_d = {}
             for line in rec.payslip_line_ids:
                 wage_type_line = line.wage_type_line_id
@@ -127,7 +125,7 @@ class Payslip(models.Model):
                     }
                 items_d[key]["amount"] += amount
 
-            ### muntem el valors
+            # mount the values
             values_l = []
             for wage_type in sorted(
                 items_d.values(), key=lambda x: x["wage_line_type"].code
@@ -139,7 +137,7 @@ class Payslip(models.Model):
                     }
                 )
 
-            ## inserim
+            # insert
             rec.write(
                 {
                     "payslip_wage_type_ids": [(0, False, v) for v in values_l],
@@ -147,8 +145,7 @@ class Payslip(models.Model):
                 }
             )
 
-    @api.multi
-    def action_paysplip_post(self):
+    def action_paysplip_post(self):  # noqa: C901
         def add2dict(items_d, tag, employee, amount):
             if tag.aggregate:
                 employee = None
@@ -167,12 +164,10 @@ class Payslip(models.Model):
                 }
 
             items_d[key]["amount"] += amount
-            if not tag.aggregate:
-                items_d[key]["description"].add(line.name.strip())
 
         for rec in self:
             if not rec.move_id:
-                ### agrupem i acumulem imports per tag
+                # group and aggregate amounts per tag
                 items_d = {}
                 for line in rec.payslip_line_ids:
                     wage_type_line = line.wage_type_line_id
@@ -199,28 +194,31 @@ class Payslip(models.Model):
                             ):
                                 amount_tag *= -1
                             add2dict(items_d, tag, line.employee_id, amount_tag)
+                            if not tag.aggregate:
+                                key = (tag.id, line.employee_id.id)
+                                items_d[key]["description"].add(line.name.strip())
 
-                ### afegim ls S.S si es payroll
+                # add the S.S if it's payroll
                 if rec.type == "payroll":
                     for tag in rec.labour_agreement_id.ss_tag_ids:
                         if not tag.aggregate:
                             raise UserError(_("S.S. Tags must be aggregated!"))
                         add2dict(items_d, tag, None, round(rec.ss_cost, 2))
                 else:
-                    ## afegm els talons si es transfer (sempre han de restar)
+                    # add checks if it's transfer (it always have to be a subtraction)
                     for check in rec.payslip_check_ids:
                         for tag in rec.labour_agreement_id.check_tag_ids:
                             add2dict(
                                 items_d, tag, check.employee_id, round(-check.amount, 2)
                             )
 
-                #### generem l'assentament
-                ## generem els apunts
+                # generate the journal entry
+                # generate the journal item
                 line_values_l = []
                 for item_d in items_d.values():
                     tag = item_d["tag"]
 
-                    ## compte
+                    # account
                     values = {
                         "account_id": tag.account_id.id,
                     }
@@ -231,29 +229,28 @@ class Payslip(models.Model):
                             {"partner_id": item_d["employee"].sudo().address_home_id.id}
                         )
 
-                    ## descripcio
-                    from_string = fields.Date.from_string
+                    # description
                     date_str = "%s/%s" % (
-                        from_string(rec.entry_date).strftime("%m"),
-                        from_string(rec.entry_date).strftime("%Y"),
+                        rec.entry_date.strftime("%m"),
+                        rec.entry_date.strftime("%Y"),
                     )
                     description_l = [date_str]
                     if tag.description and tag.description.strip():
                         description_l.append(tag.description.strip())
                     else:
-                        if item_d["description"]:
-                            if len(item_d["description"]) == 1:
-                                description_l.append(list(item_d["description"])[0])
+                        if item_d["description"] and len(item_d["description"]) == 1:
+                            description_l.append(list(item_d["description"])[0])
                     if description_l:
                         values.update({"name": " ".join(description_l)})
 
-                    # import
+                    # amount
                     credit_debit = tag.credit_debit
                     if item_d["amount"] < 0:
                         if credit_debit == "debit":
                             credit_debit = "credit"
                         else:
                             credit_debit = "debit"
+
                     values.update(
                         {
                             credit_debit: abs(round(item_d["amount"], 2)),
@@ -286,7 +283,7 @@ class Payslip(models.Model):
 
                         line_values_l.append(values)
 
-                ## generem la capçalera de l'assentament
+                # generrate he journal entry header
                 values = {
                     "date": rec.entry_date,
                     "ref": rec.name,
@@ -295,15 +292,15 @@ class Payslip(models.Model):
                     "line_ids": [(0, False, values) for values in line_values_l],
                 }
 
-                ## creem el moviment
+                # create the movement
                 move = self.env["account.move"].create(values)
 
                 if diff == 0:
-                    move.post()
+                    move.action_post()
 
                 rec.write({"move_id": move.id, "state": "posted"})
             else:
-                ## comprovem si estav descuaddrat
+                # check if it's unbalanced
                 diff = 0
                 if rec.labour_agreement_id.error_balancing_account_id:
                     diff = round(
@@ -319,16 +316,15 @@ class Payslip(models.Model):
                         2,
                     )
 
-                # si no esta desquadrat el fixem
+                # if it's not unbalanced we fixed it
                 if diff == 0:
-                    rec.move_id.post()
+                    rec.move_id.action_post()
                     rec.write({"state": "posted"})
                 else:
                     raise ValidationError(
                         _("The entry is unbalanced and it cannot be posted!")
                     )
 
-    @api.multi
     def action_paysplip_unpost(self):
         for rec in self:
             move = rec.move_id
@@ -338,7 +334,6 @@ class Payslip(models.Model):
             move.unlink()
             rec.write({"state": "validated"})
 
-    @api.multi
     def unlink(self):
         for rec in self:
             if rec.state not in ("draft",):
