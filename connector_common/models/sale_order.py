8992edc3bc8cfb0e5e2c9de51b3932d0dc9bada3
KNVx <kniubo@nuobit.com>
2021-12-09 15:14:46 +0100

    [MIG] connector_common: Migration to 14.0

--- connector_common/models/sale_order.py
+++ connector_common/models/sale_order.py
@@ -1,25 +1,21 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.multi
     def action_confirm(self):
         for rec in self:
             super(SaleOrder, rec).action_confirm()
             self._event("on_confirm_order").notify(rec)
-
         return True
 
-    @api.multi
     def action_cancel(self):
         for rec in self:
             super(SaleOrder, rec).action_cancel()
             self._event("on_cancel_order").notify(rec)
-
         return True
