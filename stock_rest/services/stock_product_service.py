b46adbb6445c47de86baa14701b8e08a433b92a8
KNVx <kniubo@nuobit.com>
2021-12-15 10:53:41 +0100

    [MIG] stock_rest: Migration to 14.0

--- stock_rest/services/stock_product_service.py
+++ stock_rest/services/stock_product_service.py
@@ -2,6 +2,7 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+from odoo import _
 from odoo.exceptions import MissingError, ValidationError
 
 from odoo.addons.component.core import Component
@@ -16,40 +17,46 @@ class ProductService(Component):
         Access to Product services
     """
 
-    def search(self, code=None, location_code=None):
+    def search(self, code=None, barcode=None, location_code=None):
         # get current user
         self._get_current_user()
         company = self._get_current_company()
         domain = [("company_id", "in", [company.id, False])]
         stock_domain = domain[:] + [("location_id.usage", "=", "internal")]
 
-        ## get locations
+        # get locations
         if location_code:
             location = self.env["stock.location"].search(
                 domain + [("code", "=", location_code), ("usage", "=", "internal")]
             )
             if not location:
-                raise MissingError("The location '%s' does not exist" % location_code)
+                raise MissingError(
+                    _("The location '%s' does not exist" % location_code)
+                )
             if len(location) > 1:
                 raise ValidationError(
-                    "There's more than one location with code '%s'" % location_code
+                    _("There's more than one location with code '%s'" % location_code)
                 )
             stock_domain.append(("location_id", "=", location.id))
 
-        ## get product
-        if code:
-            product = self.env["product.product"].search(
-                domain + [("default_code", "=", code)]
-            )
+        # get product by code
+        if code or barcode:
+            product_domain = domain[:]
+            if code:
+                product_domain += [("default_code", "=", code)]
+            if barcode:
+                product_domain += [("barcode", "=", barcode)]
+            product = self.env["product.product"].search(product_domain)
             if not product:
-                raise MissingError("The product '%s' does not exist" % code)
+                raise MissingError(
+                    _("The product not found with the code or barcode entered")
+                )
             if len(product) > 1:
                 raise ValidationError(
-                    "There's more than one product with code '%s'" % code
+                    _("There's more than one product with the code or barcode entered")
                 )
             stock_domain.append(("product_id", "=", product.id))
 
-        ## get stock
         # TODO: Use Lazy=True
         stock = self.env["stock.quant"].read_group(
             domain=stock_domain,
@@ -65,7 +72,7 @@ class ProductService(Component):
             data.setdefault(product, []).append(
                 {
                     "id": lot_id,
-                    "code": lot_name,
+                    "code": str(lot_name),
                     "quantity": s["quantity"],
                 }
             )
@@ -76,44 +83,45 @@ class ProductService(Component):
                 {
                     "id": product.id,
                     "code": product.default_code or None,
+                    "barcode": product.barcode or None,
                     "description": product.name,
                     "category_id": product.categ_id.id,
                     "category_name": product.categ_id.name,
                     "lot_type": product.tracking,
-                    "asset_category_id": product.sudo().asset_category_id.id or None,
-                    "asset_category_name": product.sudo().asset_category_id.name
-                    or None,
+                    # # "asset_category_id": product.sudo().asset_category_id.id or None,
+                    # # "asset_category_name": product.sudo().asset_category_id.name
+                    # # or None,
                     "lots": lots,
                 }
             )
-
         return {"rows": product_list}
 
     def _validator_search(self):
         return {
             "code": {"type": "string", "nullable": True, "empty": False},
+            "barcode": {"type": "string", "nullable": True, "empty": False},
             "location_code": {"type": "string", "nullable": True, "empty": False},
         }
 
     def _validator_return_search(self):
-
         return_schema = {
             "id": {"type": "integer", "required": True},
             "code": {"type": "string", "required": True, "nullable": True},
+            "barcode": {"type": "string", "required": True, "nullable": True},
             "description": {"type": "string", "required": True},
             "category_id": {"type": "integer", "required": True},
             "category_name": {"type": "string", "required": True},
             "lot_type": {"type": "string", "required": True},
-            "asset_category_id": {
-                "type": "integer",
-                "required": True,
-                "nullable": True,
-            },
-            "asset_category_name": {
-                "type": "string",
-                "required": True,
-                "nullable": True,
-            },
+            # # "asset_category_id": {
+            # #     "type": "integer",
+            # #     "required": True,
+            # #     "nullable": True,
+            # # },
+            # # "asset_category_name": {
+            # #     "type": "string",
+            # #     "required": True,
+            # #     "nullable": True,
+            # # },
             "lots": {
                 "type": "list",
                 "required": True,
