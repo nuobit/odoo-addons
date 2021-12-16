f1c71eaaa6503e3aef39471660682492eb09b825
KNVx <kniubo@nuobit.com>
2021-12-16 11:00:36 +0100

    [IMP] sale_report_product_type: black, isort, prettier

--- sale_report_product_type/__manifest__.py
+++ sale_report_product_type/__manifest__.py
@@ -10,7 +10,7 @@
     "version": "12.0.1.0.2",
     "category": "Reporting",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit",
+    "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "sale",
