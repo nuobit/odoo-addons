d3ca98afe6e5f10433a175e7c488a6eaf6e7fc94
KNVx <kniubo@nuobit.com>
2021-12-16 10:57:50 +0100

    [IMP] project_task_metatype: black, isort, prettier

--- project_task_metatype/__manifest__.py
+++ project_task_metatype/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Project",
-    "website": "https://github.com/nuobit",
+    "website": "https://github.com/nuobit/odoo-addons",
     "summary": "This module adds a metatype for task stages.",
     "depends": [
         "project",
