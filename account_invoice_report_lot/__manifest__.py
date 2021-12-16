64cb3dd17a2619b80996bc183d653f9bb5079e46
KNVx <kniubo@nuobit.com>
2021-12-16 10:47:50 +0100

    [IMP] account_invoice_report_lot: black, isort, prettier

--- account_invoice_report_lot/__manifest__.py
+++ account_invoice_report_lot/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Invoicing Management",
-    "website": "https://github.com/nuobit",
+    "website": "https://github.com/nuobit/odoo-addons",
     "summary": "This module adds a lot/serial number on lines of invoice reports",
     "depends": [
         "stock_picking_invoice_link",
