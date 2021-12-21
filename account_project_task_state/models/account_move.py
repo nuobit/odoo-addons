923eaf9b0f0c3f134e1b4b0bc603251c163d024b
KNVx <kniubo@nuobit.com>
2021-12-21 10:23:24 +0100

    [MIG] account_project_task_state: Migration to 14.0

--- account_project_task_state/models/account_move.py
+++ account_project_task_state/models/account_move.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 import datetime
 import logging
@@ -10,22 +10,25 @@ from odoo.exceptions import UserError
 _logger = logging.getLogger(__name__)
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
-    @api.multi
     def write(self, vals):
-        meta_type = {
-            ("draft", "open"): "done",
-            ("open", "paid"): "parking",
-        }.get((self.state, vals.get("state")))
-        if meta_type:
-            default_deposit_product_id = (
-                self.env["ir.config_parameter"]
-                .sudo()
-                .get_param("sale.default_deposit_product_id")
-            )
-            for rec in self:
+        for rec in self.filtered(lambda r: r.move_type == "out_invoice"):
+            if vals.get("payment_state") == "paid":
+                new_invoice_state = "paid"
+            else:
+                new_invoice_state = vals.get("state", rec.state)
+            meta_type = {
+                ("draft", "posted"): "done",
+                ("posted", "paid"): "parking",
+            }.get((rec.state, new_invoice_state))
+            if meta_type:
+                default_deposit_product_id = (
+                    self.env["ir.config_parameter"]
+                    .sudo()
+                    .get_param("sale.default_deposit_product_id")
+                )
                 if default_deposit_product_id:
                     if set(rec.invoice_line_ids.mapped("product_id").ids) == {
                         int(default_deposit_product_id)
@@ -53,14 +56,12 @@ class AccountInvoice(models.Model):
                                     )
                                 )
                             task.stage_id = task_type.id
-        return super(AccountInvoice, self).write(vals)
+        return super(AccountMove, self).write(vals)
 
     @api.model
     def archive_paid_invoices(self, days=10):
-        invoices = self.env["account.invoice"].search(
-            [
-                ("state", "=", "paid"),
-            ]
+        invoices = self.env["account.move"].search(
+            [("payment_state", "=", "paid"), ("move_type", "=", "out_invoice")]
         )
         _logger.info(
             "Archiving tasks of %i invoices paid %i days ago..." % (len(invoices), days)
@@ -68,9 +69,15 @@ class AccountInvoice(models.Model):
         archived = 0
         for invoice in invoices:
             last_payment_date = datetime.date(1900, 1, 1)
-            payment_ids = invoice.payment_ids.sorted("payment_date")
-            if payment_ids:
-                last_payment_date = payment_ids[-1].payment_date
+            payment_items = self.env["account.move.line"].search(
+                [
+                    ("id", "in", invoice.line_ids._reconciled_lines()),
+                    ("move_id", "!=", invoice.id),
+                ]
+            )
+
+            if payment_items:
+                last_payment_date = payment_items.sorted("date")[-1].date
             for order in invoice.mapped("invoice_line_ids.sale_line_ids.order_id"):
                 for task in order.tasks_ids.filtered("active"):
                     if (datetime.date.today() - last_payment_date).days >= days:
