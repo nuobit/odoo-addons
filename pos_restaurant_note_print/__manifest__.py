ee9cb5a9f27a78b938950a8119dfa051e947b0b9
KNVx <kniubo@nuobit.com>
2021-12-21 10:25:40 +0100

    [MIG] pos_restaurant_note_print: Migration to 14.0

--- pos_restaurant_note_print/__manifest__.py
+++ pos_restaurant_note_print/__manifest__.py
@@ -1,10 +1,9 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "POS restaurant line note receipt",
-    "version": "12.0.1.0.2",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Point of Sale",
