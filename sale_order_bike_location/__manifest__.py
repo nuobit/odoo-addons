23a3a74b4f8b04530c4721172885ffa8d4ce1cfb
KNVx <kniubo@nuobit.com>
2021-12-21 10:03:54 +0100

    [MIG] sale_order_bike_location: Migration to 14.0

--- sale_order_bike_location/__manifest__.py
+++ sale_order_bike_location/__manifest__.py
@@ -1,17 +1,16 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Sale order bike location",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Sale",
     "website": "https://github.com/nuobit/odoo-addons",
     "summary": "This module adds a Bike Location on sale orders",
     "depends": [
-        "sale",
+        "sale_management",
     ],
     "data": [
         "views/sale_order_views.xml",
