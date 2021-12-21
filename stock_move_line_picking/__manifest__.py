3606061de30874850488376f24d7d2443bd4e399
KNVx <kniubo@nuobit.com>
2021-12-21 10:33:47 +0100

    [MIG] stock_move_line_picking: Migration to 14.0

--- stock_move_line_picking/__manifest__.py
+++ stock_move_line_picking/__manifest__.py
@@ -1,11 +1,10 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock move line picking",
     "summary": "This module adds field picking to stock move line form view",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Custom",
