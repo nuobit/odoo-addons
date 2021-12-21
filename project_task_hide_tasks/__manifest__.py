f7d24cfba32e52723a610473897b46acace66909
KNVx <kniubo@nuobit.com>
2021-12-21 10:29:37 +0100

    [MIG] project_task_hide_tasks: Migration to 14.0

--- project_task_hide_tasks/__manifest__.py
+++ project_task_hide_tasks/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Task hide tasks",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Kilian Niubo",
     "license": "AGPL-3",
     "category": "Project",
