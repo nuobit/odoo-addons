e10548d03080a8a9935d9b83944d1de574576b9b
KNVx <kniubo@nuobit.com>
2021-12-21 09:59:51 +0100

    [MIG] project_timeline_default_view: Migration to 14.0

--- project_timeline_default_view/__manifest__.py
+++ project_timeline_default_view/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Project timeline default view",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Project",
