354dcdc0e59387a7eec2dffed0249a2f8c4e16d6
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-23 07:26:57 -0500

    [14.0][MIG] account_invoice_report_service: Migration to 14.0

--- account_invoice_report_service/models/__init__.py
+++ account_invoice_report_service/models/__init__.py
@@ -1,6 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import res_company
 from . import account_invoice
+from . import res_config_settings
