76a8f424118e8fd5a3bd37c8bb563e9337643457
KNVx <kniubo@nuobit.com>
2021-12-27 10:47:17 +0100

    [MIG] stock_barcodes_sku: Migration to 14.0

--- stock_barcodes_sku/__manifest__.py
+++ stock_barcodes_sku/__manifest__.py
@@ -1,5 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
@@ -7,7 +6,7 @@
     "summary": "This module enables you to scan barcodes that contain"
     " a SKU (default_code) additionally to the ones that "
     "contain regular barcodes.",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Extra tools",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
--- stock_barcodes_sku/wizard/stock_barcodes_read.py
+++ stock_barcodes_sku/wizard/stock_barcodes_read.py
@@ -1,10 +1,23 @@
-# Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2022 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import _, models
 
 
 class WizStockBarcodesRead(models.AbstractModel):
     _inherit = "wiz.stock.barcodes.read"
 
-    def _barcode_domain(self, barcode):
-        return ["|", ("barcode", "=", barcode), ("default_code", "=", barcode)]
+    def process_barcode(self, barcode):
+        self._set_messagge_info("success", _("Barcode read correctly"))
+        domain = self._barcode_domain(barcode)
+        product = self.env["product.product"].search(domain)
+        if not product:
+            # search product by sku
+            product = self.env["product.product"].search(
+                [("default_code", "=", barcode)]
+            )
+            if product:
+                self.action_product_scaned_post(product)
+                self.action_done()
+                return
+
+        return super(WizStockBarcodesRead, self).process_barcode(barcode)
