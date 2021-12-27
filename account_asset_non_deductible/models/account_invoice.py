74da8175d20edff855b3217d841739e60458c31d
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-27 08:40:12 -0500

    [IMP] account_asset_non_deductible: black, isort, prettier

--- account_asset_non_deductible/models/account_invoice.py
+++ account_asset_non_deductible/models/account_invoice.py
@@ -6,17 +6,20 @@ from odoo import models
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
     def _get_asset_base_value(self):
         super(AccountInvoiceLine, self)._get_asset_base_value()
         value = self.price_subtotal_signed
-        tax_non_deductible_percent = \
+        tax_non_deductible_percent = (
             self.invoice_line_tax_ids.get_non_deductible_percent(
                 self.invoice_id.date or self.invoice_id.date_invoice,
-                self.invoice_id.company_id.id or self.env.user.company_id.id
+                self.invoice_id.company_id.id or self.env.user.company_id.id,
             )
+        )
         if tax_non_deductible_percent:
-            value += round(value * tax_non_deductible_percent / 100,
-                           self.currency_id.decimal_places)
+            value += round(
+                value * tax_non_deductible_percent / 100,
+                self.currency_id.decimal_places,
+            )
         return value
