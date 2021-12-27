9d540affb6141fb8896105951ac692872921d77f
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-27 15:06:22 -0500

    [IMP] stock_orderpoint_manual_procurement_zero: black, isort, prettier

--- stock_orderpoint_manual_procurement_zero/__manifest__.py
+++ stock_orderpoint_manual_procurement_zero/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Stock Orderpoint Manual Procurement Zero",
-    'summary': "Extends the module 'stock_orderpoint_manual_procurement' allowing "
-               "users to remove procure recommendations with 0 quantity.",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
+    "name": "Stock Orderpoint Manual Procurement Zero",
+    "summary": "Extends the module 'stock_orderpoint_manual_procurement' allowing "
+    "users to remove procure recommendations with 0 quantity.",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
     "category": "Warehouse Management",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'stock_orderpoint_manual_procurement',
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock_orderpoint_manual_procurement",
     ],
     "data": [
         "wizards/make_procurement_orderpoint_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
