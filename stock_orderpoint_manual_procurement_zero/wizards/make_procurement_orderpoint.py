663c4ca06c5e002138dcf463c4ffdaf2160548b3
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-27 15:06:22 -0500

    [IMP] stock_orderpoint_manual_procurement_zero: black, isort, prettier

--- stock_orderpoint_manual_procurement_zero/wizards/make_procurement_orderpoint.py
+++ stock_orderpoint_manual_procurement_zero/wizards/make_procurement_orderpoint.py
@@ -2,26 +2,25 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
+from odoo import api, models
 
 
 class MakeProcurementOrderpoint(models.TransientModel):
-    _inherit = 'make.procurement.orderpoint'
+    _inherit = "make.procurement.orderpoint"
 
     @api.model
     def default_get(self, fields):
         res = super(MakeProcurementOrderpoint, self).default_get(fields)
-        show_zeros = self.env.context.get('show_recommended_procure_zero', True)
+        show_zeros = self.env.context.get("show_recommended_procure_zero", True)
         if not show_zeros:
-            res['item_ids'] = list(filter(lambda x: x[2]['qty'] != 0, res['item_ids']))
+            res["item_ids"] = list(filter(lambda x: x[2]["qty"] != 0, res["item_ids"]))
         return res
 
     @api.multi
     def remove_zeros(self):
-        action_id = self.env.context['params']['action']
-        wizard_action = self.env['ir.actions.act_window'].browse(action_id).read()[0]
+        action_id = self.env.context["params"]["action"]
+        wizard_action = self.env["ir.actions.act_window"].browse(action_id).read()[0]
         wizard_action_context = dict(self.env.context)
-        wizard_action_context['show_recommended_procure_zero'] = False
-        wizard_action['context'] = str(wizard_action_context)
+        wizard_action_context["show_recommended_procure_zero"] = False
+        wizard_action["context"] = str(wizard_action_context)
         return wizard_action
