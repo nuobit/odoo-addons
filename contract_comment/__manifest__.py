13608bfac0f0c11a768c504ee481cf6bebda3e03
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-30 10:07:09 -0500

    [IMP] contract_comment: black, isort, prettier

--- contract_comment/__manifest__.py
+++ contract_comment/__manifest__.py
@@ -3,19 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Contract comment',
-    'summary': 'This module adds a comment field in the contract and propagates it to the comment field of the invoice',
-    'version': '11.0.0.1.0',
-    'category': 'Contract Management',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'contract',
+    "name": "Contract comment",
+    "summary": "This module adds a comment field in the contract and propagates it to the comment field of the invoice",
+    "version": "11.0.0.1.0",
+    "category": "Contract Management",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "contract",
     ],
-    'data': [
-        'views/account_analytic_contract_view.xml',
+    "data": [
+        "views/account_analytic_contract_view.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
