d0d841cd1552b862f0516921456daadd29e5361e
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-30 10:07:09 -0500

    [IMP] contract_comment: black, isort, prettier

--- contract_comment/models/models.py
+++ contract_comment/models/models.py
@@ -2,20 +2,18 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models, fields
-from odoo.exceptions import ValidationError
-from odoo.tools.translate import _
+from odoo import api, fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit = 'account.analytic.account'
+    _inherit = "account.analytic.account"
 
-    comment = fields.Text('Additional Information')
+    comment = fields.Text("Additional Information")
 
     @api.multi
     def _prepare_invoice(self):
         invoice = super()._prepare_invoice()
 
-        invoice.update({'comment': self.comment})
+        invoice.update({"comment": self.comment})
 
         return invoice
