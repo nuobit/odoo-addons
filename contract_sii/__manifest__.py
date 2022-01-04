5110621e6fa07a0bd2d104c62538c2d3755d2d63
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-04 11:29:16 -0500

    [IMP] contract_sii: black, isort, prettier

--- contract_sii/__manifest__.py
+++ contract_sii/__manifest__.py
@@ -3,21 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Contract SII',
-    'summary': 'This module adds SII data to contracts and propagate them to invoice',
-    'version': '11.0.1.0.0',
-    'category': 'Contract Management',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://www.nuobit.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'contract',
-        'l10n_es_aeat_sii',
-
+    "name": "Contract SII",
+    "summary": "This module adds SII data to contracts and propagate them to invoice",
+    "version": "11.0.1.0.0",
+    "category": "Contract Management",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "contract",
+        "l10n_es_aeat_sii",
     ],
-    'data': [
-        'views/account_analytic_contract_view.xml',
+    "data": [
+        "views/account_analytic_contract_view.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
