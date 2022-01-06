82521d92be5526976ec43d99df00a3fee29353c4
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-06 11:56:15 -0500

    [14.0][MIG] l10n_es_facturae_service : Migration to 14.0

--- l10n_es_facturae_service/models/sale_order.py
+++ l10n_es_facturae_service/models/sale_order.py
@@ -2,48 +2,19 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
-from odoo.exceptions import UserError
-from odoo.tools.translate import _
+from odoo import models
 
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class SaleOrderLine(models.Model):
 
-    @api.multi
-    def action_invoice_create(self, grouped=False, final=False):
-        invoice_ids = super(SaleOrder, self).action_invoice_create(
-            grouped=grouped, final=final
+    _inherit = "sale.order.line"
+
+    def _prepare_invoice_line(self, **optional_values):
+        self.ensure_one()
+        order = self.order_id
+        return super()._prepare_invoice_line(
+            facturae_file_reference=order.insured_ident_cardnum,
+            facturae_receiver_transaction_reference=order.policy_number,
+            facturae_receiver_contract_reference=order.auth_number,
+            facturae_receiver_transaction_date=order.service_date,
         )
-        for invoice in self.env["account.invoice"].browse(invoice_ids):
-            if invoice.facturae:
-                for line in invoice.invoice_line_ids:
-                    order = line.sale_line_ids.mapped("order_id")
-                    if order:
-                        if len(order) > 1:
-                            raise UserError(
-                                _(
-                                    "The invoice line with id '%i' references more than one order"
-                                )
-                                % invoice.id
-                            )
-                        values = {}
-                        if order.insured_ident_cardnum:
-                            values[
-                                "facturae_file_reference"
-                            ] = order.insured_ident_cardnum
-                        if order.policy_number:
-                            values[
-                                "facturae_receiver_transaction_reference"
-                            ] = order.policy_number
-                        if order.auth_number:
-                            values[
-                                "facturae_receiver_contract_reference"
-                            ] = order.auth_number
-                        if order.service_date:
-                            values[
-                                "facturae_receiver_transaction_date"
-                            ] = order.service_date
-                        if values:
-                            line.write(values)
-        return invoice_ids
