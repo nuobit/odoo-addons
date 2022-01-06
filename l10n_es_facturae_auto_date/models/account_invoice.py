9d028404e5bd91171fd85723eae1313f980e64de
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-06 12:11:48 -0500

    [IMP] l10n_es_facturae_auto_date: black, isort, prettier

--- l10n_es_facturae_auto_date/models/account_invoice.py
+++ l10n_es_facturae_auto_date/models/account_invoice.py
@@ -4,11 +4,11 @@
 import calendar
 import datetime
 
-from odoo import fields, api, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     def _get_facturae_dates(self, date_invoice):
         date_invoice = fields.Date.from_string(date_invoice)
@@ -21,47 +21,63 @@ class AccountInvoice(models.Model):
         )
         return facturae_start_date, facturae_end_date
 
-    @api.onchange('date_invoice')
+    @api.onchange("date_invoice")
     def onchange_date_invoice(self):
         partner_id = self.partner_id.parent_id or self.partner_id
         if self.facturae and self.date_invoice and partner_id.facturae_auto_dates:
-            self.facturae_start_date, self.facturae_end_date = \
-                self._get_facturae_dates(self.date_invoice)
+            self.facturae_start_date, self.facturae_end_date = self._get_facturae_dates(
+                self.date_invoice
+            )
         else:
             self.facturae_start_date = False
             self.facturae_end_date = False
 
     @api.model
     def create(self, vals):
-        partner_id = vals.get('partner_id')
+        partner_id = vals.get("partner_id")
         if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
+            partner = self.env["res.partner"].browse(partner_id)
             partner = partner.parent_id or partner
-            facturae = vals.get('facturae', partner.facturae)
-            auto_date = vals.get('facturae_auto_dates', partner.facturae_auto_dates)
+            facturae = vals.get("facturae", partner.facturae)
+            auto_date = vals.get("facturae_auto_dates", partner.facturae_auto_dates)
             if facturae and auto_date:
-                date_invoice = vals.get('date_invoice')
+                date_invoice = vals.get("date_invoice")
                 if date_invoice:
-                    if vals.get('facturae_start_date') != False and vals.get('facturae_end_date') != False:
-                        vals['facturae_start_date'], vals['facturae_end_date'] = \
-                            self._get_facturae_dates(date_invoice)
+                    if (
+                        vals.get("facturae_start_date") != False
+                        and vals.get("facturae_end_date") != False
+                    ):
+                        (
+                            vals["facturae_start_date"],
+                            vals["facturae_end_date"],
+                        ) = self._get_facturae_dates(date_invoice)
 
         return super(AccountInvoice, self).create(vals)
 
     @api.multi
     def write(self, vals):
         for rec in self:
-            facturae = vals.get('facturae', rec.facturae)
-            auto_date = vals.get('facturae_auto_dates',
-                                 (rec.partner_id.parent_id or rec.partner_id).facturae_auto_dates)
+            facturae = vals.get("facturae", rec.facturae)
+            auto_date = vals.get(
+                "facturae_auto_dates",
+                (rec.partner_id.parent_id or rec.partner_id).facturae_auto_dates,
+            )
             if facturae and auto_date:
-                date_invoice = vals.get('date_invoice')
+                date_invoice = vals.get("date_invoice")
                 if date_invoice is not None:
-                    if vals.get('facturae_start_date') != False and vals.get('facturae_end_date') != False:
+                    if (
+                        vals.get("facturae_start_date") != False
+                        and vals.get("facturae_end_date") != False
+                    ):
                         if date_invoice:
-                            vals['facturae_start_date'], vals['facturae_end_date'] = \
-                                self._get_facturae_dates(date_invoice)
+                            (
+                                vals["facturae_start_date"],
+                                vals["facturae_end_date"],
+                            ) = self._get_facturae_dates(date_invoice)
                         else:
-                            vals['facturae_start_date'], vals['facturae_end_date'] = False, False
+                            vals["facturae_start_date"], vals["facturae_end_date"] = (
+                                False,
+                                False,
+                            )
 
         return super(AccountInvoice, self).write(vals)
--- l10n_es_facturae_auto_date/tests/test_account_invoice.py
+++ l10n_es_facturae_auto_date/tests/test_account_invoice.py
@@ -16,24 +16,28 @@ class TestAccountInvoice(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         # ARRANGE
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'Test Partner',
-            'vat': 'ESA12345674',
-            'facturae': True,
-            'is_company': True,
-            'customer': True,
-            'country_id': cls.env.ref('base.es').id,
-            'state_id': cls.env.ref('base.state_es_b').id,
-        })
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+                "vat": "ESA12345674",
+                "facturae": True,
+                "is_company": True,
+                "customer": True,
+                "country_id": cls.env.ref("base.es").id,
+                "state_id": cls.env.ref("base.state_es_b").id,
+            }
+        )
 
     def test_01(self):
         # ACT
         self.partner.facturae_auto_dates = True
         date_invoice = datetime.date(2021, 4, 1)
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.partner.id,
-            'date_invoice': fields.Date.to_string(date_invoice),
-        })
+        invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner.id,
+                "date_invoice": fields.Date.to_string(date_invoice),
+            }
+        )
         # force partner onchange to populate 'factuare' field
         invoice._onchange_partner_id()
 
@@ -41,22 +45,27 @@ class TestAccountInvoice(SavepointCase):
         _, month_days = calendar.monthrange(date_invoice.year, date_invoice.month)
         expected_values = [
             datetime.date(date_invoice.year, date_invoice.month, 1),
-            datetime.date(date_invoice.year, date_invoice.month, month_days)
+            datetime.date(date_invoice.year, date_invoice.month, month_days),
         ]
         self.assertEqual(
-            [fields.Date.from_string(x) for x in
-             [invoice.facturae_start_date, invoice.facturae_end_date]],
+            [
+                fields.Date.from_string(x)
+                for x in [invoice.facturae_start_date, invoice.facturae_end_date]
+            ],
             expected_values,
-            'The FacturaE start and end dates does not match to the invoice date')
+            "The FacturaE start and end dates does not match to the invoice date",
+        )
 
     def test_02(self):
         # ARRANGE
         self.partner.facturae_auto_dates = True
         date_invoice = datetime.date(2021, 4, 13)
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.partner.id,
-            'date_invoice': fields.Date.to_string(date_invoice),
-        })
+        invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner.id,
+                "date_invoice": fields.Date.to_string(date_invoice),
+            }
+        )
         # force partner onchange to populate 'factuare' field
         invoice._onchange_partner_id()
 
@@ -68,22 +77,27 @@ class TestAccountInvoice(SavepointCase):
         _, month_days = calendar.monthrange(date_invoice2.year, date_invoice2.month)
         expected_values = [
             datetime.date(date_invoice2.year, date_invoice2.month, 1),
-            datetime.date(date_invoice2.year, date_invoice2.month, month_days)
+            datetime.date(date_invoice2.year, date_invoice2.month, month_days),
         ]
         self.assertEqual(
-            [fields.Date.from_string(x) for x in
-             [invoice.facturae_start_date, invoice.facturae_end_date]],
+            [
+                fields.Date.from_string(x)
+                for x in [invoice.facturae_start_date, invoice.facturae_end_date]
+            ],
             expected_values,
-            'The FacturaE start and end dates does not match to the invoice date')
+            "The FacturaE start and end dates does not match to the invoice date",
+        )
 
     def test_03(self):
         # ARRANGE
         self.partner.facturae_auto_dates = True
         date_invoice = datetime.date(2021, 4, 13)
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.partner.id,
-            'date_invoice': fields.Date.to_string(date_invoice),
-        })
+        invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner.id,
+                "date_invoice": fields.Date.to_string(date_invoice),
+            }
+        )
         # force partner onchange to populate 'factuare' field
         invoice._onchange_partner_id()
 
@@ -92,31 +106,36 @@ class TestAccountInvoice(SavepointCase):
 
         # ASSERT
         self.assertEqual(
-            [fields.Date.from_string(x) for x in
-             [invoice.facturae_start_date, invoice.facturae_end_date]],
+            [
+                fields.Date.from_string(x)
+                for x in [invoice.facturae_start_date, invoice.facturae_end_date]
+            ],
             [None, None],
-            'The FacturaE start and end dates does not match to the invoice date')
+            "The FacturaE start and end dates does not match to the invoice date",
+        )
 
     def test_04(self):
         # ARRANGE
         self.partner.facturae_auto_dates = True
         date_invoice = datetime.date(2021, 4, 13)
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.partner.id,
-            'date_invoice': fields.Date.to_string(date_invoice),
-        })
+        invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.partner.id,
+                "date_invoice": fields.Date.to_string(date_invoice),
+            }
+        )
         # force partner onchange to populate 'factuare' field
         invoice._onchange_partner_id()
 
         # ACT
-        invoice.write({
-            'facturae_start_date': False,
-            'facturae_end_date': False
-        })
+        invoice.write({"facturae_start_date": False, "facturae_end_date": False})
 
         # ASSERT
         self.assertEqual(
-            [fields.Date.from_string(x) for x in
-             [invoice.facturae_start_date, invoice.facturae_end_date]],
+            [
+                fields.Date.from_string(x)
+                for x in [invoice.facturae_start_date, invoice.facturae_end_date]
+            ],
             [None, None],
-            'The FacturaE start and end dates does not match to the invoice date')
+            "The FacturaE start and end dates does not match to the invoice date",
+        )
