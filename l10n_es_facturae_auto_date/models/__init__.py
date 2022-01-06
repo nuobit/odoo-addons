3be887889a5bf0bdc139938c660e91dab5310aa6
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-06 15:38:14 -0500

    [14.0][MIG] l10n_es_facturae_auto_date : Migration to 14.0

--- l10n_es_facturae_auto_date/models/__init__.py
+++ l10n_es_facturae_auto_date/models/__init__.py
@@ -1,6 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from . import res_partner
-from . import account_invoice
+from . import account_move
