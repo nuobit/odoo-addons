8b4c389b8b77e0430dc93ce4f12a9eb8a58a116f
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-06 12:11:48 -0500

    [IMP] l10n_es_facturae_auto_date: black, isort, prettier

--- l10n_es_facturae_auto_date/__manifest__.py
+++ l10n_es_facturae_auto_date/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'FacturaE auto dates',
-    'summary': 'This module computes automatically the FacturaE start date '
-               'and end date from date invoice',
-    'version': '11.0.1.2.1',
-    'category': 'Accounting',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'website': 'https://github.com/nuobit',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es_facturae',
+    "name": "FacturaE auto dates",
+    "summary": "This module computes automatically the FacturaE start date "
+    "and end date from date invoice",
+    "version": "11.0.1.2.1",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_facturae",
     ],
-    'data': [
-        'views/res_partner_views.xml',
+    "data": [
+        "views/res_partner_views.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['eantones'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
 }
--- l10n_es_facturae_auto_date/models/res_partner.py
+++ l10n_es_facturae_auto_date/models/res_partner.py
@@ -1,13 +1,11 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import calendar
-import datetime
 
-from odoo import fields, api, models
+from odoo import fields, models
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    facturae_auto_dates = fields.Boolean('Auto Dates')
+    facturae_auto_dates = fields.Boolean("Auto Dates")
