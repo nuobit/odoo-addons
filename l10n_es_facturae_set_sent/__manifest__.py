4a5bee0b87a87c312d9913a4709bfdf0578a4d32
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-06 12:00:13 -0500

    [IMP] l10n_es_facturae_set_sent: black, isort, prettier

--- l10n_es_facturae_set_sent/__manifest__.py
+++ l10n_es_facturae_set_sent/__manifest__.py
@@ -3,17 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'FacturaE set sent flag',
-    'summary': 'Set invoice "sent" flag after successfully facturae creation',
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Accounting',
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'depends': [
-        'l10n_es_facturae',
+    "name": "FacturaE set sent flag",
+    "summary": 'Set invoice "sent" flag after successfully facturae creation',
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Accounting",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "l10n_es_facturae",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
--- l10n_es_facturae_set_sent/wizard/create_facturae.py
+++ l10n_es_facturae_set_sent/wizard/create_facturae.py
@@ -2,10 +2,8 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-import base64
 
-from odoo import api, fields, models, _
-from odoo.exceptions import Warning as UserError
+from odoo import api, models
 
 
 class CreateFacturae(models.TransientModel):
@@ -15,8 +13,8 @@ class CreateFacturae(models.TransientModel):
     def create_facturae_file(self):
         res = super(CreateFacturae, self).create_facturae_file()
 
-        invoice_ids = self.env.context.get('active_ids', [])
-        invoice = self.env['account.invoice'].browse(invoice_ids[0])
+        invoice_ids = self.env.context.get("active_ids", [])
+        invoice = self.env["account.invoice"].browse(invoice_ids[0])
         invoice.sent = True
 
         return res
