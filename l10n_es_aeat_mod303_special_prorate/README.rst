dc891f3b1993050f8e9b67e4fc3faa2bd6f39ccf
Eric Antones <eantones@nuobit.com>
2022-01-11 17:40:57 +0100

    [ADD] l10n_es_aeat_mod303_special_prorate: Adds prorate taxes to 303 mapping

---
+++ l10n_es_aeat_mod303_special_prorate/README.rst
@@ -0,0 +1,25 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+====================================
+Special prorate tax mapping for 303
+====================================
+
+* This module adds special prorate template taxes
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/nuobit/odoo-addons/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Eric Antones <eantones@nuobit.com>
---
+++ l10n_es_aeat_mod303_special_prorate/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Special prorate tax mapping for 303",
+    "summary": "This module adds the 303 model special prorate taxes",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_special_prorate",
+        "l10n_es_aeat_mod303",
+    ],
+    "data": [
+        "data/tax_code_map_mod303_data.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
