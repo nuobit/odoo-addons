be7cb2216191ac8db41412e530444309166f2010
Eric Antones <eantones@nuobit.com>
2022-01-11 17:41:34 +0100

    [ADD] l10n_es_special_prorrate: Prorrate tax templates

---
+++ l10n_es_special_prorrate/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Special prorrate template taxes",
+    "summary": "This module adds special prorrate template taxes",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es",
+    ],
+    "data": [
+        "data/account_tax_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
