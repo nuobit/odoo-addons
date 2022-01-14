cf4491ac1435f282c1d98964edbeb9522145dd71
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-01-14 15:18:06 +0100

    [ADD] oxigen_stock

---
+++ oxigen_stock/README.rst
@@ -0,0 +1,14 @@
+==================
+Oxigen Stock
+==================
+
+* This module allows to set expiry dates and description when creating a lot
+from a picking.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maria de Luna <maria.de.luna@forgeflow.com>
---
+++ oxigen_stock/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Stock",
+    "summary": "Customizations for Oxigen in Stock. "
+    "description when creating a lot from a picking",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": ["views/stock_production_lot_views.xml"],
+}
