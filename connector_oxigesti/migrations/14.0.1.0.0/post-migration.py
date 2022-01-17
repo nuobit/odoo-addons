9e98724e21effda869fb71e55e56397fb0dc722c
Miquel Raïch <miquel.raich@forgeflow.com>
2022-01-17 16:40:15 +0100

    [FIX] connector_oxigesti: company rule migration

---
+++ connector_oxigesti/migrations/14.0.1.0.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2022 ForgeFlow S.L.  <https://www.forgeflow.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# pylint: disable=C7902
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "connector_oxigesti", "migrations/14.0.1.0.0/noupdate_changes.xml"
+    )
