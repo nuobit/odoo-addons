6d49ac5c7dd6a32303d30af786556d3bfed391bc
Eric Antones <eantones@nuobit.com>
2022-02-08 12:08:32 +0100

    [ADD] account_template_view: show all account template fields

---
+++ account_template_view/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Account Template View",
+    "summary": "This module enables all fields on *Account Tax Templates* views",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account_menu"],
+    "data": [
+        "views/account_tax_template_views.xml",
+    ],
+    "installable": True,
+}
