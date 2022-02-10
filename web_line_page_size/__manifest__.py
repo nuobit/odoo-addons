a61edb4de4e74d230ab6915d6c934fa52fd8551e
KNVx <kniubo@nuobit.com>
2022-02-10 13:03:02 +0100

    [ADD] web_line_page_size: created module to increase lines rendered in the page

---
+++ web_line_page_size/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "web line page size",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module sets the size of the page lines to 200",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/webclient_templates.xml",
+    ],
+    "installable": True,
+}
