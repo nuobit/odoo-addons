550900f9b3e01d5ec551c2e385bd4d36178d150d
KNVx <kniubo@nuobit.com>
2022-02-15 10:12:57 +0100

    [ADD] sale_order_margin: new module to add margin in Sales orders

---
+++ sale_order_margin/models/sale_order.py
@@ -0,0 +1,11 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class SaleOrdermargin(models.Model):
+    _inherit = "sale.order"
+
+    margin_of = fields.Integer()
