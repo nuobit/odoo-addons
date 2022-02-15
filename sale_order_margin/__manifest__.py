a184e1e153205816e95e996ceccaa712cc8cfca4
KNVx <kniubo@nuobit.com>
2022-02-15 10:12:57 +0100

    [ADD] sale_order_margin: new module to add margin in Sales orders

---
+++ sale_order_margin/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale order margin",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module adds a margin on sale orders",
+    "depends": [
+        "sale_management",
+    ],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
