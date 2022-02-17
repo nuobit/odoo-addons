4318c2f3d147aadfa38c4d75959a81732210bb50
Lois Rilo <lois.rilo@forgeflow.com>
2022-02-17 11:22:24 +0100

    [IMP] stock_barcodes_gs1_extended: black, isort, prettier

--- stock_barcodes_gs1_extended/wizard/stock_barcodes_read_picking.py
+++ stock_barcodes_gs1_extended/wizard/stock_barcodes_read_picking.py
@@ -3,23 +3,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import logging
+
 from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
 
 class WizStockBarcodesReadPicking(models.TransientModel):
-    _inherit = 'wiz.stock.barcodes.read.picking'
+    _inherit = "wiz.stock.barcodes.read.picking"
 
     picking_type_id = fields.Many2one(
-        comodel_name='stock.picking.type',
-        string='Picking type',
+        comodel_name="stock.picking.type",
+        string="Picking type",
         readonly=True,
     )
 
     def _prepare_stock_moves_domain(self):
         domain = super(WizStockBarcodesReadPicking, self)._prepare_stock_moves_domain()
         domain.append(
-            ('picking_id.picking_type_id', '=', self.picking_type_id.id),
+            ("picking_id.picking_type_id", "=", self.picking_type_id.id),
         )
         return domain
--- stock_barcodes_gs1_extended/wizard/stock_production_lot.py
+++ stock_barcodes_gs1_extended/wizard/stock_production_lot.py
@@ -2,14 +2,14 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models
-
 import re
+
+from odoo import models
 
 
 class WizStockBarcodesNewLot(models.TransientModel):
-    _inherit = 'wiz.stock.barcodes.new.lot'
-    _description = 'Wizard to create new lot from barcode scanner'
+    _inherit = "wiz.stock.barcodes.new.lot"
+    _description = "Wizard to create new lot from barcode scanner"
 
     def on_barcode_scanned(self, barcode):
         try:
@@ -17,38 +17,40 @@ class WizStockBarcodesNewLot(models.TransientModel):
         except Exception:
             return super().on_barcode_scanned(barcode)
 
-        contained_product_barcode = barcode_decoded.get('02', False)
+        contained_product_barcode = barcode_decoded.get("02", False)
         if contained_product_barcode:
             return super().on_barcode_scanned(barcode)
 
-        lot_barcode = barcode_decoded.get('10', False)
-        serial_barcode = barcode_decoded.get('21', False)
+        lot_barcode = barcode_decoded.get("10", False)
+        serial_barcode = barcode_decoded.get("21", False)
         if not lot_barcode and not serial_barcode:
             return
 
-        product_barcode = barcode_decoded.get('01', False)
+        product_barcode = barcode_decoded.get("01", False)
         if not product_barcode:
             # Sometimes the product does not yet have a GTIN. In this case
             # try the AI 240 'Additional product identification assigned
             # by the manufacturer'.
-            product_barcode = barcode_decoded.get('240', False)
+            product_barcode = barcode_decoded.get("240", False)
 
         if product_barcode:
-            m = re.match(r'^0*([0-9]+)$', product_barcode)
+            m = re.match(r"^0*([0-9]+)$", product_barcode)
             if not m:
                 return
             product_barcode_trim = m.group(1)
 
-            product = self.env['product.product'].search([
-                ('company_id', '=', self.env.user.company_id.id),
-                ('barcode', '=like', '%' + product_barcode_trim),
-            ])
+            product = self.env["product.product"].search(
+                [
+                    ("company_id", "=", self.env.user.company_id.id),
+                    ("barcode", "=like", "%" + product_barcode_trim),
+                ]
+            )
             if not product:
                 return
             else:
                 if len(product) != 1:
                     return
-                m = re.match(r'^0*(%s)$' % product_barcode_trim, product.barcode)
+                m = re.match(r"^0*(%s)$" % product_barcode_trim, product.barcode)
                 if not m:
                     return
 
@@ -56,9 +58,9 @@ class WizStockBarcodesNewLot(models.TransientModel):
                     self.product_id = product
 
         if self.product_id:
-            if self.product_id.tracking == 'serial':
+            if self.product_id.tracking == "serial":
                 if serial_barcode:
                     self.lot_name = serial_barcode
-            elif self.product_id.tracking == 'lot':
+            elif self.product_id.tracking == "lot":
                 if lot_barcode:
                     self.lot_name = lot_barcode
