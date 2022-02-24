8ea814b57dfb72444de5c5f5c5bf4a69fc3cccb0
KNVx <kniubo@nuobit.com>
2022-02-24 09:44:55 +0100

    [FIX] base_report_gs1_barcode: barcode function don't accept optional values

--- base_report_gs1_barcode/__manifest__.py
+++ base_report_gs1_barcode/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright 2021 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2022 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Report GS1 barcodes",
     "summary": "This module adds a GS1-128 and GS1-Datamatrix barcode format support",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Reporting",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
