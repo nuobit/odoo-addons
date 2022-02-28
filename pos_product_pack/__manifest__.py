54e206f03ded216a886ae50dcae3939552aad983
KNVx <kniubo@nuobit.com>
2022-02-28 16:03:32 +0100

    [FIX] pos_product_pack: detect products in pack without available in POS check

--- pos_product_pack/__manifest__.py
+++ pos_product_pack/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "POS product pack",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Point of Sale",
