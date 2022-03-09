032ae13562d37b1c1f97aee62b440523c478ba18
Eric Antones <eantones@nuobit.com>
2022-03-09 22:43:10 +0100

    [MIG] l10n_es_aeat_vat_special_prorrate: Migration to 14.0

--- l10n_es_aeat_vat_special_prorrate/models/__init__.py
+++ l10n_es_aeat_vat_special_prorrate/models/__init__.py
@@ -1,7 +1,3 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 from . import aeat_map_special_prorrate_year
 from . import account_tax
-from . import account_invoice
+from . import account_move
---
+++ l10n_es_aeat_vat_special_prorrate/models/account_move.py
@@ -0,0 +1,49 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+
+
+def prorate_context(invoice):
+    return {
+        "date": invoice.date
+        or invoice.invoice_date
+        or fields.Date.context_today(invoice),
+        "company_id": invoice.company_id.id,
+    }
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _recompute_tax_lines(self, recompute_tax_base_amount=False):
+        self = self.with_context(prorate=prorate_context(self))
+        return super(AccountMove, self)._recompute_tax_lines(
+            recompute_tax_base_amount=recompute_tax_base_amount
+        )
+
+    @api.onchange("invoice_date", "date")
+    def _onchange_dates(self):
+        self._recompute_tax_lines()
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.model
+    def _get_price_total_and_subtotal_model(
+        self,
+        price_unit,
+        quantity,
+        discount,
+        currency,
+        product,
+        partner,
+        taxes,
+        move_type,
+    ):
+        if taxes:
+            taxes = taxes.with_context(prorate=prorate_context(self.move_id))
+        return super()._get_price_total_and_subtotal_model(
+            price_unit, quantity, discount, currency, product, partner, taxes, move_type
+        )
