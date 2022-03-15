ea042b58e8054b5b51f2d72a19d64564271501ca
Eric Antones <eantones@nuobit.com>
2022-03-15 12:43:24 +0100

    [ADD] stock_picking_date_filter: New module to filter pickings by today date

---
+++ stock_picking_date_filter/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Picking Date Filter",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module adds date filters to stock picking tree view",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
