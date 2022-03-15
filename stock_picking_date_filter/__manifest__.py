1b2458d8c6823ec0a469e241578ea4913ee81622
Eric Antones <eantones@nuobit.com>
2022-03-15 18:17:02 +0100

    [IMP] stock_picking_date_filter: Filter using transfer date

--- stock_picking_date_filter/__manifest__.py
+++ stock_picking_date_filter/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Stock Picking Date Filter",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "NuoBiT Solutions, S.L.",
     "license": "AGPL-3",
     "category": "Stock",
