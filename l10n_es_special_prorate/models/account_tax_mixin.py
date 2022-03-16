22260b11757658a60a4f59d305e56c46dc4bedc1
Lois Rilo <lois.rilo@forgeflow.com>
2022-03-16 10:31:31 +0100

    [TEMP] change ValidationError by loggers

--- l10n_es_special_prorate/models/account_tax_mixin.py
+++ l10n_es_special_prorate/models/account_tax_mixin.py
@@ -1,28 +1,39 @@
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.exceptions import ValidationError
+import logging
+
+# from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
 # TODO: This is a workaround, it should be a mixin but Odoo threw the error:
 #       Many2many fields AccountTaxTemplate.children_tax_ids and
 #           account.tax.template.children_tax_ids use the same table and columns
 
+_logger = logging.getLogger(__name__)
+
 
 def check_prorate(self):
     for tax in self:
         if tax.prorate:
             if tax.amount_type != "percent":
-                raise ValidationError(
+                _logger.error(
                     _(
                         "On prorate taxes it's only supported "
                         "'percent' as a amount type"
                     )
                 )
+                # raise ValidationError(
+                #     _(
+                #         "On prorate taxes it's only supported "
+                #         "'percent' as a amount type"
+                #     )
+                # )
             if tax.type_tax_use != "purchase":
-                raise ValidationError(
-                    _("On prorate taxes it's only supported 'purchase' type")
-                )
+                _logger.error(_("On prorate taxes it's only supported 'purchase' type"))
+                # raise ValidationError(
+                #     _("On prorate taxes it's only supported 'purchase' type")
+                # )
             invoice_tax_positive_repartition_lines = (
                 tax.invoice_repartition_line_ids.filtered(
                     lambda x: x.repartition_type == "tax" and x.factor_percent > 0
@@ -37,9 +48,12 @@ def check_prorate(self):
                 len(invoice_tax_positive_repartition_lines) != 2
                 or len(refund_tax_positive_repartition_lines) != 2
             ):
-                raise ValidationError(
-                    _(
-                        "On prorate taxes it's only supported "
-                        "two tax repartition lines"
-                    )
+                _logger.error(
+                    _("On prorate taxes it's only supported two tax repartition lines")
                 )
+                # raise ValidationError(
+                #     _(
+                #         "On prorate taxes it's only supported "
+                #         "two tax repartition lines"
+                #     )
+                # )
