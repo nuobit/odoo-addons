5d4672b8f4e5b7ebd35eb26bb4522fe24928d6d0
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-03-17 11:50:56 +0100

    [IMP] oxigen_stock: change complete_name

--- oxigen_stock/README.rst
+++ oxigen_stock/README.rst
@@ -6,6 +6,7 @@ Oxigen Stock
   from a picking.
 * Makes available to select a View Location in an Inventory Adjustment, then in the inventory lines all child locations will appear.
 * Sets default company as the current company in product creation
+* Defines Complete name of locations as in v11, taking the full path of parent locations
 
 Credits
 =======
--- oxigen_stock/models/stock_location.py
+++ oxigen_stock/models/stock_location.py
@@ -1,10 +1,38 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class StockLocation(models.Model):
     _inherit = "stock.location"
 
     name = fields.Char(translate=True)
+
+    @api.depends("name", "location_id.complete_name")
+    def _compute_complete_name(self):
+        # we set the method as in v11 where complete_name has the full path
+        """ Forms complete name of location from parent location to child location. """
+        for location in self:
+            if location.location_id.complete_name:
+                location.complete_name = "%s/%s" % (
+                    location.location_id.complete_name,
+                    location.name,
+                )
+            else:
+                location.complete_name = location.name
+
+    def name_get(self):
+        # v11 method
+        ret_list = []
+        for location in self:
+            orig_location = location
+            name = location.name
+            while location.location_id and location.usage != "view":
+                location = location.location_id
+                if not name:
+                    raise UserError(_("You have to set a name for this location."))
+                name = location.name + "/" + name
+            ret_list.append((orig_location.id, name))
+        return ret_list
