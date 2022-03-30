db569cb44838cb554ee338a7f4ea035b9dc9132a
Eric Antones <eantones@nuobit.com>
2022-03-30 10:06:09 +0200

    [ADD] l10n_es_aeat_sii_oca_mock: New module to mock SII

---
+++ l10n_es_aeat_sii_oca_mock/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "AEAT SII Mock",
+    "summary": "Module to simulate the SII connection allowing to test "
+    "without establishing any real connection to AEAT",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_sii_oca",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
