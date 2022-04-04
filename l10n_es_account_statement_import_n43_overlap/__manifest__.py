42f3fdd2856ef4ff1a5d647929ae3598a840efc3
Miquel Raïch <miquel.raich@forgeflow.com>
2022-04-04 18:41:31 +0200

    [MIG] l10n_es_account_statement_import_n43_overlap: Migration to 14.0

--- l10n_es_account_statement_import_n43_overlap/__manifest__.py
+++ l10n_es_account_statement_import_n43_overlap/__manifest__.py
@@ -4,14 +4,15 @@
 
 {
     "name": "Bank statement import N43 overlap",
-    "summary": "This module extends functionality of N43 bank statements checking overlapping dates",
-    "version": "11.0.0.1.0",
+    "summary": "This module extends functionality of N43 bank statements"
+    " checking overlapping dates",
+    "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "depends": [
-        "l10n_es_account_bank_statement_import_n43",
+        "l10n_es_account_statement_import_n43",
     ],
     "data": [
         "wizards/account_bank_statement_import_view.xml",
