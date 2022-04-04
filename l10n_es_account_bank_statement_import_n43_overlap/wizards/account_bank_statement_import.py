cc52859be0e80336456bfc61f39cd1c4f7324a71
Miquel Raïch <miquel.raich@forgeflow.com>
2022-04-04 18:00:35 +0200

    [IMP] l10n_es_account_bank_statement_import_n43_overlap: black, isort, prettier

--- l10n_es_account_bank_statement_import_n43_overlap/wizards/account_bank_statement_import.py
+++ l10n_es_account_bank_statement_import_n43_overlap/wizards/account_bank_statement_import.py
@@ -2,29 +2,30 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models, _
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
 class AccountBankStatementImport(models.TransientModel):
-    _inherit = 'account.bank.statement.import'
+    _inherit = "account.bank.statement.import"
 
     check_dates = fields.Boolean(string="Check dates", default=True)
 
     def _complete_stmts_vals(self, stmts_vals, journal, account_number):
-        res = super()._complete_stmts_vals(
-            stmts_vals, journal, account_number)
+        res = super()._complete_stmts_vals(stmts_vals, journal, account_number)
 
         if self.check_dates:
             # get max and min date for imported bank statement
             imp_dates = set()
             for st_vals in res:
-                for line_vals in st_vals['transactions']:
-                    imp_dates.add(fields.Date.from_string(line_vals['date']))
+                for line_vals in st_vals["transactions"]:
+                    imp_dates.add(fields.Date.from_string(line_vals["date"]))
             imp_min_date, imp_max_date = min(imp_dates), max(imp_dates)
 
             # check max and min date for each existing bank statement
-            for bs in self.env['account.bank.statement'].search([('journal_id', '=', journal.id)]):
+            for bs in self.env["account.bank.statement"].search(
+                [("journal_id", "=", journal.id)]
+            ):
                 bs_dates = [fields.Date.from_string(x.date) for x in bs.line_ids]
                 bs_min_date, bs_max_date = min(bs_dates), max(bs_dates)
                 if bs_max_date >= imp_min_date and bs_min_date <= imp_max_date:
@@ -32,6 +33,8 @@ class AccountBankStatementImport(models.TransientModel):
                     if bs.name:
                         bs_display_name.append("(%s)" % bs.name)
                     raise ValidationError(
-                        _("Imported file overlaps with existing Bank statement: %s") % ' '.join(bs_display_name))
+                        _("Imported file overlaps with existing Bank statement: %s")
+                        % " ".join(bs_display_name)
+                    )
 
         return res
