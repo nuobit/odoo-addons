af7ec4a3fb065df7a492c9d276a4ed105421090e
KNVx <kniubo@nuobit.com>
2022-05-23 10:38:24 +0200

    [FIX] connector_sapb1: export button is visible on draft state on backend

--- connector_sapb1/__manifest__.py
+++ connector_sapb1/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector SAP B1",
     'description': "SAP Business One connector",
-    'version': '11.0.0.1.7',
+    'version': '11.0.0.1.8',
     'author': 'NuoBiT Solutions, S.L., Kilian Niubo',
     'license': 'AGPL-3',
     'category': 'Connector',
