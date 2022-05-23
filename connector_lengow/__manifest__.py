56bd58df806c301c3b2067f11455905dc202dc15
KNVx <kniubo@nuobit.com>
2022-05-23 11:55:31 +0200

    [FIX] connector_lengow: import page is visible on draft state

--- connector_lengow/__manifest__.py
+++ connector_lengow/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector Lengow",
     'description': "Connector Lengow",
-    'version': '11.0.0.1.9',
+    'version': '11.0.0.1.10',
     'author': 'NuoBiT Solutions, S.L., Kilian Niubo',
     'license': 'AGPL-3',
     'category': 'Connector',
