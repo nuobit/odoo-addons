8cc9a1e7a2479750daf71d5a58119beaa6c2cb6d
Eric Antones <eantones@nuobit.com>
2022-05-24 12:41:38 +0200

    [ADD] partner_default_company: populates the company with the current one on partner creation

---
+++ partner_default_company/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Partner Default Company",
+    "summary": "This module populates the company with "
+               "the current one on partner creation",
+    "version": "14.0.1.0.0",
+    "category": "Contacts",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
