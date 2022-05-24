283794765fe588edf9c3a7a9526b8b38a428b0a8
Eric Antones <eantones@nuobit.com>
2022-05-24 12:41:38 +0200

    [ADD] partner_default_company: populates the company with the current one on partner creation

---
+++ partner_default_company/models/res_partner.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import models, fields
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    def _default_company(self):
+        return self.env.company.id
+
+    company_id = fields.Many2one(default=_default_company)
