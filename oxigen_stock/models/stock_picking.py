6e1f3721c9c4f8e170b38b48ee95bde2b74c402a
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-05-25 15:57:03 +0200

    [FIX] oxigen_stock: remove onchange function removed from odoo CE

--- oxigen_stock/models/stock_picking.py
+++ oxigen_stock/models/stock_picking.py
@@ -7,13 +7,6 @@ from odoo import api, models
 class Picking(models.Model):
     _inherit = "stock.picking"
 
-    @api.onchange("location_id", "location_dest_id", "picking_type_id")
-    def onchange_locations(self):
-        res = super().onchange_locations()
-        if isinstance(res, dict) and "warning" in res:
-            # Override intercompany warning.
-            return {}
-
     @api.depends("state", "move_lines", "move_ids_without_package")
     def _compute_show_mark_as_todo(self):
         super()._compute_show_mark_as_todo()
