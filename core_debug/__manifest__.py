5ec273f2cde99c32995a63cea40e65c44d4991dc
Miquel Raïch <miquel.raich@forgeflow.com>
2022-05-25 12:03:34 +0200

    [TEMP] DO NOT MERGE (2)

--- core_debug/__manifest__.py
+++ core_debug/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Debug",
     "version": "14.0.1.0.0",
     "category": "Custom",
-    "author": "ForgeFlow",
+    "author": "ForgeFlow S.L.",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
--- core_debug/models/res_partner.py
+++ core_debug/models/res_partner.py
@@ -26,3 +26,42 @@ class Partner(models.Model):
                 raise Exception()
             return NotImplemented
         return self._name == other._name and set(self._ids) == set(other._ids)
+
+    def address_get(self, adr_pref=None):
+        """ Find contacts/addresses of the right type(s) by doing a depth-first-search
+        through descendants within company boundaries (stop at entities flagged ``is_company``)
+        then continuing the search at the ancestors that are within the same company boundaries.
+        Defaults to partners of type ``'default'`` when the exact type is not found, or to the
+        provided partner itself if no type ``'default'`` is found either. """
+        adr_pref = set(adr_pref or [])
+        if 'contact' not in adr_pref:
+            adr_pref.add('contact')
+        result = {}
+        visited = set()
+        for partner in self:
+            current_partner = partner
+            while current_partner:
+                to_scan = [current_partner]
+                # Scan descendants, DFS
+                while to_scan:
+                    record = to_scan.pop(0)
+                    _logger.info("ADDRESS_GET: record %s of type %s, visited %s", record, type(record), visited)
+                    visited.add(record)
+                    if record.type in adr_pref and not result.get(record.type):
+                        result[record.type] = record.id
+                    if len(result) == len(adr_pref):
+                        return result
+                    to_scan = [c for c in record.child_ids
+                                 if c not in visited
+                                 and not c.is_company] + to_scan
+
+                # Continue scanning at ancestor if current_partner is not a commercial entity
+                if current_partner.is_company or not current_partner.parent_id:
+                    break
+                current_partner = current_partner.parent_id
+
+        # default to type 'contact' or the partner itself
+        default = result.get('contact', self.id or False)
+        for adr_type in adr_pref:
+            result[adr_type] = result.get(adr_type) or default
+        return result
