bcb3fcf485fb67af2e242d3eda482fdbc723ec32
Eric Antones <eantones@nuobit.com>
2022-05-27 16:20:59 +0200

    [REF] account_invoice_report_service: remove unnecessary widget on delivery report

--- account_invoice_report_service/__manifest__.py
+++ account_invoice_report_service/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Service invoice report",
     "summary": """Service invoice report""",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Accounting",
