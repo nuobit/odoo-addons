4131e7899022d462ef604a1af5e3f9337af739b7
Eric Antones <eantones@nuobit.com>
2022-05-27 15:45:46 +0200

    [FIX] account_invoice_report_service: on delivery report the order date does not keep the timezone when truncated by 'date' widget

---
+++ account_invoice_report_service/models/sale_order.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+import pytz
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    date_order_tz = fields.Date(
+        string="Order Date TZ", readonly=True, compute="_compute_date_order_tz"
+    )
+
+    def _compute_date_order_tz(self):
+        user_tz = pytz.timezone(self.env.user.tz or "GMT")
+        for order in self:
+            datetime_utc = pytz.utc.localize(order.date_order)
+            datetime_local = datetime_utc.astimezone(user_tz)
+            order.date_order_tz = datetime_local.date()
