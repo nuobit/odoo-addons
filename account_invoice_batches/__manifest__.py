78a038aea21cdf3bd18394fc509ff7bf1737cc05
Eric Antones <eantones@nuobit.com>
2022-06-02 23:33:52 +0200

    [IMP] account_invoice_batches: added queue support for email and facturae sent methods

--- account_invoice_batches/__manifest__.py
+++ account_invoice_batches/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Invoice batches",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Accounting",
