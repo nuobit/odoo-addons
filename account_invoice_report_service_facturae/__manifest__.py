d70c02d47522411287f5a0517f4eabd7edace2eb
Eric Antones <eantones@nuobit.com>
2022-06-27 11:21:16 +0200

    [ADD] account_invoice_report_service_facturae: allows attach services reports to facturae file

---
+++ account_invoice_report_service_facturae/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Invoice report service facturae",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Invoicing Management",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module makes facturae to use the service invoice "
+    "reports defined on invoice services.",
+    "depends": [
+        "account_invoice_report_service",
+        "l10n_es_facturae",
+    ],
+    "installable": True,
+}
