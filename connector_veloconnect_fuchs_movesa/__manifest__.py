995703191446d3e778f0b909a0dc479c2ba4456f
KNVx <kniubo@nuobit.com>
2022-07-14 17:36:26 +0200

    [ADD] connector_veloconnect_fuchs_movesa: new module

---
+++ connector_veloconnect_fuchs_movesa/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Connector Veloconnect Fuchs Movesa",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module deals with specific Fuchs Movesa behaviour",
+    "depends": ["connector_veloconnect"],
+    "data": [
+        "views/veloconnect_backend_view.xml",
+    ],
+    "installable": True,
+}
---
+++ connector_veloconnect_fuchs_movesa/models/__init__.py
@@ -0,0 +1,2 @@
+from . import common
+from . import product_template
