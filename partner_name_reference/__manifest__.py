d221b5ce191dfbaa2fe883faec4451bc7c49134d
KNVx <kniubo@nuobit.com>
2022-07-15 15:42:37 +0200

    [ADD] partner_name_reference: new module to add reference in name

---
+++ partner_name_reference/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Partner name with reference",
+    "summary": "This module concatenates reference number to partner name on display_name",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "installable": True,
+    "auto_install": False,
+}
