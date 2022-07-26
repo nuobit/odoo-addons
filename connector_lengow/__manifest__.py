8a5acb9d61d02505d371949a430896658b2fe1d3
KNVx <kniubo@nuobit.com>
2022-07-26 11:59:31 +0200

    [FIX] connector_lengow: random connection errors HTTP 503 and 504 and ConectionError

--- connector_lengow/__manifest__.py
+++ connector_lengow/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector Lengow",
     'description': "Connector Lengow",
-    'version': '11.0.0.1.16',
+    'version': '11.0.0.1.17',
     'author': 'NuoBiT Solutions, S.L.',
     'license': 'AGPL-3',
     'category': 'Connector',
