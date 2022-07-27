1c1ae730d527e7d20e0bbeb4467d0c2dabaa6015
Eric Antones <eantones@nuobit.com>
2022-07-27 17:40:09 +0200

    [FIX] connector_oxigesti: missing import_record job function

--- connector_oxigesti/__manifest__.py
+++ connector_oxigesti/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Oxigesti-Odoo connector",
-    "version": "14.0.1.0.4",
+    "version": "14.0.1.0.5",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Connector",
