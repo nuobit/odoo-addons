4f3be7cd3520328076f0824b465d3a627aa969f1
Eric Antones <eantones@nuobit.com>
2022-07-29 10:15:23 +0200

    [FIX] connector_lengow: different marketplaces uses different fields to store the fullname of the customer
    Some marketplaces use the field "full_name" and others leave the latter blank and uses "first_name" and "last_name"

--- connector_lengow/__manifest__.py
+++ connector_lengow/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector Lengow",
     'description': "Connector Lengow",
-    'version': '11.0.0.1.18',
+    'version': '11.0.0.1.19',
     'author': 'NuoBiT Solutions, S.L.',
     'license': 'AGPL-3',
     'category': 'Connector',
