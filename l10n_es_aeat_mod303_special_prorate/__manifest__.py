42add551ad398a3b193d94fa8053dcd44f919b6a
KNVx <kniubo@nuobit.com>
2022-08-11 18:39:54 +0200

    [IMP] l10n_es_aeat_mod303_special_prorate: added tax definition for fields 43 and 44

--- l10n_es_aeat_mod303_special_prorate/__manifest__.py
+++ l10n_es_aeat_mod303_special_prorate/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Special prorate tax mapping for 303",
     "summary": "This module adds the 303 model special prorate taxes",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Sales",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
@@ -15,7 +15,7 @@
         "l10n_es_aeat_mod303",
     ],
     "data": [
-        "data/tax_code_map_mod303_data.xml",
+        "data/tax_code_map_mod303_202107_data.xml",
     ],
     "installable": True,
     "auto_install": True,
