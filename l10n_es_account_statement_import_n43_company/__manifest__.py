27e6a216435ce1d59f04cd1b6db56bdd9a268c70
Eric Antones <eantones@nuobit.com>
2022-09-02 19:31:20 +0200

    [ADD] l10n_es_account_statement_import_n43_company: new module to allow only partners of the current company

---
+++ l10n_es_account_statement_import_n43_company/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Bank statement import N43 company",
+    "summary": "This module only allows to get partners from the current company",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_account_statement_import_n43",
+    ],
+}
