baa3310b81a822295a007d99b0791298d253d612
KNVx <kniubo@nuobit.com>
2022-09-27 09:45:56 +0200

    [ADD] l10n_es_aeat_prorate_asset: New Module

---
+++ l10n_es_aeat_prorate_asset/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Prorate Asset",
+    "summary": "This module add prorate fields on asset",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management_extension",
+        "l10n_es_aeat_vat_special_prorrate",
+        "l10n_es_asset_extension",
+    ],
+    "data": ["views/account_asset.xml"],
+}
