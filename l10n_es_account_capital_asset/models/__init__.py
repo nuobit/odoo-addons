065313cc1369d3b6c1406e26f6173f410c4fa906
KNVx <kniubo@nuobit.com>
2022-09-27 09:45:32 +0200

    [ADD] l10n_es_account_capital_asset: New Module

---
+++ l10n_es_account_capital_asset/models/__init__.py
@@ -0,0 +1,15 @@
+from . import account_asset
+from . import account_asset_profile
+from . import account_capital_asset_map_tax
+from . import account_move
+from . import account_move_line
+from . import account_capital_asset_type
+from . import res_company
+from . import res_config_settings
+from . import account_tax
+
+# TODO: 30-11
+# probar despues:
+# check_tax_base_amount
+# check_amount_type
+# get unit price --> get unit amount
