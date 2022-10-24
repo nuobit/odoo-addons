b369fdb5597108200b0bfc0210529d440db92a5b
Eric Antones <eantones@nuobit.com>
2022-10-24 13:46:13 +0200

    [IMP] connector_lengow: Do not allow to have shipping and invoice addresses with parent different than the main one

--- connector_lengow/models/sale_order/sale_order.py
+++ connector_lengow/models/sale_order/sale_order.py
@@ -1,7 +1,8 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -15,3 +16,24 @@ class SaleOrder(models.Model):
 
     lengow_status = fields.Char(string="Lengow State", readonly=True, track_visibility='onchange')
     marketplace_status = fields.Char(string="Marketplace State", readonly=True, track_visibility='onchange')
+
+    # TODO move this check to a standalone module and make the current module depends on it
+    @api.constrains('partner_id', 'partner_invoice_id', 'partner_shipping_id')
+    def _check_partner_parent_consistency(self):
+        for rec in self:
+            if rec.partner_invoice_id.parent_id:
+                if rec.partner_invoice_id.parent_id != rec.partner_id:
+                    raise ValidationError(_("The parent partner of the invoice address should be the same as "
+                                            "the main partner in the order"))
+            else:
+                if rec.partner_invoice_id != rec.partner_id:
+                    raise ValidationError(_("The invoice address has no parent so it should be the same as "
+                                            "the main partner in the order"))
+            if rec.partner_shipping_id.parent_id:
+                if rec.partner_shipping_id.parent_id != rec.partner_id:
+                    raise ValidationError(_("The parent partner of the shipping address should be the same as "
+                                            "the main partner in the order"))
+            else:
+                if rec.partner_shipping_id != rec.partner_id:
+                    raise ValidationError(_("The shipping address has no parent so it should be the same as "
+                                            "the main partner in the order"))
