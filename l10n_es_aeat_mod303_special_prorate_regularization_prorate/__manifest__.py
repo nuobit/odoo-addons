4b83a9a18eb3b1c1dfd485086c6c705ba3ad301b
KNVx <kniubo@nuobit.com>
2022-10-24 10:14:10 +0200

    [ADD] l10n_es_aeat_mod303_special_prorate_regularization_prorate: New module

---
+++ l10n_es_aeat_mod303_special_prorate_regularization_prorate/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "AEAT 303 - Special Prorate Regularization Prorate",
+    "summary": "This module allows to regularize prorate differences on 303 report",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management",
+        "l10n_es_aeat_mod303_special_prorate_regularization",
+    ],
+    "data": [
+        "views/mod303_views.xml",
+    ],
+}
