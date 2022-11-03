5e1e4201b3bad78c986cd543daa8de8a71efcf95
Eric Antones <eantones@nuobit.com>
2022-11-03 11:31:48 +0100

    [FIX] connector_lengow: error assigning parent_country_iso_a2 when delivery address does not exist

--- connector_lengow/__manifest__.py
+++ connector_lengow/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector Lengow",
     'description': "Connector Lengow",
-    'version': '11.0.0.1.26',
+    'version': '11.0.0.1.27',
     'author': 'NuoBiT Solutions, S.L.',
     'license': 'AGPL-3',
     'category': 'Connector',
