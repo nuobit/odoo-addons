a61a9ae466371331b0efd7b8857d6a24822a71fa
KNVx <kniubo@nuobit.com>
2022-11-15 15:10:31 +0100

    [ADD] l10n_es_asset_extension: New Module

---
+++ l10n_es_asset_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "L10n ES Asset Extension",
+    "summary": "This module extends l10n_es and Account Asset",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["l10n_es_extension", "account_asset_management_extension"],
+    "data": [],
+    "installable": True,
+}
