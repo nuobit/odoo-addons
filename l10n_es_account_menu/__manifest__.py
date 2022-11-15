e2a893043977c9d071bffa6b0fd68f485ff9ba37
KNVx <kniubo@nuobit.com>
2022-11-15 15:08:28 +0100

    [ADD] l10n_es_account_menu: New Module

---
+++ l10n_es_account_menu/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "L10n ES Account Menu",
+    "summary": "This module extends account menu",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account_menu", "l10n_es_extension"],
+    "data": [
+        "views/account_tax_views.xml",
+    ],
+    "installable": True,
+}
