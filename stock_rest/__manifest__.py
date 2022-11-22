62bed17561ee63ce62665755c7be8c593a71acf0
KNVx <kniubo@nuobit.com>
2022-11-22 08:36:38 +0100

    [IMP] stock_rest: hook added to filter results in stock lot

--- stock_rest/__manifest__.py
+++ stock_rest/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Stock REST",
     "author": "NuoBiT Solutions, S.L.",
     "category": "Stock",
-    "version": "14.0.1.1.5",
+    "version": "14.0.1.1.6",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
