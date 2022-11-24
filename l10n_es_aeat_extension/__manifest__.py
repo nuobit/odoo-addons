3452e6daa1e41b3364696178bbb42675715458d6
Eric Antones <eantones@nuobit.com>
2022-11-24 17:33:58 +0100

    [ADD] l10n_es_aeat_extension: New module

---
+++ l10n_es_aeat_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "AEAT Extension",
+    "summary": "This module extends the base AEAT module with new functionalities "
+    "and fixes that are not accepted or delayed recurrently by "
+    "the OCA l10n_spain localization community",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Accounting & Finance",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "l10n_es_aeat",
+    ],
+    "installable": True,
+}
