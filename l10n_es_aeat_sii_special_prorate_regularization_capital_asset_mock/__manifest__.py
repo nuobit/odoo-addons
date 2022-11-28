dc4d762af1dd7af5a4154f12c927f2cb602b3029
Eric Antones <eantones@nuobit.com>
2022-11-28 12:44:15 +0100

    [ADD] l10n_es_aeat_sii_special_prorate_regularization_capital_asset_mock: New module to mock the Capital Assets SII connection

---
+++ l10n_es_aeat_sii_special_prorate_regularization_capital_asset_mock/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "AEAT SII Capital Asset Mock",
+    "summary": "Module to simulate the SII connection allowing to test "
+    "without establishing any real connection to AEAT",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_sii_special_prorate_regularization_capital_asset",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
