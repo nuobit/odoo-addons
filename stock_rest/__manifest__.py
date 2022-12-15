82b8a909164018886d356a2693304cea6d7adbb6
Eric Antones <eantones@nuobit.com>
2022-12-15 23:22:02 +0100

    [FIX] stock_rest: poor performance excluding the assets

--- stock_rest/__manifest__.py
+++ stock_rest/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Stock REST",
     "author": "NuoBiT Solutions, S.L.",
     "category": "Stock",
-    "version": "14.0.1.1.8",
+    "version": "14.0.1.1.9",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
