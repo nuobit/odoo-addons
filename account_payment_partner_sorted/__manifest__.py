e0a047f85e6c42c852b69630289ff4755da3da09
Eric Antones <eantones@nuobit.com>
2022-12-19 12:49:51 +0100

    [ADD] account_payment_partner_sorted: new module

---
+++ account_payment_partner_sorted/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Payment Partner Sorted",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Banking addons",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "summary": "This module gets the payment mode banks sorted",
+    "depends": [
+        "account_payment_partner",
+    ],
+}
