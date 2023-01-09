65510a9ac3780f442335add383103043cfce12a4
KNVx <kniubo@nuobit.com>
2023-01-09 13:07:38 +0100

    [ADD] account_move_show_date_out_invoices: New Module

---
+++ account_move_show_date_out_invoices/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Account Move Show Date Out Invoice",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
