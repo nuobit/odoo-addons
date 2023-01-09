87497da823020536808a4a0fef75636cf1717eb9
KNVx <kniubo@nuobit.com>
2023-01-09 12:45:02 +0100

    [FIX] account_invoice_batches: smart buttons on lots open view_in_invoice_tree

--- account_invoice_batches/__manifest__.py
+++ account_invoice_batches/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Invoice batches",
-    "version": "14.0.1.1.2",
+    "version": "14.0.1.1.3",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Accounting",
