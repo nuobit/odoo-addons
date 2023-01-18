18926fe5291d70fa21c884e734e4b0e2da41b05d
KNVx <kniubo@nuobit.com>
2023-01-18 15:25:53 +0100

    [IMP] l10n_es_account_capital_asset: onchange asset profile on move lines and refactor of onchanges

--- l10n_es_account_capital_asset/models/account_move_line.py
+++ l10n_es_account_capital_asset/models/account_move_line.py
@@ -11,43 +11,47 @@ class AccountMoveLine(models.Model):
     @api.onchange("product_id")
     def _onchange_product_id(self):
         super()._onchange_product_id()
-        if self.tax_ids:
-            self.tax_ids = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
-                self.tax_ids, self.company_id, self.balance, self.quantity
-            )
+        self.compute_mapped_taxes()
 
     @api.onchange("product_uom_id")
     def _onchange_uom_id(self):
         super()._onchange_uom_id()
-        if self.tax_ids:
-            self.tax_ids = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
-                self.tax_ids, self.company_id, self.balance, self.quantity
-            )
+        self.compute_mapped_taxes()
 
     @api.onchange("account_id")
     def _onchange_account_id(self):
         super()._onchange_uom_id()
-        if self.tax_ids:
-            self.tax_ids = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
-                self.tax_ids, self.company_id, self.balance, self.quantity
-            )
+        self.compute_mapped_taxes()
+
+    @api.onchange("tax_ids")
+    def _onchange_tax_ids(self):
+        self.compute_mapped_taxes()
+
+    @api.onchange("asset_profile_id")
+    def _onchange_asset_profile_id(self):
+        super()._onchange_asset_profile_id()
+        self.compute_taxes_fiscal_position()
+        self.compute_mapped_taxes()
 
     @api.onchange("price_subtotal", "quantity")
     def _onchange_subtotal_quantity_id(self):
+        self.compute_taxes_fiscal_position()
+        self.compute_mapped_taxes()
+
+    def compute_mapped_taxes(self):
+        if self.tax_ids:
+            self.tax_ids = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
+                self.tax_ids,
+                self.company_id,
+                self.asset_profile_id,
+                self.balance,
+                self.quantity,
+            )
+
+    def compute_taxes_fiscal_position(self):
         taxes = self._get_computed_taxes()
         if taxes and self.move_id.fiscal_position_id:
             taxes = self.move_id.fiscal_position_id.map_tax(
                 taxes, partner=self.partner_id
             )
-        if taxes:
-            taxes = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
-                taxes, self.company_id, self.balance, self.quantity
-            )
         self.tax_ids = taxes
-
-    @api.onchange("tax_ids")
-    def _onchange_tax_ids(self):
-        if self.tax_ids:
-            self.tax_ids = self.env["l10n.es.account.capital.asset.map.tax"].map_tax(
-                self.tax_ids, self.company_id, self.balance, self.quantity
-            )
