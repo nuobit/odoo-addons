56f9c6fa1f5555fcb368d7c0353cf750a2b2aba6
KNVx <kniubo@nuobit.com>
2023-01-20 18:46:29 +0100

    [IMP] l10n_es_aeat_vat_special_prorrate: new field to mark companies with prorate

--- l10n_es_aeat_vat_special_prorrate/models/__init__.py
+++ l10n_es_aeat_vat_special_prorrate/models/__init__.py
@@ -1,3 +1,4 @@
 from . import aeat_map_special_prorrate_year
 from . import account_tax
 from . import account_move
+from . import res_company
---
+++ l10n_es_aeat_vat_special_prorrate/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    l10n_es_prorate_enabled = fields.Boolean(string="L10n ES Prorate Enabled")
