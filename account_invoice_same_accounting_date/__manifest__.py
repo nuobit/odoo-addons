349258a41196f9ed566c97ecd6758be1205ce918
Eric Antones <eantones@nuobit.com>
2023-01-23 19:09:44 +0100

    [ADD] account_invoice_same_accounting_date: new module to make the accounting date the same as the invoice date by default

---
+++ account_invoice_same_accounting_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Invoices Same Invoice Date and Accounting Date",
+    "summary": "Copy the invoice date to accounting date by default",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Invoicing",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account"],
+    "data": [],
+}
