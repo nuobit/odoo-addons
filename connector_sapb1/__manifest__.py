f717daf2d98aca7cb2fd3cf332ca58e9b2da69f0
Eric Antones <eantones@nuobit.com>
2023-02-03 00:11:41 +0100

    [FIX] connector_sapb1: address line 2 not exported to the address field and added county field to export

--- connector_sapb1/__manifest__.py
+++ connector_sapb1/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Connector SAP B1",
     'description': "SAP Business One connector",
-    'version': '11.0.0.1.21',
+    'version': '11.0.0.1.22',
     'author': 'NuoBiT Solutions, S.L.',
     'license': 'AGPL-3',
     'category': 'Connector',
