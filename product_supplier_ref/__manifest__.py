6bfca8859d10bec76924e830dc28a440b98498d6
Eric Antones <eantones@nuobit.com>
2023-02-07 22:47:30 +0100

    [ADD] product_supplier_ref: new module

---
+++ product_supplier_ref/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Product Supplier Ref",
+    "summary": "Use the product name if the supplier product name is "
+    "empty instead of the product default_code.",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "author": "NuoBiT Solutions,S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+}
