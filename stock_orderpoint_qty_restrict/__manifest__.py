d12d3e30a49e47a249fe2eecf00acb99788b5341
Eric Antones <eantones@nuobit.com>
2023-02-15 13:40:57 +0100

    [ADD] stock_orderpoint_qty_restrict: new module
    Based on:
    
    https://github.com/ForgeFlow/odoo/commits/14.0-stock-scheduler-issue
    
    https://github.com/ForgeFlow/odoo/commit/9ee0317e39642c3226974d535de09cf92c0fa745

---
+++ stock_orderpoint_qty_restrict/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Orderpoint Quantity Restrict",
+    "summary": "Restrict orderpoints by quantity greater than zero",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Warehouse",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock",
+    ],
+}
