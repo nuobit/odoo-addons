b29c1b7d291e624b9509687447874fa340d8a9c9
Eric Antones <eantones@nuobit.com>
2023-02-16 22:19:49 +0100

    [ADD] account_invoice_attachment_required: new module

---
+++ account_invoice_attachment_required/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Invoice Attachment Required",
+    "summary": "This modules does not allow to post an "
+    "invoice without an attachment.",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Sales",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account"],
+}
