e2060353aff19303d47f539169e74da49f3ba335
Eric Antones <eantones@nuobit.com>
2023-03-01 16:48:19 +0100

    [ADD] purchase_stock_order_description: new module

---
+++ purchase_stock_order_description/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Purchase Stock Order Description",
+    "summary": "Propagate the purchase order description to the picking move lines",
+    "version": "14.0.1.0.0",
+    "category": "Purchases",
+    "author": "NuoBiT Solutions,S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase_stock",
+    ],
+}
