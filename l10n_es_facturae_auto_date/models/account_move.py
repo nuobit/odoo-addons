7235bf0b2405792084655fbb599f595e47bd85a2
Eric Antones <eantones@nuobit.com>
2023-03-05 22:50:47 +0100

    [REF] l10n_es_facturae_auto_date: using compute instead of overriding write/create

--- l10n_es_facturae_auto_date/models/account_move.py
+++ l10n_es_facturae_auto_date/models/account_move.py
@@ -7,73 +7,34 @@ import datetime
 from odoo import api, fields, models
 
 
-def _get_facturae_dates(invoice_date):
-    if isinstance(invoice_date, str):
-        invoice_date = fields.Date.from_string(invoice_date)
-    _, month_days = calendar.monthrange(invoice_date.year, invoice_date.month)
-    facturae_start_date = datetime.date(invoice_date.year, invoice_date.month, 1)
-    facturae_end_date = datetime.date(invoice_date.year, invoice_date.month, month_days)
+def _get_facturae_dates(date):
+    _, month_days = calendar.monthrange(date.year, date.month)
+    facturae_start_date = datetime.date(date.year, date.month, 1)
+    facturae_end_date = datetime.date(date.year, date.month, month_days)
     return facturae_start_date, facturae_end_date
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
-    @api.onchange("invoice_date")
-    def _onchange_auto_date_invoice_date(self):
-        partner_id = self.partner_id.parent_id or self.partner_id
-        if self.facturae and self.invoice_date and partner_id.facturae_auto_dates:
-            self.facturae_start_date, self.facturae_end_date = _get_facturae_dates(
-                self.invoice_date
-            )
-        else:
-            self.facturae_start_date = False
-            self.facturae_end_date = False
+    facturae_start_date = fields.Date(
+        compute="_compute_facturae_dates",
+        store=True,
+        readonly=False,
+    )
+    facturae_end_date = fields.Date(
+        compute="_compute_facturae_dates",
+        store=True,
+        readonly=False,
+    )
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            partner_id = vals.get("partner_id")
-            if partner_id:
-                partner = self.env["res.partner"].browse(partner_id)
-                partner = partner.parent_id or partner
-                facturae = vals.get("facturae", partner.facturae)
-                auto_date = vals.get("facturae_auto_dates", partner.facturae_auto_dates)
-                if facturae and auto_date:
-                    invoice_date = vals.get("invoice_date")
-                    if invoice_date:
-                        if (
-                            vals.get("facturae_start_date") is not False
-                            and vals.get("facturae_end_date") is not False
-                        ):
-                            (
-                                vals["facturae_start_date"],
-                                vals["facturae_end_date"],
-                            ) = _get_facturae_dates(invoice_date)
-        return super(AccountInvoice, self).create(vals_list)
-
-    def write(self, vals):
+    @api.depends("date", "facturae", "partner_id")
+    def _compute_facturae_dates(self):
         for rec in self:
-            facturae = vals.get("facturae", rec.facturae)
-            auto_date = vals.get(
-                "facturae_auto_dates",
-                (rec.partner_id.parent_id or rec.partner_id).facturae_auto_dates,
-            )
-            if facturae and auto_date:
-                invoice_date = vals.get("invoice_date")
-                if invoice_date is not None:
-                    if (
-                        vals.get("facturae_start_date") is not False
-                        and vals.get("facturae_end_date") is not False
-                    ):
-                        if invoice_date:
-                            (
-                                vals["facturae_start_date"],
-                                vals["facturae_end_date"],
-                            ) = _get_facturae_dates(invoice_date)
-                        else:
-                            vals["facturae_start_date"], vals["facturae_end_date"] = (
-                                False,
-                                False,
-                            )
-        return super(AccountInvoice, self).write(vals)
+            partner = rec.partner_id.parent_id or rec.partner_id
+            if partner:
+                if rec.facturae and partner.facturae_auto_dates and rec.date:
+                    (
+                        rec.facturae_start_date,
+                        rec.facturae_end_date,
+                    ) = _get_facturae_dates(rec.date)
