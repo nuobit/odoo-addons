9bfd045984299197bff9f74522eb8662bd0a1573
Eric Antones <eantones@nuobit.com>
2023-03-05 22:51:39 +0100

    [ADD] l10n_es_facturae_auto_date_contract: new module to glue faturae auto dates and contracts

---
+++ l10n_es_facturae_auto_date_contract/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "FacturaE auto dates Contract",
+    "summary": "Glue module to make Facturae auto dates "
+    "compatible with Contract module",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_facturae_auto_date",
+        "contract",
+    ],
+    "auto_install": True,
+    "development_status": "Beta",
+    "maintainers": ["eantones"],
+}
