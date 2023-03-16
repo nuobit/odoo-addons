de84773a8333449576f1a6909ac2151aee376a26
Eric Antones <eantones@nuobit.com>
2023-03-16 14:37:17 +0100

    [ADD] account_journal_entry_filter_number: new module to filter the journal entries by number only

---
+++ account_journal_entry_filter_number/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Journal Entry Filter Number",
+    "summary": "This module adds a new filter on Journal Entry tree view "
+    "to filter only by Number",
+    "category": "Accounting",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions, S.L.",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+}
