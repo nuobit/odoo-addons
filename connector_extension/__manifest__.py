383dfd414044f01ab6fef60adfdc2db71e6a7f68
KNVx <kniubo@nuobit.com>
2023-03-23 09:21:10 +0100

    [ADD] connector_extension: New Module

---
+++ connector_extension/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Connector Extension",
+    "summary": "Modulo base para declaraciones de la AEAT",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/nuobit/dims-vertical",
+    "external_dependencies": {
+        "python": [
+            "requests",
+        ],
+    },
+    "depends": ["connector"],
+    "data": [],
+}
