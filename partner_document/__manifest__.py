a936a13a3e3f9ae70dac07995803e48bca2a2bc2
KNVx <kniubo@nuobit.com>
2023-03-23 09:22:08 +0100

    [ADD] partner_document: New Module

---
+++ partner_document/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Partner Document",
+    "summary": "This module adds the logic to classify and evaluate the partner performance",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["contacts"],
+    "data": [
+        "security/partner_documents_security.xml",
+        "security/ir.model.access.csv",
+        "views/partner_document_menu_views.xml",
+        "views/res_partner_views.xml",
+        "views/partner_classification_views.xml",
+        "views/partner_document_type_views.xml",
+        "views/partner_document_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
