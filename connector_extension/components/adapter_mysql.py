b2f113c826c5cb2f51d8b7bae8aa39ee672d9bc8
KNVx <kniubo@nuobit.com>
2023-03-23 09:21:10 +0100

    [ADD] connector_extension: New Module

---
+++ connector_extension/components/adapter_mysql.py
@@ -0,0 +1,47 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+
+from odoo import _
+from odoo.exceptions import ValidationError
+
+from odoo.addons.component.core import AbstractComponent
+
+_logger = logging.getLogger(__name__)
+
+
+class BackendMySQLAdapterCRUD(AbstractComponent):
+    _name = "base.backend.mysql.adapter.crud"
+    _inherit = "base.backend.sql.adapter.crud"
+
+    # TODO:REVIEW: GET_VERSION
+    # _sql_version = "SELECT VERSION();"
+
+    def _execute(self, op, cr, sql, params):
+        if not sql:
+            raise ValidationError(_("Empty SQL statement"))
+        sql_l = sql.split(";")
+        if op == "create":
+            if len(sql_l) > 2:
+                raise ValidationError(_("Unexpected SQL statement"))
+            if len(sql_l) == 2:
+                if not "last_insert_id()".lower() in sql_l[1].lower():
+                    raise ValidationError(
+                        _("Only last_insert_id() is allowed in insert statement.")
+                    )
+        else:
+            if len(sql_l) != 1:
+                raise ValidationError(
+                    _("Only one query is allowed on non insert SQL statements.")
+                )
+
+        res = super()._execute(op, cr, sql_l[0], params)
+        if op == "create":
+            res = cr.execute(sql_l[1])
+        return res
+
+    # TODO:REVIEW: GET_VERSION
+    # def get_version(self):
+    #     res = self._execute()
+    #     return res[0][0]
---
+++ connector_extension/readme/DESCRIPTION.rst
@@ -0,0 +1,10 @@
+The "connector_extension" module is an add-on for the Odoo ERP system that enhances the functionality of the base "connector" module. This extension provides additional features, tools, and integrations, making it easier for developers to create, manage, and maintain connections between Odoo and various third-party systems, APIs, or services.
+
+The module aims to simplify the connector development process by providing a robust and flexible framework. The "connector_extension" module allows developers to focus on implementing specific business logic and requirements, while the extension handles common tasks.
+
+
+* Extended connector framework: The module extends the base connector framework by providing new classes, methods, and utilities for easier integration with third-party systems.
+* Reusable components: Pre-built components for handling common tasks such error handling, and data synchronization.
+* Enhanced data mapping: Advanced data mapping capabilities to transform and adapt data between Odoo and external systems seamlessly.
+* Scalability and performance improvements: Optimized for handling large datasets and concurrent connections, ensuring smooth and efficient data synchronization.
+* Linking with existing records: Enables linking with existing records, using alternate keys, which allows for easier data management and reduced redundancy.
