6ccb9d6df8f3309bf540bdf4dd06058046272f2d
Eric Antones <eantones@nuobit.com>
2023-04-08 23:58:59 +0200

    [ADD] l10n_es_banking_sepa_direct_debit_order_sabadell: new module

---
+++ l10n_es_banking_sepa_direct_debit_order_sabadell/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Exportación de fichero bancario de adeudo directo para Banco Sabadell",
+    "version": "14.0.1.0.1",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account_banking_sepa_direct_debit"],
+    "data": [
+        "views/account_payment_method.xml",
+    ],
+    "maintainers": ["eantones"],
+}
---
+++ l10n_es_banking_sepa_direct_debit_order_sabadell/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Modulo para adaptar la exportación del fichero bancario de
+adeudo directo a las peculiaridades del Banco Sabadell
