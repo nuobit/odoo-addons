1a72386edc6701e294b703fe939de576489f1951
KNVx <kniubo@nuobit.com>
2023-04-26 15:03:19 +0200

    [ADD] partner_email_mobile_required: New module

---
+++ partner_email_mobile_required/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Partner email mobile required",
+    "summary": "This module make required the email or mobile fields in the partner form.",
+    "version": "16.0.1.0.0",
+    "category": "Partner",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["contacts"],
+    "data": [],
+    "installable": True,
+}
