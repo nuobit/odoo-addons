2b15f3eb3447616cc0ed25c7708589494f3645ce
Eric Antones <eantones@nuobit.com>
2023-05-01 01:23:07 +0200

    [FIX] stock_picking_partner_ref: pre-commit, company name

--- stock_picking_partner_ref/__manifest__.py
+++ stock_picking_partner_ref/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Copyright NuoBiT Solutions SL (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock picking partner reference",
     "summary": "Adds a partner reference on picking",
-    "author": "NuoBiT Solutions",
+    "author": "NuoBiT Solutions SL",
     "maintainers": ["eantones"],
     "category": "Warehouse",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
