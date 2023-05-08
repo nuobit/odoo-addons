37dc80e92dce835bb366bc7df9eab869b638726d
FrankC013 <fcespedes@nuobit.com>
2023-05-08 15:52:39 +0200

    [ADD] account_move_accrual_entry: New module to generate an accrual journal entry
    Adds smart-button in invoice to access it
    Create accrual_account_id and accrual_asset_account_type in	invoice settings

---
+++ account_move_accrual_entry/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Accrual Journal Entry",
+    "summary": "This module create journal entry with accrual date",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+        "views/res_config_settings_views.xml",
+    ],
+    "maintainers": ["FrankC013", "eantones"],
+}
---
+++ account_move_accrual_entry/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* `NuoBiT <https://www.nuobit.com>`_:
+
+  * Frank Cespedes <fcespedes@nuobit.com>
+  * Eric Antones <eantones@nuobit.com>
