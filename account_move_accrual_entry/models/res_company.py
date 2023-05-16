88007ba9acd758727b9def4656857bf13eb8380c
Eric Antones <eantones@nuobit.com>
2023-05-16 18:51:40 +0200

    [REF+IMP] account_move_accrual_entry: Various refactors and improvements

--- account_move_accrual_entry/models/res_company.py
+++ account_move_accrual_entry/models/res_company.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import fields, models
 
@@ -7,9 +8,10 @@ class Company(models.Model):
     _inherit = "res.company"
 
     accrual_account_id = fields.Many2one(
-        comodel_name="account.account", string="Accrual Account"
+        comodel_name="account.account",
     )
 
-    accrual_asset_account_type_id = fields.Many2one(
-        comodel_name="account.account.type", string="Account Type for Assets"
+    accrual_account_asset_type_id = fields.Many2one(
+        comodel_name="account.account.type",
+        default=lambda self: self.env.ref("account.data_account_type_fixed_assets"),
     )
