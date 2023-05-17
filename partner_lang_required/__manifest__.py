e120c37ef9c8e872fe907e2f4e9fc80681c4e516
FrankC013 <fcespedes@nuobit.com>
2023-05-17 17:20:28 +0200

    [ADD] partner_lang_required: create module partner_lang_required to convert lang in required

---
+++ partner_lang_required/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Partner language required",
+    "summary": "This module convert the lang partner "
+    "attribute to required by adding a restriction to it",
+    "version": "16.0.1.0.0",
+    "category": "Partner",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["base"],
+}
