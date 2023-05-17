aeb1cbd9fabca7e8e7bcb61d914a42e9bcb00ffa
FrankC013 <fcespedes@nuobit.com>
2023-05-17 17:47:56 +0200

    [ADD] res_lang_shortname: create shortname field to get the abbreviated name

---
+++ res_lang_shortname/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Res Lang Shortname",
+    "summary": "This module adds a shortname field"
+    "to the res.lang model to get the abbreviated name.",
+    "version": "16.0.1.0.0",
+    "category": "Language",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_lang_views.xml"],
+}
