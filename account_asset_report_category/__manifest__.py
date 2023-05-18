7524741e30d36fc4070f340c3ed24c7cd56c662e
Eric Antones <eantones@nuobit.com>
2023-05-18 18:44:19 +0200

    [ADD] account_asset_report_category: new module

---
+++ account_asset_report_category/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Asset Financial Report Category",
+    "summary": "This module adds the column 'Category' in the Asset Financial Report",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management",
+    ],
+    "maintainers": ["eantones"],
+}
