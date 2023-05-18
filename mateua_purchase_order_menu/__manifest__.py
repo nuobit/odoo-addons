ecc6f2b6611aa7c5770d9dd5ca36c51d35fe61bb
FrankC013 <fcespedes@nuobit.com>
2023-05-18 11:42:29 +0200

    [ADD] mateua_purchase_order_menu: change the default view in purchase module to orders

---
+++ mateua_purchase_order_menu/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Menu Purchase Order Mateua",
+    "summary": "This module change the default"
+    " view in purchase module to orders instead of quotation.",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions S.L.",
+    "license": "AGPL-3",
+    "category": "Purchase",
+    "website": "https://github.com/mateuadive/odoo-addons",
+    "depends": ["purchase"],
+    "data": ["views/purchase_views.xml"],
+    "maintainers": ["FrankC013"],
+}
