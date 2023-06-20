c494d45a8e8559c779924a6a29c2c8bbd088841a
Eric Antones <eantones@nuobit.com>
2023-06-20 14:43:27 +0200

    [ADD] connector_extension_sql: new module

---
+++ connector_extension_sql/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Connector Extension SQL",
+    "summary": "This module extends the connector extension "
+    "module to add support for generic SQL databases.",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, SL",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["connector_extension"],
+}
---
+++ connector_extension_sql/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module extends the connector extension module to add support for generic SQL databases.
