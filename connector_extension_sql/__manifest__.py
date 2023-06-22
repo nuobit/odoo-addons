837b1fc6271458f00cb387cc4f840ffd3116d39e
Eric Antones <eantones@nuobit.com>
2023-06-22 19:32:50 +0200

    [MIG] connector_extension_sql: Migration to 16.0

--- connector_extension_sql/__manifest__.py
+++ connector_extension_sql/__manifest__.py
@@ -6,8 +6,8 @@
     "name": "Connector Extension SQL",
     "summary": "This module extends the connector extension "
     "module to add support for generic SQL databases.",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, SL",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Connector",
     "website": "https://github.com/nuobit/odoo-addons",
