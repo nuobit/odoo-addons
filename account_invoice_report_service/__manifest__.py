db683f5b75d3686fa6098de1d88928d6c3f95c28
Eric Antones <eantones@nuobit.com>
2023-07-10 12:37:10 +0200

    [IMP] account_invoice_report_service: show zero taxes on service and delivery reports

--- account_invoice_report_service/__manifest__.py
+++ account_invoice_report_service/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Service invoice report",
     "summary": """Service invoice report""",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.1.0",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
     "license": "AGPL-3",
     "category": "Accounting",
