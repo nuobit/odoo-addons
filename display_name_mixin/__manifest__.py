c7163076db83ee924bbc5e79ccfd1e1ee56216e3
FrankC013 <fcespedes@nuobit.com>
2023-07-19 11:21:46 +0200

    [ADD] display_name_mixin: create generate_display_name for display names models

---
+++ display_name_mixin/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Display Name Mixin",
+    "summary": "This module adds the function generate_generic_name "
+    "for generate the display name",
+    "version": "16.0.1.0.1",
+    "category": "Mixin",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["base"],
+}
