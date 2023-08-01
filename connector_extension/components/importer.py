9ddea8c1dd38557ae8ab1e1f6618a67322faa840
KNVx <kniubo@nuobit.com>
2023-08-01 09:51:24 +0200

    [IMP] connector_extension: Created generic batch, chunk and record on importer

--- connector_extension/components/importer.py
+++ connector_extension/components/importer.py
@@ -7,7 +7,8 @@ from contextlib import contextmanager
 
 import psycopg2
 
-from odoo import _
+from odoo import _, fields
+from odoo.exceptions import ValidationError
 
 from odoo.addons.component.core import AbstractComponent
 from odoo.addons.connector.exception import IDMissingInBackend
@@ -231,3 +232,176 @@ class GenericDirectImporter(AbstractComponent):
         """Create the Internal record"""
         self._validate_create_data(data)
         return self.model.with_context(connector_no_export=True).create(data)
+
+
+class GenericBatchImporter(AbstractComponent):
+    """Generic Synchronizer for importing data from backend to Odoo"""
+
+    _name = "generic.batch.importer"
+    _inherit = "base.importer"
+
+    _usage = "batch.direct.importer"
+
+    def run(self, domain=None):
+        """Run the synchronization"""
+        if domain is None:
+            domain = []
+        chunk_size = self.backend_record.chunk_size
+        if chunk_size > 0:
+            total_items = self.backend_adapter.get_total_items(domain=domain)
+            if total_items == 0:
+                return
+            offset = 0
+            while total_items > 0:
+                if chunk_size > total_items:
+                    chunk_size = total_items
+                self._import_chunk(domain, offset, chunk_size)
+                offset += chunk_size
+                total_items -= chunk_size
+        else:
+            sync_date = fields.Datetime.now()
+            data, len_items = self.backend_adapter.search_read(domain)
+            for d in data:
+                external_id = self.binder_for().dict2id(d, in_field=False)
+                self._import_record(external_id, sync_date, external_data=d)
+
+    def _import_chunk(self, domain, offset, chunk_size):
+        raise NotImplementedError
+
+    def _import_batch(self, domain):
+        raise NotImplementedError
+
+    def _import_record(self, external_id, sync_date, external_data=None):
+        """Import a record directly or delay the import of the record.
+
+        Method to implement in sub-classes.
+        """
+        raise NotImplementedError
+
+
+class BatchDirectImporter(AbstractComponent):
+    """Import the records directly, without delaying the jobs."""
+
+    _name = "generic.batch.direct.importer"
+    _inherit = "generic.batch.importer"
+
+    _usage = "batch.direct.importer"
+
+    def _import_chunk(self, domain, offset, chunk_size):
+        self.model.import_chunk(self.backend_record, domain, offset, chunk_size)
+
+    def _import_batch(self, domain):
+        self.model.import_batch(self.backend_record, domain)
+
+    def _import_record(self, external_id, sync_date, external_data=None):
+        """Import the record directly"""
+        if external_data is None:
+            external_data = {}
+        self.model.import_record(
+            self.backend_record, external_id, sync_date, external_data=external_data
+        )
+
+
+class BatchDelayedImporter(AbstractComponent):
+    """Delay import of the records"""
+
+    _name = "generic.batch.delayed.importer"
+    _inherit = "generic.batch.importer"
+
+    _usage = "batch.delayed.importer"
+
+    def _import_chunk(self, domain, offset, chunk_size):
+        delayable = self.model.with_delay()
+        delayable.import_chunk(self.backend_record, domain, offset, chunk_size)
+
+    def _import_batch(self, domain):
+        delayable = self.model.with_delay()
+        delayable.import_batch(self.backend_record, domain)
+
+    def _import_record(
+        self, external_id, sync_date, external_data=None, job_options=None
+    ):
+        """Delay the import of the records"""
+        if external_data is None:
+            external_data = {}
+        delayable = self.model.with_delay(**job_options or {})
+        delayable.import_record(
+            self.backend_record, external_id, sync_date, external_data=external_data
+        )
+
+
+class GenericChunkImporter(AbstractComponent):
+    """The role of a ChunkImporter is to search for a list of
+    items to import, then it can either import them directly or delay
+    the import of each item separately.
+    """
+
+    _name = "generic.chunk.importer"
+    _inherit = "base.importer"
+
+    def run(self, domain, offset, chunk_size):
+        """Run the synchronization"""
+        sync_date = fields.Datetime.now()
+        data, len_items = self.backend_adapter.search_read(domain, offset, chunk_size)
+        chunk_size -= len_items
+        offset += len_items
+        if chunk_size < 0:
+            raise ValidationError(_("Unexpected Error: Chunk_size is < 0"))
+        if chunk_size != 0:
+            self.get_batch_importer()._import_chunk(domain, offset, chunk_size)
+        for d in data:
+            external_id = self.binder_for().dict2id(d, in_field=False)
+            self._import_record(external_id, sync_date, external_data=d)
+
+    def get_batch_importer(self):
+        raise NotImplementedError
+
+    def _import_record(self, external_id, sync_date, external_data=None):
+        """Import a record directly or delay the import of the record.
+
+        Method to implement in sub-classes.
+        """
+        raise NotImplementedError
+
+
+class ChunkDirectImporter(AbstractComponent):
+    """Import the records directly, without delaying the jobs."""
+
+    _name = "generic.chunk.direct.importer"
+    _inherit = "generic.chunk.importer"
+
+    _usage = "chunk.direct.importer"
+
+    def get_batch_importer(self):
+        return self.component(usage="batch.direct.importer")
+
+    def _import_record(self, external_id, sync_date, external_data=None):
+        """Import the record directly"""
+        if external_data is None:
+            external_data = {}
+        self.model.import_record(
+            self.backend_record, external_id, sync_date, external_data=external_data
+        )
+
+
+class ChunkDelayedImporter(AbstractComponent):
+    """Delay import of the records"""
+
+    _name = "generic.chunk.delayed.importer"
+    _inherit = "generic.chunk.importer"
+
+    _usage = "chunk.delayed.importer"
+
+    def get_batch_importer(self):
+        return self.component(usage="batch.delayed.importer")
+
+    def _import_record(
+        self, external_id, sync_date, external_data=None, job_options=None
+    ):
+        """Delay the import of the records"""
+        if external_data is None:
+            external_data = {}
+        delayable = self.model.with_delay(**job_options or {})
+        delayable.import_record(
+            self.backend_record, external_id, sync_date, external_data=external_data
+        )
