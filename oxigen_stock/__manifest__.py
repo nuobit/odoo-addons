9cabd0c2d0e693fbbec8685c8ab6d06b2d93b775
Eric Antones <eantones@nuobit.com>
2023-08-30 11:25:56 +0200

    [REF] oxigen_stock: replace dangerous position "replace"

--- oxigen_stock/__manifest__.py
+++ oxigen_stock/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Oxigen Stock",
     "summary": "Customizations for Oxigen in Stock. "
     "description when creating a lot from a picking",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "ForgeFlow",
     "website": "https://github.com/oxigensalud/odoo-addons",
     "category": "Warehouse",
