6ce00e8739d1d041c59e97af60d06766de64b720
Eric Antones <eantones@nuobit.com>
2023-09-15 18:30:54 +0200

    [IMP] oxigen_stock: ensure data integrity with products with inventory movements

--- oxigen_stock/models/product_template.py
+++ oxigen_stock/models/product_template.py
@@ -1,7 +1,7 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -10,14 +10,35 @@ class ProductTemplate(models.Model):
 
     company_id = fields.Many2one(default=lambda self: self.env.company)
 
-    def write(self, vals):
-        # Convert standard type/tracking warnings to blocking errors:
-        if "type" in vals:
-            has_moves = self.product_variant_ids.ids and self.env[
+    @api.constrains("type")
+    def _check_type(self):
+        for rec in self:
+            if rec.type == "service":
+                has_moves = rec.product_variant_ids.ids and self.env[
+                    "stock.move"
+                ].search_count(
+                    [
+                        ("product_id", "in", rec.product_variant_ids.ids),
+                    ]
+                )
+                if has_moves:
+                    raise ValidationError(
+                        _(
+                            "This product has been used in at least one inventory movement. "
+                            "It is not advised to change the Product Type since it can lead "
+                            "to inconsistencies. A better solution could be to archive the "
+                            "product and create a new one instead.",
+                        )
+                    )
+
+    @api.constrains("tracking")
+    def _check_tracking(self):
+        for rec in self:
+            has_moves = rec.product_variant_ids.ids and self.env[
                 "stock.move.line"
             ].sudo().search_count(
                 [
-                    ("product_id", "in", self.product_variant_ids.ids),
+                    ("product_id", "in", rec.product_variant_ids.ids),
                     ("state", "!=", "cancel"),
                 ]
             )
@@ -25,25 +46,8 @@ class ProductTemplate(models.Model):
                 raise ValidationError(
                     _(
                         "This product has been used in at least one inventory movement. "
-                        "It is not advised to change the Product Type since it can lead "
+                        "It is not advised to change the Tracking type since it can lead "
                         "to inconsistencies. A better solution could be to archive the "
                         "product and create a new one instead.",
                     )
                 )
-        new_tracking = vals.get("tracking")
-        if new_tracking and new_tracking != "none":
-            unassigned_quants = self.env["stock.quant"].search_count(
-                [
-                    ("product_id", "in", self.product_variant_ids.ids),
-                    ("lot_id", "=", False),
-                    ("location_id.usage", "=", "internal"),
-                ]
-            )
-            if unassigned_quants:
-                raise ValidationError(
-                    _(
-                        "You have product(s) in stock that have no lot/serial number. "
-                        "You can assign lot/serial numbers by doing an inventory adjustment.",
-                    )
-                )
-        return super().write(vals)
