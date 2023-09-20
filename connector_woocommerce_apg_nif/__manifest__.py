48a14db642118dc4c4147677f794746b9575de2d
KNVx <kniubo@nuobit.com>
2023-09-20 13:34:37 +0200

    [ADD] connector_woocommerce_apg_nif: New module

---
+++ connector_woocommerce_apg_nif/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Connector WooCommerce APG NIF",
+    "version": "14.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "external_dependencies": {
+        "python": [
+            "woocommerce",
+        ],
+    },
+    "depends": [
+        "connector_woocommerce",
+    ],
+    "installable": True,
+}
