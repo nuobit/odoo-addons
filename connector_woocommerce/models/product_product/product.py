6a5e3d5f0658f3f15f969ba44e7bea1ded5b174a
KNVx <kniubo@nuobit.com>
2023-09-27 09:49:31 +0200

    [IMP] connector_woocommerce: included compute in variant_is_published without depends to compute this field on install

--- connector_woocommerce/models/product_product/product.py
+++ connector_woocommerce/models/product_product/product.py
@@ -39,24 +39,16 @@ class ProductProduct(models.Model):
         translate=True,
     )
     variant_is_published = fields.Boolean(
-        default=False,
-        # inverse="_inverse_variant_is_published",
-        # compute="_compute_variant_is_published",
-        # store=True,
-        # readonly=False,
+        compute="_compute_variant_is_published",
+        store=True,
+        readonly=False,
     )
-    # def _inverse_variant_is_published(self):
-    #     for rec in self:
-    #         published_variants=rec._origin.product_tmpl_id.product_variant_ids.filtered(
-    #             lambda x: x.variant_is_published
-    #         )
-    #         rec.product_tmpl_id = bool(published_variants)
 
-    # @api.depends("product_tmpl_id.is_published")
-    # def _compute_variant_is_published(self):
-    #     for rec in self:
-    #         if not self.env.context.get("variant_published", False):
-    #             rec.variant_is_published = rec.product_tmpl_id.is_published
+    # This compute without depends is used to compute the field on install,
+    # but it's set and updated on template_is_published with inverse
+    def _compute_variant_is_published(self):
+        for rec in self:
+            rec.variant_is_published = rec.product_tmpl_id.is_published
 
     product_attachment_ids = fields.Many2many(
         comodel_name="product.attachment",
