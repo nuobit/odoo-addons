5114e49a7f0f45e2c0c6b517b0b54fe58dedd865
KNVx <kniubo@nuobit.com>
2023-10-04 10:30:37 +0200

    [IMP] Connector_woocommerce_ast: api removed from imports in stock_picking.py

--- connector_woocommerce_ast/models/stock_picking/stock_picking.py
+++ connector_woocommerce_ast/models/stock_picking/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
