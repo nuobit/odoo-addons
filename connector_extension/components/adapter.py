43379458f4821f108b1b9de249f83e6accfd3ab6
KNVx <kniubo@nuobit.com>
2023-10-11 17:23:20 +0200

    [IMP] connector_extension: License is updated to LPGL-3.0.

--- connector_extension/components/adapter.py
+++ connector_extension/components/adapter.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 import datetime
 
