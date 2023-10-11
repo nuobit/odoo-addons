ba4673b02fc61bda71f9817c7af685c295d3b406
KNVx <kniubo@nuobit.com>
2023-10-11 16:59:43 +0200

    [IMP] connector_extension_woocommerce: License is updated to LPGL-3.0.

--- connector_extension_woocommerce/__manifest__.py
+++ connector_extension_woocommerce/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 {
     "name": "Connector Extension Woocommerce",
@@ -8,7 +8,7 @@
     "to add support for Woocommerce",
     "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, SL",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "category": "Connector",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": ["connector_extension"],
