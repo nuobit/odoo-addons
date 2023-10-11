5fb5809bd15a48a15935fd13233f1247c0754bb1
KNVx <kniubo@nuobit.com>
2023-10-11 17:02:06 +0200

    [IMP] connector_extension_wordpress: License is updated to LPGL-3.0.

--- connector_extension_wordpress/components/adapter.py
+++ connector_extension_wordpress/components/adapter.py
@@ -1,5 +1,5 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 import logging
 
