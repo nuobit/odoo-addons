f52b435f77aa8eb81539eb414680f1d06e9a487d
KNVx <kniubo@nuobit.com>
2023-12-27 13:03:56 +0100

    [IMP] report_json: pre-commit stuff

--- report_json/__manifest__.py
+++ report_json/__manifest__.py
@@ -3,22 +3,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "Base report json",
-    'summary': "Base module to create json report",
-    'author': 'NuoBiT Solutions, S.L., Eric Antones',
-    'category': 'Reporting',
-    'version': '11.0.0.1.0',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/nuobit',
-    'external_dependencies': {
-        'python': [
-        ],
+    "name": "Base report json",
+    "summary": "Base module to create json report",
+    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "category": "Reporting",
+    "version": "11.0.0.1.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "external_dependencies": {
+        "python": [],
     },
-    'depends': [
-        'base', 'web',
+    "depends": [
+        "base",
+        "web",
     ],
-    'data': [
-        'views/webclient_templates.xml',
+    "data": [
+        "views/webclient_templates.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
