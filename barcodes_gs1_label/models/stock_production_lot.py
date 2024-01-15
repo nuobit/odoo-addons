babd5b848b5f35f83884b097a6dcccdcccb3e53b
FrankC013 <fcespedes@nuobit.com>
2024-01-15 10:49:45 +0100

    [IMP] barcodes_gs1_label: added a template system to store the label format and configuration

--- barcodes_gs1_label/models/stock_production_lot.py
+++ barcodes_gs1_label/models/stock_production_lot.py
@@ -2,7 +2,8 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductionLot(models.Model):
@@ -13,3 +14,26 @@ class ProductionLot(models.Model):
             "barcodes_gs1_label.barcodes_lot_option_wizard_view_action"
         )
         return action
+
+    gs1_generated = fields.Boolean()
+
+    @api.onchange("gs1_generated")
+    def _onchange_gs1_generated(self):
+        if not self.gs1_generated and self._origin.id:
+            return {
+                "warning": {
+                    "title": _("Warning"),
+                    "message": _("A GS1 code has been generated with this data."),
+                },
+            }
+
+    @api.constrains("name", "ref")
+    def _check_name(self):
+        for rec in self:
+            if rec.gs1_generated:
+                raise ValidationError(
+                    _(
+                        "If the lot has a GS1 generated, the Lot/Serial "
+                        "Number and Internal Reference cannot be modified"
+                    )
+                )
