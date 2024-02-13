41de04734be45e2801361ed99ef460652af8c0be
FrankC013 <fcespedes@nuobit.com>
2024-02-13 15:10:51 +0100

    [REM] barcodes_gs1_label: remove gs1_generated field

--- barcodes_gs1_label/models/product_product.py
+++ barcodes_gs1_label/models/product_product.py
@@ -3,8 +3,7 @@
 # Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, models
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class ProductProduct(models.Model):
@@ -15,17 +14,3 @@ class ProductProduct(models.Model):
             "barcodes_gs1_label.barcodes_product_option_wizard_view_action"
         )
         return action
-
-    @api.constrains("barcode")
-    def _check_barcode(self):
-        for rec in self:
-            lot = rec.env["stock.production.lot"].search(
-                [("product_id", "=", rec.id), ("gs1_generated", "=", True)],
-            )
-            if lot:
-                raise ValidationError(
-                    _(
-                        "Exists a lot %s with a GS1 generated, the "
-                        "barcode cannot be modified" % lot.mapped("name")
-                    )
-                )
