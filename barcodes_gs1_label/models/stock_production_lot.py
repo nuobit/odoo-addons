ee3365fbe0de46227ee59cdcf1a7d1696afc9dd5
FrankC013 <fcespedes@nuobit.com>
2024-02-13 15:10:51 +0100

    [REM] barcodes_gs1_label: remove gs1_generated field

--- barcodes_gs1_label/models/stock_production_lot.py
+++ barcodes_gs1_label/models/stock_production_lot.py
@@ -2,8 +2,7 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class ProductionLot(models.Model):
@@ -14,26 +13,3 @@ class ProductionLot(models.Model):
             "barcodes_gs1_label.barcodes_lot_option_wizard_view_action"
         )
         return action
-
-    gs1_generated = fields.Boolean()
-
-    @api.onchange("gs1_generated")
-    def _onchange_gs1_generated(self):
-        if not self.gs1_generated and self._origin.id:
-            return {
-                "warning": {
-                    "title": _("Warning"),
-                    "message": _("A GS1 code has been generated with this data."),
-                },
-            }
-
-    @api.constrains("name", "ref")
-    def _check_name(self):
-        for rec in self:
-            if rec.gs1_generated:
-                raise ValidationError(
-                    _(
-                        "If the lot has a GS1 generated, the Lot/Serial "
-                        "Number and Internal Reference cannot be modified"
-                    )
-                )
