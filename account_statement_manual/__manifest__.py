dc623f5b46af08a93ec4c059b000a34533a04632
Eric Antones <eantones@nuobit.com>
2024-03-16 17:13:22 +0100

    [ADD] account_statement_manual

---
+++ account_statement_manual/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions - Eric Antones <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Statement Manual",
+    "summary": "This module allows to create statements manually.",
+    "version": "16.0.1.0.0",
+    "category": "Account",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["views/account_bank_statement_views.xml"],
+}
