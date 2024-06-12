dabe58a15a1348946321aca93ca0a784aed77db9
KNVx <kniubo@nuobit.com>
2024-06-12 16:12:17 +0200

    [IMP] bank_multiple_acc_number: better validation error message on check_account_number_vat

--- bank_multiple_acc_number/models/partner_bank.py
+++ bank_multiple_acc_number/models/partner_bank.py
@@ -12,8 +12,14 @@ class ResPartnerBank(models.Model):
 
     @api.constrains("acc_number", "partner_id")
     def check_acc_number_vat(self):
+        def _partner_name(partner):
+            elems = [partner.name]
+            if not partner.active:
+                elems.append("(Archived)")
+            return " ".join(elems)
+
         for rec in self:
-            partner_bank = self.env["res.partner.bank"].search_count(
+            partner_bank = self.env["res.partner.bank"].search(
                 [
                     ("company_id", "=", rec.company_id.id),
                     ("sanitized_acc_number", "=", rec.sanitized_acc_number),
@@ -24,8 +30,10 @@ class ResPartnerBank(models.Model):
             if partner_bank:
                 raise ValidationError(
                     _(
-                        """Account number must be unique for
-                        partners with a different VAT number"""
+                        "Account number must be unique for "
+                        "partners with a different VAT number. "
+                        "Review the account number for partners: %s"
+                        % ", ".join([_partner_name(p) for p in partner_bank.partner_id])
                     )
                 )
 
