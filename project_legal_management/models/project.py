4d04a126cc5412e30617e97c0940b4985a44b903
KNVx <kniubo@nuobit.com>
2024-06-13 13:33:41 +0200

    [IMP] project_legal_management: projects can be filtered and group by issues

--- project_legal_management/models/project.py
+++ project_legal_management/models/project.py
@@ -1,7 +1,7 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Project(models.Model):
@@ -18,3 +18,15 @@ class Project(models.Model):
     lm_probability_id = fields.Many2one(
         string="Probability", comodel_name="lm.probability"
     )
+    # This field is not necessary on version 15 and above
+    lm_issues_complete_name = fields.Char(
+        compute="_compute_lm_issues_complete_name",
+        store=True,
+    )
+
+    @api.depends("lm_issue_ids")
+    def _compute_lm_issues_complete_name(self):
+        for rec in self:
+            rec.lm_issues_complete_name = ", ".join(
+                issue.complete_name for issue in rec.lm_issue_ids
+            )
