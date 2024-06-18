fc059ef4528bb5cac637bb64152ba784416a54f3
KNVx <kniubo@nuobit.com>
2024-06-18 17:12:19 +0200

    [ADD] tools_mimetypes_extension

---
+++ tools_mimetypes_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Tools mimetypes extension",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Tools",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [],
+    "data": [],
+    "installable": True,
+}
+# modificar a tools mimetypes extension
