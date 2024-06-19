d9bb3746e86a06a01aa7bc120d6565a0a277949e
KNVx <kniubo@nuobit.com>
2024-06-19 11:27:07 +0200

    [ADD] partner_firstname_swap_position

---
+++ partner_firstname_swap_position/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner Firstname Swap Position",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "maintainer": "NuoBiT",
+    "category": "Extra Tools",
+    "depends": [
+        "partner_firstname",
+    ],
+    "website": "https://github.com/nuobit/odoo-addons",
+    "data": [
+        "views/res_partner.xml",
+        "views/res_user.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
