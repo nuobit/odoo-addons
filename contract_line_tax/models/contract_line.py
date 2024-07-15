8c86e124a1463b23520191a2beccbc317a0f3f1c
FrankC013 <fcespedes@nuobit.com>
2024-07-15 15:45:04 +0200

    [FIX] contract_line_tax: take tha taxes based on the fiscal position

--- contract_line_tax/models/contract_line.py
+++ contract_line_tax/models/contract_line.py
@@ -19,6 +19,12 @@ class ContractLine(models.Model):
         self.ensure_one()
         res = super(ContractLine, self)._prepare_invoice_line(move_form)
         taxes = self.tax_ids or self.product_id.taxes_id
+        if taxes:
+            fiscal_position = self.contract_id.fiscal_position_id or self.env[
+                "account.fiscal.position"
+            ].get_fiscal_position(self.partner_id.id)
+            if fiscal_position:
+                taxes = fiscal_position.map_tax(taxes, partner=self.partner_id)
         res.update(
             {
                 "tax_ids": taxes.ids and [(6, 0, taxes.ids)] or [],
