1fa01a80e2e531a2da7016f9644646e8d2c82bcc
FrankC013 <fcespedes@nuobit.com>
2024-07-23 17:44:43 +0200

    [ADD] account_move_service

---
+++ account_move_service/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Move Service",
+    "summary": "Module to allow grouping service billing in account moves",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "category": "Sales/Accounting",
+    "license": "AGPL-3",
+    "depends": ["sale_order_service"],
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/res_partner_views.xml",
+    ],
+}
