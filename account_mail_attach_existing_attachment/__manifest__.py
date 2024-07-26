9489139b23b89307752d2d1c1944879e9afe59c6
FrankC013 <fcespedes@nuobit.com>
2024-07-26 14:02:37 +0200

    [ADD] account_mail_attach_existing_attachment

---
+++ account_mail_attach_existing_attachment/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Mail Attach Existing Attachment",
+    "summary": "Module to use attach existing attachment for account module",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account", "mail"],
+}
