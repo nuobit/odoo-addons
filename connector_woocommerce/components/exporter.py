765db850128e7b948186438bd0ee5a173a3421c4
KNVx <kniubo@nuobit.com>
2024-07-31 11:14:40 +0200

    [REF] connector_woocommerce: Replaced http for https in licence url

--- connector_woocommerce/components/exporter.py
+++ connector_woocommerce/components/exporter.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 import logging
 
