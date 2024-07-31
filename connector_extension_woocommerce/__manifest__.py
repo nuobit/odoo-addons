2d64ac16d55aeb882f896a5c02e3a7bf991ae97e
KNVx <kniubo@nuobit.com>
2024-07-31 11:14:53 +0200

    [REF] connector_extension_woocommerce - Replaced http for https in licence url

--- connector_extension_woocommerce/__manifest__.py
+++ connector_extension_woocommerce/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 {
     "name": "Connector Extension Woocommerce",
--- connector_extension_woocommerce/components/adapter.py
+++ connector_extension_woocommerce/components/adapter.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 import json
 import logging
