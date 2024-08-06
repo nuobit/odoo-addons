4007666745e5f3a2bd006edcb7ba5e8d18ac3b4b
FrankC013 <fcespedes@nuobit.com>
2024-08-06 14:16:40 +0200

    [IMP] sale_order_service: change 'service date' from type Date to Datetime & creation of new fields for ambugest

--- sale_order_service/models/sale_order.py
+++ sale_order_service/models/sale_order.py
@@ -18,8 +18,12 @@ class SaleOrder(models.Model):
     # service data
     plate_number = fields.Char(string="Plate")
     service_number = fields.Integer(string="Service number")
-    service_date = fields.Date(string="Service date")
+    service_date = fields.Datetime(string="Service date")
     origin = fields.Char(string="Origin")
     destination = fields.Char(string="Destination")
     round_trip_code = fields.Integer(string="Round trip code")
     return_service = fields.Boolean(string="Return service")
+    service_key = fields.Char(string="Service key")
+    service_transfer_reason = fields.Char(string="Service transfer reason")
+    service_insurer_code = fields.Char(string="Service insurer code")
+    service_insurer_name = fields.Char(string="Service insurer name")
