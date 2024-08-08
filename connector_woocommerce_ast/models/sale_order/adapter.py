1a89fceb7ff8571e4677c8d4fa305e04059ac8cd
KNVx <kniubo@nuobit.com>
2024-08-08 11:29:07 +0200

    [IMP] connector_woocommerce_ast: included manage errors to detect configuration problems

--- connector_woocommerce_ast/models/sale_order/adapter.py
+++ connector_woocommerce_ast/models/sale_order/adapter.py
@@ -1,5 +1,9 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+import re
+
+from odoo import _
+from odoo.exceptions import ValidationError
 
 from odoo.addons.component.core import Component
 
@@ -11,3 +15,29 @@ class WooCommerceSaleOrderAdapter(Component):
         meta_data_fields = super()._prepare_meta_data_fields()
         meta_data_fields.append("_wc_shipment_tracking_items")
         return meta_data_fields
+
+    def _manage_error_codes(
+        self, res_data, res, resource, raise_on_error=True, **kwargs
+    ):
+        if not res.ok:
+            if res.status_code == 400:
+                m = re.match("^[^:]+: (.+)$", res_data["message"])
+                if (
+                    m
+                    and m.group(1) == "status"
+                    and res_data["code"] == "rest_invalid_param"
+                ):
+                    error_message = _(
+                        "Error: '%s'. Probably the state %s is not defined on Woocommerce. "
+                        "Configure states in Woocommerce Advanced "
+                        "Shippment Tracking (AST) settings."
+                        % (res_data["message"], kwargs["data"]["status"])
+                    )
+                    if raise_on_error:
+                        raise ValidationError(error_message)
+                    else:
+                        return error_message
+
+        return super()._manage_error_codes(
+            res_data, res, resource, raise_on_error, **kwargs
+        )
