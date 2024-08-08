f86911bb4dc6422a859ff76f194e3b518e934da0
KNVx <kniubo@nuobit.com>
2024-08-08 12:15:31 +0200

    [ADD] connector_wordpress_wpml: new module

---
+++ connector_wordpress_wpml/models/ir_attachment/export_mapper.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import changed_by, mapping
+
+
+class WordPressIrAttachmentExportMapper(Component):
+    _inherit = "wordpress.ir.attachment.export.mapper"
+
+    @changed_by("lang")
+    @mapping
+    def lang(self, record):
+        # TODO: unify this code. Probably do a function in res lang
+        lang = self.env["res.lang"]._get_wpml_code_from_iso_code(
+            record._context.get("lang")
+        )
+        return {"lang": lang}
