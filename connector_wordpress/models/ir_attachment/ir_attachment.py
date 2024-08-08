264ba66928121f7460e7125840b7af4162c12057
KNVx <kniubo@nuobit.com>
2024-08-08 12:17:39 +0200

    [IMP] connector_wordpress: included seo data, write on adapter and other minor changes

--- connector_wordpress/models/ir_attachment/ir_attachment.py
+++ connector_wordpress/models/ir_attachment/ir_attachment.py
@@ -1,7 +1,8 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class IrAttachment(models.Model):
@@ -12,3 +13,13 @@ class IrAttachment(models.Model):
         inverse_name="odoo_id",
         string="WordPress Bindings",
     )
+
+    # TODO:REVIEW: We can move this method to another module
+    def _get_seo_meta_data(self):
+        self.ensure_one()
+        model_obj = self.env[self.res_model].browse(self.res_id)
+        if hasattr(model_obj, "_get_seo_meta_data"):
+            meta_data = model_obj._get_seo_meta_data()
+            if not isinstance(meta_data, dict):
+                raise ValidationError(_("it should always return a dictionary"))
+            return meta_data
