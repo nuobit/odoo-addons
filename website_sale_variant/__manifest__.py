6baf71a7845f42c5783336f7eb31ace037375543
KNVx <kniubo@nuobit.com>
2024-08-09 17:56:02 +0200

    [ADD] website_sale_variant: new module

---
+++ website_sale_variant/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Website Sale Variant",
+    "version": "14.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Website",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/product_product.xml",
+    ],
+    "installable": True,
+}
