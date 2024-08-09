506b9d5920612cb984f990c5aa4347811648b495
KNVx <kniubo@nuobit.com>
2024-08-09 17:54:25 +0200

    [ADD] website_sale_extra_fields: new module

---
+++ website_sale_extra_fields/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Website Sale Variant",
+    "version": "14.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Website",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "website_sale_variant",
+    ],
+    "data": [
+        "views/product_template.xml",
+        "views/product_product.xml",
+    ],
+    "installable": True,
+}
