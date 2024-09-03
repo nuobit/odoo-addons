7ae035ca79b20e89636e91d0d5183c184644978f
KNVx <kniubo@nuobit.com>
2024-09-03 16:00:55 +0200

    [IMP] website_sale_extra_fields: Included description in product_public_category

--- website_sale_extra_fields/models/__init__.py
+++ website_sale_extra_fields/models/__init__.py
@@ -1,3 +1,4 @@
-from . import product_attachment
 from . import product
+from . import product_attachment
+from . import product_public_category
 from . import product_template
---
+++ website_sale_extra_fields/models/product_public_category.py
@@ -0,0 +1,12 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class ProductPublicCategory(models.Model):
+    _inherit = "product.public.category"
+
+    description = fields.Text(
+        translate=True,
+    )
