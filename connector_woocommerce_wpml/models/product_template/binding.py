b2bd0d4b5a49f31c20267fcbc866ba9f197edc01
KNVx <kniubo@nuobit.com>
2024-09-04 09:19:13 +0200

    [FIX] connector_woocommerce_wpml: Pre filter lang to resync (See TODO)

--- connector_woocommerce_wpml/models/product_template/binding.py
+++ connector_woocommerce_wpml/models/product_template/binding.py
@@ -34,6 +34,5 @@ class WooCommerceProductTemplate(models.Model):
             for rec in self:
                 rec.product_variant_ids.woocommerce_bind_ids.filtered(
                     lambda x: x.backend_id == self.backend_id
-                ).with_context(
-                    resync_product_template=True, lang=rec._context.get("lang")
-                ).resync_export()
+                    and x.woocommerce_lang == rec.woocommerce_lang
+                ).with_context(resync_product_template=True).resync_export()
