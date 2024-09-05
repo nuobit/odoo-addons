724550ece64615bc67afdc04261eea07c8d60f8c
KNVx <kniubo@nuobit.com>
2024-09-05 16:31:12 +0200

    [IMP] connector_woocommerce: created button to export attachments of products to export. Created cron

--- connector_woocommerce/models/__init__.py
+++ connector_woocommerce/models/__init__.py
@@ -1,6 +1,7 @@
 from . import backend
 from . import binding
 from . import common
+from . import ir_attachment
 from . import product_public_category
 from . import product_attribute
 from . import product_attribute_value
