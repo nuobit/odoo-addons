5a4cc4be6a38e0da38117cd5221e7f9c01d94c41
KNVx <kniubo@nuobit.com>
2024-09-05 12:30:56 +0200

    [IMP] connector_woocommerce_wpml: commented code in _prepare_relation. it can be necessary?

--- connector_woocommerce_wpml/models/product_wpml_mixin/binding.py
+++ connector_woocommerce_wpml/models/product_wpml_mixin/binding.py
@@ -1,17 +1,16 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import models
 
 
-class WooCommerceBinding(models.AbstractModel):
-    _inherit = "woocommerce.binding"
-
-    def _prepare_relation(self, relation, record):
-        super()._prepare_relation(relation, record)
-        context = relation.env.context.copy()
-        iso_lang = self.env["res.lang"]._get_iso_code_from_wpml_code(
-            record.woocommerce_lang
-        )
-        if iso_lang:
-            context.update({"lang": iso_lang, "resync_export": True})
-        relation.env.context = context
+# TODO: delete this file
+# class WooCommerceBinding(models.AbstractModel):
+#     _inherit = "woocommerce.binding"
+#
+#     def _prepare_relation(self, relation, record):
+#         relation = super()._prepare_relation(relation, record)
+#         iso_lang = self.env["res.lang"]._get_iso_code_from_wpml_code(
+#             record.woocommerce_lang
+#         )
+#         if iso_lang:
+#             return relation.with_context(lang=iso_lang, resync_export=True)
+#         return relation
