e471dc5f9a9b7693f1056b1b6b5f0fabc85d566f
FrankC013 <fcespedes@nuobit.com>
2024-09-13 17:52:25 +0200

    [IMP] account_move_service: assign the total amount to be invoiced in the invoice generation and adjust UOM quantity.

---
+++ account_move_service/models/account_move_line.py
@@ -0,0 +1,32 @@
+# Copyright NuoBiT - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.model
+    def _get_price_total_and_subtotal_model(
+        self,
+        price_unit,
+        quantity,
+        discount,
+        currency,
+        product,
+        partner,
+        taxes,
+        move_type,
+    ):
+        res = super()._get_price_total_and_subtotal_model(
+            price_unit, quantity, discount, currency, product, partner, taxes, move_type
+        )
+        if (
+            self.move_id.partner_id.service_intermediary
+            and self.price_subtotal
+            and self.price_total
+        ):
+            res["price_subtotal"] = self.price_subtotal
+            res["price_total"] = self.price_total
+        return res
