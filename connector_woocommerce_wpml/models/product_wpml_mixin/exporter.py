a9ecbf0e087ed6eb0abb0d3565ac03e1eda9d830
FrankC013 <fcespedes@nuobit.com>
2024-10-16 12:18:50 +0200

    [FIX] connector_woocommerce_wpml: languages required

--- connector_woocommerce_wpml/models/product_wpml_mixin/exporter.py
+++ connector_woocommerce_wpml/models/product_wpml_mixin/exporter.py
@@ -1,5 +1,7 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import _
+from odoo.exceptions import ValidationError
 
 from odoo.addons.component.core import AbstractComponent
 
@@ -11,6 +13,14 @@ class WooCommerceProductWPMLMixinExporter(AbstractComponent):
     def wpml_run(self, relation, always=True, internal_fields=None):
         res = []
         langs_to_export = self.backend_record.language_ids.mapped("code")
+        if not langs_to_export:
+            raise ValidationError(
+                _(
+                    "You need to define at least one language to export "
+                    "in the WooCommerce WPML Backend (%s)."
+                )
+                % self.backend_record.name
+            )
         for lang in langs_to_export:
             result = super().run(
                 relation.with_context(lang=lang),
