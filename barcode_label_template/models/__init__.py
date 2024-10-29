1ba75722132a76b9390e6b91d81bd06811f48047
FrankC013 <fcespedes@nuobit.com>
2024-10-29 13:42:21 +0100

    [ADD] barcode_label_template

---
+++ barcode_label_template/models/__init__.py
@@ -0,0 +1,4 @@
+from . import report_barcode_label_template
+from . import barcode_label_template_configuration
+from . import stock_production_lot
+from . import ir_actions_report
---
+++ barcode_label_template/wizard/barcode_label_template_wizard.py
@@ -0,0 +1,53 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+
+
+class BarcodeLabelTemplateWizard(models.TransientModel):
+    _name = "barcode.label.template.wizard"
+    _description = "Barcode Label Template Wizard"
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(BarcodeLabelTemplateWizard, self).default_get(fields_list)
+        default_configuration = self.env["barcode.label.template.configuration"].search(
+            [("default", "=", True)],
+        )
+        if default_configuration:
+            res["barcode_label_template_configuration_id"] = default_configuration.id
+        return res
+
+    @property
+    def BARCODE_TYPE_SELECTION(self):
+        return [
+            ("gs1-datamatrix", "GS1-Datamatrix"),
+        ]
+
+    barcode_type = fields.Selection(
+        selection=lambda self: self.BARCODE_TYPE_SELECTION,
+        required=True,
+        default="gs1-datamatrix",
+    )
+    barcode_label_template_configuration_id = fields.Many2one(
+        comodel_name="barcode.label.template.configuration",
+        required=True,
+    )
+
+    def print_barcodes_with_template(self):
+        return (
+            self.env.ref("barcode_label_template.action_report_barcode_label_template")
+            .with_context(
+                template_configuration_id=self.barcode_label_template_configuration_id.id,
+            )
+            .report_action(
+                self,
+                data={
+                    "active_ids": self.env.context.get("active_ids"),
+                    "barcode_type": self.barcode_type,
+                    "position_x": self.barcode_label_template_configuration_id.position_x,
+                    "position_y": self.barcode_label_template_configuration_id.position_y,
+                    "width": self.barcode_label_template_configuration_id.width,
+                },
+            )
+        )
