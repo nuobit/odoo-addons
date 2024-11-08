20e8ecd23e2be52f8b979bf3b9acac6afb916bea
FrankC013 <fcespedes@nuobit.com>
2024-11-08 08:34:38 +0100

    [IMP] barcode_label_template: allow adding more custom field value in the template

--- barcode_label_template/models/ir_actions_report.py
+++ barcode_label_template/models/ir_actions_report.py
@@ -1,11 +1,41 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+import io
+
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
 from odoo import api, models
 
 
 class Report(models.Model):
     _inherit = "ir.actions.report"
+
+    def _render_qweb_pdf(self, res_ids=None, data=None):
+        if self.env.context.get("template_configuration_id"):
+            pdf_merger = PdfFileMerger()
+            for active_id in data["active_ids"]:
+                new_data = data.copy()
+                new_data["active_ids"] = [active_id]
+                pdf_content_chunk, _ = super()._render_qweb_pdf(
+                    res_ids=res_ids, data=new_data
+                )
+
+                pdf_mem_file_chunk = io.BytesIO()
+                pdf_mem_file_chunk.write(pdf_content_chunk)
+                pdf_chunk = PdfFileReader(pdf_mem_file_chunk)
+                pdf_merger.append(pdf_chunk, import_bookmarks=False)
+                pdf_mem_file_chunk.close()
+
+            if pdf_merger:
+                pdf_mem_file = io.BytesIO()
+                pdf_merger.write(pdf_mem_file)
+                pdf_content = pdf_mem_file.getvalue()
+                pdf_mem_file.close()
+        else:
+            pdf_content, _ = super()._render_qweb_pdf(res_ids=res_ids, data=data)
+
+        return pdf_content, "pdf"
 
     @api.model
     def _run_wkhtmltopdf(
