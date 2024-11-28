2922bac9f77f88768e00bdacf4fc174dbb25f676
FrankC013 <fcespedes@nuobit.com>
2024-11-28 18:52:25 +0100

    [IMP] connector_oxigesti: logic to mark general items as deprecated per customer

--- connector_oxigesti/models/res_partner/__init__.py
+++ connector_oxigesti/models/res_partner/__init__.py
@@ -5,3 +5,4 @@ from . import adapter
 from . import import_mapper
 from . import binder
 from . import binding
+from . import listener
--- connector_oxigesti/models/res_partner/listener.py
+++ connector_oxigesti/models/res_partner/listener.py
@@ -1,22 +1,22 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 from odoo.addons.component.core import Component
 
 
-class ProductTemplateListener(Component):
-    _name = "oxigesti.product.template.listener"
+class ProductPricelistItemListener(Component):
+    _name = "oxigesti.res.partner.listener"
     _inherit = "oxigesti.event.listener"
 
-    _apply_on = "product.template"
+    _apply_on = "res.partner"
 
     def on_record_unlink(self, relation):
         bindings = (
             relation.sudo()
             .with_context(active_test=False)
-            .product_variant_ids.oxigesti_bind_ids
+            .property_product_pricelist.item_ids.oxigesti_bind_ids.filtered(
+                lambda x: x.odoo_partner_id == relation
+            )
         )
         for backend, domain in bindings.get_external_ids_domain_by_backend().items():
-            self.env[
-                "oxigesti.product.pricelist.item"
-            ].with_delay().export_delete_batch(backend, domain)
+            bindings.with_delay().export_delete_batch(backend, domain=domain)
