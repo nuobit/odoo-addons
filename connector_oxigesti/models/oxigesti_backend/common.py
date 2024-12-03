8550cd7dc36f4bce3128d92ddd681bd3f7e45f10
FrankC013 <fcespedes@nuobit.com>
2024-12-03 11:45:44 +0100

    [IMP] connector_oxigesti: export pricelist items with chunks

--- connector_oxigesti/models/oxigesti_backend/common.py
+++ connector_oxigesti/models/oxigesti_backend/common.py
@@ -77,6 +77,7 @@ class OxigestiBackend(models.Model):
     )
     active = fields.Boolean(string="Active", default=True)
     state = fields.Selection(selection="_select_state", string="State", default="draft")
+    chunk_size = fields.Integer()
 
     def button_reset_to_draft(self):
         self.ensure_one()
