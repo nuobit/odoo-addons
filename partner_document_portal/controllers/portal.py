c6454b2da3722b74094cf2b294b41c43e0341af9
FrankC013 <fcespedes@nuobit.com>
2024-12-19 11:27:45 +0100

    [FIX] partner_document_portal: fix pre-commit and errors

--- partner_document_portal/controllers/portal.py
+++ partner_document_portal/controllers/portal.py
@@ -1,9 +1,11 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import base64
+
 from odoo import _, http
-from odoo.http import request
 from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
 
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
 
@@ -84,12 +86,14 @@ class DocumentPortal(CustomerPortal):
         )
         return request.render("partner_document_portal.portal_my_documents", values)
 
-    @http.route(["/my/documents/<int:document_id>"], type="http", auth="public", website=True)
-    def portal_my_document_detail(
-        self, document_id, access_token=None, **kw
-    ):
+    @http.route(
+        ["/my/documents/<int:document_id>"], type="http", auth="public", website=True
+    )
+    def portal_my_document_detail(self, document_id, access_token=None, **kw):
         try:
-            document_sudo = self._document_check_access("partner.document", document_id, access_token)
+            document_sudo = self._document_check_access(
+                "partner.document", document_id, access_token
+            )
         except (AccessError, MissingError):
             return request.redirect("/my")
         values = self._document_get_page_view_values(document_sudo, access_token, **kw)
@@ -104,58 +108,85 @@ class DocumentPortal(CustomerPortal):
             document, access_token, values, "my_document_history", False, **kwargs
         )
 
-    @http.route(['/my/documents/upload'], type='http', auth='user', website=True)
+    @http.route(["/my/documents/upload"], type="http", auth="user", website=True)
     def portal_upload_document(self, **kw):
         values = self._prepare_portal_layout_values()
-        return request.render('partner_document_portal.portal_upload_document', values)
+        return request.render("partner_document_portal.portal_upload_document", values)
 
-    @http.route(['/my/documents/upload/submit'], type='http', auth='user', methods=['POST'], website=True, csrf=False)
+    @http.route(
+        ["/my/documents/upload/submit"],
+        type="http",
+        auth="user",
+        methods=["POST"],
+        website=True,
+        csrf=False,
+    )
     def portal_upload_document_submit(self, **kw):
-        Document = request.env['partner.document']
-        file = request.httprequest.files.get('file')
+        Document = request.env["partner.document"]
+        file = request.httprequest.files.get("file")
         datas = base64.b64encode(file.read()) if file else None
 
         document_data = {
-            'description': kw.get('description'),
-            'expiration_date': kw.get('expiration_date'),
-            'document_type_id': int(kw.get('document_type_id')),
-            'datas': datas,
+            "description": kw.get("description"),
+            "expiration_date": kw.get("expiration_date"),
+            "document_type_id": int(kw.get("document_type_id")),
+            "datas": datas,
         }
-        document = Document.create(document_data)
-        return request.redirect('/my/documents')
+        Document.create(document_data)
+        return request.redirect("/my/documents")
 
-    @http.route(['/my/documents/update_document'], type='http', auth='user', methods=['POST'], website=True, csrf=False)
+    @http.route(
+        ["/my/documents/update_document"],
+        type="http",
+        auth="user",
+        methods=["POST"],
+        website=True,
+        csrf=False,
+    )
     def update_document(self, **post):
-        document_id = int(post.get('document_id'))
-        description = post.get('description')
-        expiration_date = post.get('expiration_date')
-        document = request.env['partner.document'].sudo().browse(document_id)
-        document.write({
-            'description': description,
-            'expiration_date': expiration_date,
-        })
-        file = post.get('attachment')
+        document_id = int(post.get("document_id"))
+        description = post.get("description")
+        expiration_date = post.get("expiration_date")
+        document = request.env["partner.document"].sudo().browse(document_id)
+        document.write(
+            {
+                "description": description,
+                "expiration_date": expiration_date,
+            }
+        )
+        file = post.get("attachment")
         if file:
             # TODO: How to create a attachment??
-            attachment = request.env['ir.attachment'].sudo().create({
-                'name': file.filename,
-                'datas': base64.b64encode(file.read()).decode('ascii'),
-                'res_model': 'partner.document',
-                'res_id': document_id,
-                'type': 'binary',
-            })
-            document.write({'datas': [(4, attachment.datas)]})
+            attachment = (
+                request.env["ir.attachment"]
+                .sudo()
+                .create(
+                    {
+                        "name": file.filename,
+                        "datas": base64.b64encode(file.read()).decode("ascii"),
+                        "res_model": "partner.document",
+                        "res_id": document_id,
+                        "type": "binary",
+                    }
+                )
+            )
+            document.write({"datas": [(4, attachment.datas)]})
 
-        return request.redirect('/my/documents/%s' % document_id)
+        return request.redirect("/my/documents/%s" % document_id)
 
-    @http.route(['/my/documents/delete/<int:document_id>'], type='http', auth='user', website=True)
+    @http.route(
+        ["/my/documents/delete/<int:document_id>"],
+        type="http",
+        auth="user",
+        website=True,
+    )
     def portal_delete_document(self, document_id, **kw):
-        Document = request.env['partner.document']
+        Document = request.env["partner.document"]
         try:
             document = Document.browse(document_id)
             if not document.exists() or document.validated:
-                raise AccessError("You cannot delete this document.")
+                raise AccessError(_("You cannot delete this document."))
             document.unlink()
         except (AccessError, MissingError):
-            return request.redirect('/my/documents')
-        return request.redirect('/my/documents')
+            return request.redirect("/my/documents")
+        return request.redirect("/my/documents")
