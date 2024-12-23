aff9219f0451a7a894637e01afe4fd23c407c2c5
FrankC013 <fcespedes@nuobit.com>
2024-12-23 12:15:12 +0100

    Revert "[IMP] account_move_service_report_xlsx: remove column and change column name"
    This reverts commit 67f19688796262a0a77866a3f6a22b40b31ca39a.

--- account_move_service_report_xlsx/models/sale_order.py
+++ account_move_service_report_xlsx/models/sale_order.py
@@ -8,6 +8,9 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    def _get_empty_column(self):
+        return False
+
     def _get_provider_code(self):
         return self.partner_id.service_report_config_id.provider_code
 
