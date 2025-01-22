7fd2f7fbcd161843b311e2e2674483fb486bbeae
Deniz <deeniiz>
2025-01-22 10:47:30 +0000

    [IMP] partner_name_vat: pre-commit auto fixes

--- partner_name_vat/__manifest__.py
+++ partner_name_vat/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.0",
     "category": "Web",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["base"],
     "post_init_hook": "post_init_hook_vat_update",
