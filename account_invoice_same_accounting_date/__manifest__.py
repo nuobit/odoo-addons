38cc604acd566d8c1513a7ca498043f777237b72
??? <dgallo@nuobit.com>
2025-01-31 13:36:53 +0100

    [MIG] account_invoice_same_accounting_date: Migration to 17.0

--- account_invoice_same_accounting_date/__manifest__.py
+++ account_invoice_same_accounting_date/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Invoices Same Invoice Date and Accounting Date",
     "summary": "Copy the invoice date to accounting date by default",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "category": "Invoicing",
-    "version": "14.0.1.0.0",
+    "version": "17.0.0.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": ["account"],
