9223f0e0bd6246a4ac9c348734a0fd02d114eb3d
??? <dgallo@nuobit.com>
2025-02-05 11:42:32 +0100

    [REF] partner_name_vat: OCA guidelines

--- partner_name_vat/models/res_partner.py
+++ partner_name_vat/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
-# Deniz Gallo <dgallo@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
