d5dd56c3a24a4f06b29162de9f784d2dd47efdd9
??? <dgallo@nuobit.com>
2025-02-06 13:43:59 +0100

    [MIG] stock_picking_employee: Migration to 17.0

--- stock_picking_employee/models/stock_picking.py
+++ stock_picking_employee/models/stock_picking.py
@@ -1,11 +1,10 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBit - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import fields, models
 
 
-class Location(models.Model):
+class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     employee_ids = fields.Many2many(
