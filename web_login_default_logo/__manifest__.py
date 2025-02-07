1b966ea015b4807dedd841c0cc8714bb446167f3
??? <dgallo@nuobit.com>
2025-02-07 13:09:03 +0100

    [MIG] web_login_default_logo: Migration to 17.0

--- web_login_default_logo/__manifest__.py
+++ web_login_default_logo/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Web login default logo",
     "summary": "This module forces default Odoo logo at login screen.",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Web",
     "author": "NuoBiT Solutions SL",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "web",
--- web_login_default_logo/controllers/main.py
+++ web_login_default_logo/controllers/main.py
@@ -1,11 +1,13 @@
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-import functools
+
+
 import logging
 
 from odoo import http
-from odoo.modules import get_resource_path
+from odoo.tools.misc import file_path
 
 _logger = logging.getLogger(__name__)
 
@@ -20,5 +22,6 @@ class Binary(http.Controller):
         cors="*",
     )
     def no_logo(self, dbname=None, **kw):
-        placeholder = functools.partial(get_resource_path, "web", "static", "img")
-        return http.Stream.from_path(placeholder("nologo.png")).get_response()
+        return http.Stream.from_path(
+            file_path("web/static/img/nologo.png")
+        ).get_response()
