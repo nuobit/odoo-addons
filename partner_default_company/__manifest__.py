f8ac35464265242f1f89305a51c260d7260ef29b
bijaya <kumlabijay05@gmail.com>
2025-02-13 12:23:00 +0000

    [MIG] partner_default_company: Migration to 17.0

--- partner_default_company/__manifest__.py
+++ partner_default_company/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "14.0.1.0.0",
     "category": "Contacts",
     "author": "NuoBiT Solutions, S.L.",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "base",
