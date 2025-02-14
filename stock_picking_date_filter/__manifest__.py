3212a0f300322121c00a24101a2db5314def6e85
bijaya <kumlabijay05@gmail.com>
2025-02-14 12:28:54 +0000

    [MIG] stock_picking_date_filter_ref: Migration to 16.0

--- stock_picking_date_filter/__manifest__.py
+++ stock_picking_date_filter/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
