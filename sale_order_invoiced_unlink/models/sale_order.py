eb10b2cf14283ab21a4d77008d3bd528d979ec52
??? <dgallo@nuobit.com>
2025-02-14 11:38:15 +0100

    [MIG] sale_order_invoiced_unlink: Migration to 16.0

--- sale_order_invoiced_unlink/models/sale_order.py
+++ sale_order_invoiced_unlink/models/sale_order.py
@@ -1,5 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, models
@@ -17,4 +16,4 @@ class SaleOrder(models.Model):
                     "linked to any invoice line."
                 )
             )
-        return super(SaleOrder, self).unlink()
+        return super().unlink()
