7f8907080118577535a98ee712e0ceeea7f7f5af
bijaya <kumlabijay05@gmail.com>
2025-02-14 12:15:04 +0000

    [IMP] product_supplier_ref: pre-commit stuff

--- product_supplier_ref/__manifest__.py
+++ product_supplier_ref/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
+# Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Product Supplier Ref",
     "summary": "Use the product name if the supplier product name is "
     "empty instead of the product default_code.",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase",
-    "author": "NuoBiT Solutions,S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
