39653163f3680d261bcbc569d735b74563d9be15
??? <dgallo@nuobit.com>
2025-02-14 11:38:15 +0100

    [MIG] sale_order_invoiced_unlink: Migration to 16.0

--- sale_order_invoiced_unlink/__manifest__.py
+++ sale_order_invoiced_unlink/__manifest__.py
@@ -1,19 +1,17 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Sale order invoiced unlink",
     "summary": "If a sale order has any of its lines linked to an invoice "
     "this module does not allow to delete the order.",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "category": "Sales",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
         "sale",
     ],
-    "data": [],
-    "installable": True,
 }
