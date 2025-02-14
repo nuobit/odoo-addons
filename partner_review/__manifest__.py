52be908396ee4938861e9ca2f261b808e8e3a3b3
bijaya <kumlabijay05@gmail.com>
2025-02-14 11:09:22 +0000

    [IMP] partner_review: pre-commit auto fixes

--- partner_review/__manifest__.py
+++ partner_review/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
+# Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Partner review",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "version": "17.0.1.0.0",
+    "author": "NuoBiT Solutions SL, Eric Antones",
     "license": "AGPL-3",
     "category": "Extra Tools",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": ["base"],
     "data": [
         "views/res_partner_view.xml",
