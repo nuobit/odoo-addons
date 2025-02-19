d9aa287c81940565db2079ccbef1cb2be4e7adba
bijaya <kumlabijay05@gmail.com>
2025-02-19 09:33:22 +0000

    [MIG] stock_move_line_picking: Migration to 17.0

--- stock_move_line_picking/models/stock_move_line.py
+++ stock_move_line_picking/models/stock_move_line.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    additional_field = fields.Char(string="Additional Field")
+    additional_field = fields.Char()
