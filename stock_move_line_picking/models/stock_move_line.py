53b332afcd8d5262acebd459f64fdb6441273adf
bijaya <kumlabijay05@gmail.com>
2025-02-19 10:27:21 +0000

    [MIG] stock_move_line_picking: Migration to 17.0

---
+++ stock_move_line_picking/models/stock_move_line.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    picking_id = fields.Many2one(related="move_id.picking_id", store=True, readonly=False)
