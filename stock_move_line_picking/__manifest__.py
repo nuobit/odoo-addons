9d9581de9d2b6f62dd190259e5503bfe83d38961
bijaya <kumlabijay05@gmail.com>
2025-02-19 09:52:56 +0000

    [MIG] stock_move_line_picking: Migration to 17.0

--- stock_move_line_picking/__manifest__.py
+++ stock_move_line_picking/__manifest__.py
@@ -1,4 +1,5 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com
+# Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
