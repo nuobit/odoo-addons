a98a71e441335028dd3a49d45b7858c24f9cb9ae
bijaya <kumlabijay05@gmail.com>
2025-02-19 12:20:43 +0000

    [IMP] repair_employee: pre-commit auto fixes

--- repair_employee/__manifest__.py
+++ repair_employee/__manifest__.py
@@ -1,15 +1,16 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
+# Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Repair Employee",
     "summary": "This module adds a comment field in the contract and "
     "propagates it to the comment field of the invoice",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Contract Management",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "author": "NuoBiT Solutions SL, Eric Antones",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["hr", "repair"],
     "data": [
