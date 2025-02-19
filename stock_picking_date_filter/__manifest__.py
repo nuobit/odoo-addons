22ea2a00ba302fb608262959eea7e7a7d31859b3
bijaya <kumlabijay05@gmail.com>
2025-02-19 11:13:16 +0000

    [IMP] stock_picking_date_filter: pre-commit auto fixes

--- stock_picking_date_filter/__manifest__.py
+++ stock_picking_date_filter/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock Picking Date Filter",
-    "version": "14.0.1.0.1",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "17.0.1.0.1",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Stock",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "summary": "This module adds date filters to stock picking tree view",
     "depends": [
         "stock",
