47f6be43dbdd30247b5858a1aacf63093e12b1db
bijaya <kumalbijay05@gmail.com>
2025-02-20 13:28:46 +0100

    [MIG] partner_default_company: Migration to 16.0

--- partner_default_company/__manifest__.py
+++ partner_default_company/__manifest__.py
@@ -1,19 +1,17 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Partner Default Company",
     "summary": "This module populates the company with "
     "the current one on partner creation",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Contacts",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "base",
     ],
-    "installable": True,
-    "auto_install": False,
 }
--- partner_default_company/models/res_partner.py
+++ partner_default_company/models/res_partner.py
@@ -2,12 +2,13 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Partner(models.Model):
     _inherit = "res.partner"
 
+    @api.model
     def _default_company(self):
         return (
             not self.env.context.get("company_creation", False)
