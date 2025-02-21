2104e4c60a9f49f77ad3f9d4bb6c695d9b29d15b
bijaya <kumalbijay05@gmail.com>
2025-02-21 12:35:29 +0100

    [MIG] partner_review: Migration to 16.0

--- partner_review/__manifest__.py
+++ partner_review/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Partner review",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL, Eric Antones",
     "license": "AGPL-3",
     "category": "Extra Tools",
     "website": "https://github.com/nuobit/odoo-addons",
@@ -13,6 +13,4 @@
     "data": [
         "views/res_partner_view.xml",
     ],
-    "installable": True,
-    "application": True,
 }
