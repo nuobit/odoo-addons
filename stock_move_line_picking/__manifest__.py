6649825dadaa9419b2b3c402c66292628cac54e1
bijaya <kumalbijay05@gmail.com>
2025-02-24 13:30:46 +0100

    [MIG] stock_move_line_picking: Migration to 16.0

--- stock_move_line_picking/__manifest__.py
+++ stock_move_line_picking/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock move line picking",
     "summary": "This module adds field picking to stock move line form view",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL, Eric Antones",
     "license": "AGPL-3",
     "category": "Custom",
     "website": "https://github.com/nuobit/odoo-addons",
@@ -15,5 +16,4 @@
     "data": [
         "views/stock_move_line_views.xml",
     ],
-    "installable": True,
 }
---
+++ stock_move_line_picking/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module adds field picking to stock move line form view
