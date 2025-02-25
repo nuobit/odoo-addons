285d00a7168178ddb0aeef1a15f36647f1daf883
bijaya <kumalbijay05@gmail.com>
2025-02-25 12:51:45 +0100

    [MIG] stock_move_line_picking: Migration to 16.0

--- stock_picking_date_filter/__manifest__.py
+++ stock_picking_date_filter/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock Picking Date Filter",
-    "version": "14.0.1.0.1",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "16.0.1.0.1",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Stock",
     "website": "https://github.com/nuobit/odoo-addons",
