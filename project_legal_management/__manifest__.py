ff12db2618f8a3ba82b24abd0de57210d7b6f175
??? <dgallo@nuobit.com>
2025-02-26 11:38:43 +0100

    [MIG] project_legal_management: Migration to 16.0

--- project_legal_management/__manifest__.py
+++ project_legal_management/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2021 NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Project legal management",
     "summary": "This module adds the necessary fields to work with "
     "the legal management in the project module",
-    "version": "14.0.1.0.1",
+    "version": "16.0.0.0.0",
     "category": "Project",
-    "author": "NuoBiT Solutions, S.L., Kilian Niubo",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
@@ -22,6 +23,4 @@
         "views/lm_resolution_views.xml",
         "views/project_tree.xml",
     ],
-    "installable": True,
-    "auto_install": False,
 }
