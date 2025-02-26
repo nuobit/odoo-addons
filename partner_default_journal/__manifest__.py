373da4a6c6d6a1f11daedef7c154a199ead6f665
??? <dgallo@nuobit.com>
2025-02-26 10:15:34 +0100

    [IMP] partner_default_journal: pre-commit auto fixes

--- partner_default_journal/__manifest__.py
+++ partner_default_journal/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Accounting",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": ["sale_purchase"],
     "data": [
         "views/res_partner_views.xml",
--- partner_default_journal/models/sale.py
+++ partner_default_journal/models/sale.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _prepare_invoice(self):
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals = super()._prepare_invoice()
         if self.partner_id.sale_journal_id:
             invoice_vals["journal_id"] = self.partner_id.sale_journal_id.id
         return invoice_vals
