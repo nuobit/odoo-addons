3136b3435554d60f48c62cc9906bb13cc92e18d7
bijaya <kumalbijay05@gmail.com>
2025-03-03 10:20:28 +0100

    [MIG] product_supplier_ref: Migration to 16.0

--- product_supplier_ref/__manifest__.py
+++ product_supplier_ref/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Product Supplier Ref",
     "summary": "Use the product name if the supplier product name is "
     "empty instead of the product default_code.",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase",
-    "author": "NuoBiT Solutions,S.L.",
+    "author": "NuoBiT Solutions SL, Eric Antones",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
---
+++ product_supplier_ref/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+* Use the product name if the supplier product name is empty instead
+  of the product default_code
