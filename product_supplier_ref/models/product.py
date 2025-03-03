3709c94927b2852e75a34145858bf7b9eb5ba2e1
bijaya <kumalbijay05@gmail.com>
2025-03-03 10:38:20 +0100

    [MIG] product_supplier_ref: Migration to 16.0

--- product_supplier_ref/models/product.py
+++ product_supplier_ref/models/product.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
@@ -20,3 +20,4 @@ class ProductProduct(models.Model):
                         partner_ref_l.insert(0, "[%s] " % product.code)
                     product.partner_ref = " ".join(partner_ref_l)
                     break
+        return
