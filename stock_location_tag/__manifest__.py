b5f5396cccfced4ed3ab7b700c5352d65db69d6e
bijaya <kumalbijay05@gmail.com>
2025-03-03 13:05:17 +0100

    [MIG] stock_location_tag: Migration to 16.0

--- stock_location_tag/__manifest__.py
+++ stock_location_tag/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock location tag",
     "summary": "This module adds tags on location",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL, Eric Antones",
     "category": "Warehouse",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
