7991f45cc77001aec4ebc13e28c1b66bce9542a4
bijaya <kumalbijay05@gmail.com>
2025-03-04 11:03:25 +0100

    [MIG] stock_location_code: Migration to 16.0

--- stock_location_code/__manifest__.py
+++ stock_location_code/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock location code",
     "summary": "This module adds unique code per company on location",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "category": "Warehouse",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
@@ -16,5 +16,4 @@
     "data": [
         "views/stock_location_views.xml",
     ],
-    "installable": True,
 }
