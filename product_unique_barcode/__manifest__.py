8da25f8ab7c9381416dbf1a868b2e9626139f512
bijaya <kumalbijay05@gmail.com>
2025-03-04 12:26:45 +0100

    [MIG] product_unique_barcode: Migration to 16.0

--- product_unique_barcode/__manifest__.py
+++ product_unique_barcode/__manifest__.py
@@ -1,19 +1,16 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 {
     "name": "Product unique barcode",
     "summary": "This module ensures that you enter a Unique Barcode for your Products",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "product",
     ],
     "pre_init_hook": "pre_init_hook_barcode_check",
-    "installable": True,
-    "auto_install": False,
 }
