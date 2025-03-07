4b1ece696f3c64df7df3b57343e713cc3ecc09ee
bijaya <kumalbijay05@gmail.com>
2025-03-07 13:11:04 +0100

    [MIG] account_invoice_line_page_size: Migration to 16.0

--- account_invoice_line_page_size/__manifest__.py
+++ account_invoice_line_page_size/__manifest__.py
@@ -1,10 +1,11 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Invoice line page size",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "version": "16.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Sale",
     "website": "https://github.com/nuobit/odoo-addons",
@@ -15,5 +16,4 @@
     "data": [
         "views/account_invoice_views.xml",
     ],
-    "installable": True,
 }
