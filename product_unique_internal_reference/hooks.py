a6e3f778b1f6a396bb4f024a40b45cc230eeedb0
bijaya <kumalbijay05@gmail.com>
2025-03-11 10:47:23 +0100

    [MIG] product_unique_internal_reference: Migration to 16.0

--- product_unique_internal_reference/hooks.py
+++ product_unique_internal_reference/hooks.py
@@ -1,6 +1,6 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
 
 from odoo import _
 from odoo.exceptions import ValidationError
