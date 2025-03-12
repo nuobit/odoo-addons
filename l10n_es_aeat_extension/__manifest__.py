a98a82f0c4262dd3eae5b739cb4541a454e574c9
??? <dgallo@nuobit.com>
2025-03-12 11:32:39 +0100

    [ADD] l10n_es_aeat_extension

---
+++ l10n_es_aeat_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Aeat extension",
+    "summary": "This module allows you to duplicate and delete a tax mapping",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "NuoBiT Solutions",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat",
+    ],
+}
