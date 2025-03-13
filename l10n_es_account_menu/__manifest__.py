048e4f284721cbdd66258aafba54b8406f971c97
bijaya <kumalbijay05@gmail.com>
2025-03-13 12:23:46 +0100

    [MIG] l10n_es_account_menu: Migration to 16.0

--- l10n_es_account_menu/__manifest__.py
+++ l10n_es_account_menu/__manifest__.py
@@ -1,17 +1,17 @@
-# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "L10n ES Account Menu",
     "summary": "This module extends account menu",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
-    "depends": ["account_menu", "l10n_es_extension"],
+    "depends": ["account", "l10n_es_extension"],
     "data": [
         "views/account_tax_views.xml",
     ],
-    "installable": True,
 }
