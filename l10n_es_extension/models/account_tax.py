e454e29e502c3fb2c8b7a244487dc8b3dbaabb13
bijaya <kumalbijay05@gmail.com>
2025-03-17 10:58:47 +0100

    [MIG] l10n_es_extension: Migration to 16.0

--- l10n_es_extension/models/account_tax.py
+++ l10n_es_extension/models/account_tax.py
@@ -1,5 +1,6 @@
 # Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
