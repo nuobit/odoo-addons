65d951253949e6e4dfda07009c0dd0b0d7e169b8
bijaya <kumalbijay05@gmail.com>
2025-03-17 10:58:47 +0100

    [MIG] l10n_es_extension: Migration to 16.0

--- l10n_es_extension/__manifest__.py
+++ l10n_es_extension/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "L10n ES Extension",
     "summary": "This module extends l10n_es",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
@@ -15,5 +16,4 @@
     "data": [
         "data/account_capital_asset_map_tax_data.xml",
     ],
-    "installable": True,
 }
