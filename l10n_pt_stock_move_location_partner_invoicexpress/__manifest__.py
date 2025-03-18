e3812e5dbbbba4c752510d6bc9b036d21ddb6272
??? <dgallo@nuobit.com>
2025-03-18 13:26:20 +0100

    [ADD] l10n_pt_stock_move_location_partner_invoicexpress

---
+++ l10n_pt_stock_move_location_partner_invoicexpress/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Move Location InvoiceXpress",
+    "summary": "This module assigns the InvoiceXpress "
+    "document type from Portugal to the picking.",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_pt_stock_invoicexpress",
+        "stock_move_location_partner",
+    ],
+}
