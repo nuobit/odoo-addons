0e5795940fdceed6795d509181c004b45ac9d42b
??? <dgallo@nuobit.com>
2025-03-19 12:09:10 +0100

    [MIG] repair_order_default_location: Migration to 16.0

--- repair_order_default_location/models/repair_line.py
+++ repair_order_default_location/models/repair_line.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
@@ -10,6 +10,7 @@ class RepairLine(models.Model):
 
     @api.onchange("type")
     def onchange_operation_type(self):
-        super().onchange_operation_type()
+        res = super().onchange_operation_type()
         if self.type == "add":
             self.location_id = self.repair_id.location_id
+        return res
