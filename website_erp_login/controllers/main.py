45cdc2ab46f44ffdf100be9c7f6f45b7844e9a32
bijaya <kumalbijay05@gmail.com>
2025-03-19 12:50:06 +0100

    [MIG] website_erp_login: Migration to 16.0

--- website_erp_login/controllers/main.py
+++ website_erp_login/controllers/main.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
@@ -12,4 +12,4 @@ from odoo.addons.website.controllers.main import Website
 class WebsiteERPLogin(Website):
     @http.route("/", type="http", auth="none")
     def index(self, s_action=None, db=None, **kw):
-        return http.local_redirect("/web", query=request.params, keep_hash=True)
+        return request.redirect_query("/web", query=request.params)
