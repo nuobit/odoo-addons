07e079d8b16bde0afd9dbf23faa3639ad319f4ae
??? <dgallo@nuobit.com>
2025-03-19 12:09:10 +0100

    [MIG] repair_order_default_location: Migration to 16.0

--- repair_order_default_location/__manifest__.py
+++ repair_order_default_location/__manifest__.py
@@ -1,12 +1,11 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Repair Order default Location",
     "summary": "This module uses the header location as a default for the new components line",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "16.0.0.0.0",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "category": "Repair",
     "license": "AGPL-3",
