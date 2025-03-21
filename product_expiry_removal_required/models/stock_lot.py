d88b2371fc7f3185b2077359baefaf6377141997
bijaya <kumalbijay05@gmail.com>
2025-03-21 11:54:30 +0100

    [MIG] product_expiry_removal_required: Migration to 16.0

--- product_expiry_removal_required/models/stock_lot.py
+++ product_expiry_removal_required/models/stock_lot.py
@@ -1,11 +1,12 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
-class StockProductionLot(models.Model):
-    _inherit = "stock.production.lot"
+class StockLot(models.Model):
+    _inherit = "stock.lot"
 
     @api.constrains("removal_date", "use_expiration_date")
     def _check_required_removal_date(self):
