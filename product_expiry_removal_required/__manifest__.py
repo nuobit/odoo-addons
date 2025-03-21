5bae61e92da8f0a22ba76295c133d37dda63af12
bijaya <kumalbijay05@gmail.com>
2025-03-21 11:54:30 +0100

    [MIG] product_expiry_removal_required: Migration to 16.0

--- product_expiry_removal_required/__manifest__.py
+++ product_expiry_removal_required/__manifest__.py
@@ -1,20 +1,20 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Product Expiry Removal Required",
     "summary": "This module makes the lot removal date mandatory if the product has"
     " the expiry date enabled",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Inventory/Inventory",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "product_expiry",
     ],
     "data": [
-        "views/stock_production_lot_views.xml",
+        "views/stock_lot_views.xml",
     ],
 }
---
+++ product_expiry_removal_required/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+* This module makes the lot removal date mandatory if the product has
+  the expiry date enabled
