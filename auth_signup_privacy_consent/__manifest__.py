139479566dfa1a0bf83887fa4062da75e04b530a
FrankC013 <fcespedes@nuobit.com>
2025-03-27 13:06:21 +0100

    [ADD] auth_signup_privacy_consent

---
+++ auth_signup_privacy_consent/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Auth Signup Privacy Consent",
+    "summary": "This module provides shared logic to manage user privacy "
+    "consent during signup and login",
+    "version": "16.0.1.0.0",
+    "category": "Authentication",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "author": "NuoBiT Solutions SL",
+    "license": "AGPL-3",
+    "depends": ["privacy_consent", "auth_signup"],
+    "data": [],
+}
---
+++ auth_signup_privacy_consent/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module provides shared logic to manage user privacy consent during signup and login.
