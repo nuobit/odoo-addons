a0abcdf76f453b911b323c4ec876a2fc346d2801
FrankC013 <fcespedes@nuobit.com>
2025-03-31 11:52:15 +0200

    [ADD] phone_validation_force_format

---
+++ phone_validation_force_format/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Phone Numbers Validation Force Format",
+    "summary": "Automatically formats phone numbers on partners using "
+    "phone_validation, even on create and write operations.",
+    "version": "16.0.1.0.0",
+    "category": "Partner",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["phone_validation"],
+}
