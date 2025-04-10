87c828dabe002035d532ebeec5ed0497d6731a53
??? <dgallo@nuobit.com>
2025-04-10 11:06:12 +0200

    [MIG] account_financial_report_number: Migration to 16.0

--- account_financial_report_number/__manifest__.py
+++ account_financial_report_number/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Account Financial Reports Number",
     "summary": "This module adds a filter to Journal Ledger "
     "report to filter by journal entry number",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Reporting",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
---
+++ account_financial_report_number/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module adds a filter to Journal Ledger report to filter by journal entry number
