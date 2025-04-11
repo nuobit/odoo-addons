42b159c33a8aa8b9418497794fcbadab6c121b8f
??? <dgallo@nuobit.com>
2025-04-11 10:26:01 +0200

    [MIG] account_invoice_mass_email: Migration to 16.0

--- account_invoice_mass_email/__manifest__.py
+++ account_invoice_mass_email/__manifest__.py
@@ -1,16 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Mass Email Invoices",
     "summary": "Sending multiple invoices by email.",
     "category": "Accounting",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "depends": ["account"],
     "data": ["views/mass_email_view.xml"],
-    "installable": True,
 }
