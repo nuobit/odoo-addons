ca576c795222a9fbf2aca4563b201527b61533dc
??? <dgallo@nuobit.com>
2025-04-11 11:03:29 +0200

    [MIG] account_template_view: Migration to 16.0

--- account_template_view/__manifest__.py
+++ account_template_view/__manifest__.py
@@ -1,15 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Account Template View",
     "summary": "This module enables all fields on *Account Tax Templates* views",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
-    "depends": ["account_menu"],
+    "depends": ["account_usability"],
     "data": [
         "views/account_tax_template_views.xml",
     ],
