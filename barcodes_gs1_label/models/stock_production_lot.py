01c9effd19a2524f5f61ac075b6f7a1cacd5fc0f
??? <dgallo@nuobit.com>
2025-04-14 12:03:33 +0200

    [MIG] barcodes_gs1_label: Migration to 16.0

--- barcodes_gs1_label/models/stock_production_lot.py
+++ barcodes_gs1_label/models/stock_production_lot.py
@@ -1,12 +1,12 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
 
 
 class ProductionLot(models.Model):
-    _inherit = "stock.production.lot"
+    _inherit = "stock.lot"
 
     def print_gs1_barcode_wizard(self):
         action = self.env["ir.actions.act_window"]._for_xml_id(
