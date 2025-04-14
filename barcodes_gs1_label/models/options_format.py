25de8c94230770a35f3c3eb7fdc2049f6bc98e40
??? <dgallo@nuobit.com>
2025-04-14 12:03:33 +0200

    [MIG] barcodes_gs1_label: Migration to 16.0

--- barcodes_gs1_label/models/options_format.py
+++ barcodes_gs1_label/models/options_format.py
@@ -1,5 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import re
