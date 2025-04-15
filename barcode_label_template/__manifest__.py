b0f3c07d87c5e6d3941e073cd5c45b552e59ee5d
??? <dgallo@nuobit.com>
2025-04-15 13:32:45 +0200

    [MIG] barcode_label_template: Migration to 16.0

--- barcode_label_template/__manifest__.py
+++ barcode_label_template/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Barcode label template",
     "summary": "This module integrates barcodes directly into the custom label "
     "template for lots/serial numbers.",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "category": "Tools",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": [
         "report_qweb_pdf_watermark",
@@ -22,4 +23,9 @@
         "views/barcode_label_template_configuration_views.xml",
         "views/stock_production_lot_views.xml",
     ],
+    "assets": {
+        "web.report_assets_pdf": [
+            "barcode_label_template/static/src/scss/styles.scss",
+        ],
+    },
 }
