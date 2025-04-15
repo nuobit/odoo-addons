dea6571bedf128fb2ed78ba53a6b670984777fe5
??? <dgallo@nuobit.com>
2025-04-15 13:32:45 +0200

    [MIG] barcode_label_template: Migration to 16.0

--- barcode_label_template/models/stock_lot.py
+++ barcode_label_template/models/stock_lot.py
@@ -1,12 +1,13 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
 from odoo import models
 
 
-class ProductionLot(models.Model):
-    _inherit = "stock.production.lot"
+class StockLot(models.Model):
+    _inherit = "stock.lot"
 
     def print_barcode_label_template_wizard(self):
         return self.env["ir.actions.act_window"]._for_xml_id(
