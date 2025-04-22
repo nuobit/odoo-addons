a9400734b1f5d2f24f36a48ff242c7c28683b4ac
??? <dgallo@nuobit.com>
2025-04-22 13:01:28 +0200

    [MIG] barcode_gs1_label: Migration to 17.0

--- barcodes_gs1_label/models/__init__.py
+++ barcodes_gs1_label/models/__init__.py
@@ -1,6 +1,6 @@
 from . import product_product
 from . import stock_picking
-from . import stock_production_lot
+from . import stock_lot
 from . import stock_quant
 from . import options_config
 from . import options_format
