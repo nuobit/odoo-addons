64607ecff85d5e5d016fa980a1919e4dd85e1b11
??? <dgallo@nuobit.com>
2025-04-25 11:59:59 +0200

    [MIG] barcodes_gs1_label_expiry: Migration to 16.0

--- barcodes_gs1_label_expiry/__manifest__.py
+++ barcodes_gs1_label_expiry/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
-# Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Barcodes label GS1 Expiry",
     "summary": "Generate barcode labels enabling barcode printing with expiry dates"
     "on products, lot/serial and picking",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "category": "Tools",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/nuobit/odoo-addons",
     "depends": ["barcodes_gs1_label", "product_expiry"],
     "installable": True,
