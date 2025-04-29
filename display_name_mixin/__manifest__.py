ade5da58a44eeca10c95f1e8fa1b5dcc2e496e61
??? <dgallo@nuobit.com>
2025-04-29 14:04:22 +0200

    [MIG] display_name_mixin: Migration to 17.0

--- display_name_mixin/__manifest__.py
+++ display_name_mixin/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
