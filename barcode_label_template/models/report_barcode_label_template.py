0ca8cce7873dcab93da8834bd78f829c68965d5d
??? <dgallo@nuobit.com>
2025-05-12 13:58:24 +0200

    [IMP] barcode_label_template: add format `ean13` in the barcode

--- barcode_label_template/models/report_barcode_label_template.py
+++ barcode_label_template/models/report_barcode_label_template.py
@@ -2,9 +2,9 @@
 # Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-
 from odoo import _, models
 from odoo.exceptions import ValidationError
+from odoo.tools import check_barcode_encoding
 from odoo.tools.safe_eval import safe_eval
 
 
@@ -13,18 +13,15 @@ class ReportBarcodeLabelTemplate(models.AbstractModel):
     _description = "Barcode Label Template Report"
     _inherit = "report.barcodes_gs1_label.report_gs1_barcode"
 
-    def _prepare_report_label_template_data(self, data, gs1_barcode, lot, temp_config):
-        barcodes_data = {}
-        if gs1_barcode:
-            barcodes_data = {
-                "product": lot.product_id,
-                "type": data["barcode_type"],
-                "values": gs1_barcode,
-                "string": self._get_gs1_barcode_string(gs1_barcode, "gs1-datamatrix"),
+    def _prepare_report_label_template_data(self, data, lot, temp_config):
+        data.update(
+            {
                 "position_x": temp_config.position_x,
                 "position_y": temp_config.position_y,
                 "width": temp_config.width,
             }
+        )
+
         fields_data = []
         for field in temp_config.configuration_field_ids:
             field_name = field.field_id.name
@@ -37,10 +34,10 @@ class ReportBarcodeLabelTemplate(models.AbstractModel):
             if field_name:
                 try:
                     expression = f"lot.{field_name}"
-                    field_value = safe_eval(expression, {"lot": lot})
+                    field_value = safe_eval(expression, globals_dict={"lot": lot})
                     if field_value and field.target_field:
                         expression = f"{expression}.{field.target_field}"
-                        field_value = safe_eval(expression, {"lot": lot})
+                        field_value = safe_eval(expression, globals_dict={"lot": lot})
                 except Exception as error:
                     raise ValidationError(
                         _(
@@ -68,7 +65,7 @@ class ReportBarcodeLabelTemplate(models.AbstractModel):
                 }
             )
         return {
-            "barcodes_data": barcodes_data,
+            "barcodes_data": data,
             "fields_data": fields_data,
         }
 
@@ -81,13 +78,38 @@ class ReportBarcodeLabelTemplate(models.AbstractModel):
             .browse(temp_config_id)
             .exists()
         )
-        datas = []
+        lots = []
         for active_id in data["active_ids"]:
             lot = self.env["stock.lot"].browse(active_id).exists()
-            gs1_barcode = self._prepare_gs1_values(lot.product_id, lot)
-            datas.append(
-                self._prepare_report_label_template_data(
-                    data, gs1_barcode, lot, temp_config
+            if data["barcode_type"] == "gs1-datamatrix":
+                data["barcode_values"] = self._prepare_gs1_values(lot.product_id, lot)
+                data["barcode_string"] = self._get_gs1_barcode_string(
+                    data["barcode_values"], data["barcode_type"]
                 )
+            elif data["barcode_type"] == "EAN13":
+                product_barcode = lot.product_id.barcode or None
+                if product_barcode and not check_barcode_encoding(
+                    product_barcode, data["barcode_type"]
+                ):
+                    raise ValidationError(
+                        _(
+                            "Barcode {barcode} of product {product_name} "
+                            "not valid for type {barcode_type}".format(
+                                barcode=product_barcode,
+                                product_name=lot.product_id.name,
+                                barcode_type=data["barcode_type"],
+                            )
+                        )
+                    )
+                data["barcode_values"] = data["barcode_string"] = product_barcode
+            else:
+                raise ValidationError(
+                    _(
+                        "Barcode type [%s] not supported for this report"
+                        % data["barcode_type"]
+                    )
+                )
+            lots.append(
+                self._prepare_report_label_template_data(data, lot, temp_config)
             )
-        return {"datas": datas}
+        return {"lots": lots}
--- barcode_label_template/wizard/barcode_label_template_wizard.py
+++ barcode_label_template/wizard/barcode_label_template_wizard.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
@@ -22,6 +23,7 @@ class BarcodeLabelTemplateWizard(models.TransientModel):
     def BARCODE_TYPE_SELECTION(self):
         return [
             ("gs1-datamatrix", "GS1-Datamatrix"),
+            ("EAN13", "EAN13"),
         ]
 
     barcode_type = fields.Selection(
