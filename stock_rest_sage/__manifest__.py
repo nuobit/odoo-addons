85f021639d392bb3f576e13f45890404e062e171
bijaya <kumalbijay05@gmail.com>
2025-05-26 12:17:14 +0200

    [ADD] stock_rest_sage

---
+++ stock_rest_sage/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Rest Sage",
+    "author": "NuoBiT Solutions, S.L.",
+    "category": "Stock",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": [
+        "stock_rest",
+        "connector_sage",
+        "stock_picking_employee",
+    ],
+    "auto_install": True,
+}
