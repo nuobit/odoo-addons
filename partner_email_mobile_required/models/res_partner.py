b0417b0790cabf83c2fdecc2dd31227cf74dab21
bijaya <kumalbijay05@gmail.com>
2025-06-10 11:17:17 +0200

    [IMP] partner_email_mobile_required: Add new security group 'enforce mobile or email'

--- partner_email_mobile_required/models/res_partner.py
+++ partner_email_mobile_required/models/res_partner.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, api, models
@@ -9,17 +10,21 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def _check_email_mobile(self):
-        for rec in self:
-            if not rec.email and not rec.mobile:
-                raise ValidationError(_("You must set an email or a mobile phone."))
+        user = self.env.user
+        if user.has_group("partner_email_mobile_required.group_enforce_mobile_email"):
+            for rec in self:
+                if not rec.email and not rec.mobile:
+                    raise ValidationError(_("You must set an email or a mobile phone."))
 
     def write(self, vals):
         res = super().write(vals)
-        self._check_email_mobile()
+        if not self._context.get("copy"):
+            self._check_email_mobile()
         return res
 
     @api.model_create_multi
     def create(self, vals_list):
         partner_ids = super().create(vals_list)
-        partner_ids._check_email_mobile()
+        if not self._context.get("copy"):
+            partner_ids._check_email_mobile()
         return partner_ids
