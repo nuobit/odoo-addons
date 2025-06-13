8102fe042e69c8608a75ec6e85df6e7a7e966f77
bijaya <kumalbijay05@gmail.com>
2025-06-13 10:07:07 +0200

    [ADD] web_report_styles

---
+++ web_report_styles/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 NuoBiT - Bijaya Kumal <bkumal@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Web Report Styling",
+    "summary": "Adds custom styling for reports in the web module.",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/report_web.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
