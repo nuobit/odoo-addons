c6fcfd23012b234ff47d84625f195fbb22148a0b
??? <dgallo@nuobit.com>
2025-06-25 13:14:06 +0200

    [MIG] l10n_es_facturae_auto_date: Migration to 17.0

--- l10n_es_facturae_auto_date/__manifest__.py
+++ l10n_es_facturae_auto_date/__manifest__.py
@@ -1,15 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "FacturaE auto dates",
     "summary": "This module computes automatically the FacturaE start date "
-    "and end date from date invoice",
-    "version": "14.0.1.1.0",
+    "and end date from posting date",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "l10n_es_facturae",
@@ -18,7 +18,6 @@
         "views/res_partner_views.xml",
         "views/account_move_view.xml",
     ],
-    "installable": True,
     "development_status": "Beta",
     "maintainers": ["eantones"],
 }
