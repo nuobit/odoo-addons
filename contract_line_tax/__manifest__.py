289e900dba13278352c341f1b8f6d5b93bc8ea73
bijaya <kumalbijay05@gmail.com>
2025-06-25 11:14:02 +0200

    [MIG] contract_line_tax: Migration to 17.0

--- contract_line_tax/__manifest__.py
+++ contract_line_tax/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Contract line tax",
     "summary": "This module adds taxes to lines and propagates it to invoice",
-    "version": "14.0.1.0.1",
+    "version": "17.0.1.0.1",
     "category": "Contract Management",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
@@ -18,5 +18,4 @@
         "views/contract_view.xml",
     ],
     "installable": True,
-    "auto_install": False,
 }
