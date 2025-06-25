c7a7b4343df9f6abbc66113498fd9328bbc312f3
bijaya <kumalbijay05@gmail.com>
2025-06-25 11:14:02 +0200

    [MIG] contract_line_tax: Migration to 17.0

--- contract_line_tax/models/contract_line.py
+++ contract_line_tax/models/contract_line.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Bijaya Kumal <bkumal@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import fields, models
@@ -15,19 +15,22 @@ class ContractLine(models.Model):
         check_company=True,
     )
 
-    def _prepare_invoice_line(self, move_form):
+    def _prepare_invoice_line(self):
         self.ensure_one()
-        res = super()._prepare_invoice_line(move_form)
+        res = super()._prepare_invoice_line()
+
         taxes = self.tax_ids or self.product_id.taxes_id
         if taxes:
             fiscal_position = self.contract_id.fiscal_position_id or self.env[
                 "account.fiscal.position"
-            ].get_fiscal_position(self.partner_id.id)
+            ]._get_fiscal_position(self.contract_id.partner_id)
             if fiscal_position:
-                taxes = fiscal_position.map_tax(taxes, partner=self.partner_id)
+                taxes = fiscal_position.map_tax(
+                    taxes, partner=self.contract_id.partner_id
+                )
         res.update(
             {
-                "tax_ids": taxes.ids and [(6, 0, taxes.ids)] or [],
+                "tax_ids": [(6, 0, taxes.ids)] if taxes else [],
             }
         )
         return res
