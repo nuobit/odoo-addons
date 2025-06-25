657720a761b94b36fe6dd4c2436559c59301e941
bijaya <kumalbijay05@gmail.com>
2025-06-25 12:19:33 +0200

    [IMP] sale_order_service: pre-commit auto fixes

--- sale_order_service/__manifest__.py
+++ sale_order_service/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.1",
     "category": "Sales",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": [
