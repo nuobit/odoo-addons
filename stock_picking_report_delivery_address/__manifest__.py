eb8a476db166754f249ee85675f2c85615163806
Eric Antones <eantones@nuobit.com>
2025-07-17 12:30:20 +0200

    [ADD] stock_picking_report_delivery_address

---
+++ stock_picking_report_delivery_address/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Picking report show delivery address in internal moves",
+    "summary": "Show the delivery address also in internal moves in the picking report",
+    "author": "NuoBiT Solutions SL",
+    "category": "Stock",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/NuoBiT/odoo-addons",
+    "depends": ["stock"],
+}
