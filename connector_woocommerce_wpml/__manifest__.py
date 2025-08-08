8282fa1ab7b79df72ec12f06ffbdf8bf49c00ba5
??? <dgallo@nuobit.com>
2025-08-08 11:56:50 +0200

    [REF] connector_woocommerce_wpml: remove lang_ids

--- connector_woocommerce_wpml/__manifest__.py
+++ connector_woocommerce_wpml/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Connector WooCommerce WMPL",
-    "version": "14.0.0.1.2",
+    "version": "14.0.0.1.3",
     "author": "NuoBiT Solutions, S.L.",
     "license": "AGPL-3",
     "category": "Connector",
