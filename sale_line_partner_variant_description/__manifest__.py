155dd81e5ece008bc526fb089f1f7d614d9efd0b
Eric Antones <eantones@nuobit.com>
2025-08-21 14:49:44 +0200

    [ADD] sale_line_partner_variant_description

---
+++ sale_line_partner_variant_description/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale line product variant description by partner",
+    "summary": "This is a glue module to add a variant description",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_line_variant_description",
+        "sale_line_partner_description",
+    ],
+    "auto_install": True,
+}
