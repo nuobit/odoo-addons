64ebd172db50e5f43048ab937089d9f43e8dd72d
Eric Antones <eantones@nuobit.com>
2025-08-21 20:35:12 +0200

    [ADD] product_default_code_mandatory

---
+++ product_default_code_mandatory/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 NuoBiT Solutions S.L. - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Product Internal Reference Mandatory",
+    "summary": "Makes the product internal reference mandatory.",
+    "version": "14.0.1.0.0",
+    "category": "Product Management",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["product_template_variant_definition"],
+    "data": [
+        "views/product_template_views.xml",
+        "views/product_product_views.xml",
+    ],
+}
