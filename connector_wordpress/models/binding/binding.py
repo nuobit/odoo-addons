d380c7f31ea5b115442055b270af647ccce78914
Eric Antones <eantones@nuobit.com>
2025-08-25 12:48:06 +0200

    [ADD] connector_wordpress

---
+++ connector_wordpress/models/binding/binding.py
@@ -0,0 +1,27 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class WordPressBinding(models.AbstractModel):
+    _name = "wordpress.binding"
+    _inherit = "connector.extension.external.binding"
+    _description = "WordPress Binding"
+
+    # binding fields
+    backend_id = fields.Many2one(
+        comodel_name="wordpress.backend",
+        string="WordPress Backend",
+        required=True,
+        ondelete="restrict",
+    )
+
+    _sql_constraints = [
+        (
+            "internal_uniq",
+            "unique(backend_id, odoo_id)",
+            "A binding already exists with the same External (odoo_id) ID.",
+        ),
+    ]
