557f7b43028107347d44a27b1ad8ac250571acca
Eric Antones <eantones@nuobit.com>
2025-08-29 10:38:35 +0200

    [REF] connector_woocommerce: move product attachment auxiliar fields from website_sale_extra_fields

--- connector_woocommerce/models/__init__.py
+++ connector_woocommerce/models/__init__.py
@@ -2,6 +2,7 @@ from . import backend
 from . import binding
 from . import common
 from . import ir_attachment
+from . import product_attachment
 from . import product_public_category
 from . import product_attribute
 from . import product_attribute_value
