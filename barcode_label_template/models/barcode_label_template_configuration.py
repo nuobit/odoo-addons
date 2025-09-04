d88556de9fe7ff98152ea420464659871d01842c
??? <dgallo@nuobit.com>
2025-09-04 17:28:18 +0200

    [MIG] barcode_label_template: Migration to 18.0

--- barcode_label_template/models/barcode_label_template_configuration.py
+++ barcode_label_template/models/barcode_label_template_configuration.py
@@ -1,4 +1,4 @@
-# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions SL - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
--- barcode_label_template/models/barcode_label_template_configuration_field.py
+++ barcode_label_template/models/barcode_label_template_configuration_field.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
-# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
