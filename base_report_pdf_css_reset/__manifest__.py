22e457e4c6e3be5b80128ec411cd9cb894c048d4
??? <dgallo@nuobit.com>
2025-09-04 14:50:34 +0200

    [MIG] base_report_pdf_css_reset: Migration to 18.0

--- base_report_pdf_css_reset/__manifest__.py
+++ base_report_pdf_css_reset/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Report PDF CSS reset",
     "summary": "This module enables you with the capability to reset the default "
     "paddings on the pdf minimal_layout template",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Reporting",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
@@ -15,5 +15,4 @@
     "installable": True,
     "development_status": "Beta",
     "maintainers": ["eantones"],
-    "auto_install": False,
 }
--- base_report_pdf_css_reset/models/ir_actions_report.py
+++ base_report_pdf_css_reset/models/ir_actions_report.py
@@ -1,4 +1,4 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import lxml.html
