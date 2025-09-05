b3f7d4fa7fb2a377b70ab470102ec52a13685594
??? <dgallo@nuobit.com>
2025-09-05 13:10:52 +0200

    [MIG] barcodes_gs1_label_expiry: Migration to 18.0

--- barcodes_gs1_label_expiry/__manifest__.py
+++ barcodes_gs1_label_expiry/__manifest__.py
@@ -1,19 +1,18 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
-# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Barcodes label GS1 Expiry",
     "summary": "Generate barcode labels enabling barcode printing with expiry dates"
     "on products, lot/serial and picking",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Tools",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": ["barcodes_gs1_label", "product_expiry"],
-    "installable": True,
     "development_status": "Beta",
     "maintainers": ["eantones", "fcespedes"],
 }
--- barcodes_gs1_label_expiry/report/report_barcode_gs1.py
+++ barcodes_gs1_label_expiry/report/report_barcode_gs1.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Frank Cespedes <fcespedes@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
