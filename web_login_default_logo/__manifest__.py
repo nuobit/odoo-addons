1a8f3bd02623183cf9f34c0b6c89a148d5f595c6
??? <dgallo@nuobit.com>
2025-09-08 09:58:18 +0200

    [MIG] web_login_default_logo: Migration to 18.0

--- web_login_default_logo/__manifest__.py
+++ web_login_default_logo/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Web login default logo",
     "summary": "This module forces default Odoo logo at login screen.",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Web",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
--- web_login_default_logo/controllers/main.py
+++ web_login_default_logo/controllers/main.py
@@ -1,6 +1,6 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 
