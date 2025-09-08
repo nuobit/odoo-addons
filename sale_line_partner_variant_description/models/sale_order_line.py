b8523624f44c440b9eca8545ccc2cf38197b9185
Eric Antones <eantones@nuobit.com>
2025-09-08 14:29:34 +0200

    [FIX] sale_line_partner_variant_description: customer descriptions not shown on certain contexts

--- sale_line_partner_variant_description/models/sale_order_line.py
+++ sale_line_partner_variant_description/models/sale_order_line.py
@@ -8,18 +8,12 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _prepare_description_values(self, product):
-        values = super()._prepare_description_values(product)
-        if product.variant_description_sale:
-            values.pop("description_sale", None)
-            values["variant_description_sale"] = product.variant_description_sale
-        return values
-
     @api.onchange("product_id")
     def product_id_change(self):
-        res = super(
-            SaleOrderLine, self.with_context(no_set_description=True)
-        ).product_id_change()
+        res = super().product_id_change()
         if self.product_id:
-            self._set_description()
+            buyer_product_name = self.get_buyer_product_name()
+            if buyer_product_name:
+                if buyer_product_name != self.name:
+                    self.name = buyer_product_name
         return res
