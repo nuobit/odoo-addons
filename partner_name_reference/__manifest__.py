6110610d000e4adfb1bf3209a74c39bf9876b65f
??? <dgallo@nuobit.com>
2025-09-08 15:41:45 +0200

    [MIG] partner_name_reference: Migration to 18.0

--- partner_name_reference/__manifest__.py
+++ partner_name_reference/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Partner name with reference",
     "summary": "This module concatenates reference"
     " number to partner name on display_name",
-    "version": "17.0.0.0.0",
+    "version": "18.0.1.0.0",
     "category": "Web",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
--- partner_name_reference/models/res_partner.py
+++ partner_name_reference/models/res_partner.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
