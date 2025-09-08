e6fa19a1b10898faa55ca2b087f1dda9e6af6bd4
??? <dgallo@nuobit.com>
2025-09-08 11:03:47 +0200

    [MIG] account_invoice_report_lot: Migration to 18.0

--- account_invoice_report_lot/__manifest__.py
+++ account_invoice_report_lot/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Invoice report lot",
-    "version": "17.0.0.0.0",
+    "version": "18.0.1.0.0",
     "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Invoicing Management",
