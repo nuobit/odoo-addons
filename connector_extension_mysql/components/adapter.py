7cf996f9c80b085345581d1b7ce0f170a3a5a6f9
??? <dgallo@nuobit.com>
2025-09-09 17:52:04 +0200

    [MIG] connector_extension_mysql: Migration to 18.0

--- connector_extension_mysql/components/adapter.py
+++ connector_extension_mysql/components/adapter.py
@@ -1,6 +1,6 @@
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 
