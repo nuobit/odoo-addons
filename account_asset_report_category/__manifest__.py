e0f062655ee4f30ad21d415cad59c1b71879fbf2
??? <dgallo@nuobit.com>
2025-09-10 10:37:48 +0200

    [MIG] account_asset_report_category: Migration to 18.0

--- account_asset_report_category/__manifest__.py
+++ account_asset_report_category/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Asset Financial Report Category",
     "summary": "This module adds the column 'Category' in the Asset Financial Report",
-    "version": "16.0.0.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
