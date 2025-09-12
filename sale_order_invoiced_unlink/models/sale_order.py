4b36adbf4d5de316ee8fb46e79ab5c0d3caeb006
??? <dgallo@nuobit.com>
2025-09-12 11:09:53 +0200

    [MIG] sale_order_invoiced_unlink: Migration to 18.0

--- sale_order_invoiced_unlink/models/sale_order.py
+++ sale_order_invoiced_unlink/models/sale_order.py
@@ -1,4 +1,4 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, models
