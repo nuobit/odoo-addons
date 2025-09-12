f68b5381f207f696fa9368bd8f65de218e650f71
??? <dgallo@nuobit.com>
2025-09-12 12:05:16 +0200

    [IMP] account_asset_invoice_line_link: pre-commit auto fixes

--- account_asset_invoice_line_link/__manifest__.py
+++ account_asset_invoice_line_link/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "16.0.1.0.0",
     "category": "Accounting",
     "author": "NuoBiT Solutions SL",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "account_asset_management",
