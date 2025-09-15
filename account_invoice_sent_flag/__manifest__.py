e45bfbb07feee74cc1198aff339a6835cdd558cb
??? <dgallo@nuobit.com>
2025-09-15 15:11:59 +0200

    [MIG] account_invoice_sent_flag: Migration to 18.0

--- account_invoice_sent_flag/__manifest__.py
+++ account_invoice_sent_flag/__manifest__.py
@@ -1,12 +1,13 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Account invoice sent flag",
-    "summary": 'This module shows the "is_move_sent" flag on invoices and adds a filter',
+    "summary": 'This module shows the "is_move_sent" '
+    "flag on invoices and adds a filter",
     "category": "Accounting",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "author": "NuoBiT Solutions SL",
