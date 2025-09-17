36053b815138e2dc15ed87aedabcee1f3a3cbca0
??? <dgallo@nuobit.com>
2025-09-17 17:33:24 +0200

    [IMP] account_invoice_hide_payment_button: pre-commit auto fixes

--- account_invoice_hide_payment_button/__manifest__.py
+++ account_invoice_hide_payment_button/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.0",
     "category": "Accounting",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": ["views/account_invoice_view.xml"],
