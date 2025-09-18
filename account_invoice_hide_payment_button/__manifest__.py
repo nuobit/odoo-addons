aa37e350ac341e5fbfb7db96fa8cd0a6014c3b1d
??? <dgallo@nuobit.com>
2025-09-18 09:43:05 +0200

    [MIG] account_invoice_hide_payment_button: Migration to 18.0

--- account_invoice_hide_payment_button/__manifest__.py
+++ account_invoice_hide_payment_button/__manifest__.py
@@ -1,17 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Invoice hide payment button",
     "summary": "This module hides payment button on invoices",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": ["views/account_invoice_view.xml"],
-    "installable": True,
-    "auto_install": False,
 }
