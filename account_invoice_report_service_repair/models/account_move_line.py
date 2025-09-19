1d0ed3f06fc1727edab809c1c13d974e962ad925
Eric Antones <eantones@nuobit.com>
2025-09-19 16:20:19 +0200

    [ADD] account_invoice_report_service_repair: enhance invoice report with repair order details

---
+++ account_invoice_report_service_repair/models/account_move_line.py
@@ -0,0 +1,15 @@
+# Copyright 2025 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+
+from odoo import models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def _get_orders(self):
+        # we use a list to allow different types of orders, sale, repair, etc
+        return super()._get_orders() + [
+            x for x in self.repair_fee_ids.repair_id | self.repair_line_ids.repair_id
+        ]
---
+++ account_invoice_report_service_repair/models/repair_order.py
@@ -0,0 +1,30 @@
+# Copyright 2025 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class Repair(models.Model):
+    _inherit = "repair.order"
+
+    date_order = fields.Datetime(related="move_id.date", readonly=True)
+
+    client_order_ref = fields.Char(compute="_compute_client_order_ref")
+
+    def _compute_client_order_ref(self):
+        for order in self:
+            order.client_order_ref = False
+
+    service_number = fields.Integer(compute="_compute_service_number")
+
+    def _compute_service_number(self):
+        for order in self:
+            order.service_number = 0
+
+    date_order_tz = fields.Date(
+        string="Order Date TZ", readonly=True, compute="_compute_date_order_tz"
+    )
+
+    def _compute_date_order_tz(self):
+        for rec in self:
+            rec.date_order_tz = fields.Date.context_today(rec, rec.date_order)
