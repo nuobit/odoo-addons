914f99888ec95a6bece57b5e079f8efe8188cedb
Eric Antones <eantones@nuobit.com>
2025-09-19 16:20:19 +0200

    [ADD] account_invoice_report_service_repair: enhance invoice report with repair order details

---
+++ account_invoice_report_service_repair/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Service invoice report repair",
+    "summary": "This module displays the related repair order on the "
+    "invoice delivery report, providing a clear link between "
+    "service repairs and their corresponding invoices.",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account_invoice_report_service", "repair"],
+    "auto_install": True,
+}
