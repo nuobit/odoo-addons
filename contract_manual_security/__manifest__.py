0956fc4549f5f8f29e0be90f2162abd8764e720c
??? <dgallo@nuobit.com>
2025-09-29 16:37:33 +0200

    [MIG] contract_manual_security: Migration to 18.0

--- contract_manual_security/__manifest__.py
+++ contract_manual_security/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Contract Manual Security",
     "summary": "This module conditionally displays the 'Create invoices' "
     "button within contracts based on a permission",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Contract Management",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
