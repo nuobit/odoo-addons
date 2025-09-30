b3f7fe0e817d83a22676b829148c57b7a37d1ae9
??? <dgallo@nuobit.com>
2025-09-30 10:37:25 +0200

    [MIG] maintenance_request_date_editable: Migration to 18.0

--- maintenance_request_date_editable/__manifest__.py
+++ maintenance_request_date_editable/__manifest__.py
@@ -1,17 +1,17 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions SL - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Maintenance Request Date Editable",
     "summary": "This module allows to edit the date of a maintenance request"
     " if the company parameter is set.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Manufacturing/Maintenance",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
-    "depends": ["base_maintenance_config", "maintenance"],
+    "depends": ["maintenance"],
     "data": [
         "views/res_config_settings.xml",
         "views/maintenance_views.xml",
