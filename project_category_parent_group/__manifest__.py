8b1579578a1a7ff0a1864594d8150a08ccc72787
??? <dgallo@nuobit.com>
2025-09-30 17:17:14 +0200

    [MIG] project_category_parent_group: Migration to 18.0

--- project_category_parent_group/__manifest__.py
+++ project_category_parent_group/__manifest__.py
@@ -1,15 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Project Category Parent Group",
     "summary": "This module allows to group projects by parent category",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
-    "depends": ["project_category"],
+    "depends": ["project_type"],
     "data": [
         "views/project_project_views.xml",
     ],
