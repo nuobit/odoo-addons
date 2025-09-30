df17f01ce784589f52e621919b4f4d1626d8e664
??? <dgallo@nuobit.com>
2025-09-30 14:37:32 +0200

    [IMP] product_multi_unique_barcode: pre-commit auto fixes

--- product_multi_unique_barcode/__manifest__.py
+++ product_multi_unique_barcode/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.0",
     "category": "Product Management",
     "author": "NuoBiT Solutions, S.L.",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["product_unique_barcode", "product_multi_barcode"],
     "auto_install": True,
