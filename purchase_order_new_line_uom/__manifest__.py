c209de245bff8bdfbb8180588d0efae55e818377
??? <dgallo@nuobit.com>
2025-10-01 15:31:30 +0200

    [MIG] purchase_order_new_line_uom: Migration to 18.0

--- purchase_order_new_line_uom/__manifest__.py
+++ purchase_order_new_line_uom/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Purchase order new line uom",
     "summary": "This module allows to edit the uom on new purchase "
     "order lines that don't have any invoice or stock move linked.",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "category": "Purchases",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": [
@@ -18,5 +18,4 @@
     "data": [
         "views/purchase_views.xml",
     ],
-    "installable": True,
 }
---
+++ purchase_order_new_line_uom/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module allows to edit the uom on new purchase order lines that don't have any invoice or stock move linked.
