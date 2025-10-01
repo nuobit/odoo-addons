66f48f176845a5eaf706cd621bc7fe10350f55b7
??? <dgallo@nuobit.com>
2025-10-01 16:38:37 +0200

    [MIG] purchase_stock_order_description: Migration to 18.0

--- purchase_stock_order_description/__manifest__.py
+++ purchase_stock_order_description/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Purchase Stock Order Description",
     "summary": "Propagate the purchase order description to the picking move lines",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchases",
-    "author": "NuoBiT Solutions,S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
