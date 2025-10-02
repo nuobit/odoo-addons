e70a53ced9e26091f4ff6bd9330d0736b7c6d2ec
??? <dgallo@nuobit.com>
2025-10-02 15:39:11 +0200

    [MIG] stock_inventory_additional_groups: Migration to 18.0

--- stock_inventory_additional_groups/__manifest__.py
+++ stock_inventory_additional_groups/__manifest__.py
@@ -1,18 +1,17 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock inventory additional groups",
     "summary": "This module adds adittional permission groups on inventory module",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "category": "Warehouse",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": [
         "stock",
     ],
-    "data": ["security/stock_security.xml", "views/stock_inventory_views.xml"],
-    "installable": True,
+    "data": ["security/stock_security.xml", "views/stock_quant_views.xml"],
 }
