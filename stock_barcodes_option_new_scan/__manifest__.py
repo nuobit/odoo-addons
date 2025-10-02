b8aa272ae0df1d6535166772b6f79ec5b8b5a8b0
??? <dgallo@nuobit.com>
2025-10-02 12:58:58 +0200

    [MIG] stock_barcodes_option_new_scan: Migration to 18.0

--- stock_barcodes_option_new_scan/__manifest__.py
+++ stock_barcodes_option_new_scan/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Frank Cespedes <fcespedes@nuobit.com>
+# Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2026 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock Barcodes Option New Scan",
     "summary": "Enable the new scan picking button in the barcode when the barcode "
     "option group of the new picking is informed",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "category": "Extra Tools",
