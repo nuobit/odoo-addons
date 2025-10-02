3912a5a63e31aaefe1d2277043e65822c44a454f
??? <dgallo@nuobit.com>
2025-10-02 17:59:07 +0200

    [MIG] stock_orderpoint_manual_procurement_zero: Migration to 18.0

--- stock_orderpoint_manual_procurement_zero/__manifest__.py
+++ stock_orderpoint_manual_procurement_zero/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock Orderpoint Manual Procurement Zero",
     "summary": "Extends the module 'stock_orderpoint_manual_procurement' allowing "
     "users to remove procure recommendations with 0 quantity.",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "category": "Warehouse Management",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": [
@@ -17,5 +17,4 @@
     "data": [
         "wizards/make_procurement_orderpoint_view.xml",
     ],
-    "installable": True,
 }
--- stock_orderpoint_manual_procurement_zero/wizards/make_procurement_orderpoint.py
+++ stock_orderpoint_manual_procurement_zero/wizards/make_procurement_orderpoint.py
@@ -1,11 +1,11 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
 
 
-class MakeProcurementOrderpoint(models.TransientModel):
+class MakeProcurementBuffer(models.TransientModel):
     _inherit = "make.procurement.orderpoint"
 
     @api.model
