be82a7e28d7083ad8c4d1444e419f823db3da3e1
??? <dgallo@nuobit.com>
2025-10-02 17:32:15 +0200

    [MIG] stock_move_no_merge: Migration to 18.0

--- stock_move_no_merge/__manifest__.py
+++ stock_move_no_merge/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock move no merge",
     "summary": "This module disables the stock moves merge",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Warehouse",
     "website": "https://github.com/NuoBiT/odoo-addons",
