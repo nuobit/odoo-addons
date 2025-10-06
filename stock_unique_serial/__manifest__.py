98e23160b310a354398b7824b5f28566c8273ad3
??? <dgallo@nuobit.com>
2025-10-06 15:34:20 +0200

    [IMP] stock_unique_serial: pre-commit auto fixes

--- stock_unique_serial/__manifest__.py
+++ stock_unique_serial/__manifest__.py
@@ -9,7 +9,7 @@
     "stock location with quantity",
     "version": "14.0.1.0.0",
     "author": "NuoBiT Solutions, S.L.",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "category": "Warehouse",
     "depends": ["stock"],
     "license": "AGPL-3",
--- stock_unique_serial/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-This module ensures that the serial number have only one stock location with quantity
