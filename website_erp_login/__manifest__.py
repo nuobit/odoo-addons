f07fd5914752a3375f1cf01de4ac23a74898a27d
??? <dgallo@nuobit.com>
2025-10-07 12:06:04 +0200

    [MIG] website_erp_login: Migration to 18.0

--- website_erp_login/__manifest__.py
+++ website_erp_login/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Website ERP login",
     "summary": "Put the default ERP login back disabling the new login page added "
     "by website module.",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Website",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
