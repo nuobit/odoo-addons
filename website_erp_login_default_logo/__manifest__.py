06dbd3b03109d262fa98634bcf85cf6f4f1ab1b1
??? <dgallo@nuobit.com>
2025-10-07 15:37:18 +0200

    [MIG] website_erp_login_default_logo: Migration to 18.0

--- website_erp_login_default_logo/__manifest__.py
+++ website_erp_login_default_logo/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eatones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Website ERP login default logo",
     "summary": "This module forces default Odoo logo at "
     "login screen on website decoupling module",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Website",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
