f0265aa579462f48a64aa059fa72741f9308acbe
Eric Antones <eantones@nuobit.com>
2025-10-30 00:04:59 +0100

    [FIX] sale_line_partner_description: wrong sale variant description, no reference

--- sale_line_partner_description/__manifest__.py
+++ sale_line_partner_description/__manifest__.py
@@ -12,7 +12,9 @@
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
-        "sale",
+        # "sale",
+        "product_sale_uniline_description",
+        "product_sale",
     ],
     "data": [
         "security/ir.model.access.csv",
