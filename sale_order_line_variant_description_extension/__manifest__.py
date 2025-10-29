3385c28578e6d6a99ceb6fcc11efe6a54d0efe5b
Eric Antones <eantones@nuobit.com>
2025-10-30 00:05:36 +0100

    [ADD] sale_order_line_variant_description_extension: wrong sale variant description, no reference

---
+++ sale_order_line_variant_description_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale order line variant description extension",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_line_variant_description",
+        "product_sale_uniline_description",
+    ],
+}
