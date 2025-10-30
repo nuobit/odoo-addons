83408a291ce96c0aca2a1636d18fee2a5ab310a7
Eric Antones <eantones@nuobit.com>
2025-10-30 20:34:07 +0100

    [IMP] sale_line_partner_description: better approach separating display_name from sale descriptions (description sale and variant_description_sale)

--- sale_line_partner_description/__manifest__.py
+++ sale_line_partner_description/__manifest__.py
@@ -12,8 +12,6 @@
     "website": "https://github.com/nuobit/odoo-addons",
     "license": "AGPL-3",
     "depends": [
-        # "sale",
-        "product_sale_uniline_description",
         "product_sale",
     ],
     "data": [
--- sale_line_partner_description/models/product_product.py
+++ sale_line_partner_description/models/product_product.py
@@ -1,6 +1,4 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
-# Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 import re
 
@@ -15,65 +13,48 @@ class ProductProduct(models.Model):
         comodel_name="product.buyerinfo", inverse_name="product_id", string="Customers"
     )
 
-    # def _name_get_buyers(self, pairs):
-    #     partner_id = self.env.context.get("sale_partner_id")
-    #     if partner_id:
-    #         buyers = self.env["product.buyerinfo"].search_by_partner(
-    #             partner_id,
-    #             [
-    #                 ("product_id", "in", self.ids),
-    #             ],
-    #         )
-    #         buyer_d = {b.product_id.id: b for b in buyers}
-    #         res = []
-    #         for product_id, name in pairs:
-    #             if product_id in buyer_d:
-    #                 buyer = buyer_d[product_id]
-    #                 if buyer.code:
-    #                     m = re.match(r"^\[[^]]+\] (.+)$", name)
-    #                     if m:
-    #                         name = f"[{buyer.code}] {m.group(1)}"
-    #                 if buyer.name:
-    #                     name = self._smart_update_name(name, buyer.name)
-    #                     # m = re.match(r"^(\[[^]]+\]) .+$", name)
-    #                     # if m:
-    #                     #     name = f"{m.group(1)} {buyer.name}"
-    #                     # else:
-    #                     #     name = buyer.name
-    #             res.append((product_id, name))
-    #     else:
-    #         res = pairs
-    #     return res
-    #
-    # # TODO: Move to a mixin or sale_order_line_description_base class
-    # def _context_sale_partner(self):
-    #     ctx = dict(partner_id=False, seller_id=False)
-    #     partner_id = self.env.context.get("partner_id")
-    #     if "sale_partner_id" in self.env.context:
-    #         sale_partner_id = self.env.context.get("sale_partner_id")
-    #         if not sale_partner_id:
-    #             raise ValidationError(
-    #                 _(
-    #                     "sale_partner_id cannot be False, it should be either set or absent"
-    #                 )
-    #             )
-    #         if partner_id:
-    #             raise ValidationError(
-    #                 _("Cannot have both partner_id and sale_partner_id in context")
-    #             )
-    #     else:
-    #         if partner_id:
-    #             ctx["sale_partner_id"] = partner_id
-    #     return ctx
+    def _get_buyer_data(self):
+        buyer_d = {}
+        partner = self.env.context.get("partner")
+        if partner:
+            buyers = self.env["product.buyerinfo"].search_by_partner(
+                partner.id,
+                [
+                    ("product_id", "in", self.ids),
+                ],
+            )
+            buyer_d = {b.product_id.id: b for b in buyers}
+        return buyer_d
 
-    # def name_get(self):
-    #     """Override name_get to include buyer code in the product name."""
-    #     if "default_description_sale" in self.env.context:
-    #         self = self.with_context(**self._context_sale_partner())
-    #     res = super(ProductProduct, self).name_get()
-    #     if "default_description_sale" in self.env.context:
-    #         res = self._name_get_buyers(res)
-    #     return res
+    def _name_get_buyers(self, pairs):
+        buyer_d = self._get_buyer_data()
+        if buyer_d:
+            res = []
+            for product_id, name in pairs:
+                if product_id in buyer_d:
+                    buyer = buyer_d[product_id]
+                    if buyer.code:
+                        m = re.match(r"^\[[^]]+\] (.+)$", name)
+                        if m:
+                            name = f"[{buyer.code}] {m.group(1)}"
+                    if buyer.name:
+                        m = re.match(r"^(\[[^]]+\]) .+$", name)
+                        if m:
+                            name = f"{m.group(1)} {buyer.name}"
+                        else:
+                            name = buyer.name
+                res.append((product_id, name))
+        else:
+            res = pairs
+        return res
+
+    def name_get(self):
+        """Override name_get to include buyer code in the product name."""
+        self = self._clear_context_suppliers()
+        res = super(ProductProduct, self).name_get()
+        if "default_description_sale" in self.env.context:
+            res = self._name_get_buyers(res)
+        return res
 
     def _update_name_buyer(self, name):
         partner_id = self.env.context.get("partner_id")
@@ -96,11 +77,6 @@ class ProductProduct(models.Model):
                         name = f"[{buyer.code}] {m.group(1)}"
                 if buyer.name:
                     name = self._smart_update_name(name, buyer.name)
-        return name
-
-    def get_product_multiline_description_sale(self):
-        name = super().get_product_multiline_description_sale()
-        name = self._update_name_buyer(name)
         return name
 
     @api.model
