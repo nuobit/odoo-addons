93413cff7a0cbed157d50b9c5853496735e1f8a3
Eric Antones <eantones@nuobit.com>
2025-10-30 20:33:55 +0100

    [IMP] product_sale: better approach separating display_name from sale descriptions (description sale and variant_description_sale)

--- product_sale/models/product_product.py
+++ product_sale/models/product_product.py
@@ -7,41 +7,27 @@ from odoo import models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    # def _context_sale_partner(self):
-    #     ctx = dict(partner_id=False, seller_id=False)
-    #     partner_id = self.env.context.get("partner_id")
-    #     if "sale_partner_id" in self.env.context:
-    #         sale_partner_id = self.env.context.get("sale_partner_id")
-    #         if not sale_partner_id:
-    #             raise ValidationError(
-    #                 _(
-    #                     "sale_partner_id cannot be False, it should be either set or absent"
-    #                 )
-    #             )
-    #         if partner_id:
-    #             raise ValidationError(
-    #                 _("Cannot have both partner_id and sale_partner_id in context")
-    #             )
-    #     else:
-    #         if partner_id:
-    #             ctx["sale_partner_id"] = partner_id
-    #     return ctx
-
     def _clear_context_suppliers(self):
         """
         Clear context keys that would make name_get use suppliers
         instead of sale order partner.
         :return: self with updated context
         """
-        # This 'partner' (not 'partner_id') is the sale order partner
-        # we cannot use this here because is product_id_change who sets
-        # the 'partner' in context and not always this method is executed
-        # triggered by that onchange.
-        # if self.env.context.get('partner'):
+        # We cannot use self.env.context.get('partner') here because is
+        # product_id_change who sets the 'partner' in context and not
+        # always this method is executed triggered by that onchange.
         if "default_description_sale" in self.env.context:
-            self = self.with_context(partner_id=False, seller_id=False)
+            ctx = dict(partner_id=False, seller_id=False)
+            partner = self.env.context.get("partner")
+            if not partner:
+                partner_id = self.env.context.get("partner_id")
+                if partner_id:
+                    partner = self.env["res.partner"].browse(partner_id)
+                    ctx["partner"] = partner
+            self = self.with_context(**ctx)
         return self
 
     def name_get(self):
         self = self._clear_context_suppliers()
-        return super(ProductProduct, self).name_get()
+        res = super(ProductProduct, self).name_get()
+        return res
