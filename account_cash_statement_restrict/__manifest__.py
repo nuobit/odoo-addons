e06b9494e4ee3d8e5c21b940a2eff4aa43597301
??? <dgallo@nuobit.com>
2025-11-06 16:42:39 +0100

    [MIG] account_cash_statement_restrict: Migration to 18.0

--- account_cash_statement_restrict/__manifest__.py
+++ account_cash_statement_restrict/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Cash statement restrict",
-    "version": "14.0.1.0.2",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "summary": "Restrict cash statement access to assigned users",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Custom",
     "website": "https://github.com/NuoBiT/odoo-addons",
@@ -15,9 +16,7 @@
         "security/ir_rule.xml",
         "security/ir.model.access.csv",
         "views/account_journal_dashboard_views.xml",
-        "views/account_bank_statement_views.xml",
         "views/res_users_views.xml",
         "views/menu.xml",
     ],
-    "installable": True,
 }
