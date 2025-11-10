61b7142790cd25f2f5e74dd52910df3906fb2d0f
??? <dgallo@nuobit.com>
2025-11-10 13:02:56 +0100

    [IMP] account_move_show_date_out_invoices: pre-commit auto fixes

--- account_move_show_date_out_invoices/__manifest__.py
+++ account_move_show_date_out_invoices/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "NuoBiT Solutions, S.L.",
     "license": "AGPL-3",
     "category": "Accounting",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": [
         "account",
     ],
