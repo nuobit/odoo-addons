6a9604fa4904d9594dc55f6f8618a2b05b16fb80
??? <dgallo@nuobit.com>
2025-11-14 09:34:31 +0100

    [IMP] pricelist_massive_update_contract: pre-commit auto fixes

--- pricelist_massive_update_contract/__manifest__.py
+++ pricelist_massive_update_contract/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Sale",
     "license": "AGPL-3",
     "author": "NuoBiT Solutions, S.L.",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": ["pricelist_massive_update", "contract"],
     "data": [
         "views/pricelist_update.xml",
