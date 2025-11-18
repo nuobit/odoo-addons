5a1e2969b55bc2f8ef69e2febece304ce75714ac
Eric Antones <eantones@nuobit.com>
2025-11-18 16:04:06 +0100

    [REF+IMP] connector_woocommerce_wpml: deduplicate code using a binding mixin

--- connector_woocommerce_wpml/models/product_public_category/binding.py
+++ connector_woocommerce_wpml/models/product_public_category/binding.py
@@ -1,26 +1,17 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import models
+
+from ..binding.binding import WoocommerceWPMLBindingMixin
 
 
 class WooCommerceProductPublicCategory(models.Model):
-    _inherit = "woocommerce.product.public.category"
-
-    woocommerce_lang = fields.Char(
-        string="Language",
-        required=True,
-    )
-
-    _sql_constraints = [
-        (
-            "internal_uniq",
-            "unique(backend_id, woocommerce_lang, odoo_id)",
-            "A binding already exists with the same Internal (Odoo) ID.",
-        ),
-        (
-            "external_uniq",
-            "unique(backend_id, woocommerce_lang, woocommerce_idpubliccategory)",
-            "A binding already exists with the same External (idpublidcategory) ID.",
-        ),
+    _name = "woocommerce.product.public.category"
+    _inherit = [
+        "woocommerce.product.public.category",
+        "woocommerce.wpml.binding.mixin",
     ]
+
+    _sql_constraints = WoocommerceWPMLBindingMixin._sql_constraints
