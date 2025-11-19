04f1990cc5a80c7a109922563d3bd6355496ef4a
??? <dgallo@nuobit.com>
2025-11-19 15:29:21 +0100

    [MIG] account_invoice_batches_accrual_entry: Migration to 18.0

--- account_invoice_batches_accrual_entry/__manifest__.py
+++ account_invoice_batches_accrual_entry/__manifest__.py
@@ -1,16 +1,15 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solution - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Invoice batches Accrual Entry",
     "summary": "Glue module to avoid create the accrual entry "
     "inside the same invoice batch as its origin",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["account_invoice_batches", "account_move_accrual_entry"],
     "maintainers": ["eantones"],
-    "auto_install": True,
 }
--- account_invoice_batches_accrual_entry/models/account_move.py
+++ account_invoice_batches_accrual_entry/models/account_move.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import models
 
@@ -18,4 +19,5 @@ class AccountMove(models.Model):
         for key in keys_to_remove:
             if key in context:
                 del context[key]
+        # pylint: disable=W8121
         return super(AccountMove, self.with_context(context))._create_accrual_move()
