2fc7385ac5df3320b90d4a7c8ced449092050a31
??? <dgallo@nuobit.com>
2025-11-20 10:53:49 +0100

    [MIG] account_invoice_report_service_facturae: Migration to 18.0

--- account_invoice_report_service_facturae/__manifest__.py
+++ account_invoice_report_service_facturae/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Invoice report service facturae",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Invoicing Management",
     "website": "https://github.com/NuoBiT/odoo-addons",
@@ -14,5 +15,4 @@
         "account_invoice_report_service",
         "l10n_es_facturae",
     ],
-    "installable": True,
 }
---
+++ account_invoice_report_service_facturae/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module makes facturae to use the service invoice reports defined on invoice services.
