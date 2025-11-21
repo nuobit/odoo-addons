22000fe4df254edb3dec68c35a316e42ab72d71b
??? <dgallo@nuobit.com>
2025-11-21 10:11:14 +0100

    [MIG] account_invoices_batches_mail_attach_existing_attachment: Migration to 18.0

--- account_invoices_batches_mail_attach_existing_attachment/__manifest__.py
+++ account_invoices_batches_mail_attach_existing_attachment/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Account Invoices Batches Mail Attach Existing Attachment",
     "summary": "Glue module for account invoices batches and account mail attach "
     "existing attachment",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["account_invoice_batches", "account_mail_attach_existing_attachment"],
     "data": [
--- account_invoices_batches_mail_attach_existing_attachment/models/account_move.py
+++ account_invoices_batches_mail_attach_existing_attachment/models/account_move.py
@@ -1,13 +1,14 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     invoice_batch_sending_method = fields.Selection(
-        selection_add=[("emailattachments", _("e-mail (Attach documents)"))],
+        selection_add=[("emailattachments", "e-mail (Attach documents)")],
         ondelete={"emailattachments": "set null"},
     )
