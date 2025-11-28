dd6cdd28175f9cb91a9ba59e01be011ee8ee25ed
??? <dgallo@nuobit.com>
2025-11-28 13:00:10 +0100

    [IMP] report_qweb_pdf_chunks: pre-commit auto fixes

--- report_qweb_pdf_chunks/__manifest__.py
+++ report_qweb_pdf_chunks/__manifest__.py
@@ -10,7 +10,7 @@
     "version": "14.0.1.0.1",
     "category": "Reporting",
     "author": "NuoBiT Solutions, S.L., Eric Antones",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["base"],
     "data": [
