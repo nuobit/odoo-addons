f4313fe006a8bb05cf1a0a1393533e9dbbbff0bf
??? <dgallo@nuobit.com>
2025-11-28 13:57:10 +0100

    [MIG] report_qweb_pdf_chunks: Migration to 18.0

--- report_qweb_pdf_chunks/__manifest__.py
+++ report_qweb_pdf_chunks/__manifest__.py
@@ -1,21 +1,20 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Report qweb PDF chunks",
-    "summary": "This module temporarily splits all selected documents to print into chunks "
-    "and invokes the external Wkhtmltopdf (0.12.5) program for each chunk to "
-    "avoid the well known memory problems when printing many documents.",
-    "version": "14.0.1.0.1",
+    "summary": "This module temporarily splits all selected "
+    "documents to print into chunks and invokes the external Wkhtmltopdf"
+    " (0.12.5) program for each chunk to avoid the well known memory "
+    "problems when printing many documents.",
+    "version": "18.0.1.0.0",
     "category": "Reporting",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["base"],
     "data": [
         "data/ir_config_parameter.xml",
     ],
-    "installable": True,
-    "auto_install": False,
 }
