59648eb2c011e6d02b50d500f5bd8e62f7265941
??? <dgallo@nuobit.com>
2025-12-02 12:34:28 +0100

    [MIG] stock_orderpoint_qty_restrict: Migration to 18.0

--- stock_orderpoint_qty_restrict/models/stock_rule.py
+++ stock_orderpoint_qty_restrict/models/stock_rule.py
@@ -1,13 +1,14 @@
 # Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
 
 
-class StockRule(models.Model):
-    _inherit = "stock.rule"
+class ProcurementGroup(models.Model):
+    _inherit = "procurement.group"
 
     @api.model
     def _get_orderpoint_domain(self, company_id=False):
         domain = super()._get_orderpoint_domain(company_id=company_id)
-        return domain + [("qty_to_o2rder", ">", 0.0)]
+        return domain + [("qty_to_order", ">", 0.0)]
