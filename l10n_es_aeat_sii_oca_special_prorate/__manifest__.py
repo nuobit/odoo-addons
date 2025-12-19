e149acaf8f356e3a1419938a15df888d834d93cf
??? <dgallo@nuobit.com>
2025-12-19 09:13:59 +0100

    [MIG] l10n_es_aeat_sii_oca_special_prorate: Migration to 18.0

--- l10n_es_aeat_sii_oca_special_prorate/__manifest__.py
+++ l10n_es_aeat_sii_oca_special_prorate/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Special prorate tax mapping for SII",
     "summary": "This module adds special prorate template taxes to SII mapping",
-    "version": "14.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL ",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
@@ -15,8 +15,8 @@
         "l10n_es_aeat_sii_oca",
     ],
     "data": [
+        "data/l10n.es.aeat.map.tax.line.tax.csv",
         "data/aeat_sii_map_data.xml",
     ],
-    "installable": True,
     "auto_install": True,
 }
