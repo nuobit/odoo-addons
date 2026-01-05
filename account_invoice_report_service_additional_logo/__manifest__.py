a068cf5a5fd6de2c01f5c5733a6639977e2275c2
Eric Antones <eantones@nuobit.com>
2026-01-05 13:14:53 +0100

    [ADD] account_invoice_report_service_additional_logo

---
+++ account_invoice_report_service_additional_logo/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Service invoice report additional logo",
+    "summary": "Service invoice report with additional logo",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "depends": ["account_invoice_report_service", "company_additional_logo"],
+    "data": [
+        "views/report_invoice_service.xml",
+    ],
+}
