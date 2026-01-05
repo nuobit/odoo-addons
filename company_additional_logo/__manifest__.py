4422b40e8ec41cd11a9699cc424092fc6ee735b2
Eric Antones <eantones@nuobit.com>
2026-01-05 13:14:40 +0100

    [ADD] company_additional_logo

---
+++ company_additional_logo/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 NuoBiT - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Company additional logo",
+    "summary": "This module adds an additional logo on company",
+    "version": "14.0.1.0.0",
+    "category": "Base",
+    "author": "NuoBiT Solutions, S.L.",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "views/res_company_views.xml",
+    ],
+    "maintainers": ["eantones"],
+}
