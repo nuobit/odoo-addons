b18387523cccdee3888658e3fb042aa1b3161ce1
??? <dgallo@nuobit.com>
2026-01-22 11:14:16 +0100

    [IMP] oxigen_delivery_mrw: pre-commit auto fixes

--- oxigen_delivery_mrw/__manifest__.py
+++ oxigen_delivery_mrw/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Customizations for Oxigen in MRW deliveries",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Stock",
     "depends": ["delivery_mrw"],
     "installable": True,
