5713409d16d26f7b47946554eba6eb4b162f834e
??? <dgallo@nuobit.com>
2026-01-26 12:40:31 +0100

    [MIG] stock_barcodes_qty_virtual_location: Migration to 18.0

--- stock_barcodes_qty_virtual_location/__manifest__.py
+++ stock_barcodes_qty_virtual_location/__manifest__.py
@@ -1,15 +1,16 @@
 # Copyright 2021 NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2026 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Qty available in Virtual Location",
-    "summary": "This module extends the functionality of the barcode reader in Odoo to"
-    " allow the calculation of the available quantity of products in virtual locations.",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "summary": "This module extends the functionality of "
+    "the barcode reader in Odoo to allow the calculation "
+    "of the available quantity of products in virtual locations.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "category": "Extra Tools",
     "depends": ["stock_barcodes"],
-    "installable": True,
     "license": "AGPL-3",
 }
--- stock_barcodes_qty_virtual_location/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-* This module extends the functionality of the barcode reader in Odoo to allow the calculation of the available quantity of products in virtual locations.
