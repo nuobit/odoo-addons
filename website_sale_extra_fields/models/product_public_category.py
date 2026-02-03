9a7313992e3f5415c973e63101cf5b1a79a8e2c9
??? <dgallo@nuobit.com>
2026-02-03 17:30:55 +0100

    [MIG] website_sale_extra_fields: Migration to 18.0

--- website_sale_extra_fields/models/product_public_category.py
+++ website_sale_extra_fields/models/product_public_category.py
@@ -1,9 +1,8 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2026 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
-
-from odoo.addons.http_routing.models.ir_http import slugify
 
 
 class ProductPublicCategory(models.Model):
@@ -25,4 +24,4 @@ class ProductPublicCategory(models.Model):
     def _compute_slug_name(self):
         for rec in self:
             if not rec.slug_name:
-                rec.slug_name = slugify(rec.name)
+                rec.slug_name = self.env["ir.http"]._slugify_one(rec.name)
