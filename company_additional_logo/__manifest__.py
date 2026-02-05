377f7f3f86c2984fbf0e24e9affddfd6ee9b8cde
??? <dgallo@nuobit.com>
2026-02-05 15:34:08 +0100

    [MIG] company_additional_logo: Migration to 18.0

--- company_additional_logo/__manifest__.py
+++ company_additional_logo/__manifest__.py
@@ -1,12 +1,13 @@
-# Copyright 2026 NuoBiT - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Company additional logo",
     "summary": "This module adds an additional logo on company",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Base",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
