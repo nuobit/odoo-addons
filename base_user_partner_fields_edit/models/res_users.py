839f9ea7aa1ce6540e58e75db99b7a6c57bf6325
??? <dgallo@nuobit.com>
2026-02-05 11:51:03 +0100

    [MIG] base_user_partner_fields_edit: Migration to 18.0

--- base_user_partner_fields_edit/models/res_users.py
+++ base_user_partner_fields_edit/models/res_users.py
@@ -1,17 +1,17 @@
 # Copyright 2025 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, models
+from odoo import models
 
 
 class Users(models.Model):
     _inherit = "res.users"
 
-    @api.model
-    def check_access_rights(self, operation, raise_exception=True):
-        if operation == "write" and self.user_has_groups(
+    def _check_access(self, operation):
+        if operation == "write" and self.env.user.has_group(
             "base_user_partner_fields_edit.group_user_partner_fields_edit"
         ):
             if self.env.context.get("partner_only_fields"):
-                return True
-        return super().check_access_rights(operation, raise_exception=raise_exception)
+                return None
+        return super()._check_access(operation)
