e1b5f541d9e8741fb463f6fa66ce3c2d3fd6c733
??? <dgallo@nuobit.com>
2026-02-05 16:15:26 +0100

    [MIG] account_invoice_report_service_additional_logo: Migration to 18.0

--- account_invoice_report_service_additional_logo/__manifest__.py
+++ account_invoice_report_service_additional_logo/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Service invoice report additional logo",
     "summary": "Service invoice report with additional logo",
-    "version": "14.0.1.0.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Accounting",
     "website": "https://github.com/NuoBiT/odoo-addons",
