1d67542438bd188d650f629b302bb5d5574400bb
??? <dgallo@nuobit.com>
2026-02-13 14:02:05 +0100

    [MIG] product_template_variant_definition: Migration to 18.0

--- product_template_variant_definition/__manifest__.py
+++ product_template_variant_definition/__manifest__.py
@@ -1,4 +1,5 @@
-# Copyright 2024 NuoBiT Solutions S.L. - Eric Antones <eantones@nuobit.com>
+# Copyright 2024 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBit Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
@@ -7,9 +8,9 @@
     "has or not has variants. Not counting the "
     "product.product but using the attributes "
     "defined instead.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product Management",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["product"],
--- product_template_variant_definition/models/product_product.py
+++ product_template_variant_definition/models/product_product.py
@@ -1,4 +1,4 @@
-# Copyright 2024 NuoBiT Solutions S.L. - Eric Antones <eantones@nuobit.com>
+# Copyright 2024 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
