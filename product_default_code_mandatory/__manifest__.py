99de7ffd089f6a269be3657587b042ee9e999fcc
??? <dgallo@nuobit.com>
2026-02-16 10:47:40 +0100

    [MIG] product_default_code_mandatory: Migration to 18.0

--- product_default_code_mandatory/__manifest__.py
+++ product_default_code_mandatory/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright 2024 NuoBiT Solutions S.L. - Eric Antones <eantones@nuobit.com>
+# Copyright 2024 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Product Internal Reference Mandatory",
     "summary": "Makes the product internal reference mandatory.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Product Management",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": ["product_template_variant_definition"],
