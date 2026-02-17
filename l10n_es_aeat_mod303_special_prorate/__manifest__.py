9f1e05d7e9f61c423a87500170e8be3309d505b0
??? <dgallo@nuobit.com>
2026-02-17 10:54:10 +0100

    [MIG] l10n_es_aeat_mod303_special_prorate: Migration to 18.0

--- l10n_es_aeat_mod303_special_prorate/__manifest__.py
+++ l10n_es_aeat_mod303_special_prorate/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Special prorate tax mapping for 303",
     "summary": "This module adds the 303 model special prorate taxes",
-    "version": "14.0.1.0.6",
+    "version": "18.0.1.0.0",
     "category": "Sales",
-    "author": "NuoBiT Solutions, S.L., Eric Antones",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
@@ -15,10 +15,10 @@
         "l10n_es_aeat_mod303",
     ],
     "data": [
-        "data/tax_code_map_mod303_202107_data.xml",
+        "data/l10n.es.aeat.map.tax.line.tax.csv",
+        "data/l10n.es.aeat.map.tax.line.csv",
         "data/tax_code_map_mod303_2023_data.xml",
         "data/tax_code_map_mod303_202410_data.xml",
     ],
-    "installable": True,
     "auto_install": True,
 }
