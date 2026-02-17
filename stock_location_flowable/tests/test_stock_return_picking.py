19ac4d8e870a57f679822564da5b2979a819d1ab
Eric Antones <eantones@nuobit.com>
2026-02-17 17:27:06 +0100

    [IMP] stock_location_flowable: singleton error message for flowable location returns

--- stock_location_flowable/tests/test_stock_return_picking.py
+++ stock_location_flowable/tests/test_stock_return_picking.py
@@ -93,3 +93,70 @@ class TestStockReturnPicking(TestCommon):
         )
         msg_error = self.get_error_message_regex(msg_error)
         self.assertRegex(error.exception.args[0], msg_error)
+
+    def test_return_from_non_flowable_location_succeeds(self):
+        """
+        Test that returning a product that was delivered to a non-flowable
+        location is allowed.
+
+        PRE:    - A completed incoming picking to a non-flowable location
+        ACT:    - Create a return for that picking
+        POST:   - Return picking is created successfully
+        """
+        # ARRANGE
+        lot = self.env["stock.production.lot"].create(
+            {
+                "name": "TEST-RETURN-NOFLO",
+                "product_id": self.product_flowable_1.id,
+            }
+        )
+
+        picking = self.env["stock.picking"].create(
+            {
+                "picking_type_id": self.picking_type_incoming_1.id,
+                "location_id": self.env.ref("stock.stock_location_suppliers").id,
+                "location_dest_id": self.location_1.id,
+            }
+        )
+        move = self.env["stock.move"].create(
+            {
+                "name": self.product_flowable_1.name,
+                "product_id": self.product_flowable_1.id,
+                "product_uom_qty": 50,
+                "product_uom": self.product_flowable_1.uom_id.id,
+                "picking_id": picking.id,
+                "location_id": self.env.ref("stock.stock_location_suppliers").id,
+                "location_dest_id": self.location_1.id,
+            }
+        )
+        picking.action_confirm()
+        picking.action_assign()
+        move.move_line_ids.write(
+            {
+                "qty_done": 50,
+                "lot_id": lot.id,
+            }
+        )
+        picking.button_validate()
+
+        # ACT
+        return_wizard = (
+            self.env["stock.return.picking"]
+            .with_context(
+                active_id=picking.id,
+                active_model="stock.picking",
+            )
+            .create(
+                {
+                    "picking_id": picking.id,
+                    "location_id": self.env.ref("stock.stock_location_suppliers").id,
+                }
+            )
+        )
+        return_wizard._onchange_picking_id()
+        new_picking_id, pick_type_id = return_wizard._create_returns()
+
+        # ASSERT
+        self.assertTrue(new_picking_id)
+        return_picking = self.env["stock.picking"].browse(new_picking_id)
+        self.assertEqual(return_picking.state, "assigned")
