e3a8473539f63f9b1beb3a7a2a9e3a26a4c9a842
??? <dgallo@nuobit.com>
2026-02-23 12:48:20 +0100

    [MIG] l10n_es_account_capital_asset_tax_map_prorate: Migration to 18.0

--- l10n_es_account_capital_asset_tax_map_prorate/__manifest__.py
+++ l10n_es_account_capital_asset_tax_map_prorate/__manifest__.py
@@ -1,21 +1,19 @@
-# Copyright NuoBiT - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Account Capital Asset Tax Map",
-    "summary": "This module adds l10n_es_special_prorate data to capital assets tax map",
-    "version": "14.0.1.0.0",
+    "summary": "This module adds l10n_es_special_prorate "
+    "data to capital assets tax map",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "l10n_es_special_prorate",
         "l10n_es_account_capital_asset",
     ],
-    "data": [
-        "data/account_capital_asset_map_tax_data_prorate.xml",
-    ],
-    "installable": True,
-    "auto_install": True,
+    "post_init_hook": "post_init_hook",
 }
---
+++ l10n_es_account_capital_asset_tax_map_prorate/hooks.py
@@ -0,0 +1,55 @@
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+import csv
+
+from odoo.tools import file_open
+
+_TEMPLATE_FILE = (
+    "l10n_es_account_capital_asset_tax_map_prorate"
+    "/data/template"
+    "/l10n.es.account.capital.asset.map.tax-es_common_mainland.csv"
+)
+
+
+def post_init_hook(env):
+    companies = (
+        env["res.company"]
+        .search([])
+        .filtered(lambda c: c.chart_template and c.chart_template.startswith("es_"))
+    )
+    if not companies:
+        return
+    with file_open(_TEMPLATE_FILE) as template_file:
+        records = list(csv.DictReader(template_file))
+    for company in companies:
+        existing = env["l10n.es.account.capital.asset.map.tax"].search_count(
+            [
+                ("company_id", "=", company.id),
+                (
+                    "tax_src_id.name",
+                    "ilike",
+                    "priva",
+                ),
+            ]
+        )
+        if existing:
+            continue
+        for record in records:
+            tax_src = env.ref(
+                f"account.{company.id}_{record['tax_src_id']}",
+                raise_if_not_found=False,
+            )
+            tax_dest = env.ref(
+                f"account.{company.id}_{record['tax_dest_id']}",
+                raise_if_not_found=False,
+            )
+            if not tax_src or not tax_dest:
+                continue
+            env["l10n.es.account.capital.asset.map.tax"].create(
+                {
+                    "company_id": company.id,
+                    "tax_src_id": tax_src.id,
+                    "tax_dest_id": tax_dest.id,
+                }
+            )
