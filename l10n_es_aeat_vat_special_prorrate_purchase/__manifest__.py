acc81b10fce8b343a9d1faf8b0d95b1ff578a8d5
??? <dgallo@nuobit.com>
2026-02-24 10:59:37 +0100

    [IMP] l10n_es_aeat_vat_special_prorrate_purchase: pre-commit auto fixes

--- l10n_es_aeat_vat_special_prorrate_purchase/__manifest__.py
+++ l10n_es_aeat_vat_special_prorrate_purchase/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.0",
     "category": "Purchases",
     "author": "NuoBiT Solutions, S.L.",
-    "website": "https://github.com/nuobit/odoo-addons",
+    "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
     "depends": [
         "purchase",
