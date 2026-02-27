86dd9e7edf4b882f9b6809cdd3491d1903339f06
Eric Antones <eantones@nuobit.com>
2026-02-27 20:06:36 +0100

    [FIX] stock_location_flowable: prevent premature blocking and reservation conflicts
    Before this fix, flowable locations were blocked as soon as raw materials
    entered confirmed or partially_available state, causing false rejections
    of incoming receipts.
    
    Now blocking only occurs when all raw materials reach assigned state.
    Additionally, _trigger_assign is bypassed during picking completion to
    prevent automatic reservation of unrelated moves at the flowable location,
    and a post-reservation check (action_assign override) ensures the mixing
    order can be fully reserved before proceeding.

--- stock_location_flowable/models/mrp_production.py
+++ stock_location_flowable/models/mrp_production.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 
 
 class MrpProduction(models.Model):
@@ -56,3 +56,76 @@ class MrpProduction(models.Model):
                     )
                 )
         return super().write(vals)
+
+    def action_assign(self):
+        res = super().action_assign()
+        for rec in self:
+            if rec.picking_type_id.flowable_operation and rec.state not in (
+                "to_close",
+                "done",
+                "cancel",
+            ):
+                rec._check_flowable_reservation()
+        return res
+
+    def _check_flowable_reservation(self):
+        self.ensure_one()
+        if all(m.state == "assigned" for m in self.move_raw_ids):
+            return
+        location = self.location_src_id
+        reserved_move_lines = self.env["stock.move.line"].search(
+            [
+                ("location_id", "=", location.id),
+                ("product_uom_qty", ">", 0),
+                ("state", "not in", ("done", "cancel", "draft")),
+                ("move_id.raw_material_production_id", "!=", self.id),
+            ]
+        )
+        if reserved_move_lines:
+            details = []
+            for ml in reserved_move_lines:
+                move = ml.move_id
+                if move.picking_id:
+                    origin = "%s (%s)" % (
+                        move.picking_id.name,
+                        move.picking_id.picking_type_id.name,
+                    )
+                elif move.raw_material_production_id:
+                    origin = "%s (%s)" % (
+                        move.raw_material_production_id.name,
+                        move.raw_material_production_id.picking_type_id.name,
+                    )
+                else:
+                    origin = _("Unknown origin (move %s)", move.id)
+                details.append(
+                    "  - %s: %s %s (lot %s) - %s"
+                    % (
+                        ml.product_id.display_name,
+                        ml.product_uom_qty,
+                        ml.product_uom_id.name,
+                        ml.lot_id.name or _("no lot"),
+                        origin,
+                    )
+                )
+            raise UserError(
+                _(
+                    "Cannot merge at flowable location '%s'"
+                    " because there are reserved quantities."
+                    " After the merge, the current lot(s) will"
+                    " have 0 stock and these reservations will"
+                    " become invalid.\n\n"
+                    "The following operations must be unreserved"
+                    " or completed first:\n\n%s",
+                    location.name,
+                    "\n".join(details),
+                )
+            )
+        raise UserError(
+            _(
+                "Cannot fully reserve the mixing order at"
+                " flowable location '%s'. Raw materials are"
+                " in state '%s'.",
+                location.name,
+                ", ".join(self.move_raw_ids.mapped("state")),
+            )
+        )
