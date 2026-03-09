c2f7b6ab5d5b1419e532cbb66953e90b060b59d9
Eric Antones <eantones@nuobit.com>
2026-03-09 22:38:07 +0100

    [ADD] rental_base_extension
    Move Rental Positions screen from Configuration/Debug to the top-level
    Rentals menu, making it accessible to all sales users without needing
    debug mode.

---
+++ rental_base_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Rental Base Extension",
+    "summary": "Improves rental orders with menu visibility" " and product filtering.",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Rental",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "rental_base",
+    ],
+    "data": [
+        "views/sale_views.xml",
+    ],
+}
