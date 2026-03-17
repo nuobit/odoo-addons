6b1f9c5b72dfb60ca2efcdb48d5867df25f7eb2f
??? <dgallo@nuobit.com>
2026-03-17 18:17:04 +0100

    [MIG] l10n_es_aeat_sii_oca_extension: Migration to 18.0

--- l10n_es_aeat_sii_oca_extension/models/account_move.py
+++ l10n_es_aeat_sii_oca_extension/models/account_move.py
@@ -1,16 +1,16 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions- Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, models
 from odoo.exceptions import UserError, ValidationError
 
-from odoo.addons.l10n_es_aeat_sii_oca.models.account_move import round_by_keys
+from odoo.addons.l10n_es_aeat.models.aeat_mixin import round_by_keys
 
 
 def sum_key(elem, key):
     value = 0
-    if isinstance(elem, (list, tuple)):
+    if isinstance(elem, (list | tuple)):
         for v in elem:
             value += sum_key(v, key)
     elif isinstance(elem, dict):
@@ -41,11 +41,18 @@ def merge_nested_dicts(target_dict, source_dict, key_funcs=None, path=None, leve
         key_funcs = {}
     if path is None:
         path = []
-    if isinstance(source_dict, (str, int, float, bool)):
+    if isinstance(source_dict, (str | int | float | bool)):
         if get_path_value(target_dict, path) != source_dict:
             raise ValidationError(
-                _("SII data merge error: No match at path %s: %s != %s")
-                % (path, get_path_value(target_dict, path), source_dict)
+                _(
+                    "SII data merge error: No match at path %(path)s:"
+                    " %(target_value)s != %(source_value)s"
+                )
+                % {
+                    "path": path,
+                    "target_value": get_path_value(target_dict, path),
+                    "source_value": source_dict,
+                }
             )
     elif isinstance(source_dict, dict):
         for key, value in source_dict.items():
@@ -68,8 +75,8 @@ def merge_nested_dicts(target_dict, source_dict, key_funcs=None, path=None, leve
                 set_path_value(target_dict, new_path, value)
     else:
         raise UserError(
-            _("SII data merge error: Unsupported type: %s: %s")
-            % (type(source_dict), source_dict)
+            _("SII data merge error: Unsupported type: %(type)s: %(value)s")
+            % {"type": type(source_dict), "value": source_dict}
         )
 
 
@@ -81,8 +88,8 @@ def merge_detalle_iva(tax_details_list1, tax_details_list2):
     for tax_detail in tax_details_list1 + tax_details_list2:
         if rate_key not in tax_detail:
             raise ValidationError(
-                _("SII tax detail merge error: Missing '%s' key in %s")
-                % (rate_key, tax_detail)
+                _("SII tax detail merge error: Missing '%(key)s' key in %(detail)s")
+                % {"key": rate_key, "detail": tax_detail}
             )
         tax_detail = dict(tax_detail)
         tax_rate = tax_detail.pop(rate_key)
@@ -93,8 +100,11 @@ def merge_detalle_iva(tax_details_list1, tax_details_list2):
             for key, value in tax_detail.items():
                 if key not in merged_by_rate[tax_rate]:
                     raise ValidationError(
-                        _("SII tax detail merge error: Missing '%s' key in %s")
-                        % (key, merged_by_rate[tax_rate])
+                        _(
+                            "SII tax detail merge error:"
+                            " Missing '%(key)s' key in %(detail)s"
+                        )
+                        % {"key": key, "detail": merged_by_rate[tax_rate]}
                     )
                 merged_by_rate[tax_rate][key] += value
 
@@ -114,8 +124,8 @@ class AccountMove(models.Model):
 
     def _get_sii_in_taxes_deductible(self):
         self.ensure_one()
-        taxes_sfrs = self._get_sii_taxes_map(["SFRS"])
-        taxes_sfrisp = self._get_sii_taxes_map(["SFRISP"])
+        taxes_sfrs = self._get_aeat_taxes_map(["SFRS"], self.date)
+        taxes_sfrisp = self._get_aeat_taxes_map(["SFRISP"], self.date)
         tax_deductible_amount = 0.0
         tax_lines = self._get_aeat_tax_info()
         for tax_line in tax_lines.values():
@@ -124,8 +134,8 @@ class AccountMove(models.Model):
                 tax_deductible_amount += tax_line["actual_deductible_amount"]
         return tax_deductible_amount
 
-    def _get_sii_invoice_dict_in(self, cancel=False):
-        inv_dict = super()._get_sii_invoice_dict_in(cancel=cancel)
+    def _get_aeat_invoice_dict_in(self, cancel=False):
+        inv_dict = super()._get_aeat_invoice_dict_in(cancel=cancel)
         if not cancel:
             inv_dict["FacturaRecibida"]["CuotaDeducible"] = (
                 self._get_sii_in_taxes_deductible()
@@ -158,8 +168,8 @@ class AccountMove(models.Model):
                 tipo_desglose = {"DesgloseFactura": merged_data}
         return tipo_desglose, not_in_amount_total
 
-    def _get_sii_invoice_dict_out(self, cancel=False):
-        inv_dict = super()._get_sii_invoice_dict_out(cancel=cancel)
+    def _get_aeat_invoice_dict_out(self, cancel=False):
+        inv_dict = super()._get_aeat_invoice_dict_out(cancel=cancel)
         if not cancel:
             if "06" in (
                 self.sii_registration_key.code,
@@ -171,8 +181,8 @@ class AccountMove(models.Model):
                 )
         return inv_dict
 
-    def _get_sii_invoice_dict(self):
-        inv_dict = super()._get_sii_invoice_dict()
+    def _get_aeat_invoice_dict(self):
+        inv_dict = super()._get_aeat_invoice_dict()
         round_by_keys(
             inv_dict,
             [
@@ -189,9 +199,11 @@ class AccountMoveLine(models.Model):
         super()._process_aeat_tax_base_info(res, tax, sign)
         for tax in res.keys():
             res[tax].setdefault("actual_deductible_amount", 0)
+        return res
 
     def _process_aeat_tax_fee_info(self, res, tax, sign):
         super()._process_aeat_tax_fee_info(res, tax, sign)
         res[tax].setdefault("actual_deductible_amount", 0)
         if self.tax_repartition_line_id.account_id:
             res[tax]["actual_deductible_amount"] += self.balance * sign
+        return res
