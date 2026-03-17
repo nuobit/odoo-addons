aef2439a0a21fb5662677b61639397bf09373c8a
Eric Antones <eantones@nuobit.com>
2026-03-17 16:54:43 +0100

    [IMP] sale_rental_extension: editable tree with rental_qty and product domain
    Enable editable bottom on rental order lines tree view.
    Add invisible rental fields (rental, rental_type, can_sell_rental)
    for onchange support. Replace product_uom_qty with rental_qty
    column in the same position. Filter product dropdown on both
    product_id and product_template_id to only show rental services.

---
+++ sale_rental_extension/models/sale_order_line.py
@@ -0,0 +1,8 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
