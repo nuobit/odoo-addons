345025be61ce1412118c1b27a16912dafb703a8b
??? <dgallo@nuobit.com>
2026-03-23 17:56:42 +0100

    [MIG] account_invoice_same_accounting_date_prorate: Migration to 18.0

--- account_invoice_same_accounting_date_prorate/__manifest__.py
+++ account_invoice_same_accounting_date_prorate/__manifest__.py
@@ -1,19 +1,18 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Invoices Same Invoice Date and Accounting Date",
     "summary": "Glue module to make the prorate module compatible with the accounting"
     " date default date as a invoicing date",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "category": "Invoicing",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "depends": [
         "account_invoice_same_accounting_date",
         "l10n_es_aeat_vat_special_prorrate",
     ],
-    "autoinstall": True,
 }
--- account_invoice_same_accounting_date_prorate/models/account_move.py
+++ account_invoice_same_accounting_date_prorate/models/account_move.py
@@ -1,5 +1,5 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
@@ -8,10 +8,22 @@ from odoo import api, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.onchange("invoice_date", "highest_name", "company_id")
-    def _onchange_invoice_date(self):
-        super()._onchange_invoice_date()
+    @api.onchange("invoice_date")
+    def _onchange_invoice_date_prorate(self):
         if self.line_ids.tax_ids.filtered("prorate"):
-            self._recompute_dynamic_lines(
-                recompute_all_taxes=True, recompute_tax_base_amount=True
+            AccountTax = self.env["account.tax"]
+            base_lines, tax_lines = self._get_rounded_base_and_tax_lines(
+                round_from_tax_lines=False
             )
+            AccountTax._add_accounting_data_in_base_lines_tax_details(
+                base_lines,
+                self.company_id,
+                include_caba_tags=self.always_tax_exigible,
+            )
+            tax_results = AccountTax._prepare_tax_lines(
+                base_lines,
+                self.company_id,
+                tax_lines=tax_lines,
+            )
+            for tax_vals, _key, to_update in tax_results["tax_lines_to_update"]:
+                tax_vals["record"].update(to_update)
