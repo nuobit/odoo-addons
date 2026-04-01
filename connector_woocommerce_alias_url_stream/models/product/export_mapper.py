69616e2585c9e5c258fbce60f63702237a0f9784
??? <dgallo@nuobit.com>
2026-04-01 12:21:13 +0200

    [MIG] connector_woocommerce_alias_url_stream: Migration to 18.0

--- connector_woocommerce_alias_url_stream/models/product/export_mapper.py
+++ connector_woocommerce_alias_url_stream/models/product/export_mapper.py
@@ -1,4 +1,5 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from urllib.parse import quote
@@ -12,4 +13,4 @@ class WooCommerceProductProductExportMapper(AbstractComponent):
     def _prepare_url(self, binding, document):
         source_url = binding.wordpress_source_url
         datas_fname = quote(document.datas_fname)
-        return "%s/alias/%s" % (source_url, datas_fname)
+        return f"{source_url}/alias/{datas_fname}"
