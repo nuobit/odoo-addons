5b44d8e92a7a59ce71b34bfbe86de345390a94d0
??? <dgallo@nuobit.com>
2026-04-20 17:28:55 +0200

    [FIX] connector_woocommerce_ast: send latest shipment tracking to WooCommerce

--- connector_woocommerce_ast/models/sale_order/export_mapper.py
+++ connector_woocommerce_ast/models/sale_order/export_mapper.py
@@ -1,5 +1,6 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
 from odoo import _
 from odoo.exceptions import ValidationError
 
@@ -22,18 +23,22 @@ class WooCommerceSaleOrderExportMapper(Component):
     @mapping
     def shipment_tracking(self, record):
         tracking = {}
-        for picking in record.picking_ids:
-            if picking.carrier_id:
-                carrier = self.backend_record.carrier_provider_ids.filtered(
-                    lambda x: picking.carrier_id.delivery_type == x.delivery_type
+        picking = record.picking_ids.filtered(
+            lambda p: p.state == "done" and p.carrier_id
+        ).sorted(key=lambda p: p.id,)[-1:]
+        if picking:
+            carrier = self.backend_record.carrier_provider_ids.filtered(
+                lambda x: picking.carrier_id.delivery_type == x.delivery_type
+            )
+            if not carrier:
+                raise ValidationError(
+                    _("carrier is not defined on backend for carrier %s")
+                    % picking.carrier_id.name
                 )
-                if not carrier:
-                    raise ValidationError(
-                        _("carrier is not defined on backend for tax %s")
-                        % record.get("carrier_id")
-                    )
-                tracking["tracking_provider"] = carrier.woocommerce_provider
+            elif len(carrier) > 1:
+                raise ValidationError(_("Carrier is duplicated"))
+            tracking["tracking_provider"] = carrier.woocommerce_provider
             if picking.carrier_tracking_ref:
                 tracking["tracking_number"] = picking.carrier_tracking_ref
-            if tracking:
-                return {"_wc_shipment_tracking_items": [tracking]}
+        if tracking:
+            return {"_wc_shipment_tracking_items": [tracking]}
