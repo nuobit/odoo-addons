826b7391f97b112a76c8b2b8ef0ae0882dcc605b
Eric Antones <eantones@nuobit.com>
2026-06-01 14:57:10 +0200

    [ADD] website_sale_checkout_login_redirect_cart

---
+++ website_sale_checkout_login_redirect_cart/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Sale Checkout Login Redirect Cart",
+    "summary": "Redirect mandatory-login checkout visitors to cart review first",
+    "version": "16.0.1.0.0",
+    "category": "Website/eCommerce",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "website_sale",
+    ],
+    "data": [],
+    "installable": True,
+}
