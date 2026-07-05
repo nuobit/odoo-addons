cd60813c190f52db6ab50ce67a9c3e8bfa5f5137
Eric Antones <eantones@nuobit.com>
2026-07-05 19:05:48 +0200

    [FIX] l10n_es_account_menu: non-automatic conflict resolution re-applying "[MIG] l10n_es_account_menu: Migration to 18.0"
    Split out of the re-applied migration commit per review doctrine
    (2026-07-05): git-automatic hunks stay in the migration commit;
    this commit carries only the indeterministic criterion.
    Baseline: regen->theirs, code->ours. Original re-apply: 64fffa2d18.

--- l10n_es_account_menu/__manifest__.py
+++ l10n_es_account_menu/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright NuoBiT - Eric Antones <eantones@nuobit.com>
-# Copyright NuoBiT 2025 - Bijaya Kumal <bkumal@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "L10n ES Account Menu",
     "summary": "This module extends account menu",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "author": "NuoBiT Solutions SL",
     "website": "https://github.com/NuoBiT/odoo-addons",
     "license": "AGPL-3",
-    "depends": ["account", "l10n_es_extension"],
+    "depends": ["l10n_es_extension"],
     "data": [
         "views/account_tax_views.xml",
     ],
