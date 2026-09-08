587290118d2bcb79098e98c87a29b42e565690f8
Eric Antones <eantones@nuobit.com>
2026-09-08 16:13:04 +0200

    [FIX] connector_woocommerce: declare sale payment dependency
    The backend payment mapping requires account.payment.mode and the sale
    import mapper writes sale.order.payment_mode_id. Require account_payment_sale
    so both are available on a fresh installation instead of relying on modules
    that happened to be installed in an existing database.

--- connector_woocommerce/__manifest__.py
+++ connector_woocommerce/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
-# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 {
@@ -15,6 +15,7 @@
         ],
     },
     "depends": [
+        "account_payment_sale",
         "connector_extension_woocommerce",
         "connector_wordpress",
         "sale_stock",
