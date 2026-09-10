f04e34e4f53ddca82850569e40d4bb53acbdce50
Eric Antones <eantones@nuobit.com>
2026-09-10 19:37:00 +0200

    [REF] connector_woocommerce: split pricelist model packages
    Keep each pricelist ORM extension in its own model package.
    Update package imports while preserving model code and import order.

--- connector_woocommerce/models/__init__.py
+++ connector_woocommerce/models/__init__.py
@@ -9,6 +9,8 @@ from . import product_attribute_value
 from . import product
 from . import product_template
 from . import product_product
+from . import product_pricelist_item
+from . import product_pricelist
 from . import res_partner
 from . import sale_order_line
 from . import sale_order
