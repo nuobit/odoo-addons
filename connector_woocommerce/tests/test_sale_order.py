49cf50a36954aae3b6efb89ebb976d3065b6b3fc
Deniz Gallo <dgallo@nuobit.com>
2026-09-17 13:10:40 +0200

    [FIX] connector_woocommerce: use TransactionCase in sale order tests
    SavepointCase does not exist in Odoo 18: importing it aborts the test
    collection of the module, so none of its tests run. TransactionCase
    already runs the class setup once and each test in a savepoint.

--- connector_woocommerce/tests/test_sale_order.py
+++ connector_woocommerce/tests/test_sale_order.py
@@ -1,10 +1,10 @@
 # Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class TestSaleOrder(SavepointCase):
+class TestSaleOrder(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
