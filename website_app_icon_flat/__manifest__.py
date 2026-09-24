ed54390fbc7530bb6e175b99eb7216ad4de496b5
Eric Antones <eantones@nuobit.com>
2026-09-24 11:43:31 +0200

    [ADD] website_app_icon_flat
    Show the flat Website icon on the website's Editor button.
    
    The "Editor" button of the website frontend carries the icon URL of
    ``website`` in its own template, out of reach of ``web_app_icon_flat``.
    This glue module, installed automatically with ``website``, extends the
    layout so the button shows the flat icon.

---
+++ website_app_icon_flat/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Website App Icon Flat",
+    "summary": "Glue module between web_app_icon_flat and website.",
+    "version": "16.0.1.0.0",
+    "category": "Hidden",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/nuobit/odoo-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "web_app_icon_flat",
+        "website",
+    ],
+    "data": [
+        "views/website_templates.xml",
+    ],
+    "auto_install": True,
+}
---
+++ website_app_icon_flat/tests/test_website_layout.py
@@ -0,0 +1,23 @@
+# Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from lxml import html
+
+from odoo.tests import HttpCase, tagged
+
+
+@tagged("post_install", "-at_install")
+class TestWebsiteLayout(HttpCase):
+    def test_editor_button_shows_flat_icon(self):
+        self.authenticate("admin", "admin")
+        # Not the home page: website_erp_login, installed with every other
+        # addon of this repository on its CI, reroutes it to the backend.
+        page = html.fromstring(self.url_open("/website/info").content)
+        [icon] = page.xpath(
+            "//a[contains(concat(' ', @class, ' '), ' o_frontend_to_backend_edit_btn ')]"
+            "/img"
+        )
+        flat_icon_urls = self.env["ir.module.module"]._get_flat_icon_urls()
+        self.assertEqual(
+            icon.get("src"), flat_icon_urls["/website/static/description/icon.png"]
+        )
