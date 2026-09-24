883c777a5c3fbea3fa690f145148cecea0220e40
Eric Antones <eantones@nuobit.com>
2026-09-24 11:43:46 +0200

    [ADD] note_app_icon_flat
    Show the flat Notes icon where the activities menu adds a note.
    
    The "Add a note" entry of the Activities dropdown carries the Notes icon
    URL in its own template, out of reach of ``web_app_icon_flat``. This glue
    module, installed automatically with ``note``, takes that URL from the
    flat icon mapping instead.

---
+++ note_app_icon_flat/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Glue module between ``web_app_icon_flat`` and ``note``: it makes Odoo 16 show
+the flat Notes icon, instead of the Odoo 16 one, where the activities menu of
+the top bar adds a note.
---
+++ note_app_icon_flat/static/src/models/activity_menu_view.esm.js
@@ -0,0 +1,18 @@
+/** @odoo-module **/
+/* Copyright 2026 NuoBiT Solutions SL - Eric Antones <eantones@nuobit.com>
+   License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
+
+import {attr} from "@mail/model/model_field";
+import {flatIconUrl} from "@web_app_icon_flat/js/flat_icon_url.esm";
+import {registerPatch} from "@mail/model/model_core";
+
+registerPatch({
+    name: "ActivityMenuView",
+    fields: {
+        noteIconUrl: attr({
+            compute() {
+                return flatIconUrl("/note/static/description/icon.svg");
+            },
+        }),
+    },
+});
